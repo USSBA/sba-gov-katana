41f128839f8ec91c27e48ca2983e11cdd84aa6a6
Jonathan King <jking@fearless.tech>
2016-11-03 17:25:57 +0000

    DT-40 adds info on creating a commit-message hook to validate commit messages

--- development.md
+++ development.md
@@ -4,4 +4,4 @@ This is for the development/build setup guide
 
 1. Install Node and NPM
 2. Install Gulp: `npm install -g gulp`
-3.  
+3. Setup a git precommit client hook `cp scripts/check-commit-message.sh .git/hooks/commit-msg && chmod 700 .git/hooks/commit-msg`
---
+++ scripts/check-commit-message.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# regex to validate in commit msg
+commit_regex='(DT-[0-9]+|merge)'
+error_msg="Aborting commit. Your commit message is missing either a JIRA Issue ('DT-1111') or 'Merge'"
+
+if ! grep -iqE "$commit_regex" "$1"; then
+    echo "$error_msg" >&2
+    exit 1
+fi
