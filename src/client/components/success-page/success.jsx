937c1a5e297918e7655909248b3ddb2e6a503e39
menuka-sba <menuka.samaranayake@sba.gov>
2016-11-22 10:00:41 -0500

    DT-129 Cleans up success page with some basic formatting

--- src/client/components/success-page/success.jsx
+++ src/client/components/success-page/success.jsx
@@ -4,11 +4,9 @@ import React from 'react';
 class Success extends React.Component{
     render(){
         return (
-            <div >
-                <h2> Success </h2>
-                <div>
-                    Next Step Confirm Email
-                </div>
+            <div className="full-width header">
+                <h2 className="text-center">Success</h2>
+                <div className="text-center">Next Step Confirm Email</div>
             </div>
         );
     };
