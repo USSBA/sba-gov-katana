2479cb320f748382462b45ef6850dbb6594ed962
zander312 <iskander93@gmail.com>
2016-11-22 21:54:51 -0500

    DT-131 adds bootstrap grid to footer

--- src/client/components/common/footer.jsx
+++ src/client/components/common/footer.jsx
@@ -7,8 +7,8 @@ import React from 'react';
 class Footer extends React.Component{
     render(){
       return (
-      <div className="full-width footer">
-        <h3 className="text-center"> U.S. Small Business Administration, 409 3rd St, SW. Washington DC 20416. </h3>
+      <div className="col-xs-8 col-xs-offset-2">
+        <h3 className="footer"> U.S. Small Business Administration, 409 3rd St, SW. Washington DC 2046 </h3>
       </div>
       );
     };
