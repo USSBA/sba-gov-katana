120c56af4771c6d844b668ab05d9fb27e6be09d8
aadeogun <aadeogun@fearless.tech>
2016-11-25 16:30:29 -0500

    DT-87 review and submit form completion of configuration

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -3,12 +3,18 @@ import {routerReducer} from 'react-router-redux';
 
 // our customer reducers
 import loanReducer from '../reducers/loan-form.js';
-import additionalInfoReducer from '../reducers/additional-info.js'
+import additionalInfoReducer from '../reducers/additional-info.js';
+import contactInfoReducer from '../reducers/contact-info.js';
+import businessInfoReducer from '../reducers/business-info.js';
+import reviewInfoReducer from '../reducers/review-info.js';
 
 // combine the custom reducers with the routing reducer
 const rootReducer = combineReducers({
   loanReducer: loanReducer,
   additionalInfoReducer: additionalInfoReducer,
+  contactInfoReducer: contactInfoReducer,
+  businessInfoReducer: businessInfoReducer,
+  reviewInfoReducer: reviewInfoReducer,
   routing: routerReducer
 });
 
