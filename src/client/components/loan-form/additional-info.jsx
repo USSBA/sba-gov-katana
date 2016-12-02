37eb2e934541c18530897c9656cd3fa038cc1a78
menuka-sba <menuka.samaranayake@sba.gov>
2016-12-02 11:05:42 -0500

    DT-281 Adds required form fields to industry page

--- src/client/components/loan-form/additional-info.jsx
+++ src/client/components/loan-form/additional-info.jsx
@@ -48,16 +48,6 @@ export class AdditionalInfoForm extends React.Component {
             <FormPanel title="Additional Info">
                 <form ref={(input) => this.addInfoForm = input} onSubmit={(e) => this.handleSubmit(e)}>
 
-                    <SelectBox
-                        label="What's your industry experience?"
-                        name = "industryExp"
-                        handleChange={this.handleChange.bind(this)}>
-                        <option value="" disabled>- Select use of funds -</option>
-                        <option defaultValue=""></option>
-                        <option value="none">none</option>
-                        <option value="lots">lots</option>
-                    </SelectBox>
-
                     <FormGroup className="col-xs-6 col-xs-offset-3">
                         <label className={styles.controlLabel}>Check all that apply to you: </label>
                         <Checkbox name = "hasWrittenPlan" onClick={this.handleClick.bind(this)}>
