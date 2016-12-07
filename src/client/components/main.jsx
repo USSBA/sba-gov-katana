f389c4ef136d4d0a49721c926476b54ab196cbd9
aadeogun <aadeogun@fearless.tech>
2016-12-07 08:56:03 -0500

    DT-89 removed unnecessary code from main before pushing to master.

--- src/client/components/main.jsx
+++ src/client/components/main.jsx
@@ -4,7 +4,6 @@ import { Provider }
 from 'react-redux';
 import { applyMiddleware, createStore }
 from 'redux';
-//import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from '../reducers/index.js'
 
