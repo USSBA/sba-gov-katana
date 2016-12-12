15d29088265805791e2b5e140b5fabeffec2fd93
menuka-sba <menuka.samaranayake@sba.gov>
2016-12-12 16:06:46 -0500

    DT-287 Finishes basic parts of success page

--- src/client/components/success-page/success-page.jsx
+++ src/client/components/success-page/success-page.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import DynamicLenderMatches from './dynamic-lender-matches.jsx';
-import DynamicCounselingAndTools from './dynamic-counseling-and-tools.jsx';
+import DynamicCounselingAndTools from './counseling-and-tools.jsx';
 import ConfirmSection from './confirm-section.jsx';
 
 
@@ -9,13 +8,8 @@ class SuccessPage extends React.Component {
     render(){
         return (
             <div>
-                <div className="container">
-                    <ConfirmSection />
-                    <div className="row">
-                        <DynamicLenderMatches />
-                        <DynamicCounselingAndTools />
-                    </div>
-                </div>
+                <ConfirmSection />
+                <DynamicCounselingAndTools />
             </div>
         )
     }
