dcba1135b8ddb0f293ed1a0cf600a4564268390b
menuka-sba <menuka.samaranayake@sba.gov>
2016-12-19 14:16:19 -0500

    DT-378 Adds social icons and respective links

--- src/client/components/common/footer.jsx
+++ src/client/components/common/footer.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from '../../styles/footer.scss';
 
+import facebookThumbnail from "../../../../public/assets/images/footer/facebook.png"
+import twitterThumbnail from "../../../../public/assets/images/footer/twitter.png"
+import googlePlusThumbnail from "../../../../public/assets/images/footer/googleplus.png"
+import youtubeThumbnail from "../../../../public/assets/images/footer/youtube-play.png"
+import rssThumbnail from "../../../../public/assets/images/footer/rss.png"
+
 
 class Footer extends React.Component{
     render(){
       return (
       <footer className={styles.footer}>
           <div className={styles.linkSectionContainer  + " container-fluid"}>
-              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-xs-4 col-xs-offset-4"}>
+              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-4"}>
                   <h4>Customer Service</h4>
                   <ul>
                       <li><a href="https://www.sba.gov/about-sba">About SBA</a></li>
@@ -18,7 +24,8 @@ class Footer extends React.Component{
                       <li><a href="https://www.sba.gov/about-sba/sba-team">SBA Team</a></li>
                   </ul>
               </div>
-              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-xs-4 col-xs-offset-4"}>                  <h4>About SBA.gov</h4>
+              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-4"}>
+                  <h4>About SBA.gov</h4>
                   <ul>
                       <li><a href="https://www.sba.gov/sitemap">Site Map</a></li>
                       <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/about-sbagov-website/privacy-policy">Privacy Policy</a></li>
@@ -30,7 +37,8 @@ class Footer extends React.Component{
                   </ul>
 
               </div>
-              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-xs-4 col-xs-offset-4"}>                  <h4>SBA Information</h4>
+              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-4"}>
+                  <h4>SBA Information</h4>
                   <ul>
                       <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/foia">Freedom of Information Act</a></li>
                       <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/about-sbagov-website/no-fear-act">No Fear Act</a></li>
@@ -39,7 +47,8 @@ class Footer extends React.Component{
                       <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/other-plans-reports/plain-language-page">Plain Language</a></li>
                   </ul>
               </div>
-              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-xs-4 col-xs-offset-4"}>                  <h4>SBA Performance</h4>
+              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-4"}>
+                  <h4>SBA Performance</h4>
                   <ul>
                       <li><a href="https://www.sba.gov/about-sba/sba-performance/strategic-planning">Strategic Planning</a></li>
                       <li><a href="https://www.sba.gov/about-sba/sba-performance/performance-budget-finances">Performance, Budget & Financing</a></li>
@@ -48,7 +57,8 @@ class Footer extends React.Component{
                       <li><a href="https://www.sba.gov/about-sba/sba-performance/policy-regulations/eliminating-fraud-waste-abuse">Eliminating Fraud, Waste, and Abuse </a></li>
                   </ul>
               </div>
-              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-xs-4 col-xs-offset-4"}>                  <h4>Oversight</h4>
+              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-4"}>
+                  <h4>Oversight</h4>
                   <ul>
                       <li><a href="https://www.sba.gov/office-of-inspector-general">Inspector General</a></li>
                       <li><a href="https://www.sba.gov/advocacy">Advocacy</a></li>
@@ -60,7 +70,8 @@ class Footer extends React.Component{
 
                   </ul>
               </div>
-              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-xs-4 col-xs-offset-4"}>                  <h4>Tools and Features</h4>
+              <div className = {styles.linkSubject + " col-xl-2 col-md-2 col-md-offset-0 col-sm-4 col-sm-offset-4"}>
+                  <h4>Tools and Features</h4>
                   <ul>
                       <li><a href="https://www.sba.gov/tools/sba-learning-center/search/training">Online Training</a></li>
                       <li><a href="https://www.sba.gov/tools/business-plan">Create a Business Plan</a></li>
@@ -71,10 +82,34 @@ class Footer extends React.Component{
               </div>
           </div>
           <div className={ styles.bottom + " container-fluid"}>
-              <div className={styles.social + " col-md-6 container"}>
-                  
+              <div className={styles.social + " col-md-6 container col-sm-12"}>
+                  <div className = " col-md-1 col-md-offset-0 col-sm-2 col-sm-offset-1">
+                      <a href="https://www.facebook.com/SBAgov">
+                        <img src={ facebookThumbnail } alt="Facebook Link"/>
+                      </a>
+                  </div>
+                  <div className = " col-md-1 col-sm-2">
+                      <a href="https://www.twitter.com/sbagov">
+                          <img src={ twitterThumbnail } alt="Facebook Link"/>
+                      </a>
+                  </div>
+                  <div className = " col-md-1 col-sm-2">
+                      <a href="https://plus.google.com/+sbagov">
+                          <img src={ googlePlusThumbnail } alt="Facebook Link"/>
+                      </a>
+                  </div>
+                  <div className = " col-md-1 col-sm-2">
+                      <a href="https://www.youtube.com/sba">
+                          <img src={ youtubeThumbnail } alt="Facebook Link"/>
+                      </a>
+                  </div>
+                  <div className = " col-md-1 col-sm-2">
+                      <a href="https://www.sba.gov/blogs">
+                          <img src={ rssThumbnail } alt="Facebook Link"/>
+                      </a>
+                  </div>
               </div>
-              <div className={ styles.address + " col-md-6 container"}>
+              <div className={ styles.address + " col-md-6 container col-sm-12"}>
                   <div className = " col-md-12 text-right">U.S. Small Business Administration   |   409 3rd St, SW. Washington DC 20416</div>
               </div>
           </div>
