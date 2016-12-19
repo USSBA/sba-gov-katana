ee76b81166b65b5721657063348af26a3dd69f76
menuka-sba <menuka.samaranayake@sba.gov>
2016-12-18 20:57:44 -0500

    Add basic styling to footer

--- src/client/components/common/footer.jsx
+++ src/client/components/common/footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from '../../styles/footer.scss';
-import {Row, Col} from 'react-bootstrap';
 
 class Footer extends React.Component{
     render(){
       return (
-      <footer className={styles.body}>
-          <Row>
-              <Col md={2} mdOffset={0} xs={4} xsOffset={4}>
+      <footer className={styles.footer}>
+          <div className={styles.container + " container"}>
+              <div className = " col-md-2 col-md-offset-0 col-xs-8 col-xs-offset-2">
                   <h4>Customer Service</h4>
                   <ul>
                       <li><a href="https://www.sba.gov/about-sba">About SBA</a></li>
@@ -17,64 +16,73 @@ class Footer extends React.Component{
                       <li><a href="https://www.sba.gov/about-sba/sba-locations">SBA Locations</a></li>
                       <li><a href="https://www.sba.gov/about-sba/sba-team">SBA Team</a></li>
                   </ul>
-              </Col>
-              <Col md={2} mdOffset={0} xs={4} xsOffset={4}>
+              </div>
+          <div className = " col-md-2 col-md-offset-0 col-xs-8 col-xs-offset-2">
                   <h4>About SBA.gov</h4>
                   <ul>
-                      <li><a href="">Site Map</a></li>
-                      <li><a href="">Privacy Policy</a></li>
-                      <li><a href="">Linking Policy</a></li>
-                      <li><a href="">Accessibility</a></li>
-                      <li><a href="">Disclaimers</a></li>
-                      <li><a href="">Social Media</a></li>
-                      <li><a href="">Data Store</a></li>
+                      <li><a href="https://www.sba.gov/sitemap">Site Map</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/about-sbagov-website/privacy-policy">Privacy Policy</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/about-sbagov-website/linking-policy">Linking Policy</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/about-sbagov-website/accessibility">Accessibility</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/about-sbagov-website/disclaimer">Disclaimers</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/about-sbagov-website/social-media">Social Media</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/digital-sba/open-data">Data Store</a></li>
                   </ul>
 
-              </Col>
-              <Col md={2} mdOffset={0} xs={4} xsOffset={4}>
+              </div>
+          <div className = " col-md-2 col-md-offset-0 col-xs-8 col-xs-offset-2">
                   <h4>SBA Information</h4>
                   <ul>
-                      <li><a href="">Freedom of Information Act</a></li>
-                      <li><a href="">No Fear Act</a></li>
-                      <li><a href="">Report Fraud, Waste and Abuse</a></li>
-                      <li><a href="">Initiatives</a></li>
-                      <li><a href="">Plain Language</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/foia">Freedom of Information Act</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/about-sbagov-website/no-fear-act">No Fear Act</a></li>
+                      <li><a href="https://www.sba.gov/category/navigation-structure/contracting/contracting-officials/report-fraud-waste-abuse">Report Fraud, Waste and Abuse</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-initiatives">Initiatives</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government/other-plans-reports/plain-language-page">Plain Language</a></li>
                   </ul>
-              </Col>
-              <Col md={2} mdOffset={0} xs={4} xsOffset={4}>
+              </div>
+          <div className = " col-md-2 col-md-offset-0 col-xs-8 col-xs-offset-2">
                   <h4>SBA Performance</h4>
                   <ul>
-                      <li><a href="">Strategic Planning</a></li>
-                      <li><a href="">Performance, Budget & Financing</a></li>
-                      <li><a href="">Open Government</a></li>
-                      <li><a href="">Policy and Regulations</a></li>
-                      <li><a href="">Eliminating Fraud, Waste, and Abuse </a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/strategic-planning">Strategic Planning</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/performance-budget-finances">Performance, Budget & Financing</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/open-government">Open Government</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/policy-regulations">Policy and Regulations</a></li>
+                      <li><a href="https://www.sba.gov/about-sba/sba-performance/policy-regulations/eliminating-fraud-waste-abuse">Eliminating Fraud, Waste, and Abuse </a></li>
                   </ul>
-              </Col>
-              <Col md={2} mdOffset={0} xs={4} xsOffset={4}>
+              </div>
+          <div className = " col-md-2 col-md-offset-0 col-xs-8 col-xs-offset-2">
                   <h4>Oversight</h4>
                   <ul>
-                      <li><a href="">Inspector General</a></li>
-                      <li><a href="">Advocacy</a></li>
-                      <li><a href="">Hearings and Appeals</a></li>
-                      <li><a href="">Ombudsman</a></li>
-                      <li><a href="">WhiteHouse.gov</a></li>
-                      <li><a href="">USA.gov</a></li>
-                      <li><a href="">Regulations.gov</a></li>
+                      <li><a href="https://www.sba.gov/office-of-inspector-general">Inspector General</a></li>
+                      <li><a href="https://www.sba.gov/advocacy">Advocacy</a></li>
+                      <li><a href="https://www.sba.gov/oha">Hearings and Appeals</a></li>
+                      <li><a href="https://www.sba.gov/ombudsman">Ombudsman</a></li>
+                      <li><a href="https://www.whitehouse.gov">WhiteHouse.gov</a></li>
+                      <li><a href="https://www.usa.gov">USA.gov</a></li>
+                      <li><a href="https://www.regulations.gov">Regulations.gov</a></li>
 
                   </ul>
-              </Col>
-              <Col md={2} mdOffset={0} xs={4} xsOffset={4}>
+              </div>
+          <div className = " col-md-2 col-md-offset-0 col-xs-8 col-xs-offset-2">
                   <h4>Tools and Features</h4>
                   <ul>
-                      <li><a href="">Online Training</a></li>
-                      <li><a href="">Create a Business Plan</a></li>
-                      <li><a href="">Find Events</a></li>
-                      <li><a href="">Qualify for Government Contracts</a></li>
-                      <li><a href="">SBA Videos</a></li>
+                      <li><a href="https://www.sba.gov/tools/sba-learning-center/search/training">Online Training</a></li>
+                      <li><a href="https://www.sba.gov/tools/business-plan">Create a Business Plan</a></li>
+                      <li><a href="https://www.sba.gov/tools/events">Find Events</a></li>
+                      <li><a href="https://www.sba.gov/tools/size-standards-tool">Qualify for Government Contracts</a></li>
+                      <li><a href="https://www.sba.gov/videos">SBA Videos</a></li>
                   </ul>
-              </Col>
-          </Row>
+              </div>
+          </div>
+          <div className={ styles.bottom + " container"}>
+              <div className=" container">
+                  
+              </div>
+              <div className={ styles.address + " col-md-6 col-md-offset-6 container"}>
+                  <div className = " col-md-7 text-right">U.S. Small Business Administration</div>
+                  <div >409 3rd St, SW. Washington DC 20416</div>
+              </div>
+          </div>
       </footer>
       );
     };
