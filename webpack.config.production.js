37720f556e924d0816c13132c47c47809a9f9791
menuka-sba <menuka.samaranayake@sba.gov>
2016-12-22 15:25:16 -0500

    Re-adds webpack.config.production which the previous commit deleted

---
+++ webpack.config.production.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'webpack-hot-middleware/client',
+        'bootstrap-loader',
+        'babel-polyfill',
+        './src/client/components/main.jsx'
+    ],
+    output: {
+        path: path.join(__dirname, 'public', "build"),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel-loader'],
+            include: path.join(__dirname, 'src')
+        }, {
+            test: /\.jsx?$/,
+            loaders: ['babel-loader'],
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader?modules'
+        }, {
+            test: /\.scss$/,
+            loaders: ["style-loader", "css-loader?modules", "sass-loader?modules"]
+        }, {
+            test: /\.less$/,
+            loader: "style-loader!css-loader!less-loader"
+        }, {
+            test: /\.json$/,
+            loader: 'json-loader'
+        }, {
+            test: /\.(png|jpg|gif|woff|woff2|ttf|otf|eot|svg)$/,
+            loader: 'url-loader?limit=100000'
+        }, {
+            test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+            loader: 'imports-loader?jQuery=jquery'
+        }]
+    }
+};
