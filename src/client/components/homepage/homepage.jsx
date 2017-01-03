575513db2ba1116ca90716c79e3f114740e2ee88
Jonathan King <jking@fearless.tech>
2017-01-03 10:08:49 -0500

    DT-538 some changes to help with merging

--- src/client/components/homepage/homepage.jsx
+++ src/client/components/homepage/homepage.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from '../common/header.jsx';
 import Footer from '../common/footer.jsx';
-import HappeningNow from "./happening-now.jsx";
 
 class Homepage extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <HappeningNow />
         <Footer />
       </div>
     )
