cd134137be974401a3d904c84143545224b327c5
zander312 <iskander93@gmail.com>
2017-01-09 13:58:22 -0500

    lints code

--- src/client/components/homepage/happening-now.jsx
+++ src/client/components/homepage/happening-now.jsx
@@ -85,12 +85,12 @@ class HappeningNow extends React.Component {
                   </Col>
                 </Row>
                 { /*<Row>
-                                                    <Col xs={ 12 } sm={ 12 } mdHidden lgHidden>
-                                                    <div className="happeningNowCarousel">
-                                                      <ModifiedCarousel imageStyle={ styles.carouselImage } titleStyle={ styles.happeningNowCarouselItemTitle } items={ carouselItems } />
-                                                    </div>
-                                                    </Col>
-                                                  </Row>*/ }
+                                                                                    <Col xs={ 12 } sm={ 12 } mdHidden lgHidden>
+                                                                                    <div className="happeningNowCarousel">
+                                                                                      <ModifiedCarousel imageStyle={ styles.carouselImage } titleStyle={ styles.happeningNowCarouselItemTitle } items={ carouselItems } />
+                                                                                    </div>
+                                                                                    </Col>
+                                                                                  </Row>*/ }
               </Grid>
             </div>);
   }
