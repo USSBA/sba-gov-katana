e2692a10da2116dab33e10312d16512cfe4d5d4a
menuka-sba <menuka.samaranayake@sba.gov>
2017-01-09 14:20:14 -0500

    start refactoring some homepage styles

--- src/client/components/homepage/finance-your-business.jsx
+++ src/client/components/homepage/finance-your-business.jsx
@@ -9,9 +9,9 @@ export const FinanceYourBusinessSection = (props) => <div className={ styles.fyb
                                                        <img className={ styles.fybBanner } src={ fYBImage } alt="Finance your business." />
                                                        <div className={ styles.fybAccentBox }>
                                                          <ul>
-                                                           <li><a href="https://www.sba.gov/loans-grants/see-what-sba-offers/what-sba-offers-help-small-businesses-grow">What SBA offers</a></li>
-                                                           <li><a href="https://www.sba.gov/loans-grants/see-what-sba-offers/sba-loan-programs">Loan programs</a></li>
-                                                           <li><a href="https://www.sba.gov/tools/linc">Connect with SBA lenders</a></li>
+                                                             <a href="https://www.sba.gov/loans-grants/see-what-sba-offers/what-sba-offers-help-small-businesses-grow"><li className = { styles.borderBox }>What SBA offers</li></a>
+                                                             <a href="https://www.sba.gov/loans-grants/see-what-sba-offers/sba-loan-programs"><li className = { styles.borderBox }>Loan programs</li></a>
+                                                             <a href="https://www.sba.gov/tools/linc"><li>Connect with SBA lenders</li></a>
                                                          </ul>
                                                          <img className="hidden-xs" src={ diagonalLines } alt="" />
                                                          <img className="visible-xs" src={ diagonalLinesMobile } alt="" />
