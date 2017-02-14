9570c339c6108f92a16308d340c9a6e598415a67
zander312 <iskander93@gmail.com>
2017-02-14 13:52:40 -0500

    adds exit modal for katana

--- src/client/components/common/leave-sba-modal.jsx
+++ src/client/components/common/leave-sba-modal.jsx
@@ -1,34 +1,38 @@
 import React from "react";
-import { Modal, Row, Col } from 'react-bootstrap';
+import Modal from 'react-modal'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as ModalActions from '../../actions/show-modal.js'
 import styles from '../../styles/modals/leave-sba-modal.scss'
+import sbaLogo from '../../../../public/assets/images/logo.png';
+import exitIcon from '../../../../public/assets/svg/exit-modal-close.svg';
 
 class LeaveSbaModal extends React.Component {
   render() {
-    return <Modal show={ true } onHide={ this.props.actions.closeLeaveSba }>
-             <Modal.Header closeButton>
-               <Modal.Title className={ styles.title }>You are leaving the Small Business Administration website</Modal.Title>
-             </Modal.Header>
-             <Modal.Body>
-               <p className={ styles.text }>SBA does not endorse any non-government websites, companies, or applications and cannot attest to the accuracy of the information provided by third-party sites
-                 or any other linked site.</p>
-               <p className={ styles.text }>Link to website:
-                 <a>
-                   { this.props.url }
-                 </a>
-               </p>
-               <Row>
-                 <Col className={ styles.btnContainer } xs={ 3 } xsOffset={ 2 } sm={ 2 } smOffset={ 3 }>
-                 <button className={ styles.btnCancel + " btn-block" } onClick={ this.props.actions.closeLeaveSba }>Cancel</button>
-                 </Col>
-                 <Col className={ styles.btnContainer } xs={ 3 } xsOffset={ 1 } sm={ 2 } smOffset={ 2 }>
-                 <button className={ styles.btnContinue + " btn-block" } onClick={ this.props.actions.navigateLeaveSba }>Continue</button>
-                 </Col>
-               </Row>
-             </Modal.Body>
-           </Modal>;
+    return (
+      <Modal isOpen={ true } className={ styles.content } overlayClassName={ styles.overlay }>
+        <img className={ styles.logo } src={ sbaLogo } />
+        <a onClick={ this.props.actions.closeLeaveSba }><img className={ styles.exitIcon } src={ exitIcon } /></a>
+        <h2 className={ styles.title }> You're leaving the Small Business Administration website.</h2>
+        <div className={styles.divider}></div>
+        <p className={styles.text}>This link is provided for your reference only. The SBA doesn’t endorse non-government websites, companies, or applications. The SBA doesn’t attest to the accuracy
+          of information provided by third-parties and other linked sites.</p>
+        <div className={styles.linkContainer}><span>Link to website: </span>
+          <a tabIndex="0">
+            { this.props.url }
+          </a>
+        </div>
+
+        <div className={styles.btnContainer}>
+          <button onClick={ this.props.actions.closeLeaveSba }
+                  className={ styles.btnCancel }>CANCEL
+          </button>
+          <button onClick={(e) => document.location = this.props.url} className={ styles.btnContinue }>CONTINUE</button>
+        </div>
+
+      </Modal>
+
+      );
   }
 }
 
