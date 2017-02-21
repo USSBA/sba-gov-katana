05949de71046367c985e67570bf41307965ccf50
aadeogun <aadeogun@fearless.tech>
2017-02-21 16:18:07 -0500

    DT 1029 commit client changes.

--- src/controllers/main-menu.js
+++ src/controllers/main-menu.js
@@ -2,18 +2,18 @@
  * Created by aadeogun on 2/13/17.
  */
 import HttpStatus from "http-status-codes";
-import fetchMainMenuFromDB from '../models/dao/main-menu.js';
+import { fetchMainMenuFromDb } from "../models/dao/main-menu.js";
 
 function fetchMainMenu(req, res) {
 
-    fetchMainMenuFromDB()
-        .then(function(data) {
-            res.status(HttpStatus.OK).send(data);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(error.response.status).send("Error retrieving main-menu");
-        });
+  fetchMainMenuFromDb()
+    .then(function(data) {
+      res.status(HttpStatus.OK).send(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(error.response.status).send("Error retrieving main-menu");
+    });
 }
 
-export {fetchMainMenu};
+export { fetchMainMenu };
