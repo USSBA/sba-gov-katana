a8d155c47d2c65d0c1ea857f9d839de453abcd1d
zander312 <iskander93@gmail.com>
2017-03-02 19:22:04 -0500

    completes counselor tool and lint formats

--- src/client/components/success-page/counseling-and-tools.jsx
+++ src/client/components/success-page/counseling-and-tools.jsx
@@ -13,39 +13,55 @@ export class DynamicCounselingAndTools extends React.Component {
   }
 
   componentWillMount() {
-    this.props.actions.fetchContentIfNeeded('counselors', 'counselors-by-location', {zip: this.props.businessInfoData.businessInfoZipcode})
+    this.props.actions.fetchContentIfNeeded('counselors', 'counselors-by-location', {
+      zip: this.props.businessInfoData.businessInfoZipcode
+    })
   }
 
-  redirectLocalAssistance(){
+  redirectLocalAssistance() {
     let newTab = window.open("", "_blank");
-    this.props.actions.fetchContentIfNeeded('counselorsRedirect', 'counselors-redirect', {zip: this.props.businessInfoData.businessInfoZipcode})
+    this.props.actions.fetchContentIfNeeded('counselorsRedirect', 'counselors-redirect', {
+      zip: this.props.businessInfoData.businessInfoZipcode
+    })
       .then((res) => {
         console.log(res.data.redirectTo);
         newTab.location = res.data.redirectTo;
       })
   }
 
-  trimStr(str){
-    return str.length > 40 ? str.slice(0,40) + "..." : str
+  trimStr(str) {
+    return str.length > 40 ? str.slice(0, 40) + "..." : str
   }
 
-  getMiles(num){
-    let miles = parseInt(num)*0.000621371192;
+  getMiles(num) {
+    let miles = parseInt(num) * 0.000621371192;
     return Math.round(miles * 100) / 100
   }
 
-  createCounselorBoxes(){
+  createCounselorBoxes() {
     const counselorsData = this.props.counselorsData;
     const counselorBox = Object.keys(counselorsData).map((key, index) => {
       let counselor = counselorsData[key];
-      return(
-        <div className={styles.counselorBox}>
-          <p className={styles.counselorTitle}>{this.trimStr(counselor['title']) || "Not Available"}</p>
-          <p className={styles.counselorAttr}>{counselor['name'] || "Not Available"}</p>
-          <p className={styles.counselorAttr}>{counselor['street'] + ", " + counselor['additional'] || "Not Available"}</p>
-          <p className={styles.counselorAttr}>{counselor['city'] + ", " + counselor['province'] + " " + counselor['postal_code']}</p>
-          <p className={styles.counselorAttr}>{this.getMiles(counselor['location_distance']) + " miles away"}</p>
-          <p className={styles.counselorAttr}>{'Phone: ' + counselor['phone'] || "Not Available"}</p>
+      return (
+        <div className={ styles.counselorBox }>
+          <p className={ styles.counselorTitle }>
+            { this.trimStr(counselor['title']) || "Not Available" }
+          </p>
+          <p className={ styles.counselorAttr }>
+            { counselor['name'] || "Not Available" }
+          </p>
+          <p className={ styles.counselorAttr }>
+            { counselor['street'] + ", " + counselor['additional'] || "Not Available" }
+          </p>
+          <p className={ styles.counselorAttr }>
+            { counselor['city'] + ", " + counselor['province'] + " " + counselor['postal_code'] }
+          </p>
+          <p className={ styles.counselorAttr }>
+            { this.getMiles(counselor['location_distance']) + " miles away" }
+          </p>
+          <p className={ styles.counselorAttr }>
+            { 'Phone: ' + counselor['phone'] || "Not Available" }
+          </p>
         </div>
       )
     });
@@ -56,13 +72,13 @@ export class DynamicCounselingAndTools extends React.Component {
   render() {
     const counselorBoxes = this.props.counselorsData;
     return (
-      <div className={styles.section}>
-        <h1 className={styles.title}>Get free, personalized help from a local counselor</h1>
-        <p className={styles.subTitle}>Local Counselors can help you prepare materials and introduce you to additional lenders</p>
-        <div className={styles.counselorContainer}>
-          {counselorBoxes ? this.createCounselorBoxes() : <div className={styles.loader}></div>}
+      <div className={ styles.section }>
+        <h1 className={ styles.title }>Get free, personalized help from a local counselor</h1>
+        <p className={ styles.subTitle }>Local Counselors can help you prepare materials and introduce you to additional lenders</p>
+        <div className={ styles.counselorContainer }>
+          { counselorBoxes ? this.createCounselorBoxes() : <div className={ styles.loader }></div> }
         </div>
-        <button className={styles.seeMoreBtn} onClick={() =>  this.redirectLocalAssistance()}>SEE MORE</button>
+        <button className={ styles.seeMoreBtn } onClick={ () => this.redirectLocalAssistance() }>SEE MORE</button>
       </div>
     )
   }
