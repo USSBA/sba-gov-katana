8a3fe89297fa847d998bf98d399c3da3051b9383
aadeogun <aadeogun@fearless.tech>
2017-03-08 03:57:31 -0500

    DT-1300 added new images.

--- src/client/components/landing-page/how-it-works.jsx
+++ src/client/components/landing-page/how-it-works.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from '../../styles/landing-page/how-it-works.scss';
-import howItWorksImg1 from '../../../../public/assets/images/lender-match/howItWorksImg1.png';
-import howItWorksImg2 from '../../../../public/assets/images/lender-match/howItWorksImg2.png';
-import howItWorksImg3 from '../../../../public/assets/images/lender-match/howItWorksImg3.png';
-import howItWorksImg4 from '../../../../public/assets/images/lender-match/howItWorksImg4.png';
+import howItWorksImg1 from '../../../../public/assets/images/lender-match/howItWorksImg1.jpg';
+import howItWorksImg2 from '../../../../public/assets/images/lender-match/howItWorksImg2.jpg';
+import howItWorksImg3 from '../../../../public/assets/images/lender-match/howItWorksImg3.jpg';
+import howItWorksImg4 from '../../../../public/assets/images/lender-match/howItWorksImg4.jpg';
 
 
 export const HowItWorksSection = (props) => <div className={ styles.sectionWrapper }>
                                               <p className={ styles.sectionTitle }>How it works.</p>
                                               <div className={ styles.sectionItemWrapper }>
                                                 <ul className={ styles.sectionItem }>
-                                                  <li><img src={ howItWorksImg1 } alt="Describe your needs" /></li>
+                                                  <li><div className={ styles.sectionImage }><p>1</p><img src={ howItWorksImg1 } alt="Describe your needs" /></div></li>
                                                   <li>
                                                     <p className={ styles.sectionItemTitle }>Describe your needs</p>
                                                   </li>
@@ -22,7 +22,7 @@ export const HowItWorksSection = (props) => <div className={ styles.sectionWrapp
                                                   </li>
                                                 </ul>
                                                 <ul className={ styles.sectionItem }>
-                                                  <li><img src={ howItWorksImg2 } alt="Get matched in 2 days" /></li>
+                                                  <li><div className={ styles.sectionImage }><p>2</p><img src={ howItWorksImg2 } alt="Get matched in 2 days" /></div></li>
                                                   <li>
                                                     <p className={ styles.sectionItemTitle }>Get matched in 2 days</p>
                                                   </li>
@@ -34,7 +34,7 @@ export const HowItWorksSection = (props) => <div className={ styles.sectionWrapp
                                                   </li>
                                                 </ul>
                                                 <ul className={ styles.sectionItem }>
-                                                  <li><img src={ howItWorksImg3 } alt="Talk to lenders" /></li>
+                                                  <li><div className={ styles.sectionImage }><p>3</p><img src={ howItWorksImg3 } alt="Talk to lenders" /></div></li>
                                                   <li>
                                                     <p className={ styles.sectionItemTitle }>Talk to lenders</p>
                                                   </li>
@@ -46,7 +46,7 @@ export const HowItWorksSection = (props) => <div className={ styles.sectionWrapp
                                                   </li>
                                                 </ul>
                                                 <ul className={ styles.sectionItem }>
-                                                  <li><img src={ howItWorksImg4 } alt="Apply for a loan" /></li>
+                                                  <li><div className={ styles.sectionImage }><p>4</p><img src={ howItWorksImg4 } alt="Apply for a loan" /></div></li>
                                                   <li>
                                                     <p className={ styles.sectionItemTitle }>Apply for a loan</p>
                                                   </li>
