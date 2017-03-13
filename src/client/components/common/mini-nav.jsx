108045c91ed6509d6cf4b94aa1a2e39b3dbabe3e
Jonathan King <jking@fearless.tech>
2017-03-13 07:48:15 -0400

    DT-1351 changes the katana-side default log-in link to the user profile (because the katana link directs the user to the login page without an explicit destination)

--- src/client/components/common/mini-nav.jsx
+++ src/client/components/common/mini-nav.jsx
@@ -20,9 +20,7 @@ class MiniNav extends React.Component {
 
   handleSearchChange(e) {
     event.preventDefault();
-    this.setState({
-      searchValue: e.target.value
-    });
+    this.setState({searchValue: e.target.value});
   }
 
   handleGoogleTranslateClick(e) {
@@ -41,36 +39,36 @@ class MiniNav extends React.Component {
   render() {
     const searchBar = this.state.searchIsExpanded
       ? (
-      <form id={ styles.searchBarNew } onBlur={ this.handleSearchToggle.bind(this) } onSubmit={ this.submitSearch.bind(this) }>
-        <input autoFocus id={ styles.searchInputNew } type='text' placeholder='Search' onChange={ this.handleSearchChange.bind(this) } onKeyPress={ this.handleKeyPressOnSearch }></input>
-        <i id="search-btn-new" tabIndex="0" alt="search button" className={ styles.searchIconNew + " fa fa-search" } aria-hidden="true" onMouseDown={ this.submitSearch.bind(this) }></i>
-      </form>
+        <form id={styles.searchBarNew} onBlur={this.handleSearchToggle.bind(this)} onSubmit={this.submitSearch.bind(this)}>
+          <input autoFocus id={styles.searchInputNew} type='text' placeholder='Search' onChange={this.handleSearchChange.bind(this)} onKeyPress={this.handleKeyPressOnSearch}></input>
+          <i id="search-btn-new" tabIndex="0" alt="search button" className={styles.searchIconNew + " fa fa-search"} aria-hidden="true" onMouseDown={this.submitSearch.bind(this)}></i>
+        </form>
       )
       : (
-      <a id="search-toggle-link" tabIndex="0" onClick={ this.handleSearchToggle.bind(this) }>
-        <i id="search-toggle" alt="search icon" className={ styles.searchIconNew + " fa fa-search" } aria-hidden="true"></i>
-      </a>
+        <a id="search-toggle-link" tabIndex="0" onClick={this.handleSearchToggle.bind(this)}>
+          <i id="search-toggle" alt="search icon" className={styles.searchIconNew + " fa fa-search"} aria-hidden="true"></i>
+        </a>
       );
 
     let googleTranslateBtn = this.state.translateIsExpanded
       ? ""
-      : <a tabIndex="0" id="translate-toggle-new" className={ styles.miniNavLinkNew } onClick={ this.handleGoogleTranslateClick.bind(this) } href="#">Translate</a>;
+      : <a tabIndex="0" id="translate-toggle-new" className={styles.miniNavLinkNew} onClick={this.handleGoogleTranslateClick.bind(this)} href="#">Translate</a>;
 
     return (
-      <nav role="navigation" aria-label="mini navigation" className={ styles.miniNavNew }>
-        <div className={ this.state.translateIsExpanded
-                         ? styles.googleTranslateElementVisible
-                         : styles.googleTranslateElement } id="google_translate_element"></div>
-        { googleTranslateBtn }
-        <a tabIndex="0" className={ styles.miniNavLinkNew } href="https://es.sba.gov/">SBA en español</a>
-        <a tabIndex="0" className={ styles.miniNavLinkNew } href="/for-lenders">For Lenders</a>
-        <a tabIndex="0" className={ styles.miniNavLinkNew } href="/about-sba/sba-newsroom">Newsroom</a>
-        <a tabIndex="0" className={ styles.miniNavLinkNew } href="/about-sba/what-we-do/contact-sba">Contact Us</a>
-        <a tabIndex="0" className={ styles.miniNavLinkNew } href="/user/register">Register</a>
-        <a tabIndex="0" className={ styles.miniNavLinkNew } href="/user/login?destination=about-sba%2Fwhat-we-do%2Fcontact-sba">Log In</a>
-        { searchBar }
+      <nav role="navigation" aria-label="mini navigation" className={styles.miniNavNew}>
+        <div className={this.state.translateIsExpanded
+          ? styles.googleTranslateElementVisible
+          : styles.googleTranslateElement} id="google_translate_element"></div>
+        {googleTranslateBtn}
+        <a tabIndex="0" className={styles.miniNavLinkNew} href="https://es.sba.gov/">SBA en español</a>
+        <a tabIndex="0" className={styles.miniNavLinkNew} href="/for-lenders">For Lenders</a>
+        <a tabIndex="0" className={styles.miniNavLinkNew} href="/about-sba/sba-newsroom">Newsroom</a>
+        <a tabIndex="0" className={styles.miniNavLinkNew} href="/about-sba/what-we-do/contact-sba">Contact Us</a>
+        <a tabIndex="0" className={styles.miniNavLinkNew} href="/user/register">Register</a>
+        <a tabIndex="0" className={styles.miniNavLinkNew} href="/user/login">Log In</a>
+        {searchBar}
       </nav>
-      );
+    );
   }
 }
 
