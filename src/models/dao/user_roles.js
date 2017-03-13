c550cc3081573b5a94a56f2340ac967bbaccd893
aadeogun <aadeogun@fearless.tech>
2017-03-13 10:00:54 -0400

    DT-1373 corrected issue with response status error.

--- src/models/dao/user_roles.js
+++ src/models/dao/user_roles.js
@@ -1,9 +1,9 @@
 import { executeQuery } from "../drupal-db.js";
 import mysql from "mysql";
 
-function fetchUserRoles(userId){
-    const sqlQuery = mysql.format("select role.name as userRole from users inner join users_roles on users.uid = users_roles.uid inner join role on users_roles.rid = role.rid where users.uid = ?;", userId);
-    return executeQuery(sqlQuery);
+function fetchUserRoles(userId) {
+  const sqlQuery = mysql.format("select role.name as userRole from users inner join users_roles on users.uid = users_roles.uid inner join role on users_roles.rid = role.rid where users.uid = ?;", userId);
+  return executeQuery(sqlQuery);
 }
 
-export {fetchUserRoles};
+export { fetchUserRoles };
