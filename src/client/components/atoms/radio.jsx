352d6f2d8f75adb50a5e9b79c3ae98bb93d69d8d
zander312 <iskander93@gmail.com>
2017-03-23 11:46:43 -0400

    lints code

--- src/client/components/atoms/radio.jsx
+++ src/client/components/atoms/radio.jsx
@@ -4,32 +4,31 @@ import _ from "lodash";
 import formHelperStyles from "../helpers/form-helpers.scss";
 
 class RadioButtonGroup extends React.Component {
-    handleClick(event) {
-      this.props.onChange(event.target.value);
-    }
+  handleClick(event) {
+    this.props.onChange(event.target.value);
+  }
   render() {
     let me = this;
     let radioButtons = this.props.options.map(function(item, index) {
-        let id = "radio"+index;
-        let radioInput = (
-          <input className={styles.regularRadio} type="radio" name={me.props.name} checked={item.value === me.props.value}
-              onClick={me.handleClick.bind(this)} id={id} value={item.value}></input>
-        );
-        let radioLabel = (
-          <label clasName={styles.myLabel} for={id}>
-            {item.text}
-          </label>
-        );
-        return [radioInput, radioLabel];
+      let id = "radio" + index;
+      let radioInput = (
+      <input className={ styles.regularRadio } type="radio" name={ me.props.name } checked={ item.value === me.props.value } onClick={ me.handleClick.bind(this) } id={ id } value={ item.value }></input>
+      );
+      let radioLabel = (
+      <label clasName={ styles.myLabel } for={ id }>
+        { item.text }
+      </label>
+      );
+      return [radioInput, radioLabel];
     });
     return (
-      <div className={styles.container}>
-        <label className={formHelperStyles.controlLabel}>
-          {this.props.label}
+      <div className={ styles.container }>
+        <label className={ formHelperStyles.controlLabel }>
+          { this.props.label }
         </label>
-        {radioButtons}
+        { radioButtons }
       </div>
-    );
+      );
   }
 }
 
