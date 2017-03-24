c0b9824fa62c6fb71522bc6e7b3b0a9ee2b27a4f
Jonathan King <jking@fearless.tech>
2017-03-24 08:06:37 -0400

    DT-1519 finalizes many of the styles for the radio buttons

--- src/client/components/atoms/radio.jsx
+++ src/client/components/atoms/radio.jsx
@@ -28,24 +28,25 @@ class RadioButtonGroup extends React.Component {
     let radioButtons = this.props.options.map(function(item, index) {
       let id = "radio" + index;
       let isChecked = item.value === me.props.value;
-      return <div className={styles.radioItem + " " + (isChecked ? styles.radioItemSelected : styles.radioItemNotSelected)} onClick={(event) => me.handleClick(index, me)} key={index} tabIndex="0" onKeyPress={(event) => me.handleKeyPress(event, index, me)}  >
-        <input className={styles.regularRadio} type="radio" name={me.props.name} checked={isChecked} tabIndex="0" onChange={me.handleChange} id={id} value={item.value}></input>
-        <label className={styles.myLabel} htmlFor={id}/>
-        <span>
-          {item.text}
-        </span>
-      </div>;
+      return <div className={ styles.radioItem + " " + (isChecked ? styles.radioItemSelected : styles.radioItemNotSelected) } onClick={ (event) => me.handleClick(index, me) } key={ index } tabIndex="0" onKeyPress={ (event) => me.handleKeyPress(event, index, me) }>
+               <input className={ styles.regularRadio } type="radio" name={ me.props.name } checked={ isChecked } tabIndex="0" onChange={ me.handleChange } id={ id }
+                 value={ item.value }></input>
+               <label className={ styles.myLabel } htmlFor={ id } />
+               <span className={ styles.radioText }>
+                       { item.text }
+                     </span>
+             </div>;
     });
     return (
       <div>
-        <label className={formHelperStyles.controlLabel}>
-          {this.props.label}
+        <label className={ formHelperStyles.controlLabel }>
+          { this.props.label }
         </label>
-        <div className={styles.container}>
-          {radioButtons}
+        <div>
+          { radioButtons }
         </div>
       </div>
-    );
+      );
   }
 }
 
