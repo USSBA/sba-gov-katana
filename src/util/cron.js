e89391b02513c0bf22a3f99c4df2d6c7708a172f
Jonathan King <jking@fearless.tech>
2017-04-01 06:41:13 -0400

    DT-71 adds a way to enable or disable the jobs

--- src/util/cron.js
+++ src/util/cron.js
@@ -1,15 +1,24 @@
 var CronJob = require("cron").CronJob; // eslint-disable-line id-match
 import config from "config";
 
-import { followupEmailJob, sendDataToOcaJob } from "../service/linc-service.js";
+import {
+  followupEmailJob,
+  sendDataToOcaJob
+} from "../service/linc-service.js";
 
 function init() {
   /* eslint-disable no-new, id-match */
-  console.log("Starting resendConfirmationEmail job");
-  new CronJob(config.get("linc.resendEmailCron"), followupEmailJob, null, true, "America/New_York");
-  console.log("Starting sending failed or pending messages to OCA");
-  new CronJob(config.get("linc.ocaSoapCron"), sendDataToOcaJob, null, true, "America/New_York");
-/* eslint-enable no-new, id-match */
+  if (config.get("linc.resendEmailJobEnabled")) {
+    console.log("Starting resendConfirmationEmail job");
+    new CronJob(config.get("linc.resendEmailCron"), followupEmailJob, null, true, "America/New_York");
+  }
+  if (config.get("linc.sendToOcaJobEnabled")) {
+    console.log("Starting sending failed or pending messages to OCA");
+    new CronJob(config.get("linc.ocaSoapCron"), sendDataToOcaJob, null, true, "America/New_York");
+  }
+  /* eslint-enable no-new, id-match */
 }
 
-export { init };
+export {
+  init
+};
