085852b2bbd77ffff3e72f8e7308a6cb49bfbf03
Jonathan King <jking@fearless.tech>
2017-04-01 14:30:35 -0400

    DT-381 adds some formatting fixes and corrects the default config

--- src/util/cron.js
+++ src/util/cron.js
@@ -1,10 +1,7 @@
 var CronJob = require("cron").CronJob; // eslint-disable-line id-match
 import config from "config";
 
-import {
-  followupEmailJob,
-  sendDataToOcaJob
-} from "../service/linc-service.js";
+import { followupEmailJob, sendDataToOcaJob } from "../service/linc-service.js";
 
 function init() {
   /* eslint-disable no-new, id-match */
@@ -16,9 +13,7 @@ function init() {
     console.log("Starting sending failed or pending messages to OCA");
     new CronJob(config.get("linc.ocaSoapCron"), sendDataToOcaJob, null, true, "America/New_York");
   }
-  /* eslint-enable no-new, id-match */
+/* eslint-enable no-new, id-match */
 }
 
-export {
-  init
-};
+export { init };
