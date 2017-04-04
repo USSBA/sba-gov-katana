5952cfb68b268a03e46137a90401740c5e787d56
Jonathan King <jking@fearless.tech>
2017-04-04 07:22:18 -0400

    DT-1687 changes the title of the review and submit page

--- src/client/components/lender-match/loan-form.jsx
+++ src/client/components/lender-match/loan-form.jsx
@@ -1,43 +1,55 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import * as LenderMatchActions from '../../actions/lender-match.js';
 import * as LocationChangeActions from '../../actions/location-change.js';
-import { indexOf, chain, startCase } from 'lodash';
-import { Panel, ButtonToolbar, Glyphicon } from 'react-bootstrap';
+import {indexOf, chain, startCase} from 'lodash';
+import {Panel, ButtonToolbar, Glyphicon} from 'react-bootstrap';
 import lenderMatchStyles from './lender-match.scss';
 import styles from './lender-match.scss';
-import { ProgressBar } from './progress-bar.jsx'
-
+import {ProgressBar} from './progress-bar.jsx'
 
 class LoanForm extends React.Component {
   getBackButtonText(locationIndex) {
-    return locationIndex === 0 ? "EXIT" : "BACK";
+    return locationIndex === 0
+      ? "EXIT"
+      : "BACK";
   }
   render() {
-    let pages = ['contact', 'business', 'industry', 'loan', 'additional', 'review']; // TODO make this static or configuration
+    let pages = [
+      'contact',
+      'business',
+      'industry',
+      'loan',
+      'additional',
+      'review'
+    ]; // TODO make this static or configuration
     let page = this.props.location.replace('/linc/form/', '');
     let locationIndex = indexOf(pages, page);
-    let backButton = locationIndex === 5 ? "" : (<button type="button" className={ styles.backBtn } onClick={ this.props.locationActions.goBack }>
-                                                   { this.getBackButtonText(locationIndex) }
-                                                 </button>);
+    let backButton = locationIndex === 5
+      ? ""
+      : (
+        <button type="button" className={styles.backBtn} onClick={this.props.locationActions.goBack}>
+          {this.getBackButtonText(locationIndex)}
+        </button>
+      );
+    let title = locationIndex === 5
+      ? "Review and Submit"
+      : "Lender Match";
     return (
-      <div className={ styles.formContainer }>
-        <main className={ styles.formPanel }>
-          <h1 className={ styles.title }>Lender Match</h1>
-          <ProgressBar pages={ pages.length + 1 } locationIndex={ locationIndex + 1 } />
-          { this.props.children }
-          { backButton }
+      <div className={styles.formContainer}>
+        <main className={styles.formPanel}>
+          <h1 className={styles.title}>{title}</h1>
+          <ProgressBar pages={pages.length + 1} locationIndex={locationIndex + 1}/> {this.props.children}
+          {backButton}
         </main>
       </div>
-      );
+    );
   }
 }
 
 function mapReduxStateToProps(reduxState, ownProps) {
-  return {
-    location: ownProps.location.pathname
-  };
+  return {location: ownProps.location.pathname};
 }
 
 function mapDispatchToProps(dispatch) {
