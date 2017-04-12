254cc01a1955167ea1abe79a7fc757a956a8b0cb
Jonathan King <jking@fearless.tech>
2017-04-12 18:11:07 -0400

    DT-1783 fixes some of theme

--- src/client/components/organisms/header-footer/mobile-nav.jsx
+++ src/client/components/organisms/header-footer/mobile-nav.jsx
@@ -18,8 +18,8 @@ class MobileNav extends React.Component {
   }
   render() {
     return (
-      <div>
-        <div className={styles.mobileMainHeaderNew + " " + styles[this.props.theme]}>
+      <div className={this.props.theme}>
+        <div className={styles.mobileMainHeaderNew}>
           <a href="/">
             <img className={styles.logoNew} alt="Small Business Administration" src={sbaLogo}/>
           </a>
