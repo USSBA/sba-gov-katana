449946ab6672364e8c50b3c11179278a860c1ed2
zander312 <iskander93@gmail.com>
2017-04-13 10:09:58 -0400

    adds theme generator, inserts it into main.jsx

---
+++ src/client/components/templates/themer/themer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const themes = {
+      "sba-blue": [""],
+      "byzantine": [],
+      "money-green": ["linc"],
+      "cobalt-blue": []
+    }
+
+class Themer extends React.Component {
+
+    generateTheme(){
+      let theme;
+      Object.keys(themes).map((key) => {
+        let path = window.location.pathname.split("/")[1]
+        themes[key].forEach((themePath) => {
+          if(path == themePath){
+            theme = key
+          }
+        })
+      });
+      return theme
+    }
+
+  render() {
+    return (
+      <div className={this.generateTheme()}>
+        {this.props.children}
+      </div>
+      );
+  }
+}
+
+export default Themer;
