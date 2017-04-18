23840cdd3345a803331f8131580abedc7cee24db
Jonathan King <jking@fearless.tech>
2017-04-18 15:58:40 -0400

    DT-1783 changes some test variables name for clarity

--- test/mocha/server/models/dao/drupal8-rest.test.js
+++ test/mocha/server/models/dao/drupal8-rest.test.js
@@ -9,22 +9,22 @@ let expect = chai.expect;
 
 
 describe("Drupal 8 ReST client", () => {
-  let mock;
+  let mockAxiosHttpClient;
 
   beforeEach(() => {
-    mock = new MockAdapter(axios);
+    mockAxiosHttpClient = new MockAdapter(axios);
   })
 
   afterEach(() => {
-    mock.reset();
+    mockAxiosHttpClient.reset();
   });
 
   after(() => {
-    mock.restore();
+    mockAxiosHttpClient.restore();
   })
 
   it('should return the response data in the normal case', (done) => {
-    mock.onGet('/node/1/').reply(200, {
+    mockAxiosHttpClient.onGet('/node/1/').reply(200, {
       value: 1
     });
 
@@ -36,7 +36,7 @@ describe("Drupal 8 ReST client", () => {
   });
 
   it('should respond with null if no data is found', (done) => {
-    mock.onGet('/node/2/').reply(204);
+    mockAxiosHttpClient.onGet('/node/2/').reply(204);
 
     fetchById('node', 2).then(function(result) {
       expect(result).to.be.null;
@@ -44,7 +44,7 @@ describe("Drupal 8 ReST client", () => {
   });
 
   it('should respond with null if the server returns a 404', (done) => {
-    mock.onGet('/node/3/').reply(404);
+    mockAxiosHttpClient.onGet('/node/3/').reply(404);
 
     fetchById('node', 3).then(function(result) {
       expect(result).to.be.null;
@@ -52,7 +52,7 @@ describe("Drupal 8 ReST client", () => {
   });
 
   it('should throw an error when receiving a 500', (done) => {
-    mock.onGet('/node/4/').reply(500, "Internal Server Error");
+    mockAxiosHttpClient.onGet('/node/4/').reply(500, "Internal Server Error");
 
     fetchById('node', 4).then(function(result) {
         done(new Error("Test Failed.  Promise resolved, but it should not have."));
