bc635cef37658a0984ec838d490419d47074f94e
zander312 <iskander93@gmail.com>
2017-04-20 14:34:55 -0400

    adds styles for sectionHeader

--- src/client/components/molecules/section-header/section-header.jsx
+++ src/client/components/molecules/section-header/section-header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './section-header.scss'
 
 class SectionHeader extends React.Component {
 
 	render(){
 		return(
-				<h2>{this.props.text}</h2>
+				<h2 className={styles.sectionHeader}>{this.props.text}</h2>
 			)
 	}
 }
