24279d7c39138e41920d0beb008fda45cd7af586
aadeogun <aadeogun@fearless.tech>
2017-04-24 15:56:48 -0400

    DT-1877 First cut styling applied.

--- src/client/components/molecules/title-section/title-section.jsx
+++ src/client/components/molecules/title-section/title-section.jsx
@@ -8,7 +8,7 @@ class TitleSection extends React.Component{
         let titleLinks = [];
         titleLinks = paragraphData.map(function(item, index) {
             return (
-            (item && item.type === "sectionHeader") ? <li><a href={"#section-header" + "-" + index}>{item.text}</a></li> : ""
+            (item && item.type === "sectionHeader") ? <li ><a className={styles.titleLink} href={"#section-header" + "-" + index}>{item.text}</a></li> : ""
             );
         });
         return titleLinks;
@@ -18,9 +18,9 @@ class TitleSection extends React.Component{
         let titleLinks = this.makeTitleLinks(this.props.paragraphs);
         return (<div className={styles.titleSection}>
                     <h1>{this.props.title}</h1>
-                    <p>{this.props.summary}</p>
-                    <hr/>
-                    <p>Content</p>
+                    <p className={styles.summary}>{this.props.summary}</p>
+                    <hr className={styles.lineCopy}/>
+                    <p className={styles.content}>Content</p>
                     <ul>{titleLinks}</ul>
                 </div>);
     }
