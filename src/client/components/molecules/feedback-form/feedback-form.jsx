2a65ff8a2e94f286da79efeb783a0d0d1811a6eb
Jonathan King <jking@fearless.tech>
2017-04-26 07:20:14 -0400

    DT-745 adds the three states and structure to the feedback form

--- src/client/components/molecules/feedback-form/feedback-form.jsx
+++ src/client/components/molecules/feedback-form/feedback-form.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import styles from '../../styles/buttons.scss';
 import SmallSecondaryButton from "../../atoms/small-secondary-button/small-secondary-button.jsx"
 
 let question = "Was this article Helpful?";
+let states = ["INITIAL", "INPUT", "THANKYOU"];
 class FeedbackForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      showingInput: false
+      displayState: states[0]
     };
   }
   handleClick() {}
   render() {
-    if (showingInput) {
+    if (this.state.displayState === state[1]) {
       return (
-        <div></div>
+        <div>
+          <p>Thanks for your feedback!</p>
+          <p>Please let us know how you think we can improve this article</p>
+        </div>
+      );
+  } else if (this.state.displayState === state[2]) {
+      return (
+        <div>
+          <i>Thanks for your feedback!</p>
+          <p>Thanks again for your feedback!</p>
+        </div>
       );
     } else {
       return (
