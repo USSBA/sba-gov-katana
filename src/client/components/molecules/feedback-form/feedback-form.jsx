890cbd6733edfae7c56d13762233c2b29382ce77
Jonathan King <jking@fearless.tech>
2017-04-26 12:19:12 -0400

    DT-745 adds the check icon to the feedback thank you

--- src/client/components/molecules/feedback-form/feedback-form.jsx
+++ src/client/components/molecules/feedback-form/feedback-form.jsx
@@ -16,37 +16,38 @@ class FeedbackForm extends React.Component {
     };
   }
   handleYesClick() {
-      this.setState({displayState: states[1]});
+    this.setState({displayState: states[1]});
   }
   handleNoClick() {
-      this.setState({displayState: states[1]});
+    this.setState({displayState: states[1]});
   }
-  handleSubmit(){
-      this.setState({displayState: states[2]});
+  handleSubmit() {
+    this.setState({displayState: states[2]});
   }
   render() {
     if (this.state.displayState === states[1]) {
       return (
-        <div className={styles.InputContainer}>
+        <div id="feedback-module-input" className={styles.InputContainer}>
           <h4 className={styles.question}>{firstThankYou}</h4>
           <p>{subtext}</p>
-          <TextArea placeholder={placeholder} length={250} showCounter={false}/>
-          <SmallPrimaryButton text="SUBMIT" onClick={this.handleSubmit.bind(this)} />
+          <TextArea id="feedback-input" placeholder={placeholder} showCounter={false}/>
+          <SmallPrimaryButton id="feedback-submit-button" text="SUBMIT" onClick={this.handleSubmit.bind(this)}/>
         </div>
       );
     } else if (this.state.displayState === states[2]) {
       return (
-        <div className={styles.ThankYouContainer}>
-          <h4>Thanks again for your feedback!</h4>
+        <div id="feedback-module-thankyou" className={styles.ThankYouContainer}>
+          <i id="thank-you-check-circle" tabIndex="-1" alt="thank-you" className={"fa fa-check-circle " + styles.checkIcon} aria-hidden="true"></i>
+          <h4 className={styles.thanksAgain} >Thanks again for your feedback!</h4>
         </div>
       );
     } else {
       return (
-        <div className={styles.QuestionContainer}>
+        <div id="feedback-module-question" className={styles.QuestionContainer}>
           <h4 className={styles.question}>{question}</h4>
           <div>
-            <button className={styles.GreenButton} onClick={this.handleYesClick.bind(this)}>YES</button>
-            <button className={styles.RedButton} onClick={this.handleYesClick.bind(this)}>NO</button>
+            <button id="feedback-yes-button" className={styles.GreenButton} onClick={this.handleYesClick.bind(this)}>YES</button>
+            <button id="feedback-no-button" className={styles.RedButton} onClick={this.handleYesClick.bind(this)}>NO</button>
           </div>
         </div>
       );
