09b788ac033bb33d9b8067a51d4fb617c785065d
Jonathan King <jking@fearless.tech>
2017-04-26 11:08:19 -0400

    DT-745 starts the server side work

---
+++ src/service/feedback-service.js
@@ -0,0 +1,9 @@
+function saveFeedback(feedback) {
+  return Promise.resolve();
+}
+
+function saveFeedbackText(id, text) {
+  return Promise.resolve();
+}
+
+export { saveFeedback, saveFeedbackText };
