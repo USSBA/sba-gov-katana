94333e2de8d4cd57caeda12e18b488876dd18ed9
jrwatts <Fearless@Jordans-MacBook-Pro.local>
2017-04-27 12:05:55 -0400

    DT-1656 Updates disabled button styles

--- src/client/components/atoms/small-inverse-primary-button/small-inverse-primary-button.jsx
+++ src/client/components/atoms/small-inverse-primary-button/small-inverse-primary-button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './small-inverse-primary-button.scss';
 
  const SmallInversePrimaryButton = (props) =>
-		<button id={props.id} className={ styles.SmallInversePrimaryButton } href={ props.URL } onClick={props.onClick} disabed={props.disabled}>{ props.text }</button>;
+		<button id={props.id} className={ styles.SmallInversePrimaryButton } href={ props.URL } onClick={props.onClick} disabled={props.disabled}>{ props.text }</button>;
 
 export default SmallInversePrimaryButton;
 
--- src/client/components/atoms/small-secondary-button/small-secondary-button.jsx
+++ src/client/components/atoms/small-secondary-button/small-secondary-button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './small-secondary-button.scss';
 
 const SmallSecondaryButton = (props) => <div>
-  <button id={props.id} className={styles.SmallSecondaryButton} href={props.URL} onClick={props.onClick}disabed={props.disabled}>{props.text}</button>
+  <button id={props.id} className={styles.SmallSecondaryButton} href={props.URL} onClick={props.onClick}disabled={props.disabled}>{props.text}</button>
 </div>;
 
 export default SmallSecondaryButton;
