bf1b1f9b58bb7c88788b152b1bb12b7e098f6cb2
aadeogun <aadeogun@fearless.tech>
2017-05-01 13:01:59 -0400

    DT-1885 Styling for desktop completed.

--- src/client/components/molecules/readmore-section/readmore-section.jsx
+++ src/client/components/molecules/readmore-section/readmore-section.jsx
@@ -3,32 +3,28 @@ import styles from "./readmore-section.scss";
 import btnStyles from "../../atoms/small-secondary-button/small-secondary-button.scss";
 
 class ReadMoreSection extends React.Component{
-    constructor(props) {
-        super();
-        this.state = {
-            expanded: false
-        };
-        this.handleClick = this.handleClick.bind(this);
-    }
+
     handleClick(e){
         e.preventDefault();
-        this.setState({expanded: !this.state.expanded});
+        this.props.readMoreStatus(!this.props.expanded);
     }
     render(){
-        let btnText = this.state.expanded ? "CLOSE" :  "READ MORE";
-        let expandedTextSection = this.state.expanded ? <p className={styles.expandedCopyText}>{this.props.readMoreSectionItem.expandedCopyText}</p> : "" ;
+        let btnText = this.props.expanded ? "CLOSE" :  "READ MORE";
+        let expandedTextSection = this.props.expanded ? <p className={styles.expandedCopyText}>{this.props.readMoreSectionItem.expandedCopyText}</p> : "" ;
 
         return (<div className={styles.readMoreSection}>
             <h3 className={styles.title}>{this.props.readMoreSectionItem.titleText}</h3>
             <p className={styles.preview}>{this.props.readMoreSectionItem.preview}</p>
             {expandedTextSection}
-            <button className={btnStyles.SmallSecondaryButton} href="#" onClick={this.handleClick}>{btnText}</button>
+            <button className={btnStyles.SmallSecondaryButton} href="#" onClick={this.handleClick.bind(this)}>{btnText}</button>
         </div>);
     }
 }
 
 ReadMoreSection.propTypes ={
-    readMoreSectionItem: React.PropTypes.object.isRequired
+    readMoreSectionItem: React.PropTypes.object.isRequired,
+    expanded: React.PropTypes.bool.isRequired,
+    readMoreStatus: React.PropTypes.func.isRequired
 };
 
 export default ReadMoreSection;
--- src/client/components/molecules/text-readmore-section/text-readmore-section.jsx
+++ src/client/components/molecules/text-readmore-section/text-readmore-section.jsx
@@ -7,12 +7,32 @@ import ReadMoreSection from "../readmore-section/readmore-section.jsx";
 
 class TextReadMoreSection extends React.Component{
 
+    constructor(props) {
+        super();
+        this.state = {
+            readMoreExpanded: false
+        };
+
+        this.handleReadMoreStatus = this.handleReadMoreStatus.bind(this);
+    }
+
+    handleReadMoreStatus(readMoreStatus){
+        this.setState({readMoreExpanded: readMoreStatus});
+    }
+
     render(){
         let cleaned = DOMPurify.sanitize(this.props.textSectionItem.text);
-        return (<div className={styles.textReadMoreSection}>
-            <div className={textSectionStyles.readMoreSectionClosed}><TextSection text={cleaned}/></div>
-            <div className={readMoreSectionStyles.readMoreSectionClosed}><ReadMoreSection readMoreSectionItem={this.props.readMoreSectionItem}/></div>
-        </div>);
+        let textReadMoreSection = this.state.readMoreExpanded ? (
+            <div className={styles.textReadMoreSection}>
+                <div className={readMoreSectionStyles.readMoreSectionExpanded}><ReadMoreSection readMoreStatus={this.handleReadMoreStatus} expanded={this.state.readMoreExpanded} readMoreSectionItem={this.props.readMoreSectionItem}/></div>
+                <div className={textSectionStyles.readMoreSectionExpanded}><TextSection text={cleaned}/></div>
+            </div>
+        ) :
+            (<div className={styles.textReadMoreSection}>
+                <div className={textSectionStyles.readMoreSectionClosed}><TextSection text={cleaned}/></div>
+                <div className={readMoreSectionStyles.readMoreSectionClosed}><ReadMoreSection readMoreStatus={this.handleReadMoreStatus} expanded={this.state.readMoreExpanded}  readMoreSectionItem={this.props.readMoreSectionItem}/></div>
+            </div>);
+        return textReadMoreSection;
     }
 }
 
