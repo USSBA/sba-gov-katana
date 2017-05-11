87f07eb93573880b66f82bcfe034625dd16baaff
zander312 <iskander93@gmail.com>
2017-05-11 14:58:04 -0400

    parses text-section html and adds data-attributes to tables

--- src/client/components/molecules/text-section/text-section.jsx
+++ src/client/components/molecules/text-section/text-section.jsx
@@ -1,11 +1,31 @@
 import React from "react"
 import styles from "./text-section.scss";
+import cheerio from "cheerio";
+
+
 
 
 class TextSection extends React.Component{
-    render(){
-        return (<div className={styles.textSection} dangerouslySetInnerHTML={{__html: this.props.text}}/>);
-    }
+	parseTables() {
+		const $ = cheerio.load(this.props.text)
+		$('table').each((i, table) => {
+			let headers = []
+			$(table).find('thead > tr > th').each((i, theader) => {
+				headers.push(theader.children[0].data)
+			})
+			$(table).find('tbody > tr').each((i, trow) => {
+				$(trow).find('td').each((i, tdata) => {
+					$(tdata).attr('data-label', headers[i])
+				})
+			})
+		})
+		return $.html()
+	}
+
+  render(){
+  	this.parseTables()
+    return (<div className={styles.textSection} dangerouslySetInnerHTML={{__html: this.parseTables()}}/>);
+  }
 }
 
 TextSection.propTypes ={
