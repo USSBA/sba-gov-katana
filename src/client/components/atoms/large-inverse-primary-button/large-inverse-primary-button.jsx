fc377778a1fc42b9073951c2d78ee3cd1d8d9ea8
Jonathan King <jking@fearless.tech>
2017-05-16 13:58:28 -0400

    DT-2000 some homepages component fixes

--- src/client/components/atoms/large-inverse-primary-button/large-inverse-primary-button.jsx
+++ src/client/components/atoms/large-inverse-primary-button/large-inverse-primary-button.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './large-inverse-primary-button.scss';
 
  const LargeInversePrimaryButton = (props) =>
-		<button id={props.id} className={ styles.LargeInversePrimaryButton + " " + props.classname } href={ props.URL } onClick={props.onClick} disabled={props.disabled}>{ props.text }</button>;
+		<button id={props.id} className={ styles.LargeInversePrimaryButton + " " + (props.extraClassName ? props.extraClassName : "") } href={ props.URL } onClick={props.onClick} disabled={props.disabled}>{ props.text }</button>;
 
 export default LargeInversePrimaryButton;
-
--- src/client/components/atoms/small-inverse-primary-button/small-inverse-primary-button.jsx
+++ src/client/components/atoms/small-inverse-primary-button/small-inverse-primary-button.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import styles from './small-inverse-primary-button.scss';
 
  const SmallInversePrimaryButton = (props) =>
-		<button id={props.id} 
-		        className={ styles.SmallInversePrimaryButton + " " + props.classname} 
-		        href={ props.URL } 
-		        onClick={props.onClick} 
+		<button id={props.id}
+		        className={ styles.SmallInversePrimaryButton + " " + (props.extraClassName ? props.extraClassName : "")} 
+		        href={ props.URL }
+		        onClick={props.onClick}
 		        disabled={props.disabled}>
 		        { props.text }
 		</button>;
 
 export default SmallInversePrimaryButton;
-
