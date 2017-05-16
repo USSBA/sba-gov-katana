92e9b7051c58483fa5be6bc747c106c4490304a4
zander312 <iskander93@gmail.com>
2017-05-16 16:37:21 -0400

    completes styling for HowItWorks

--- src/client/components/organisms/lender-match/landing-page/how-it-works.jsx
+++ src/client/components/organisms/lender-match/landing-page/how-it-works.jsx
@@ -7,33 +7,33 @@ import howItWorksImg4 from '../../../../../../public/assets/images/lender-match/
 
 
 export const HowItWorksSection = (props) => <div className={ styles.section }>
-                                              <h2>How it works.</h2>
+                                              <h2 className={ styles.title }>How it works.</h2>
                                               <div className={ styles.step }>
                                                 <span className={ styles.numbers }>1</span>
                                                 <img className={ styles.img } src={ howItWorksImg1 } alt="Hands typing on a laptop." />
-                                                <h3>Describe your needs</h3>
-                                                <hr/>
-                                                <p>Answer a few questions about your business in as little as five minutes.</p>
+                                                <h3 className={styles.header} >Describe your needs</h3>
+                                                <hr className={styles.line}/>
+                                                <p className={styles.blurb}>Answer a few questions about your business in as little as five minutes.</p>
                                               </div>
                                               <div className={ styles.step }>
                                                 <span className={ styles.numbers }>2</span>
                                                 <img className={ styles.img } src={ howItWorksImg2 } alt="A person reading email on a smartwatch." />
-                                                <h3>Get matched in 2 days</h3>
-                                                <hr/>
-                                                <p>Receive an email with contact information of lenders who express interest in your loan.</p>
+                                                <h3 className={styles.header}>Get matched in 2 days</h3>
+                                                <hr className={styles.line}/>
+                                                <p className={styles.blurb}>Receive an email with contact information of lenders who express interest in your loan.</p>
                                               </div>
                                               <div className={ styles.step }>
                                                 <span className={ styles.numbers }>3</span>
                                                 <img className={ styles.img } src={ howItWorksImg3 } alt="Two people in discussion around a conference table." />
-                                                <h3>Talk to lenders</h3>
-                                                <hr/>
-                                                <p>Compare rates, terms, fees, and more.</p>
+                                                <h3 className={styles.header}>Talk to lenders</h3>
+                                                <hr className={styles.line}/>
+                                                <p className={styles.blurb}>Compare rates, terms, fees, and more.</p>
                                               </div>
                                               <div className={ styles.step }>
                                                 <span className={ styles.numbers }>4</span>
                                                 <img className={ styles.img } src={ howItWorksImg4 } alt="A piggy bank." />
-                                                <h3>Apply for a loan</h3>
-                                                <hr/>
-                                                <p>Submit loan applications and paperwork. You're well on your way to securing a business loan!</p>
+                                                <h3 className={styles.header}>Apply for a loan</h3>
+                                                <hr className={styles.line}/>
+                                                <p className={styles.blurb}>Submit loan applications and paperwork. You're well on your way to securing a business loan!</p>
                                               </div>
                                             </div>
