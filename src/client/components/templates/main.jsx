7f9ade647908535cc4eb9bf6fbc34cb4ab3687cf
Jonathan King <jking@fearless.tech>
2017-05-23 12:15:58 -0400

    DT-2000 fixes the disaster alert visibility issues

--- src/client/components/templates/main.jsx
+++ src/client/components/templates/main.jsx
@@ -5,48 +5,68 @@ import cookie from 'react-cookie';
 import DisasterAlerts from '../organisms/header-footer/disaster-alerts.jsx'
 import ModalController from '../modal-controller.jsx';
 
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as ContentActions from "../../actions/content.js";
+
 import styles from '../organisms/header-footer/header/header.scss';
 
 class Main extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      disasterAlertIsVisible: false,
+      disasterAlertIsVisible: false
     };
   }
 
+  componentDidMount() {
+    this.props.actions.fetchContentIfNeeded("disaster", "disaster", {});
+  }
+
   componentWillMount() {
-    let visible = cookie.load('close_disaster_loan_parature') ?
-      false :
-      true;
-    this.setState({
-      disasterAlertIsVisible: visible
-    });
+    let visible = cookie.load('close_disaster_loan_parature') === '1'
+      ? false
+      : true;
+    this.setState({disasterAlertIsVisible: visible});
   }
 
   handleClose() {
-    this.setState({
-      disasterAlertIsVisible: false
-    });
+    this.setState({disasterAlertIsVisible: false});
     cookie.save('close_disaster_loan_parature', '1', {
       path: '/',
       secure: true
     });
   }
 
-
   render() {
+    let showDisasterAlert = this.props.disaster.visible && this.state.disasterAlertIsVisible;
+    let disasterAlertClass = showDisasterAlert ? styles.alertIsActive : "";
     return (
-      <div className={ this.state.disasterAlertIsVisible ? styles.alertIsActive : ""}>
-        <DisasterAlerts disasterAlertIsVisible={this.state.disasterAlertIsVisible} onClose={this.handleClose.bind(this)}/>
-        <Header />
+      <div className={disasterAlertClass}>
+        <DisasterAlerts visible={showDisasterAlert} text={this.props.disaster.description} onClose={this.handleClose.bind(this)}/>
+        <Header/>
         <div className={styles.mainContent}>{this.props.children}</div>
-
-         <Footer/>
+        <Footer/>
         <ModalController/>
       </div>
     )
   }
 }
 
-export default Main;
+Main.defaultProps = {
+  disaster: {
+    visible: false,
+    description: ""
+  }
+}
+
+function mapReduxStateToProps(reduxState) {
+  return {disaster: reduxState.contentReducer["disaster"]};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(ContentActions, dispatch)
+  };
+}
+export default connect(mapReduxStateToProps, mapDispatchToProps)(Main);
