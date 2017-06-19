749b49f193fde8c9fdb667a5ea08c06a39c07aee
Jonathan King <jking@fearless.tech>
2017-06-19 09:20:25 -0400

    DT-2320 small refactors to the main pug for modularity; removes the GA event for location changes to fix the bounce rate issue

--- src/client/services/analytics.js
+++ src/client/services/analytics.js
@@ -18,29 +18,28 @@ function logPageView() {
       page: window.location.pathname
     });
     reactGa.pageview(window.location.pathname);
-  // console.log("Posting Location Change to GA:", window.location.pathname);
+    console.log("Posting Location Change to GA:", window.location.pathname);
   }
 }
 
 function logEvent(eventToLog) {
   if (isEnabled()) {
-    // console.log("Posting Event to GA:", eventToLog);
+    console.log("Posting Event to GA:", eventToLog);
     reactGa.event(eventToLog);
   }
 }
 
 /* eslint-disable callback-return */
-function googleAnalyticsMiddleware({getState}) {
+function googleAnalyticsMiddleware({
+  getState
+}) {
   return (next) => {
     return (action) => {
       if (isEnabled()) {
-        if (action.type === "@@router/LOCATION_CHANGE") {
-          logEvent({
-            category: "Navigation",
-            action: "Changed Location",
-            label: action.payload.pathname
-          });
-        }
+        //example
+        // if (action.type === "@@router/LOCATION_CHANGE") {
+        // do something
+        // }
       }
       return next(action);
     };
@@ -48,4 +47,8 @@ function googleAnalyticsMiddleware({getState}) {
 }
 /* eslint-enable callback-return */
 
-export { logPageView, googleAnalyticsMiddleware, logEvent };
+export {
+  logPageView,
+  googleAnalyticsMiddleware,
+  logEvent
+};
