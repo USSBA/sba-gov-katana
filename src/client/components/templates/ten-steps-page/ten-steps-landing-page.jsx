56d173f2c9cb2ffcae869b49860acb06c9a54874
aadeogun <aadeogun@fearless.tech>
2017-06-20 11:54:11 -0400

    DT-2355 Initial code for ten steps page setup.

--- src/client/components/templates/ten-steps-page/ten-steps-landing-page.jsx
+++ src/client/components/templates/ten-steps-page/ten-steps-landing-page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from "../../templates/ten-steps-page/ten-steps-landing-page.scss";
+import TenStepsSection from "../../molecules/ten-steps-section/ten-steps-section.jsx";
 
 class TenStepsLandingPage extends React.Component {
     render() {
         return (
             <div className={styles.tenStepsLandingPage}>
+                <TenStepsSection/>
             </div>
         );
     }
