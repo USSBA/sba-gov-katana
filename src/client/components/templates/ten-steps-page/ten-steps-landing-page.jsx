48643e16a7cdd8e9577a2c2271a3a59f940aa910
aadeogun <aadeogun@fearless.tech>
2017-06-20 12:24:14 -0400

    DT-2355 Initial code for ten steps page setup.

--- src/client/components/templates/ten-steps-page/ten-steps-landing-page.jsx
+++ src/client/components/templates/ten-steps-page/ten-steps-landing-page.jsx
@@ -6,7 +6,10 @@ class TenStepsLandingPage extends React.Component {
     render() {
         return (
             <div className={styles.tenStepsLandingPage}>
-                <TenStepsSection/>
+                <TenStepsSection index={1} title={"Do market research."} leftAlignBox solidBox={false} image={"assets/images/tensteps/research.png"} imageAlt={"Do market research."}/>
+                <TenStepsSection index={2} title={"Write your business plan."} leftAlignBox={false} solidBox image={"assets/images/tensteps/plan.jpg"} imageAlt={"Write your business plan."}/>
+                <TenStepsSection index={3} title={"Fund your business."} leftAlignBox solidBox={false} image={"assets/images/tensteps/fund.png"} imageAlt={"Fund your business."}/>
+                <TenStepsSection index={6} title={"Choose your business name."} leftAlignBox solidBox image={"assets/images/tensteps/name.png"} imageAlt={"Choose your business name."}/>
             </div>
         );
     }
