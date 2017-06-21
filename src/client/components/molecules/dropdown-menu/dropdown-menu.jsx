81f36ecff439a351868ab12e7f16bba010d7baa2
jrwatts <Fearless@Jordans-MacBook-Pro.local>
2017-06-21 10:32:22 -0400

    DT-717 Styles main nav dropdown menus

--- src/client/components/molecules/dropdown-menu/dropdown-menu.jsx
+++ src/client/components/molecules/dropdown-menu/dropdown-menu.jsx
@@ -47,8 +47,10 @@ class DropdownMenu extends React.Component {
   render() {
     let sizingStyle = "";
     let menuId = this.props.menuId;
+    let smallInverseCta = undefined;
     if (menuId === 0) {
       sizingStyle = styles.one;
+      smallInverseCta = (<SmallInverseCta />);
     }
     if (menuId === 1) {
       sizingStyle = styles.two;
@@ -86,7 +88,7 @@ class DropdownMenu extends React.Component {
           {this.props.featuredCallout
             ? <FeaturedCallout {...this.props.featuredCallout}/>
             : undefined}
-        <SmallInverseCta />
+        <div className={styles.businessGuideCTA}>{smallInverseCta}</div>
         </ul>
       );
     } else {
