7fa9040fbf8e3d9fecc4259f53ebbfa5fb1db13d
aadeogun <aadeogun@fearless.tech>
2017-06-21 12:35:24 -0400

    DT-2355 More styling for the desktop.

--- src/client/components/molecules/ten-steps-section/ten-steps-section.jsx
+++ src/client/components/molecules/ten-steps-section/ten-steps-section.jsx
@@ -3,12 +3,20 @@ import styles from './ten-steps-section.scss';
 import TitleBox from "../title-box/title-box.jsx";
 
 class TenStepsSection extends React.Component {
+    calculateSectionStyle(){
+        if(this.props.sectionItem.leftAlignBox){
+            return this.props.sectionItem.solidBox ? styles.leftAlignedSolidBoxSection : styles.leftAlignedBoxSection;
+        }else {
+            return this.props.sectionItem.solidBox ? styles.rightAlignedSolidBoxSection : styles.rightAlignedBoxSection;
+        }
+    }
 
     render() {
+        let sectionStyle = this.calculateSectionStyle();
         return (
-            <div className={styles.Container}>
+            <div className={sectionStyle}>
                 <img className={styles.Banner} src={this.props.sectionItem.image} alt={this.props.sectionItem.imageAlt}/>
-                <TitleBox solidBox={this.props.sectionItem.solidBox} sectionNum={this.props.sectionItem.sectionNum} title={this.props.sectionItem.title} text={this.props.sectionItem.text} link={this.props.sectionItem.link} />
+                <div className={styles.titleBox}><TitleBox solidBox={this.props.sectionItem.solidBox} sectionNum={this.props.sectionItem.sectionNum} title={this.props.sectionItem.title} text={this.props.sectionItem.text} link={this.props.sectionItem.link} /></div>
             </div>
         )
     }
