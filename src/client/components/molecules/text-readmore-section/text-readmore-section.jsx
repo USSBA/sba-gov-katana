5106b3f6de07f66f676c8116ab0fdb19d83556ee
Jonathan King <jking@fearless.tech>
2017-06-21 11:00:53 -0400

    DT-2278 refactors some of the text section readmore

--- src/client/components/molecules/text-readmore-section/text-readmore-section.jsx
+++ src/client/components/molecules/text-readmore-section/text-readmore-section.jsx
@@ -1,50 +1,69 @@
 import React from "react"
 import styles from "./text-readmore-section.scss";
-import readMoreSectionStyles from "../readmore-section/readmore-section.scss";
-import textSectionStyles from "../text-section/text-section.scss";
 import TextSection from "../text-section/text-section.jsx";
 import ReadMoreSection from "../readmore-section/readmore-section.jsx";
 
-class TextReadMoreSection extends React.Component{
+class TextReadMoreSection extends React.Component {
 
-    constructor(props) {
-        super();
-        this.state = {
-            readMoreExpanded: false
-        };
+  constructor(props) {
+    super();
+    this.state = {
+      readMoreExpanded: false
+    };
 
-        this.handleReadMoreStatus = this.handleReadMoreStatus.bind(this);
-    }
+    this.handleReadMoreStatus = this.handleReadMoreStatus.bind(this);
+  }
 
-    handleReadMoreStatus(readMoreStatus){
-        this.setState({readMoreExpanded: readMoreStatus});
-    }
+  handleReadMoreStatus(readMoreStatus) {
+    this.setState({readMoreExpanded: readMoreStatus});
+  }
+  makeTextSection(divStyle) {
+    return (
+      <div id={this.props.iD + "-text-section"} className={styles.textSectionSectionMobile}><TextSection text={cleaned}/></div>
+    );
+  }
 
-    render(){
-        let cleaned = DOMPurify.sanitize(this.props.textSectionItem.text);
-        let textReadMoreSection = this.state.readMoreExpanded ? (
-            <div id={this.props.iD} className={styles.textReadMoreSection}>
-                <div  className={readMoreSectionStyles.readMoreSectionExpanded}><ReadMoreSection iD={this.props.iD + "-read-more"} readMoreStatus={this.handleReadMoreStatus} expanded={this.state.readMoreExpanded} readMoreSectionItem={this.props.readMoreSectionItem}/></div>
-                <div id={this.props.iD + "-text-section"} className={textSectionStyles.readMoreSectionExpanded}><TextSection text={cleaned}/></div>
-           </div>
-        ) :
-            (<div id={this.props.iD} className={styles.textReadMoreSection}>
-                <div id={this.props.iD + "-text-section"} className={textSectionStyles.readMoreSectionClosed}><TextSection text={cleaned}/></div>
-                <div className={readMoreSectionStyles.readMoreSectionClosed}><ReadMoreSection iD={this.props.iD + "-read-more"} readMoreStatus={this.handleReadMoreStatus} expanded={this.state.readMoreExpanded}  readMoreSectionItem={this.props.readMoreSectionItem}/></div>
-            </div>);
+  makeReadMoreSection(divStyle) {
+    return (
+      <div className={divStyle}><ReadMoreSection iD={this.props.iD + "-read-more"} readMoreStatus={this.handleReadMoreStatus} expanded={this.state.readMoreExpanded} readMoreSectionItem={this.props.readMoreSectionItem}/></div>
+    );
+  }
 
-        return <div>{textReadMoreSection}<div className={styles.textReadMoreSection}>
-                    <div className={readMoreSectionStyles.readMoreSectionMobile}><ReadMoreSection iD={this.props.iD + "-read-more"} readMoreStatus={this.handleReadMoreStatus} expanded={this.state.readMoreExpanded}  readMoreSectionItem={this.props.readMoreSectionItem}/></div>
-                    <div id={this.props.iD + "-text-section"} className={textSectionStyles.readMoreSectionMobile}><TextSection text={cleaned}/></div>
-                </div><div>
+  render() {
+    let cleaned = DOMPurify.sanitize(this.props.textSectionItem.text);
+    let textReadMoreSection = this.state.readMoreExpanded
+      ? (
+        <div id={this.props.iD} className={styles.textReadMoreSection}>
+          {this.makeReadMoreSection(styles.readMoreSectionExpanded)}
+          {this.makeTextSection(styles.textSectionExpanded)}
+        </div>
+      )
+      : (
+        <div id={this.props.iD} className={styles.textReadMoreSection}>
+          {this.makeTextSection(styles.textSectionClosed)}
+          {this.makeReadMoreSection(styles.readMoreSectionClosed)}
+        </div>
+      );
 
-        </div></div>;
-    }
+    let textReadMoreSectionMobile = (
+      <div className={styles.textReadMoreSection}>
+        {this.makeReadMoreSection(styles.readMoreSectionMobile)}
+        {this.makeTextSection(styles.textSectionSectionMobile)}
+      </div>
+    );
+
+    return (
+      <div>
+        {textReadMoreSection}
+        {textReadMoreSectionMobile}
+      </div>
+    );
+  }
 }
 
-TextReadMoreSection.propTypes ={
-    textSectionItem: React.PropTypes.object.isRequired,
-    readMoreSectionItem: React.PropTypes.object.isRequired
+TextReadMoreSection.propTypes = {
+  textSectionItem: React.PropTypes.object.isRequired,
+  readMoreSectionItem: React.PropTypes.object.isRequired
 };
 
 export default TextReadMoreSection;
