544eafca7a89c13a10dfdda32e533a5ab685f64c
Jonathan King <jking@fearless.tech>
2017-06-23 12:56:59 +0000

    DT-2429 fixes an issue where some margin would cause hover menu issues

--- src/client/components/atoms/section-link/section-link.jsx
+++ src/client/components/atoms/section-link/section-link.jsx
@@ -3,16 +3,17 @@ import styles from "./section-link.scss";
 
 class SectionLink extends React.Component {
   render() {
-    let underlineStyle = this.props.showUnderline
-      ? " " + styles.underline
-      : "";
+    let underlineStyle = this.props.showUnderline ?
+      " " + styles.underline :
+      "";
+    let triangleColor = this.props.showTriangleMarker ?
+      " " + styles.triangleTheme :
+      "";
     return (
       <span id={this.props.id + "-container"} className={styles.link + underlineStyle} onMouseOver={this.props.onMouseOver} onMouseOut={this.props.onMouseOut}>
         <a id={this.props.id} className={styles.sectionLink} href={this.props.url} onKeyDown={this.props.onKeyDown}>{this.props.text}</a>
-        <div className={styles.triangle + " " + (this.props.showTriangleMarker
-          ? styles.triangleTheme
-          : styles.triangleWhite)}></div>
-      {this.props.children}
+        <div className={styles.triangle + triangleColor}></div>
+        {this.props.children}
       </span>
     )
   }
