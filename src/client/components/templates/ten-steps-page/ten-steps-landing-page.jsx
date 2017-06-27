aba012b9fc48da8a2253a2ba8e2d07322f79d3dd
jrwatts <Fearless@Jordans-MacBook-Pro.local>
2017-06-27 16:40:05 -0400

    DT-573 Replaces safe image

--- src/client/components/templates/ten-steps-page/ten-steps-landing-page.jsx
+++ src/client/components/templates/ten-steps-page/ten-steps-landing-page.jsx
@@ -100,7 +100,7 @@ class TenStepsLandingPage extends React.Component {
                 title: "Open a business bank account.",
                 leftAlignBox: true,
                 solidBox: false,
-                image: "assets/images/tensteps/bank.jpg",
+                image: "assets/images/tensteps/bank.png",
                 imageAlt: "Choose your business name.",
                 link: "/business-guide/launch/52",
                 text: "A small business checking account can help you handle legal, tax, and day-to-day issues. The good news is it’s easy to set one up if you have the right registrations and paperwork ready."
