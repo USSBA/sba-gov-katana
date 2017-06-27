8e21ba064ccbc0fe9366ab9120d852ca9592ea18
zander312 <iskander93@gmail.com>
2017-06-27 15:36:13 -0400

    adds external image for Hero

--- src/client/components/organisms/hero/hero.jsx
+++ src/client/components/organisms/hero/hero.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import styles from './hero.scss';
 import Callout from '../../molecules/callout/callout.jsx'
 
+class Hero extends React.Component {
 
-const Hero = (props) =>
-  <div className={ styles.heroContainer}>
-	  <img className={ styles.heroDesktopImage } src={ props.desktopImage }/>
-	  <img className={ styles.heroMobileImage } src={ props.mobileImage }/>
-	  <div className={ styles.callout }>
-		<Callout title={ props.title } message={ props.message } buttons={props.buttons} />
-	  </div>
-  </div>;
+	render(){
+		return(
+			<div className={ styles.heroContainer}>
+	  		<img className={ styles.heroDesktopImage } src="http://content.sbagov.fearlesstesters.com/sites/default/files/2017-06/Loans_Hero%20%281%29.jpg"/>
+	  		<div className={ styles.callout }>
+					<Callout title={ this.props.title } message={ this.props.message } buttons={this.props.buttons}/>
+	  		</div>
+  		</div>
+		);
+	}
+}
 
 export default Hero;
