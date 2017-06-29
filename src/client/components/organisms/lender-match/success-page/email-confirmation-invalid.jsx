8b143295061826e2415e9c644f5897d30c4148b3
jrwatts <Fearless@Jordans-MacBook-Pro.local>
2017-06-29 13:41:18 -0400

    DT-2250 Updates email confirmation page message style

--- src/client/components/organisms/lender-match/success-page/email-confirmation-invalid.jsx
+++ src/client/components/organisms/lender-match/success-page/email-confirmation-invalid.jsx
@@ -4,13 +4,13 @@ import styles from './confirm-section.scss';
 class EmailConfirmationInvalid extends React.Component {
   render() {
     return (
-      <div className={styles.section}>
+      <div className={styles.section + " " +styles.emailConfirmationText}>
         <h2 className={styles.title}>
           Your email confirmation link has expired.
         </h2>
-        <p>
+        <h5>
           Please submit a new application with your updated information at <a href="https://www.sba.gov/lendermatch">https://www.sba.gov/lendermatch</a>.
-        </p>
+        </h5>
       </div>
     )
   }
--- src/client/components/organisms/lender-match/success-page/email-confirmed-page.jsx
+++ src/client/components/organisms/lender-match/success-page/email-confirmed-page.jsx
@@ -4,13 +4,13 @@ import styles from './confirm-section.scss';
 class EmailConfirmedPage extends React.Component {
   render() {
     return (
-      <div className={styles.section}>
+      <div className={styles.section  + " " +styles.emailConfirmationText}>
         <h2 className={styles.title}>
           Your email has been confirmed.
         </h2>
-        <p>
+        <h5>
           Your submission is being forwarded to lenders in your area. Please allow at least two business days to be matched with a lender in our network. We'll email you with any updates.
-        </p>
+        </h5>
       </div>
     )
   }
