aca8d26c010a636c6e80f9c82a7b628cc7712420
Jonathan King <jking@fearless.tech>
2017-06-29 13:01:36 -0400

    DT-2436 some formatting fixes

--- src/client/components/organisms/homepage/front-page-lady/front-page-lady.jsx
+++ src/client/components/organisms/homepage/front-page-lady/front-page-lady.jsx
@@ -8,16 +8,20 @@ class FrontPageLady extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <img alt="banner image of small business owner" className={styles.hero} src={"assets/images/homepage/primarylanding-hero.jpg"}/>
-        <img alt="banner image of small business owner" className={styles.mobileHero} src={"assets/images/homepage/primarylanding-mobile-hero.png"}/>
-        <div className={styles.box}>
-          <div className={styles.title}>
-            Start and grow your business.</div>
-          <div className={styles.text}>
-            Whether you're already up and running or just getting started, we can help. Come take a look how.</div>
-        <LargeInversePrimaryButton text="LET'S GO" URL="./starting-managing-business"/>
-          <img alt="" src={Triangle} className={styles.triangle}/>
-          <img alt="" src={MobileTriangle} className={styles.mobileTriangle}/>
+        <div className={styles.imageContainer}>
+          <img alt="banner image of small business owner" className={styles.hero} src={"assets/images/homepage/primarylanding-hero.jpg"}/>
+          <img alt="banner image of small business owner" className={styles.mobileHero} src={"assets/images/homepage/primarylanding-mobile-hero.png"}/>
+        </div>
+        <div className={styles.boxContainer}>
+          <div className={styles.box}>
+            <div className={styles.title}>
+              Start and grow your business.</div>
+            <div className={styles.text}>
+              Whether you're already up and running or just getting started, we can help. Come take a look how.</div>
+            <LargeInversePrimaryButton text="LET'S GO" URL="./starting-managing-business"/>
+            <img alt="" src={Triangle} className={styles.triangle}/>
+            <img alt="" src={MobileTriangle} className={styles.mobileTriangle}/>
+          </div>
         </div>
       </div>
     )
