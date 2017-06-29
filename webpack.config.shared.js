a7749a6f3d28a8581baee47d74a16ee913ee0273
Jonathan King <jking@fearless.tech>
2017-06-29 07:43:19 -0400

    DT-2436 adds a new style name format for css-modules in sass files

--- webpack.config.shared.js
+++ webpack.config.shared.js
@@ -36,7 +36,13 @@ module.exports = function() {
         },
         {
           test: /\.scss$/,
-          use: ["style-loader", "css-loader?modules", {
+          use: ["style-loader", {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]'
+            }
+          }, {
             loader: "sass-loader",
             options: {
               includePaths: [
