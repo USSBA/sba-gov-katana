da27bc8038fd2cc5269a2c9eed400847b55f8b02
Jonathan King <jking@fearless.tech>
2017-06-29 14:24:36 -0400

    DT-2436 more work on using the neat grid for the front page lady

--- src/client/components/organisms/homepage/front-page-lady/front-page-lady.jsx
+++ src/client/components/organisms/homepage/front-page-lady/front-page-lady.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './front-page-lady.scss';
 import LargeInversePrimaryButton from '../../../atoms/large-inverse-primary-button/large-inverse-primary-button.jsx'
 import Triangle from '../../../../../../public/assets/images/homepage/primary-landing/desktop-corner-graphic.png';
-import MobileTriangle from '../../../../../../public/assets/images/homepage/primary-landing/mobile-corner-graphic.png';
 
 class FrontPageLady extends React.Component {
   render() {
@@ -20,7 +19,6 @@ class FrontPageLady extends React.Component {
               Whether you're already up and running or just getting started, we can help. Come take a look how.</div>
             <LargeInversePrimaryButton text="LET'S GO" URL="./starting-managing-business"/>
             <img alt="" src={Triangle} className={styles.triangle}/>
-            <img alt="" src={MobileTriangle} className={styles.mobileTriangle}/>
           </div>
         </div>
       </div>
