2c5f7923c17b2ec163d3dd33674a553cee9e5aa6
Jonathan King <jking@fearless.tech>
2017-07-06 09:04:49 -0400

    DT-2436 adds alt text to the LM landing page hero

--- src/client/components/organisms/hero/hero.jsx
+++ src/client/components/organisms/hero/hero.jsx
@@ -5,8 +5,8 @@ import Callout from '../../molecules/callout/callout.jsx'
 
 const Hero = (props) =>
   <div className={ styles.heroContainer}>
-	  <img className={ styles.heroDesktopImage } src={ props.desktopImage }/>
-	  <img className={ styles.heroMobileImage } src={ props.mobileImage }/>
+	  <img className={ styles.heroDesktopImage } src={ props.desktopImage } alt={props.alt}/>
+	  <img className={ styles.heroMobileImage } src={ props.mobileImage } alt={props.alt}/>
 	  <div className={ styles.callout }>
 		<Callout title={ props.title } message={ props.message } buttons={props.buttons} />
 	  </div>
