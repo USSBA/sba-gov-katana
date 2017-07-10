119910a110ec318bcccc06465d475b7f9cd74d61
Charles Bushong <bushong1@gmail.com>
2017-07-10 15:39:16 -0400

    DT-0: Cleaning up docs, adding error messages to dev-scripts

--- scripts/db/sql/import-sql.sh
+++ scripts/db/sql/import-sql.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-echo "create database sba_katana" | mysql --user=root --password=password123
-echo "create database sba_drupal8" | mysql --user=root --password=password123
-echo "create database sba_drupal" | mysql --user=root --password=password123
+printf "Creating katana database..."
+echo "create database IF NOT EXISTS sba_katana;" | mysql --user=root --password=password123
+printf "OK\n"
+printf "Importing katana data..."
 cat /mnt/database/sba_katana.sql | mysql --user=root --password=password123 sba_katana
+printf "OK"
+printf "\nCreating drupal8 database..."
+echo "create database IF NOT EXISTS sba_drupal8" | mysql --user=root --password=password123
+printf "OK\n"
+printf "Importing drupal8 data..."
 cat /mnt/database/sba_drupal8.sql | mysql --user=root --password=password123 sba_drupal8
+printf "OK\n"
+printf "Creating drupal database..."
+echo "create database IF NOT EXISTS sba_drupal" | mysql --user=root --password=password123
+printf "OK\n"
+printf "Importing drupal data..."
 cat /mnt/database/sba_drupal.sql | mysql --user=root --password=password123 sba_drupal
+printf "OK\n"
