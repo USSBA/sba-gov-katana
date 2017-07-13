32a82c4fd79a98a2a8f7662b5105066ef7956610
Jonathan King <jking@fearless.tech>
2017-07-13 10:17:04 -0400

    DT-0000 fixes an issue with the endpoint for hte counsels redirect on the LM success page

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ import config from "config";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import HttpStatus from "http-status-codes";
-import { enableWebpackHotModuleReplacement, addDevelopmentErrorHandler } from "./util/dev.js";
+import {
+  enableWebpackHotModuleReplacement,
+  addDevelopmentErrorHandler
+} from "./util/dev.js";
 
 const app = express();
 app.use(cookieParser());
@@ -71,7 +74,7 @@ import * as lenderMatchController from "./controllers/lender-match-controller.js
 app.post("/lendermatch/matchFormData", jsonParser, lenderMatchController.handleLenderMatchSubmission);
 app.get("/actions/lendermatch/confirmEmail", lenderMatchController.handleEmailConfirmation);
 app.post("/lendermatch/resend", jsonParser, lenderMatchController.handleResendEmailConfirmation);
-app.get("/content/counselors-redirect.json", function(req, res) {
+app.get("/api/content/counselors-redirect.json", function(req, res) {
   const zipStr = "zip:" + req.query.zip + ":distance:50";
   zlib.deflate(zipStr, function(err, buffer) {
     if (err) {
@@ -97,16 +100,25 @@ import * as lincCounselorController from "./controllers/linc-counselor.js";
 app.get("/api/content/counselors-by-location.json", lincCounselorController.getCounselorsByLocation);
 
 
-import { getUserRoles } from "./controllers/user-roles.js";
+import {
+  getUserRoles
+} from "./controllers/user-roles.js";
 app.get("/api/content/:userId/roles.json", getUserRoles);
 
-import { getDrupalUserEmail } from "./controllers/user-email.js";
+import {
+  getDrupalUserEmail
+} from "./controllers/user-email.js";
 app.get("/api/content/:userId/email.json", getDrupalUserEmail);
 
-import { registerUserForNewsletter } from "./controllers/newsletter-registration.js";
+import {
+  registerUserForNewsletter
+} from "./controllers/newsletter-registration.js";
 app.get("/api/content/newsletter-registration.json", registerUserForNewsletter);
 
-import { fetchContentById, fetchContentByType } from "./controllers/content.js";
+import {
+  fetchContentById,
+  fetchContentByType
+} from "./controllers/content.js";
 app.get("/api/content/:type/:id.json", fetchContentById);
 app.get("/api/content/:type.json", fetchContentByType);
 
