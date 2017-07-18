4a554952634a57682c712d1c1a1a1b5f59afa720
Jonathan King <jking@fearless.tech>
2017-07-18 08:21:08 -0400

    DT-1945 refactors the business guide panel page to abstract it for funding programs
    DT-1945 fixes the styling for the funding programs landing page
    
    DT-1945 changes the funding programs to load five tiles instead of four
    
    DT-1945 changes the funding programs to load five tiles instead of four
    
    DT-1945 fixes the font size for hte five panel display
    
    DT-1945 adds some margin at the bottom to add space between the 5th tile and the footer

---
+++ src/client/components/organisms/funding-programs-tile-collection/icons.jsx
@@ -0,0 +1,31 @@
+import fundingProgramsIconDisasterColor from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Disaster_color.svg"
+import fundingProgramsIconDisasterWhite from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Disaster_white.svg"
+import fundingProgramsIconGrantsColor from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Grants_color.svg"
+import fundingProgramsIconGrantsWhite from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Grants_white.svg"
+import fundingProgramsIconInvestmentColor from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Investment_color.svg"
+import fundingProgramsIconInvestmentWhite from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Investment_white.svg"
+import fundingProgramsIconLoansColor from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Loans_color.svg"
+import fundingProgramsIconLoansWhite from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Loans_white.svg"
+import fundingProgramsIconSuretyColor from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Surety_color.svg"
+import fundingProgramsIconSuretyWhite from "../../../../../public/assets/svg/funding-programs/Funding_Programs_Icon_Surety_white.svg"
+
+let fundingProgramIcons = [
+  {
+    icon: fundingProgramsIconLoansColor,
+    iconWhite: fundingProgramsIconLoansWhite
+  }, {
+    icon: fundingProgramsIconInvestmentColor,
+    iconWhite: fundingProgramsIconInvestmentWhite
+  }, {
+    icon: fundingProgramsIconDisasterColor,
+    iconWhite: fundingProgramsIconDisasterWhite
+  }, {
+    icon: fundingProgramsIconSuretyColor,
+    iconWhite: fundingProgramsIconSuretyWhite
+  }, {
+    icon: fundingProgramsIconGrantsColor,
+    iconWhite: fundingProgramsIconGrantsWhite
+  }
+]
+
+export default fundingProgramIcons;
--- src/client/components/pages/section-page/section-page.jsx
+++ src/client/components/pages/section-page/section-page.jsx
@@ -2,20 +2,38 @@ import React from 'react'
 import styles from "./section-page.scss";
 import path from 'path';
 import SimpleCta from "../../molecules/simple-cta/simple-cta.jsx"
-import BizguideTileCollection from "../../molecules/bizguide-tile/bizguide-tile-collection.jsx"
+import BusinessGuideTileCollection from "../../organisms/business-guide-tile-collection/business-guide-tile-collection.jsx";
+import FundingProgramsTileCollection from "../../organisms/funding-programs-tile-collection/funding-programs-tile-collection.jsx";
 import constants from "../../../services/constants.js"
 
 class SectionPage extends React.Component {
 
   render() {
     if (this.props.sectionData) {
+      console.log("this.props.sectionData", this.props.sectionData);
       let sectionData = this.props.sectionData;
+      let sectionName = this.props.sectionData.title;
+
+      let tenSteps = undefined;
+      if (sectionName === constants.sections.businessGuide) {
+        tenSteps = (
+          <div className={styles.nineStepsCtaContainer}>
+            <SimpleCta id="business-guide-panel-10-steps-callout" actionText="Start your business in 10 steps" buttonText="SEE THE GUIDE" url={constants.routes.tenSteps} eventCategory="Ten Steps CTA" labelCategory="Small"/>
+          </div>
+        );
+      }
+
+      let tileCollection = undefined;
+      if (sectionName === constants.sections.businessGuide) {
+        tileCollection = (<BusinessGuideTileCollection sectionData={sectionData}/>);
+      } else if (sectionName === constants.sections.fundingPrograms) {
+        tileCollection = (<FundingProgramsTileCollection sectionData={sectionData}/>);
+      }
+
       return (
         <div className={styles.container}>
-          <BizguideTileCollection sectionData={sectionData}/>
-          <div className={styles.nineStepsCtaContainer}>
-            <SimpleCta id="business-guide-panel-10-steps-callout" actionText="Start your business in 10 steps" buttonText="SEE THE GUIDE" url={constants.routes.tenSteps} eventCategory="Ten Steps CTA" labelCategory="Small" />
-          </div>
+          {tileCollection}
+          {tenSteps}
         </div>
       );
     }
