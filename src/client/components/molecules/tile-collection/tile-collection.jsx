98b9fad6f1338a85daa78ba3ba77cbdaef5df122
Jonathan King <jking@fearless.tech>
2017-07-18 10:45:27 -0400

    DT-1944 fixes the styling for the funding programs landing page

--- src/client/components/molecules/tile-collection/tile-collection.jsx
+++ src/client/components/molecules/tile-collection/tile-collection.jsx
@@ -14,7 +14,8 @@ class TileCollection extends React.Component {
       data: object,
       icon: iconElement.icon,
       backgroundLines: iconElement.background,
-      iconWhite: iconElement.iconWhite
+      iconWhite: iconElement.iconWhite,
+      hoverShowsInverseOnly: this.props.hoverShowsInverseOnly
     };
     return (<Tile {...tileProps}/>);
   }
@@ -24,6 +25,8 @@ class TileCollection extends React.Component {
       return (<div/>);
     } else if (this.props.data.length !== this.props.icons.length) {
       console.error("Invalid number of icons provided to tile collection");
+      console.log(this.props.data);
+      console.log(this.props.icons);
       return (<div/>);
     }
     return (
@@ -36,7 +39,8 @@ class TileCollection extends React.Component {
 
 TileCollection.defaultProps = {
   data: [],
-  icons: {}
+  icons: {},
+  hoverShowsInverseOnly: false
 }
 
 export default TileCollection;
--- src/client/components/molecules/tile-collection/tile.jsx
+++ src/client/components/molecules/tile-collection/tile.jsx
@@ -7,134 +7,150 @@ import * as ModalActions from '../../../actions/show-modal.js'
 import cornerLines from "../../../../../public/assets/images/corner-diagonal-lines-grey.png"
 
 class Tile extends React.Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       displayHoverMenu: false
     }
   }
 
-  _formatLargeTitle(){
+  _formatLargeTitle() {
     return this.props.data.title.split(" ")[0]
   }
 
-  _formatSmallTitle(){
+  _formatSmallTitle() {
     let arr = this.props.data.title.split(" ")
     arr.shift()
     return arr.join(" ")
   }
 
-  _openNavMenu(){
-    if(window.innerWidth <= 1080) {
+  _openNavMenu() {
+    if (window.innerWidth <= 1080) {
       this.props.actions.showMobileSectionNav(this.props.data, this.props.iconWhite, false)
     }
   }
 
-  _toggleTileClassOnTab(){
-    if(this.state.displayMenuOnFocus === false){
+  _toggleTileClassOnTab() {
+    if (this.state.displayMenuOnFocus === false) {
       return ""
-    } else if(this.state.displayMenuOnFocus === true){
+    } else if (this.state.displayMenuOnFocus === true) {
       return s.tabChangeTileBackground
     }
   }
 
-  _mouseEnterTile(){
-    if(window.innerWidth >= 1080) {
+  _mouseEnterTile() {
+    if (window.innerWidth >= 1080) {
       this.setState({displayHoverMenu: true})
     }
   }
 
-  _mouseExitTile(){
-    if(window.innerWidth >= 1080) {
+  _mouseExitTile() {
+    if (window.innerWidth >= 1080) {
       this.setState({displayHoverMenu: false})
     }
   }
 
-  _tabFocusTile(){
-    this.setState({displayHoverMenu: !this.state.displayHoverMenu})
+  _tabFocusTile() {
+    this.setState({
+      displayHoverMenu: !this.state.displayHoverMenu
+    })
   }
 
   render() {
-      return (
-          <div id={this.props.id}
-              className={s.tile}
-              onClick={() => {this._openNavMenu()}}
-              onMouseEnter={() => {this._mouseEnterTile()}}
-              onMouseLeave={() => {this._mouseExitTile()}}
-           >
-            <a className={s.tabDisplayMenu} href="" onClick={(e) => {e.preventDefault()}} onFocus={() => {this._mouseEnterTile()}}>toggle {this.props.data.title} menu</a>
-            {this.state.displayHoverMenu ? (
-              <BizguideTileHover
-                data={this.props.data}
-                id={this.props.id + '-hover'}
-                largeTitle={this._formatLargeTitle()}
-                smallTitle={this._formatSmallTitle()}
-              />
-            ) : (
-              <BizguideTileStatic
-                data={this.props.data}
-                id={this.props.id + '-static'}
-                icon={this.props.icon}
-                largeTitle={this._formatLargeTitle()}
-                smallTitle={this._formatSmallTitle()}
-              />
-            )}
-            <img className={s.backgroundLines} src={this.props.backgroundLines} alt=""/>
-          </div>
-      );
-  }
-}
+    let baseTileData = {
+      data: this.props.data,
+      icon: this.props.icon,
+      iconWhite: this.props.iconWhite,
+      largeTitle: this._formatLargeTitle(),
+      smallTitle: this._formatSmallTitle()
+    };
 
-class BizguideTileStatic extends React.Component {
+    let hoverTile = this.props.hoverShowsInverseOnly
+      ? (<StaticTile {...baseTileData} inverse id={this.props.id + '-hover'}/>)
+      : (<HoverTileWithHoverLinks {...baseTileData} id={this.props.id + '-hover'}/>);
 
-  render() {
-    return(
-      <div id={this.props.id} className={s.tileNormal}>
-        <img id={this.props.id + "-icon"} className={s.icon} src={this.props.icon} alt=""/>
-          <div id={this.props.id + "-title"} className={s.titleContainer}>
-            <h2 className={s.largeTitle}>{this.props.largeTitle}</h2>
-            <h4 className={s.smallTitle}>{this.props.smallTitle}</h4>
-          </div>
-          <i className={s.rightArrow + " fa fa-angle-right"}></i>
-          <div className={s.line}></div>
-          <p id={this.props.id + "-blurb"} className={s.blurb}>{this.props.data.description}</p>
-          <img className={s.cornerLines} src={cornerLines} alt=""/>
+    return (
+      <div id={this.props.id} className={s.tile} onClick={() => {
+        this._openNavMenu()
+      }} onMouseEnter={() => {
+        this._mouseEnterTile()
+      }} onMouseLeave={() => {
+        this._mouseExitTile()
+      }}>
+        <a className={s.tabDisplayMenu} href="" onClick={(e) => {
+          e.preventDefault()
+        }} onFocus={() => {
+          this._mouseEnterTile()
+        }}>toggle {this.props.data.title}
+          menu</a>
+        {this.state.displayHoverMenu
+          ? hoverTile
+          : (<StaticTile id={this.props.id + '-static'} {...baseTileData}/>)}
+        {this.props.backgroundLines
+          ? <img className={s.backgroundLines} src={this.props.backgroundLines} alt=""/>
+          : undefined}
       </div>
     );
   }
 }
 
-class BizguideTileHover extends React.Component {
+class StaticTile extends React.Component {
 
-  _handleClick(linkObject){
+  render() {
+    return (
+      <div id={this.props.id} className={s.tileNormal + " " + (this.props.inverse
+        ? s.tileInverse
+        : "")}>
+        <img id={this.props.id + "-icon"} className={s.icon} src={this.props.inverse
+          ? this.props.iconWhite
+          : this.props.icon} alt=""/>
+        <div id={this.props.id + "-title"} className={s.titleContainer+ " " + (this.props.inverse
+          ? s.titleInverse
+          : "")}>
+          <h2 className={s.largeTitle}>{this.props.largeTitle}</h2>
+          <h4 className={s.smallTitle}>{this.props.smallTitle}</h4>
+        </div>
+        <i className={s.rightArrow + " fa fa-angle-right"}></i>
+        <div className={s.line + " " + (this.props.inverse
+          ? s.lineInverse
+          : "")}></div>
+        <p id={this.props.id + "-blurb"} className={s.blurb + " " + (this.props.inverse
+          ? s.blurbInverse
+          : "")}>{this.props.data.description}</p>
+        <img className={s.cornerLines} src={cornerLines} alt=""/>
+      </div>
+    );
+  }
+}
+
+class HoverTileWithHoverLinks extends React.Component {
+
+  _handleClick(linkObject) {
     document.location = linkObject.fullUrl
   }
 
   render() {
-    return(
+    return (
       <div id={this.props.id} className={s.tileHover}>
         <h2 className={s.largeTitleHover}>{this.props.largeTitle}</h2>
         <h4 className={s.smallTitleHover}>{this.props.smallTitle}</h4>
         <div className={s.topLine}></div>
-        {
-          this.props.data.children.map((object, index) => {
-            return <HoverLink id={this.props.id + "-link-" + index } key={index} link={object} handleClick={this._handleClick}/>
-          })
-        }
+        {this.props.data.children.map((object, index) => {
+          return <HoverLink id={this.props.id + "-link-" + index} key={index} link={object} handleClick={this._handleClick}/>
+        })
+}
       </div>
     );
   }
 }
 
-const HoverLink = (props) =>
-  <div className={s.linkContainer} onClick={() => {props.handleClick(props.link)}}>
-    <a id={props.id}
-      className={s.link}
-      href={props.link.fullUrl}>
-      {props.link.title}
-    </a>
-  </div>
-
+const HoverLink = (props) => <div className={s.linkContainer} onClick={() => {
+  props.handleClick(props.link)
+}}>
+  <a id={props.id} className={s.link} href={props.link.fullUrl}>
+    {props.link.title}
+  </a>
+</div>
 
 function mapReduxStateToProps(reduxState) {
   return {};
