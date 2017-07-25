fe005dfb5d77d3b312a554e72ef29051249e3e06
Avery Smith <asmith@Averys-MacBook-Pro.local>
2017-07-25 13:39:50 -0400

    DT-2255-add-node-type made hero component more configurable to be attached to Charle's program-page json object

--- src/client/components/molecules/callout/callout.jsx
+++ src/client/components/molecules/callout/callout.jsx
@@ -4,28 +4,37 @@ import LargeInverseSecondaryButton from "../../atoms/large-inverse-secondary-but
 import LargeInversePrimaryButton from "../../atoms/large-inverse-primary-button/large-inverse-primary-button.jsx";
 
 class Callout extends React.Component {
- render(){
+  
+  render() {
 
-   return (
-       <div className={styles.callout}>
+    let calloutStyles = styles.callout
+    let buttonStyles = styles.button
+
+    if (this.props.inHeroWithNoImage) {
+      calloutStyles +=  " " + styles.noDecoration
+      buttonStyles += " " + styles.noFloat
+    }
+
+    return (
+       <div className={calloutStyles}>
           <h2 className={styles.title}>{ this.props.title }</h2>
           <p className={styles.message}>{ this.props.message }</p>
           <div className={styles.buttonContainer}>
               {
                this.props.buttons.map((item, index)=>{
                    if(item.btnType === "LargeInverseSecondaryButton"){
-                       return (<div className={styles.button} key={index}><LargeInverseSecondaryButton text={item.btnText} key={index} onClick={item.onClickHandler}/></div>);
+                       return (<div className={buttonStyles} key={index}><LargeInverseSecondaryButton text={item.btnText} key={index} onClick={item.onClickHandler}/></div>);
                    }
                    if(item.btnType === "LargeInversePrimaryButton"){
-                       return (<div className={styles.button} key={index}><LargeInversePrimaryButton text={item.btnText} key={index} onClick={item.onClickHandler}/></div>);v
+                       return (<div className={buttonStyles} key={index}><LargeInversePrimaryButton text={item.btnText} key={index} onClick={item.onClickHandler}/></div>);
                    }
                })
               }
            </div>
        </div>
-   ) ;
- }
-
+    );
+  
+  }
 
  }
 export default Callout;
--- src/client/components/organisms/hero/hero.jsx
+++ src/client/components/organisms/hero/hero.jsx
@@ -11,21 +11,25 @@ class Hero extends React.Component {
   render() {
     return (
       <div className={styles.heroContainer}>
-
-        <div className={styles.heroMobileImageContainer}>
-          <div title={this.props.alt} style={this.imgStyles()}></div>
+        { this.props.imageUrl && 
+        <div>
+          <div className={styles.heroMobileImageContainer}>
+            <div title={this.props.alt} style={this.imgStyles()}></div>
+          </div>
+          <img className={styles.heroDesktopImage} src={this.props.imageUrl} alt={this.props.alt}/>
+          <div className={styles.callout}>
+            <Callout title={this.props.title} message={this.props.message} buttons={this.props.buttons}/>
+          </div>
         </div>
-        <img className={styles.heroDesktopImage} src={this.props.imageUrl} alt={this.props.alt}/>
-        <div className={styles.callout}>
-          <Callout title={this.props.title} message={this.props.message} buttons={this.props.buttons}/>
+        }
+        { !this.props.imageUrl && 
+        <div className={styles.inHeroWithNoImage}>
+          <Callout inHeroWithNoImage={true} title={this.props.title} message={this.props.message} buttons={this.props.buttons}/>
         </div>
+        }
       </div>
     );
   }
-}
-
-Hero.defaultProps = {
-    imageUrl: React.PropTypes.string
 }
 
 export default Hero;
