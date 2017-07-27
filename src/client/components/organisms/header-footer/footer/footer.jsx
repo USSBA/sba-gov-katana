b6261effdcdec0d3e1441f6d62374195e769175e
Avery Smith <asmith@Averys-MacBook-Pro.local>
2017-07-27 13:40:10 -0400

    DT-2866 added instagram icon to footer

--- src/client/components/organisms/header-footer/footer/footer.jsx
+++ src/client/components/organisms/header-footer/footer/footer.jsx
@@ -10,6 +10,7 @@ import twitterThumbnail from "../../../../../../public/assets/images/footer/twit
 import googlePlusThumbnail from "../../../../../../public/assets/images/footer/googleplus.png"
 import youtubeThumbnail from "../../../../../../public/assets/images/footer/youtube-play.png"
 import linkedInThumbnail from "../../../../../../public/assets/images/footer/linkedin.png"
+import instagramThumbnail from "../../../../../../public/assets/images/footer/instagram.png"
 
 const SocialMediaLinkSet = () => {
   return (
@@ -18,7 +19,7 @@ const SocialMediaLinkSet = () => {
       <SocialMediaLink id="twitter-link" key={2} image={twitterThumbnail} altText="SBA Twitter page" url="https://www.twitter.com/sbagov"/>
       <SocialMediaLink id="google-plus-link" key={3} image={googlePlusThumbnail} altText="SBA Google Plus page" url="https://plus.google.com/+sbagov"/>
       <SocialMediaLink id="youtube-link" key={4} image={youtubeThumbnail} altText="SBA Youtube page" url="https://www.youtube.com/user/sba"/>
-      <SocialMediaLink id="linkedin-link" key={5} image={linkedInThumbnail} altText="SBA LinkedIn page" url="https://www.linkedin.com/company/us-small-business-administration"/>
+      <SocialMediaLink id="instagram-link" key={5} image={instagramThumbnail} altText="SBA Instagram page" url="https://www.instagram.com/sbagov"/>
     </div>
   );
 }
