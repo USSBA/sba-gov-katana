afd6927943c8f719e0af33658c0b38f6cfa3f7bf
zander312 <iskander93@gmail.com>
2017-07-28 09:48:59 -0400

    adds document page

---
+++ src/client/components/templates/document-page/document-page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import s from './document-page.scss'
+
+class DocumentPage extends React.Component {
+
+  render() {
+     return (
+     	<div>
+     		react component
+     	</div>
+     );
+	}
+}
+
+export default DocumentPage 
