838655868e15460c1455cabec7bbb8742e890d88
Jonathan King <jking@fearless.tech>
2017-08-01 12:16:19 -0400

    DT-2872 expands to clear cache functionality

--- src/controllers/cache.js
+++ src/controllers/cache.js
@@ -4,9 +4,32 @@ import cache from "memory-cache";
 
 
 function clearCache(req, res) {
-  console.log("Clearing the Cache");
-  cache.clear();
+  console.log("Clearing the Cache (but not contacts or documents)");
+  const keys = cache.keys();
+  const keysWithoutContacts = _.filter(keys, (item) => {
+    return !item.includes("contact");
+  });
+
+  keysWithoutContacts.forEach((key) => {
+    cache.del(key);
+    console.log("Cleared from cache", key);
+  });
   res.status(HttpStatus.NO_CONTENT).send();
 }
 
-export { clearCache };
+
+
+function clearContactCache(req, res) {
+  console.log("Clearing the Cache");
+  const keys = cache.keys();
+  const contactKeys = _.filter(keys, (item) => {
+    return item.includes("contact");
+  });
+  contactKeys.forEach((key) => {
+    cache.del(key);
+    console.log("Cleared from cache", key);
+  });
+  res.status(HttpStatus.NO_CONTENT).send();
+}
+
+export { clearCache, clearContactCache };
