967d71c97b82faf73a7b89f8da9f8edf04dbe986
zander312 <iskander93@gmail.com>
2017-08-01 11:22:13 -0400

    add document page to devtest, add divider comonent to devtest

--- src/client/components/organisms/developer-tester/developer-tester.jsx
+++ src/client/components/organisms/developer-tester/developer-tester.jsx
@@ -1,73 +1,78 @@
-import React from 'react';
-import {
-  connect
-}
-from 'react-redux';
-import {
-  bindActionCreators
-}
-from 'redux';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import styles from "./developer-tester.scss";
 
-import DocumentCard from '../../molecules/document-card/document-card.jsx';
+import DocumentCard from "../../molecules/document-card/document-card.jsx";
+import DocumentPage from "../../templates/document-page/document-page.jsx";
 import _ from "lodash";
 
 export class DeveloperTester extends React.Component {
   constructor() {
     super();
-
   }
 
-
   render() {
-
     let myprops = {
-      "type": "document",
-      "summary": "Document Summary",
-      "title": "DocumentTitle",
-      "programs": [
-        "Authorization",
-        "General"
+      type: "document",
+      summary: "Document Summary",
+      title: "DocumentTitle",
+      programs: ["Authorization", "General"],
+      relatedDocuments: [],
+      files: [
+        {
+          type: "docFile",
+          effectiveDate: "1867-11-07",
+          expirationDate: "1934-07-04",
+          fileUrl:
+            "http://drupal8.content.hostname/sites/default/files/1898-12/88.txt",
+          version: "226"
+        },
+        {
+          type: "docFile",
+          effectiveDate: "1970-01-01",
+          expirationDate: "2038-01-19",
+          fileUrl:
+            "http://drupal8.content.hostname/sites/default/files/1970-01/0.txt",
+          version: "4"
+        }
       ],
-      "relatedDocuments": [],
-      "files": [{
-        "type": "docFile",
-        "effectiveDate": "1867-11-07",
-        "expirationDate": "1934-07-04",
-        "fileUrl": "http://drupal8.content.hostname/sites/default/files/1898-12/88.txt",
-        "version": "226"
-      }, {
-        "type": "docFile",
-        "effectiveDate": "1970-01-01",
-        "expirationDate": "2038-01-19",
-        "fileUrl": "http://drupal8.content.hostname/sites/default/files/1970-01/0.txt",
-        "version": "4"
-      }],
-      "documents": [{
-        "idType": "Authorization",
-        "number": "17 01 A",
-        "type": "documentId"
-      }, {
-        "idType": "General",
-        "number": "17 01 D",
-        "type": "documentId"
-      }],
-      "body": "<p>With a body</p>\r\n",
-      "officeLink": {
-        "url": "http://office-link.example.com",
-        "title": "Office Example Test"
+      documents: [
+        {
+          idType: "Authorization",
+          number: "17 01 A",
+          type: "documentId"
+        },
+        {
+          idType: "General",
+          number: "17 01 D",
+          type: "documentId"
+        }
+      ],
+      body: "<p>With a body</p>\r\n",
+      officeLink: {
+        url: "http://office-link.example.com",
+        title: "Office Example Test"
       },
-      "activitys": [
-        "Plan",
-        "Export express"
-      ]
+      activitys: ["Plan", "Export express"]
     };
     return (
-      <div className={styles.container}>
-        <DocumentCard></DocumentCard>
+      <div>
+        <div className={styles.container}>
+          <DocumentCard />
+        </div>
+        <Divider />
+        <DocumentPage />
+        <Divider />
       </div>
     );
   }
+}
+
+const Divider = (props) => {
+  return(
+    <div className={styles.divider}> COMPONENT DIVIDER  </div>
+  )
 }
 
 function mapReduxStateToProps(reduxState) {
@@ -77,4 +82,6 @@ function mapReduxStateToProps(reduxState) {
 function mapDispatchToProps(dispatch) {
   return {};
 }
-export default connect(mapReduxStateToProps, mapDispatchToProps)(DeveloperTester);
+export default connect(mapReduxStateToProps, mapDispatchToProps)(
+  DeveloperTester
+);
