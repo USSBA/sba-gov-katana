850c5f5bd432e2999e84c2da4c5eed32d6b1e01c
zander312 <iskander93@gmail.com>
2017-08-02 15:48:46 -0400

    modify document card to work for both document lookup and page

--- src/client/components/molecules/document-card/document-card.jsx
+++ src/client/components/molecules/document-card/document-card.jsx
@@ -1,73 +1,80 @@
-import React from 'react';
-import {
-    DecorativeDash,
-    PdfIcon
-}
-from "../../atoms";
+import React from "react";
+import { DecorativeDash, PdfIcon } from "../../atoms";
 import s from "./document-card.scss";
+import DocumentType from "../../atoms/document-type/document-type.jsx";
 
 class DocumentCard extends React.Component {
     render() {
-        const id = this.props.documents[0].number;
-
-        const ActivityRow = this.props.activitys && this.props.activitys.length >0 ?
-            (<div className={s.row}> 
-                    <div className={s.columnOne}>Activity:</div>
-                    <div className={s.columnTwo}>{this.props.activitys.join(", ")}</div>
-                </div>) :
-            undefined;
+        console.log(this.props.latestFile);
+        const ActivityRow =
+            this.props.activities && this.props.activities.length > 0
+                ? <div className={s.row}>
+                      <div className={s.columnOne}>Activity:</div>
+                      <div className={s.columnTwo}>
+                          {this.props.activities.join(", ")}
+                      </div>
+                  </div>
+                : undefined;
 
         return (
             <div className={s.container}>
-            <div className={s.typeAndId}>
-                <div className={s.type}>{this.props.type}</div>
-                <div className={s.id}>{id}</div>
-            </div>
-            <div>
-            <h6 className={s.title}>
-                {this.props.title}
-            </h6>
-            </div>
-            <div className={s.dash}><DecorativeDash/></div>
-            <div  className={s.details}>
-                <div className={s.row}> 
-                    <div className={s.columnOne}>Program:</div>
-                    <div className={s.columnTwo}>{this.props.programs.join(", ")}</div>
+                <div className={s.documentTypeContainer}>
+                   <DocumentType className={s.documentType} type={this.props.type} number={this.props.number}/>
                 </div>
-                {ActivityRow}
-                <div className={s.row}> 
-                    <div className={s.columnOne}>Summary:</div>
-                    <div className={s.columnTwo}>{this.props.summary}</div>
+                <div>
+                    <h6 className={s.title}>
+                        {this.props.title}
+                    </h6>
+                </div>
+
+                {this.props.programs ||
+                this.props.activities ||
+                this.props.summary
+                    ? <div>
+                          <div className={s.dash}>
+                              <DecorativeDash />
+                          </div>
+                          <div className={s.details}>
+                              {this.props.programs
+                                  ? <div className={s.row}>
+                                        <div className={s.columnOne}>
+                                            Program:
+                                        </div>
+                                        <div className={s.columnTwo}>
+                                            {this.props.programs.join(", ")}
+                                        </div>
+                                    </div>
+                                  : null}
+                              {this.props.activities ? ActivityRow : null}
+                              {this.props.summary
+                                  ? <div className={s.row}>
+                                        <div className={s.columnOne}>
+                                            Summary:
+                                        </div>
+                                        <div className={s.columnTwo}>
+                                            {this.props.summary}
+                                        </div>
+                                    </div>
+                                  : null}
+                          </div>
+                      </div>
+                    : null}
+
+                <div className={s.download}>
+                    <a
+                        onClick={() =>
+                            this.props.downloadClick(this.props.latestFile)}
+                        className={s.link}
+                    >
+                        Download PDF
+                    </a>
+                    <PdfIcon />
                 </div>
             </div>
-            <div className={s.download}><a className={s.link}>Download PDF</a><PdfIcon/></div>
-        </div>
         );
     }
 }
 
-
-DocumentCard.defaultProps = {
-    "type": "MyType",
-    "summary": "Document Summary",
-    "title": "Document Title ABC",
-    "programs": [
-        "Program 1",
-        "Program 2"
-    ],
-    "documents": [{
-        "idType": "Authorization",
-        "number": "00 01 A",
-        "type": "documentId"
-    }],
-    "activitys": [
-        "Activity 1",
-        "Activity 2"
-    ]
-};
-
-DocumentCard.propTypes = {
-
-};
+DocumentCard.propTypes = {};
 
 export default DocumentCard;
