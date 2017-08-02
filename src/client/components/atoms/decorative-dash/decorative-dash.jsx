34126b3f8e5328e4ffb51e684959acc6a81338eb
Jonathan King <jking@fearless.tech>
2017-08-02 13:28:19 -0400

    DT-0000 fixes the jest tests after the document page updates

--- src/client/components/atoms/decorative-dash/decorative-dash.jsx
+++ src/client/components/atoms/decorative-dash/decorative-dash.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import styles from './decorative-dash.scss'
 
 class DecorativeDash extends React.Component {
-	
+
 	render() {
-	
-		return <hr className={styles.decorativeDash + " " + this.props.className} />
-	
+		return <hr className={styles.decorativeDash +  (this.props.className ? " " + this.props.className: "")} />
 	}
 
 }
