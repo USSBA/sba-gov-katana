052a131bccb5a3617db78e51abdbff9a11875d89
Jonathan King <jking@fearless.tech>
2017-08-02 12:07:37 -0400

    DT-2657 hooks up the document lookup and the server endpoint

--- src/client/components/molecules/document-lookup/document-lookup.jsx
+++ src/client/components/molecules/document-lookup/document-lookup.jsx
@@ -1,262 +1,232 @@
 import React from "react";
-import { pick } from "lodash";
+import {pick} from "lodash";
 import _ from "lodash";
 
 import s from "./document-lookup.scss";
 import SmallInverseSecondaryButton from "../../atoms/small-inverse-secondary-button/small-inverse-secondary-button.jsx";
-import { Multiselect, TextInput, SearchIcon } from "../../atoms";
+import {Multiselect, TextInput, SearchIcon} from "../../atoms";
+import DocumentCardCollection from "../../organisms/document-card-collection/document-card-collection.jsx"
 
 const createSlug = (str) => {
-	return str.toLowerCase()
-		.replace(/[^\w\s-]/g, "") // remove non-word [a-z0-9_], non-whitespace, non-hyphen characters
-		.replace(/[\s_-]+/g, "-") // swap any length of whitespace, underscore, hyphen characters with a single -
-		.replace(/^-+|-+$/g, "");
+  return str.toLowerCase().replace(/[^\w\s-]/g, ""). // remove non-word [a-z0-9_], non-whitespace, non-hyphen characters
+  replace(/[\s_-]+/g, "-"). // swap any length of whitespace, underscore, hyphen characters with a single -
+  replace(/^-+|-+$/g, "");
 };
 
 const createCamelCase = (str) => {
-	const sliceIndex = 1;
-	const _str = str[0].toLowerCase() + str.slice(sliceIndex);
-	return _str.replace(" ", "");
+  const sliceIndex = 1;
+  const _str = str[0].toLowerCase() + str.slice(sliceIndex);
+  return _str.replace(" ", "");
 };
 
 class DocumentLookup extends React.Component {
-		constructor(ownProps) {
-			console.log("ownProps", ownProps)
-			super();
-			this.state = {
-				documents: ownProps.items,
-				documentType: "all",
-				programName: "all",
-				documentActivity: "all",
-				sortBy: "last-updated",
-				taxonomies: []
-			};
-		}
+  constructor(ownProps) {
+    console.log("ownProps", ownProps)
+    super();
+    this.state = {
+      documents: ownProps.items,
+      documentType: "all",
+      programName: "all",
+      documentActivity: "all",
+      sortBy: "last-updated",
+      taxonomies: []
+    };
+  }
 
-		componentWillReceiveProps(nextProps, ownProps) {
-			console.log("nextProps", nextProps);
-			this.setState({
-				documents: nextProps.items
-			}, () => {
-				this.sortAndFilterDocuments();
-			});
-		}
+  componentWillReceiveProps(nextProps, ownProps) {
+    console.log("nextProps", nextProps);
+    this.setState({
+      documents: nextProps.items
+    }, () => {
+      this.sortAndFilterDocuments();
+    });
+  }
 
-		componentWillMount() {
+  componentWillMount() {
 
-			const taxonomies = [{
-			"group": "Document Type",
-			"entries": ["All", "OMB","SBA form", "SOP", "Public Law (PL)"]
-			}, {
-				"group": "Program Name",
-				"entries": ["All", "Lending programs","504/CDC", "Microloans"]
-			}, {
-				"group": "Document Activity",
-				"entries": ["All", "Lending","Authorization", "Servicing"]
-			}, {
-				"group": "Sort By",
-				"entries": ["Last Updated", "Name","Number"]
-			}];
+    const taxonomies = [
+      {
+        "group": "Document Type",
+        "entries": ["All", "OMB", "SBA form", "SOP", "Public Law (PL)"]
+      }, {
+        "group": "Program Name",
+        "entries": ["All", "Lending programs", "504/CDC", "Microloans"]
+      }, {
+        "group": "Document Activity",
+        "entries": ["All", "Lending", "Authorization", "Servicing"]
+      }, {
+        "group": "Sort By",
+        "entries": ["Last Updated", "Name", "Number"]
+      }
+    ];
 
-			const mockResponseObj = Promise.resolve(taxonomies);
-			mockResponseObj.then((response) => {
-				
-				this.setState({
-					"taxonomies": response
-				});
+    const mockResponseObj = Promise.resolve(taxonomies);
+    mockResponseObj.then((response) => {
 
-			});
-		}
+      this.setState({"taxonomies": response});
 
-		handleChange(event, selectStateKey) {
+    });
+  }
 
-			const obj = {};
-			obj[selectStateKey] = event.value;
+  handleChange(event, selectStateKey) {
 
-			this.setState(obj, () => {
-				this.sortAndFilterDocuments();
-					if(this.props.afterChange) {
-						this.props.afterChange(
-							"document-lookup",
-							"Filter Status : " + JSON.stringify(pick(this.state,
-								[
-									"documentType",
-									"programName",
-									"documentActivity",
-									"sortBy"
-								])
-							),
-						null
-					);
-				}
-			});
-		}
+    const obj = {};
+    obj[selectStateKey] = event.value;
 
-		sortAndFilterDocuments() {
-			
-			const documents = this.props.items;
-			const sortedDocuments = this.sortDocuments(documents);
-			const filteredDocuments = this.filterDocuments(sortedDocuments);
-			
-			this.setState({
-				documents: filteredDocuments
-			});
+    this.setState(obj, () => {
+      this.sortAndFilterDocuments();
+      if (this.props.afterChange) {
+        this.props.afterChange("document-lookup", "Filter Status : " + JSON.stringify(pick(this.state, ["documentType", "programName", "documentActivity", "sortBy"])), null);
+      }
+    });
+  }
 
-		}
+  sortAndFilterDocuments() {
 
-		sortDocuments(contacts) {
-			
-			let direction; // asc|desc
-			let titleOrActiveSince; // title|activeSince
+    const documents = this.props.items;
+    const sortedDocuments = this.sortDocuments(documents);
+    const filteredDocuments = this.filterDocuments(sortedDocuments);
 
-			if (this.state.sortBy === "Investor Name") {
-	
-				direction = "asc";
-				titleOrActiveSince = "title";
+    this.setState({documents: filteredDocuments});
 
-			} else if (this.state.sortBy === "Active Since") {
+  }
 
-				direction = "desc";
-				titleOrActiveSince = "activeSince";
+  sortDocuments(contacts) {
 
-			}
+    let direction; // asc|desc
+    let titleOrActiveSince; // title|activeSince
 
-			const orders = [direction];
-			const iteratees = [titleOrActiveSince];
+    if (this.state.sortBy === "Investor Name") {
 
-			return _.orderBy(contacts, iteratees, orders);
+      direction = "asc";
+      titleOrActiveSince = "title";
 
-		}
+    } else if (this.state.sortBy === "Active Since") {
 
-		//TODO remove outer array brackets [contact.industry] when industry field becomes an actual array. keep intersection though.
-		//this filtering was intended to match multiple user selections to multiple industry types.
-		filterDocuments(contacts) {
-			let filteredContacts = contacts
-			if (this.state.industryValue !== "All") {
-				filteredContacts = _.filter(contacts, (contact) => {
-					return !_.isEmpty(_.intersection(_.castArray(contact.industry), _.castArray(this.state.industryValue)))
-				})
-			}
-			if (this.state.investingStatusValue !== "All") {
-				filteredContacts = _.filter(filteredContacts, (contact) => {
-					return contact.investingStatus === this.state.investingStatusValue
-				})
-			}
-			return filteredContacts
-		}
+      direction = "desc";
+      titleOrActiveSince = "activeSince";
 
-	renderMultiSelects() {
+    }
 
-		const _multiselects = this.state.taxonomies.map((object, index) => {
-		
-			const groupName = object.group;
-			const id = `${createSlug(groupName)}-select`;
-			const stateName = createCamelCase(groupName);
-			const options = object.entries.map((entry) => {
+    const orders = [direction];
+    const iteratees = [titleOrActiveSince];
 
-				return {
-					"label": entry,
-					"value": createSlug(entry, "-")
-				};
+    return _.orderBy(contacts, iteratees, orders);
 
-			});
+  }
 
-			const _ms = {
-				"id": id,
-				onChange: (event) => {
-					this.handleChange(event, stateName);
-				},
-				name: id,
-				"label": groupName,
-				value: this.state[stateName],
-				"options": options
-			};
+  //TODO remove outer array brackets [contact.industry] when industry field becomes an actual array. keep intersection though.
+  //this filtering was intended to match multiple user selections to multiple industry types.
+  filterDocuments(contacts) {
+    let filteredContacts = contacts
+    if (this.state.industryValue !== "All") {
+      filteredContacts = _.filter(contacts, (contact) => {
+        return !_.isEmpty(_.intersection(_.castArray(contact.industry), _.castArray(this.state.industryValue)))
+      })
+    }
+    if (this.state.investingStatusValue !== "All") {
+      filteredContacts = _.filter(filteredContacts, (contact) => {
+        return contact.investingStatus === this.state.investingStatusValue
+      })
+    }
+    return filteredContacts
+  }
 
-			return _ms;
-		});
+  renderMultiSelects() {
 
-		return _multiselects.map((multiSelectProps, index) => {
-			return (
-				<div className={s.multiSelect} key={index}>
-					<Multiselect
-						{...multiSelectProps}
-						onBlur={() => {return null}}
-						onFocus={() => {return null}}
-						validationState=""
-						errorText=""
-						autoFocus={false}
-						multi={false}
-					>
-					</Multiselect>
-				</div>
-			)
-		})
-	}
+    const _multiselects = this.state.taxonomies.map((object, index) => {
 
-	renderDocuments() {
-		return <div>document list here</div>;
-	}
+      const groupName = object.group;
+      const id = `${createSlug(groupName)}-select`;
+      const stateName = createCamelCase(groupName);
+      const options = object.entries.map((entry) => {
 
-	handleKeyUp(event) {
+        return {
+          "label": entry,
+          "value": createSlug(entry, "-")
+        };
 
-		const returnKeyCode = 13
-		
-		if (event.keyCode === returnKeyCode) {
-			console.log('A', 'validate', event.target.value)
-		}
+      });
 
-	}
+      const _ms = {
+        "id": id,
+        onChange: (event) => {
+          this.handleChange(event, stateName);
+        },
+        name: id,
+        "label": groupName,
+        value: this.state[stateName],
+        "options": options
+      };
 
-	applyFilters() {
+      return _ms;
+    });
 
-		const { documentType, programName, documentActivity, sortBy } = this.state
+    return _multiselects.map((multiSelectProps, index) => {
+      return (
+        <div className={s.multiSelect} key={index}>
+          <Multiselect {...multiSelectProps} onBlur={() => {
+            return null
+          }} onFocus={() => {
+            return null
+          }} validationState="" errorText="" autoFocus={false} multi={false}></Multiselect>
+        </div>
+      )
+    })
+  }
 
-		const filters = {
-			documentType,
-			programName,
-			documentActivity,
-			sortBy
-		};
+  renderDocuments() {
+    return <DocumentCardCollection documents={this.state.documents}/>;
+  }
 
-		console.log('B', 'apply filters', filters)
+  handleKeyUp(event) {
 
-	}
+    const returnKeyCode = 13
 
-	render() {
+    if (event.keyCode === returnKeyCode) {
+      console.log('A', 'validate', event.target.value)
+    }
 
-		const { taxonomies } = this.state;
+  }
 
-		return (
-			<div>
-				<div className={s.banner}>
-					<h2 className={s.header}>{this.props.title}</h2>
-					{taxonomies.length > 0 &&
-					<div>
-						<div className={s.searchBox}>
-							<TextInput
-								placeholder="Title or number"
-								id="document-lookup"
-								errorText={"Please enter the correct thing."}
-								label="Search"
-								validationState={""}
-								onKeyUp={(e) => this.handleKeyUp(e)}
-							/>
-							<div className={s.searchIcon}>
-								<SearchIcon aria-hidden="true" />
-							</div>
-						</div>
-						{this.renderMultiSelects()}
-						<SmallInverseSecondaryButton
-							onClick={() => this.applyFilters()}
-							extraClassName={s.applyFiltersBtn}
-							text="Apply Filters"
-						/>
-					</div>}
-				</div>
-				{this.renderDocuments()}
-			</div>
-		);
-	}
+  applyFilters() {
+
+    const {documentType, programName, documentActivity, sortBy} = this.state
+
+    const filters = {
+      documentType,
+      programName,
+      documentActivity,
+      sortBy
+    };
+
+    console.log('B', 'apply filters', filters)
+
+  }
+
+  render() {
+
+    const {taxonomies} = this.state;
+
+    return (
+      <div>
+        <div className={s.banner}>
+          <h2 className={s.header}>{this.props.title}</h2>
+          {taxonomies.length > 0 && <div>
+            <div className={s.searchBox}>
+              <TextInput placeholder="Title or number" id="document-lookup" errorText={"Please enter the correct thing."} label="Search" validationState={""} onKeyUp={(e) => this.handleKeyUp(e)}/>
+              <div className={s.searchIcon}>
+                <SearchIcon aria-hidden="true"/>
+              </div>
+            </div>
+            {this.renderMultiSelects()}
+            <SmallInverseSecondaryButton onClick={() => this.applyFilters()} extraClassName={s.applyFiltersBtn} text="Apply Filters"/>
+          </div>}
+        </div>
+        {this.renderDocuments()}
+      </div>
+    );
+  }
 }
 
 export default DocumentLookup;
