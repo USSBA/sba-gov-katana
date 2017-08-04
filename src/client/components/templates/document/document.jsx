1cb61d5a7386027759744bc00230693d128a24db
Avery Smith <asmith@Averys-MacBook-Pro.local>
2017-08-04 17:41:05 -0400

    DT-2794 added Previous Versions code to Document Component, and connected it to the document.files object

--- src/client/components/templates/document/document.jsx
+++ src/client/components/templates/document/document.jsx
@@ -7,19 +7,52 @@ import RelatedDocumentCards from "../../organisms/related-document-cards/related
 class DocumentPage extends React.Component {
 
   render() {
-    if (this.props.document) {
-      return (
-        <div>
-          <DocumentArticle data={this.props.document}/>
-          <RelatedDocumentCards documentObj={this.props.document}/>
-        </div>
-      );
 
-    } else {
+    const {document:doc} = this.props;
+
+    let versionNumber = doc.files.length;
+    const previousVersionsList = doc.files.map((obj) => {
+      versionNumber--;
       return (
+          <li>
+            <strong>Version {versionNumber + 1}</strong> <strong>|</strong> Effective: {obj.effectiveDate}. <a href={obj.url} target="_blank">Download PDF <i className="fa fa-file-pdf-o" aria-hidden="true" /></a>
+          </li>
+        );
+    });
+
+    return (
+      <div>
+      
+      { doc &&
+        
+        <div>
+          
+          <DocumentArticle data={this.props.document}/>
+
+            <div className={s.previousVersionsList}>
+            
+              <h3>Previous versions</h3>
+            
+              <ul>
+                {previousVersionsList}
+              </ul>
+
+              <hr className={s.hr}/>
+
+            </div>
+
+          <RelatedDocumentCards documentObj={this.props.document}/>
+
+        </div>
+
+      }{!doc &&
+        
         <div>Loading....</div>
-      );
-    }
+
+      }
+
+      </div>
+    );
   }
 }
 
