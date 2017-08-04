564786588ce91df49be2bb899bd40e7c3f861b56
Charles Bushong <bushong1@gmail.com>
2017-08-04 14:12:43 -0400

    DT-3052: Fix run-db to not reload data every time

--- scripts/db/sql/import-sql.sh
+++ scripts/db/sql/import-sql.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-printf "Creating katana database..."
-echo "create database IF NOT EXISTS sba_katana;" | mysql --user=root --password=password123
-printf "OK\n"
-printf "Importing katana data..."
-cat /mnt/database/sba_katana.sql | mysql --user=root --password=password123 sba_katana
-printf "OK"
-printf "\nCreating drupal8 database..."
-echo "create database IF NOT EXISTS sba_drupal8" | mysql --user=root --password=password123
-printf "OK\n"
-printf "Importing drupal8 data..."
-cat /mnt/database/sba_drupal8.sql | mysql --user=root --password=password123 sba_drupal8
-printf "OK\n"
-printf "Creating drupal database..."
-echo "create database IF NOT EXISTS sba_drupal" | mysql --user=root --password=password123
-printf "OK\n"
-printf "Importing drupal data..."
-cat /mnt/database/sba_drupal.sql | mysql --user=root --password=password123 sba_drupal
-printf "OK\n"
+#declare -a dbs=("katana" "drupal" "drupal8")
+for db in "katana" "drupal" "drupal8"
+do
+  printf "Creating ${db} database..."
+  echo "create database sba_${db};" | mysql --user=root --password=password123 > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    printf "OK\n"
+    printf "Importing ${db} data..."
+    cat /mnt/database/sba_${db}.sql | mysql --user=root --password=password123 sba_${db}
+  else
+    printf " already created; not importing ${db} data\n"
+  fi
+done
