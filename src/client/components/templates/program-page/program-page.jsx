bdc6d49fca86aba514ce0fde6cb02a2f92c605c7
Jonathan King <jking@fearless.tech>
2017-08-09 09:08:11 -0400

    DT-0000 reverts the program page

--- src/client/components/templates/program-page/program-page.jsx
+++ src/client/components/templates/program-page/program-page.jsx
@@ -25,25 +25,22 @@ class ProgramPage extends React.Component {
   render() {
 
     let paragraphs = this.makeParagraphs(this.props.paragraphs);
-    
+
     const { title, summary, buttons, bannerImage } = this.props.heroData
     const hasBannerImage = bannerImage.hasOwnProperty('image')
 
-      const previousVersionsMockData = [{
-        'version': 'Version 2',
-        'date': 'Oct 17, 2015',
-        'url': '#'
-      }, {
-        'version': 'Version 1',
-        'date': 'Oct 24, 2014',
-        'url': '#'
-      }]
-
-      const previousVersionsList = previousVersionsMockData.map(o => {
-        return (
-            <li><strong>{o.version}</strong> <strong>|</strong> Effective: {o.date}. <a href={o.url}>Download PDF <i className="fa fa-file-pdf-o" aria-hidden="true" /></a></li>
-          )
-      })
+    const _formattedButtons = buttons === undefined ? [] : (
+        buttons.map((button) => {
+           return {
+            onClickHandler: function(e) {
+              e.preventDefault()
+              window.location.href = button.url
+            },
+            btnText: button.title,
+            btnType: "LargeInversePrimaryButton"
+          }
+        })
+      )
 
     return (
       <div>
@@ -64,12 +61,6 @@ class ProgramPage extends React.Component {
           />
         }
         <div className={styles.container}>
-          <div>
-            <h3>Previous versions</h3>
-            <ul className={styles.previousVersionsList}>
-              {previousVersionsList}
-            </ul>
-          </div>
           {paragraphs}
         </div>
       </div>
