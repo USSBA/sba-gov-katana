5f9388df13a2c916efa2a77b52caaa5ee7dafd49
Avery Smith <asmith@Averys-MacBook-Pro.local>
2017-08-10 15:03:15 -0400

    DT-3076 changed Documentation Lookup submit button label from 'Apply Filters' to 'Apply' turned the 'Apply' button into an atom turned the 'submit()' method into a component property

---
+++ src/client/components/atoms/apply-button/apply-button.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import s from "./apply-button.scss";
+import SmallInverseSecondaryButton from "../small-inverse-secondary-button/small-inverse-secondary-button.jsx";
+
+class ApplyButton extends React.Component {
+
+	render() {
+
+		return (
+
+			<div>
+				
+				<SmallInverseSecondaryButton
+					onClick={() => {
+						this.props.submit();
+					}}
+					extraClassName={s.applyFiltersBtn}
+					text="Apply"
+				/>
+
+			</div>
+
+		);
+
+	}
+
+}
+
+export default ApplyButton;
