7a516462dea58e85649b10c6b0bbdb5210feeafe
zander312 <iskander93@gmail.com>
2017-08-16 08:35:31 -0400

    adds basic quick-links components

---
+++ src/client/components/molecules/quick-links/latest-document-card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import s from './latest-document-card.scss'
+
+class LatestDocumentCard extends React.Component {
+
+  render() {
+     return (
+       <div>
+         LatestDocumentCard  component
+       </div>
+     );
+  }
+}
+
+export default LatestDocumentCard 
---
+++ src/client/components/molecules/quick-links/quick-links.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import s from './quick-links.scss'
+
+class QuickLinks extends React.Component {
+
+  render() {
+     return (
+     	<div>
+     		QuickLinks  component
+     	</div>
+     );
+	}
+}
+
+const LatestDocumentsCard = (props) => {
+	return(
+		<div> LatestDocumentsCard  </div>
+	)
+}
+
+const RatesCard = (props) => {
+	return(
+		<div> RatesCard  </div>
+	)
+}
+
+const QuickLinksCard = (props) => {
+	return(
+		<div> QuickLinksCard  </div>
+	)
+}
+
+export default QuickLinks 
