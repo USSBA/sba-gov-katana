8cf30bfb47f1d8579253b6b61928638f79810658
Charles Bushong <bushong1@gmail.com>
2017-08-17 16:32:00 -0400

    DT-2861: Enabling Page component to check node type when rendering

--- src/client/components/pages/page.jsx
+++ src/client/components/pages/page.jsx
@@ -18,9 +18,9 @@ class Page extends React.Component {
   render() {
     const section = this.props.lineage[0].url;
     if (this.props.data && this.props.lineage) {
-      if (section === "business-guide" || section === "for-partners") {
+      if (section === "business-guide" || this.props.data.type === "page") {
         return (<BasicPage title={this.props.data.title} paragraphs={this.props.data.paragraphs} summary={this.props.data.summary} lineage={this.props.lineage}/>);
-      } else if (section === "funding-programs") {
+      } else if (section === "funding-programs" || this.props.data.type === "programPage") {
         let heroData;
         if (this.props.data) {
           heroData = {
@@ -59,3 +59,5 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapReduxStateToProps, mapDispatchToProps)(Page);
+
+export { Page };
---
+++ test/jest/client/components/pages/page/page.test.jsx
@@ -0,0 +1,65 @@
+/*global expect*/
+
+/* eslint-disable no-unused-vars,no-undef */
+import React from "react";
+/*eslint-enable no-unused-vars*/
+import {Page} from "client/components/pages/page.jsx";
+import ProgramPage from "client/components/templates/program-page/program-page.jsx";
+import BasicPage from "client/components/templates/basic-page/basic-page.jsx";
+import {shallow} from "enzyme";
+// import renderer from "react-test-renderer";
+import _ from "lodash";
+import lineageBusinessGuide from "../../test-data/lineage-business-guide.json";
+import lineageForPartners from "../../test-data/lineage-for-partners.json";
+
+jest.mock("client/services/client-config.js", function(){
+  return {
+    googleAnalytics: {
+      enabled: false
+    }
+  };
+});
+
+describe("SectionNav", () => {
+  describe("Business Guide", () => {
+    test("Third-level taxonomy renders BasicPage", () => {
+      const lineage = _.cloneDeep(lineageBusinessGuide);
+      const data = {
+        title: "Market research and competitive analysis",
+        summary: "Blah blah",
+        type: "page",
+        id: 47
+      };
+      const component = shallow(<Page data={data} lineage={lineage} />);
+      expect(component.at(0).type()).toBe(BasicPage);
+    });
+  });
+  describe("For Partners", () => {
+    test("Third-level taxonomy renders Page component", () => {
+      const lineage = [
+        _.cloneDeep(lineageForPartners[0]),
+        _.cloneDeep(lineageForPartners[1]),
+        _.cloneDeep(lineageForPartners[2])
+      ];
+      const data = {
+        title: "7(a) Loans",
+        summary: "Blah blah",
+        type: "programPage",
+        id: 3419
+      };
+      const component = shallow(<Page data={data} lineage={lineage} />);
+      expect(component.at(0).type()).toBe(ProgramPage);
+    });
+    test("Fourth-level taxonomy renders BasicPage component", () => {
+      const lineage = _.cloneDeep(lineageForPartners);
+      const data = {
+        title: "Operate as a 7(a) Lender",
+        summary: "Blah blah",
+        type: "page",
+        id: 3414
+      };
+      const component = shallow(<Page data={data} lineage={lineage} />);
+      expect(component.at(0).type()).toBe(BasicPage);
+    });
+  });
+});
