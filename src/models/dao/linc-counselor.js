93ff3880bf73a8d39dfcbc6a9e7b633cfb9b4dd2
Jonathan King <jking@fearless.tech>
2017-08-21 10:02:51 -0400

    DT-2124 fixes the call to daisho for the LM counselors

--- src/models/dao/linc-counselor.js
+++ src/models/dao/linc-counselor.js
@@ -1,7 +1,9 @@
 import { get } from "./daisho-client.js";
 
-function fetchCounselorsByLocation(userId) {
-  return get("counselors-by-location");
+function fetchCounselorsByLocation(zip) {
+  return get("counselors-by-location", {
+    zip: zip
+  });
 }
 
 export { fetchCounselorsByLocation };
