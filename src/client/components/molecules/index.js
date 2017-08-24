787e93d13df09c0d864d2c1046f9da4b5113d64d
Avery Smith <asmith@fearless.tech>
2017-08-24 14:41:45 -0400

    DT-3256 further formatted text section and jest tests

--- src/client/components/molecules/index.js
+++ src/client/components/molecules/index.js
@@ -1,5 +1,6 @@
 import TitleSection from "./title-section/title-section.jsx";
 import Breadcrumb from "./breadcrumb/breadcrumb.jsx";
 import Paginator from "./paginator/paginator.jsx";
+import TextSection from "./text-section/text-section.jsx";
 
-export { TitleSection, Breadcrumb, Paginator };
+export { TitleSection, Breadcrumb, Paginator, TextSection };
