9c90939db4b6d3f258292fc148f27fcfa230d840
Avery Smith <asmith@fearless.tech>
2017-08-30 12:28:46 -0400

    DT-2965 added atoms alias to webpack added atoms module path to jest

--- test/jest/client/components/atoms/apply-button/apply-button.test.jsx
+++ test/jest/client/components/atoms/apply-button/apply-button.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {shallow} from "enzyme";
 
-import {ApplyButton} from "client/components/atoms/";
+import {ApplyButton} from "atoms";
 
 describe("ApplyButton", () => {
 
