32ee2c17aa06e78de6b52f67bb5c1aa8ce1e2403
zander312 <iskander93@gmail.com>
2017-08-31 16:25:55 -0400

    adds main loader and main loader trigger components

---
+++ src/client/components/molecules/main-loader/main-loader-trigger.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as LoadingActions from '../../../actions/loading.js';
+
+class MainLoaderTrigger extends React.Component {
+	//when this component mounts, it will fire an action that removes the main loader from main.jsx
+	componentDidMount() {
+		this.props.loadingActions.loaded();
+	}
+
+	render() {
+		return <div />;
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		loadingActions: bindActionCreators(LoadingActions, dispatch),
+	};
+}
+
+export default connect(null, mapDispatchToProps)(MainLoaderTrigger);
