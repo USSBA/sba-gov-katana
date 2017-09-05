f54eedad5aff528c56eba1244c7fc6872d572123
Jonathan King <jking@fearless.tech>
2017-09-04 20:51:25 -0400

    DT-2819 some test fixes for the multiselect and some lint fixes for zander

--- src/client/reducers/loading.js
+++ src/client/reducers/loading.js
@@ -1,14 +1,14 @@
 const loadingReducer = (state = {}, action) => {
-	if (action.type === 'REMOVE_LOADER') {
-		return {
-			displayLoader: false,
-		};
-	} else if (action.type === 'SHOW_LOADER') {
-		return {
-			displayLoader: true,
-		};
-	}
-	return state;
+  if (action.type === "REMOVE_LOADER") {
+    return {
+      displayLoader: false
+    };
+  } else if (action.type === "SHOW_LOADER") {
+    return {
+      displayLoader: true
+    };
+  }
+  return state;
 };
 
 export default loadingReducer;
