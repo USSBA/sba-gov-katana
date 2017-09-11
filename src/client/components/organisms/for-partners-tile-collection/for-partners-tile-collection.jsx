0db812ac7fa24b931aaa81ed4e0fa8219df737f2
Jonathan King <jking@fearless.tech>
2017-09-11 09:10:31 -0400

    DT-2715 adds the for partners landing page

---
+++ src/client/components/organisms/for-partners-tile-collection/for-partners-tile-collection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {TileCollection} from "molecules";
+import ForPartnersIcons from "./icons.jsx";
+
+class ForPartnersTileCollection extends React.Component {
+
+  render() {
+    if (!this.props.sectionData || !this.props.sectionData.children) {
+      return <div/>
+    }
+    return (<TileCollection data={this.props.sectionData.children} icons={ForPartnersIcons} hoverShowsInverseOnly/>);
+  }
+}
+
+ForPartnersTileCollection.defaultProps = {
+  sectionData: []
+}
+
+export default ForPartnersTileCollection;
---
+++ src/client/components/organisms/for-partners-tile-collection/icons.jsx
@@ -0,0 +1,21 @@
+import forPartnersIconLendersColor from "../../../../../public/assets/images/for-partners/For_Partners_Icon_Lenders_color.png"
+import forPartnersIconLendersWhite from "../../../../../public/assets/images/for-partners/For_Partners_Icon_Lenders_white.png"
+import forPartnersIconSBICsColor from "../../../../../public/assets/images/for-partners/For_Partners_Icon_SBICs_color.png"
+import forPartnersIconSBICsWhite from "../../../../../public/assets/images/for-partners/For_Partners_Icon_SBICs_white.png"
+import forPartnersIconSuretyProvidersColor from "../../../../../public/assets/images/for-partners/For_Partners_Icon_Surety_Providers_color.png"
+import forPartnersIconSuretyProvidersWhite from "../../../../../public/assets/images/for-partners/For_Partners_Icon_Surety_Providers_white.png"
+
+let fundingProgramIcons = [
+  {
+    icon: forPartnersIconLendersColor,
+    iconWhite: forPartnersIconLendersWhite
+  }, {
+    icon: forPartnersIconSBICsColor,
+    iconWhite: forPartnersIconSBICsWhite
+  }, {
+    icon: forPartnersIconSuretyProvidersColor,
+    iconWhite: forPartnersIconSuretyProvidersWhite
+  }
+]
+
+export default fundingProgramIcons;
