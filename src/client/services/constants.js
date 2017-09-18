f136b57b23cc0caadbd7bccb76e93810c5016ed0
zander312 <iskander93@gmail.com>
2017-09-18 08:42:06 -0400

    changes constants for businessGuide and fundingPrograms so that page renders

--- src/client/services/constants.js
+++ src/client/services/constants.js
@@ -3,7 +3,7 @@ const constants = {
   address: "409 3rd St, SW. Washington DC 20416",
   sections: {
     businessGuide: "Business Guide",
-    fundingPrograms: "Funding Programs",
+    fundingPrograms: "Funding programs",
     disaster: "Disaster Assistance"
   },
   routes: {
