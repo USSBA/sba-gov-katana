a18ad62fe5633a94b1b6c6515216d9d72b4d01c5
Jonathan King <jking@fearless.tech>
2017-09-21 14:12:56 -0400

    DT-3571 fixes variable name

--- src/client/services/navigation.js
+++ src/client/services/navigation.js
@@ -22,8 +22,8 @@ function navigateNow(targetLocation, eventConfig) {
       return targetLocation.indexOf(path) !== -1; //eslint-disable-line no-magic-numbers
     });
     const isHandledRoute = _.compact(mapped).length > 0;
-    let startsWithHttps = _.startsWith(targetLocation, "http");
-    if ((targetLocation === "/" || isHandledRoute) && !startsWithHttps) {
+    let startsWithHttp = _.startsWith(targetLocation, "http");
+    if ((targetLocation === "/" || isHandledRoute) && !startsWithHttp) {
       browserHistory.push(targetLocation);
       if (targetLocation.indexOf("#") === -1) { //eslint-disable-line no-magic-numbers
         window.scrollTo(0, 0);
