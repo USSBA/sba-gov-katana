4e1bbfdd3cdd2f7378f5d82f8239a8456e81ebce
Jonathan King <jking@fearless.tech>
2017-09-27 11:46:34 -0400

    DT-0000 removes the for partners references

--- src/client/components/organisms/header-footer/mobile-nav/mobile-nav.jsx
+++ src/client/components/organisms/header-footer/mobile-nav/mobile-nav.jsx
@@ -58,11 +58,6 @@ class MobileNav extends React.Component {
           </form>
             {menuItems}
           <div className={styles.menuLinkSection}>
-            <a className={styles.navLinkSpecialNew} href="/partners">
-              For Partners
-            </a>
-          </div>
-          <div className={styles.menuLinkSection}>
             <a id="mobile-nav-near-you" className={styles.navLinkSpecialNew} href="/tools/local-assistance#locations-page">
               <img className={styles.linkIcon} src={nearyouIcon} alt=""/>
               SBA Near You
