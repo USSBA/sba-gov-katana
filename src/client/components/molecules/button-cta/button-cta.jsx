d45943fba22e2a40acfb980b1d65d130377f0687
Avery Smith <asmith@fearless.tech>
2017-09-28 16:16:42 -0400

    DT-3598 added margin-bottom 50px

--- src/client/components/molecules/button-cta/button-cta.jsx
+++ src/client/components/molecules/button-cta/button-cta.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import _ from "lodash"
 import {LargePrimaryButton} from "atoms";
-
+import styles from "./button-cta.scss";
 
 const ButtonCta = (props) => {
 	let eventConfig = {
@@ -9,7 +9,7 @@ const ButtonCta = (props) => {
 		action: _.camelCase(props.title) + ": " + props.url
 	}
 	return(
-		<div>
+		<div className={styles.buttonCTA}>
 			<LargePrimaryButton text={props.title} url={props.url} eventConfig={eventConfig}/>
 		</div>
 	)
