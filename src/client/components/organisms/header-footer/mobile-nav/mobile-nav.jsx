7907496d7a6c9ed510dc7a798c1840cfe09d47b9
Charles Bushong <bushong1@gmail.com>
2017-09-29 14:26:35 -0400

    DT-2816: Adding scroll capability for MobileNav

--- src/client/components/organisms/header-footer/mobile-nav/mobile-nav.jsx
+++ src/client/components/organisms/header-footer/mobile-nav/mobile-nav.jsx
@@ -14,8 +14,10 @@ class MobileNav extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      expanded: false
+      expanded: false,
+      navHeight: 450
     };
+    this.updateNavHeight = this.updateNavHeight.bind(this);
   }
   toggleNav(e) {
     e.preventDefault();
@@ -24,19 +26,67 @@ class MobileNav extends React.Component {
     });
   }
 
+  updateNavHeight() {
+    const heightOffset = 130;
+    const newHeight = window.innerHeight - heightOffset;
+    this.setState({ navHeight: newHeight });
+  }
+
+  componentDidMount() {
+    this.updateNavHeight();
+    window.addEventListener('resize', this.updateNavHeight);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateNavHeight);
+  }
+
+  createMenuItems() {
+    let menuItems = [];
+    let me = this;
+    if (this.props.mainMenuData) {
+      menuItems = this.props.mainMenuData.map((item, index) => {
+        return (
+          <div key={index} className={styles.mobileNavMenuLink} onClick={me.toggleNav.bind(me)}>
+            <SectionLink id={"main-menu-link-" + index} url={item.link} text={item.linkTitle}/>
+          </div>
+        );
+      });
+    } else {
+      menuItems.push(
+        <div key={1}></div>
+      );
+    }
+
+    menuItems.push(
+      <div className={styles.mobileNavMenuLink}>
+        <a className={styles.navLinkSpecialNew} href="/partners">
+          For Partners
+        </a>
+      </div>
+    );
+    menuItems.push(
+      <div className={styles.mobileNavMenuLink}>
+        <a id="mobile-nav-near-you" className={styles.navLinkSpecialNew} href="/tools/local-assistance#locations-page">
+          <img className={styles.linkIcon} src={nearyouIcon} alt=""/>
+          SBA Near You
+        </a>
+      </div>
+    );
+    menuItems.push(
+      <div className={styles.mobileNavMenuLink}>
+        <a id="mobile-nav-events" className={styles.navLinkSpecialNew} href="/tools/events#events-page">
+          <img className={styles.linkIcon} src={calendarIcon} alt=""/>
+          Small Business Events
+        </a>
+      </div>
+    );
+    return menuItems;
+  }
 
   render() {
-      let menuItems = [];
-      let me = this;
-      if (this.props.mainMenuData) {
-          menuItems = this.props.mainMenuData.map((item, index) => {
-              return <div key={index} className={styles.menuLinkSection} onClick={me.toggleNav.bind(me)}><SectionLink id={"main-menu-link-" + index} url={item.link} text={item.linkTitle}/></div>
-          });
-      } else {
-          menuItems.push(
-              <div key={1}></div>
-          );
-      }
+    const menuItems = this.createMenuItems();
+    const menuMaxHeight = menuItems.length * 78;
     return (
       <div>
         <div className={styles.mobileHeader}>
@@ -56,23 +106,8 @@ class MobileNav extends React.Component {
             </div>
             <input type="text" className={styles.searchInputFieldNew} placeholder="Search SBA.gov"></input>
           </form>
+          <div className={styles.mobileNavMenuLinks} style={{height: this.state.navHeight, maxHeight: menuMaxHeight }}>
             {menuItems}
-          <div className={styles.menuLinkSection}>
-            <a className={styles.navLinkSpecialNew} href="/partners">
-              For Partners
-            </a>
-          </div>
-          <div className={styles.menuLinkSection}>
-            <a id="mobile-nav-near-you" className={styles.navLinkSpecialNew} href="/tools/local-assistance#locations-page">
-              <img className={styles.linkIcon} src={nearyouIcon} alt=""/>
-              SBA Near You
-            </a>
-          </div>
-          <div className={styles.menuLinkSection}>
-            <a id="mobile-nav-events"  className={styles.navLinkSpecialNew} href="/tools/events#events-page">
-              <img className={styles.linkIcon} src={calendarIcon} alt=""/>
-              Small Business Events
-            </a>
           </div>
         </div>
       </div>
