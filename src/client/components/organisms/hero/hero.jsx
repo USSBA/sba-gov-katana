f806376d1865c6d608aabb90e41d1548d3378ffd
zander312 <iskander93@gmail.com>
2017-09-29 14:51:08 -0400

    adds anchor button under hero component

--- src/client/components/organisms/hero/hero.jsx
+++ src/client/components/organisms/hero/hero.jsx
@@ -1,34 +1,58 @@
-import React from "react";
-import {Callout} from "molecules";
-import styles from "./hero.scss";
+import React from 'react';
+import { Callout } from 'molecules';
+import styles from './hero.scss';
+import { ArrowAnchorButton } from 'atoms';
 
 class Hero extends React.Component {
-
   imgStyles() {
-    return {"background": "url("+this.props.imageUrl+") no-repeat center center", "backgroundSize": "cover", "width": "100%", "height": "100%"}
+    return {
+      background: 'url(' + this.props.imageUrl + ') no-repeat center center',
+      backgroundSize: 'cover',
+      width: '100%',
+      height: '100%',
+    };
   }
 
   render() {
     return (
       <div className={styles.heroContainer}>
-        { this.props.imageUrl &&
-        <div className="hero">
-          <div className={styles.heroMobileImageContainer}>
-            <div className="hero-image-mobile" title={this.props.alt} style={this.imgStyles()}></div>
+        {this.props.imageUrl && (
+          <div className="hero">
+            <div className={styles.heroMobileImageContainer}>
+              <div className="hero-image-mobile" title={this.props.alt} style={this.imgStyles()} />
+            </div>
+            {/* <img className={`hero-image-desktop ${styles.heroDesktopImage}`} src={this.props.imageUrl} alt={this.props.alt}/> */}
+            <img
+              className={`hero-image-desktop ${styles.heroDesktopImage}`}
+              src="http://ll-c.ooyala.com/e1/MyOTRuYjE6Zudy0Sz1U7BtPHK4l96dIv/promo322253740"
+              alt={this.props.alt}
+            />
+            <div className={`hero-callout ${styles.callout}`}>
+              <Callout
+                title={this.props.title}
+                message={this.props.message}
+                buttons={this.props.buttons}
+              />
+            </div>
           </div>
-          <img className={`hero-image-desktop ${styles.heroDesktopImage}`} src={this.props.imageUrl} alt={this.props.alt}/>
-          <div className={`hero-callout ${styles.callout}`}>
-            <Callout title={this.props.title} message={this.props.message} buttons={this.props.buttons}/>
+        )}
+        {!this.props.imageUrl && (
+          <div className={`hero-noimage ${styles.inHeroWithNoImage}`}>
+            <div className={`hero-callout ${styles.calloutContainer}`}>
+              <Callout
+                inHeroWithNoImage={true}
+                title={this.props.title}
+                message={this.props.message}
+                buttons={this.props.buttons}
+              />
+            </div>
           </div>
-        </div>
-        }
-        { !this.props.imageUrl &&
-        <div className={`hero-noimage ${styles.inHeroWithNoImage}`}>
-          <div className={`hero-callout ${styles.calloutContainer}`}>
-            <Callout inHeroWithNoImage={true} title={this.props.title} message={this.props.message} buttons={this.props.buttons}/>
+        )}
+        {this.props.anchorButton && (
+          <div className={styles.anchorButton}>
+            <ArrowAnchorButton />
           </div>
-        </div>
-        }
+        )}
       </div>
     );
   }
