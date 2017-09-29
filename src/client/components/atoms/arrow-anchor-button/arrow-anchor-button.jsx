34751b72e41c571798354d5767468b1a61ac368b
zander312 <iskander93@gmail.com>
2017-09-29 14:59:04 -0400

    fixes formatting for arrow anchor button

--- src/client/components/atoms/arrow-anchor-button/arrow-anchor-button.jsx
+++ src/client/components/atoms/arrow-anchor-button/arrow-anchor-button.jsx
@@ -3,17 +3,17 @@ import { CaretIcon } from 'atoms';
 import s from './arrow-anchor-button.scss';
 
 class ArrowAnchorButton extends React.Component {
-	render() {
-		return (
-			<div>
-				<a href="#paragraph-1">
-					<div className={s.container}>
-						<i className={`fa fa-angle-down ${s.arrow}`} aria-hidden="true"></i>
-					</div>
-				</a>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <a href="#paragraph-1">
+          <div className={s.container}>
+            <i className={`fa fa-angle-down ${s.arrow}`} aria-hidden="true" />
+          </div>
+        </a>
+      </div>
+    );
+  }
 }
 
 export default ArrowAnchorButton;
