5f9800543c02e04384dbcfb3404e883e49efaf9d
Avery Smith <asmith@Averys-MacBook-Pro.local>
2017-10-13 18:45:47 -0400

    DT-3165 added size-standards-tool base code

---
+++ src/client/components/pages/size-standards-tool-page/size-standards-tool-page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {SizeStandardsTool} from "organisms";
+import styles from "./size-standards-tool-page.scss";
+
+const sizeStandardsToolPage = () => {
+	
+	return (
+
+		<div />
+
+	);
+
+};
+
+export default sizeStandardsToolPage;
