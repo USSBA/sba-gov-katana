a58338089b3965068463bfd6d1cb3be1a2048d9b
Brian Le <blsymph@gmail.com>
2017-10-13 16:34:29 -0400

    DT-3712 Refactor; add close icon when suggestions are visible; add scrolling for suggestions

--- src/client/components/molecules/naics-lookup/naics-lookup.jsx
+++ src/client/components/molecules/naics-lookup/naics-lookup.jsx
@@ -17,61 +17,59 @@ import styles from './naics-lookup.scss'
 import theme from './theme.scss';
 
 import {
-  SmallIcon,
-  TextInput
+  SmallIcon
 } from 'atoms';
 
-// // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
 function escapeRegexCharacters(string) {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
-class NaicsLookup extends React.Component {
+// Format the list of naics from the API into a structure suitable for React
+// Autosuggest, i.e. into a list of sections (naics categories/industries) that
+// contain entries (naics codes and descriptions).
+function formatNaics (naics) {
+  const industriesMap = {};
+  for (let i = 0; i < naics.length; i++) {
+    const {
+      code,
+      description,
+      sectorDescription: industryDescription,
+      sectorId: industryCode,
+    } = naics[i];
+
+    if (!industriesMap.hasOwnProperty(industryCode)) {
+      industriesMap[industryCode] = {
+        description: industryDescription,
+        entries: []
+      };
+    }
+
+    industriesMap[industryCode].entries.push({ code, description, industryDescription });
+  }
+
+  return reduce(industriesMap, (acc, val, key) => {
+    acc.push({
+      description: val.description,
+      entries: val.entries
+    });
+    return acc;
+  }, []);
+}
+
+class NaicsLookup extends React.PureComponent {
   static defaultProps = {
-    naics: (() => {
-      const industriesMap = {};
-      for (let i = 0; i < naics.length; i++) {
-        const {
-          code,
-          description,
-          sectorDescription: industryDescription,
-          sectorId: industryCode,
-        } = naics[i];
-
-        if (!industriesMap.hasOwnProperty(industryCode)) {
-          industriesMap[industryCode] = {
-            description: industryDescription,
-            entries: [{
-              code,
-              description
-            }]
-          };
-        } else {
-          industriesMap[industryCode].entries.push({
-            code,
-            description
-          });
-        }
-      }
-
-      const formattedNaics = reduce(industriesMap, (acc, val, key) => {
-        acc.push({
-          description: val.description,
-          entries: val.entries
-        });
-        return acc;
-      }, []);
-
-      return formattedNaics;
-    })()
+    naics: formatNaics(naics)
   };
 
   constructor(props) {
     super();
 
     this.state = {
+      isSuggestionsContainerOpen: false,
+      suggestions: [],
+      suggestionsContainerOpenHeight: 0,
       value: '',
-      suggestions: []
     };
   }
 
@@ -84,7 +82,6 @@ class NaicsLookup extends React.Component {
       onChange: this.onChange
     };
 
-    // 804px
     return (
       <AutoSuggest
         theme={theme}
@@ -99,86 +96,71 @@ class NaicsLookup extends React.Component {
         renderSectionTitle={this.renderSectionTitle}
         getSectionSuggestions={this.getSectionSuggestions}
         renderInputComponent={this.renderInputComponent}
+        renderSuggestionsContainer={this.renderSuggestionsContainer}
       />
     );
   }
 
   // # React-Autosuggest-specific
 
-  // Format the NAICs codes from naics.json into a list of suggestions.
+  // Filter the list of NAICS codes against the input.
   getSuggestions = value => {
-    const {
-      naics
-    } = this.props;
+    const { naics } = this.props;
+    const sanitizedValue = value.trim().toLowerCase();
 
-    const escapedValue = escapeRegexCharacters(value.trim());
-    const parsedValue = parseInt(escapedValue);
-
-    if (escapedValue === '') {
+    if (sanitizedValue === '') {
       return [];
     }
 
-    let sections;
-    // Check if the entire input string is a valid integer 1 or greater.
-    if (!isNaN(escapedValue) && parsedValue > 0) {
-      sections = map(naics, section => {
+    const sections = map(naics, section => {
+      const { description, entries } = section;
+      // If the industry description contains the input string, return all the
+      // entries under that industry.
+      if (description.toLowerCase().indexOf(sanitizedValue) !== -1) {
         return {
-          description: section.description,
-          entries: filter(section.entries, entry => startsWith(entry.code, parsedValue))
-        };
-      });
-    } else {
-      const regex = new RegExp(`${escapedValue}`, 'i');
-      sections = map(naics, section => {
-        return {
-          description: section.description,
-          entries: filter(section.entries, entry => regex.test(entry.description))
-        };
-      });
-    }
-
-    const LIMIT = 5;
-    let entriesCount = 0;
-    const filteredSections = filter(sections, section => section.entries.length > 0);
-    let limitedSections = [];
-    for (let i = 0; i < filteredSections.length; i++) {
-      const section = filteredSections[i];
-      const {
-        description,
-        entries
-      } = section;
-      const entriesLength = entries.length;
-
-      if (entriesCount + entriesLength < LIMIT) {
-        limitedSections.push(section);
-      } else {
-        limitedSections.push({
           description,
-          entries: take(entries, LIMIT - entriesCount)
-        })
-        break;
+          entries
+        };
       }
 
-      entriesCount += entries.length;
-    }
+      // If the input string is an integer, return entries whose codes start
+      // with the input. Otherwise, return entries whose descriptions contain
+      // the input.
+      const filterFunction = !isNaN(sanitizedValue)
+        ? entry => startsWith(entry.code, sanitizedValue)
+        : entry => entry.description.toLowerCase().indexOf(sanitizedValue) !== -1;
 
-    return limitedSections;
+      return {
+        description,
+        entries: filter(entries, filterFunction)
+      };
+    });
+
+    return filter(sections, section => section.entries.length > 0);
   };
 
   getSectionSuggestions = section => section.entries;
+
   // Set the value to show in the input when a suggestion is selected.
   getSuggestionValue = suggestion => suggestion.description;
 
   // ## Render methods
 
-  renderInputComponent = inputProps => (
-    <div style={{
-      position: 'relative',
-    }}>
-      <TextInput {...inputProps} />
-      <SmallIcon extraClassName={styles.icon} alt="search icon" fontAwesomeIconClassName="search" />
-    </div>
-  );
+  renderInputComponent = inputProps => {
+    const { isSuggestionsContainerOpen } = this.state;
+    return (
+      <div style={{
+        position: 'relative',
+      }}>
+        <input {...inputProps} />
+        {isSuggestionsContainerOpen
+          ? <SmallIcon extraClassName={`${styles.icon} ${styles.closeIcon}`} alt="close icon" fontAwesomeIconClassName="times" />
+          : <i className={`fa fa-search ${styles.icon} ${styles.searchIcon}`} alt="search icon"></i>}
+      </div>
+    );
+  }
+
+  renderSectionTitle = section => (<span>{section.description}</span>);
 
   renderSuggestion = suggestion => (
     <div>
@@ -186,9 +168,20 @@ class NaicsLookup extends React.Component {
       <p className={styles.entryDescription}>{suggestion.description}</p>
     </div>
   );
-  renderSectionTitle = section => (<span>{section.description}</span>);
 
-  // ## React Autosuggest event handlers
+  renderSuggestionsContainer = ({ containerProps, children, query }) => {
+    const { suggestionsContainerOpenHeight } = this.state;
+    console.log(containerProps);
+    return (
+      <div style={{
+        maxHeight: `${suggestionsContainerOpenHeight}px`
+      }} {...containerProps}>
+        {children}
+      </div>
+    );
+  };
+
+  // ## Event handlers
 
   onChange = (e, { newValue }) => {
     this.setState({
@@ -197,14 +190,44 @@ class NaicsLookup extends React.Component {
   };
 
   onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: this.getSuggestions(value)
-    });
+    const suggestions = this.getSuggestions(value);
+
+    // The suggestions container should expand to show up to LIMIT entries,
+    // considering the height of each entry and the height of any included
+    // categories.
+    const LIMIT = 10;
+    let entriesCount = 0;
+    // add extra top padding + extra bottom padding to container height
+    let suggestionsContainerOpenHeight = 8 + 6;
+    // Declare an anonymous function, and `return` to break from the nested
+    // loop.
+    (() => {
+      for (let i = 0; i < suggestions.length; i++) {
+        // add height of section title (category) to container height
+        suggestionsContainerOpenHeight += 18;
+
+        const { entries } = suggestions[i];
+        for (let j = 0; j < entries.length; j++) {
+          // Add height of suggestion (entry) to container height
+          suggestionsContainerOpenHeight += 57;
+          entriesCount++;
+          if (entriesCount >= LIMIT) return;
+        }
+      }
+    })();
+
+    if (suggestions.length > 0) {
+      this.setState({
+        isSuggestionsContainerOpen: true,
+        suggestions,
+        suggestionsContainerOpenHeight
+      });
+    }
   };
 
-  // call every time suggestions need to be cleared
   onSuggestionsClearRequested = () => {
     this.setState({
+      isSuggestionsContainerOpen: false,
       suggestions: []
     });
   };
