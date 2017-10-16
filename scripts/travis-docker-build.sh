2ba0409db04b3349f7912f2cf100d5fdca4e9487
Jonathan King <jking@fearless.tech>
2017-10-16 17:13:19 -0400

    DT-0000 fixes a bug in the travis script where release branches were being tagged as latest

--- scripts/travis-docker-build.sh
+++ scripts/travis-docker-build.sh
@@ -6,8 +6,10 @@ if [ $TRAVIS_PULL_REQUEST == "false" ]; then
     export PATH=$PATH:$HOME/.local/bin # put aws in the path
     eval $(aws ecr get-login --region us-east-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
     docker build -t katana .
-    docker tag katana:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/sba-gov-katana:latest
-    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/sba-gov-katana:latest
+    if [ ${TRAVIS_BRANCH} = "master" ]; then
+      docker tag katana:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/sba-gov-katana:latest
+      docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/sba-gov-katana:latest
+    fi
     docker tag katana:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/sba-gov-katana:${TRAVIS_BUILD_NUMBER}
     docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/sba-gov-katana:${TRAVIS_BUILD_NUMBER}
     if [ ${TRAVIS_BRANCH} != "master" ]; then
