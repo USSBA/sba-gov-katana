b939cdaebf8fd57a47eee1df16ab2759be19a416
Brian Le <blsymph@gmail.com>
2017-10-16 15:55:12 -0400

    DT-3810 Fix tests

--- test/jest/client/components/templates/document/document.test.jsx
+++ test/jest/client/components/templates/document/document.test.jsx
@@ -6,67 +6,70 @@ import DocumentPage from "client/components/templates/document/document.jsx";
 
 describe("Document", () => {
 
-	describe("All Versions", () => {
+  describe("All Versions", () => {
 
-		const mockDocumentData = {
-			"files": [{
-				"type": "docFile",
-				"effectiveDate": null,
-				"expirationDate": null,
-				"fileUrl": "#",
-				"version": null
-			},
-			{
-				"type": "docFile",
-				"effectiveDate": "October 25, 2015",
-				"expirationDate": null,
-				"fileUrl": "https://content.sbagov.fearlesstesters.com/sites/default/files/2017-08/Special%20Text%20Document_1.txt",
-				"version": "2.5"
-			}]
-		};
+    const mockDocumentData = {
+      "documentIdNumber": 123,
+      "files": [{
+        "type": "docFile",
+        "effectiveDate": null,
+        "expirationDate": null,
+        "fileUrl": "#",
+        "version": null
+      },
+        {
+          "type": "docFile",
+          "effectiveDate": "October 25, 2015",
+          "expirationDate": null,
+          "fileUrl": "https://content.sbagov.fearlesstesters.com/sites/default/files/2017-08/Special%20Text%20Document_1.txt",
+          "version": "2.5"
+        }]
+    };
 
-		const component = shallow(
-			<DocumentPage document={mockDocumentData} />
-		);
+    const component = shallow(
+      <DocumentPage document={mockDocumentData} />
+    );
 
-		test("properly maps files array members to subsequent li tags", () => {
+    test("properly maps files array members to subsequent li tags", () => {
 
-			expect(component.find(".allVersionsList li")).toHaveLength(mockDocumentData.files.length);
+      expect(component.find(".allVersionsList li")).toHaveLength(mockDocumentData.files.length);
 
-		});
+    });
 
-		test("file array member is rendered as html structure when version, effectiveDate ARE NOT available, ", function() {
+    test("file array member is rendered as html structure when version, effectiveDate ARE NOT available, ", function() {
 
-			const fileIndex = 0;
-			const file = mockDocumentData.files[fileIndex];
+      const { documentIdNumber, files } = mockDocumentData;
+      const fileIndex = 0;
+      const file = files[fileIndex];
 
-			let mockHtml = `<li><strong>Version: N/A</strong>`;
-            mockHtml += `<strong>|</strong>`;
-            mockHtml += `Effective: N/A.`;
-            mockHtml += `<a href="${file.fileUrl}" target="_blank">Download PDF`;
-            mockHtml += `<i class="fa fa-file-pdf-o" aria-hidden="true"></i>`;
-            mockHtml += `</a></li>`;
+      let mockHtml = `<li><strong>Version ${documentIdNumber}: N/A</strong>`;
+      mockHtml += `<strong>|</strong>`;
+      mockHtml += `Effective: N/A.`;
+      mockHtml += `<a href="${file.fileUrl}" target="_blank">Download PDF`;
+      mockHtml += `<i class="fa fa-file-pdf-o" aria-hidden="true"></i>`;
+      mockHtml += `</a></li>`;
 
-			expect(component.find(".allVersionsList li").at(fileIndex).html()).toBe(mockHtml);
+      expect(component.find(".allVersionsList li").at(fileIndex).html()).toBe(mockHtml);
 
-		});
+    });
 
-		test("file array member is rendered as html structure when a version, effectiveDate ARE available, ", function() {
+    test("file array member is rendered as html structure when a version, effectiveDate ARE available, ", function() {
 
-			const fileIndex = 1;
-			const file = mockDocumentData.files[fileIndex];
+      const { documentIdNumber, files } = mockDocumentData;
+      const fileIndex = 1;
+      const file = files[fileIndex];
 
-			let mockHtml = `<li><strong>Version ${file.version}</strong>`;
-            mockHtml += `<strong>|</strong>`;
-            mockHtml += `Effective: ${file.effectiveDate}.`;
-            mockHtml += `<a href="${mockDocumentData.files[fileIndex].fileUrl}" target="_blank">Download PDF`;
-            mockHtml += `<i class="fa fa-file-pdf-o" aria-hidden="true"></i>`;
-            mockHtml += `</a></li>`;
+      let mockHtml = `<li><strong>Version ${documentIdNumber} ${file.version}</strong>`;
+      mockHtml += `<strong>|</strong>`;
+      mockHtml += `Effective: ${file.effectiveDate}.`;
+      mockHtml += `<a href="${mockDocumentData.files[fileIndex].fileUrl}" target="_blank">Download PDF`;
+      mockHtml += `<i class="fa fa-file-pdf-o" aria-hidden="true"></i>`;
+      mockHtml += `</a></li>`;
 
-			expect(component.find(".allVersionsList li").at(fileIndex).html()).toBe(mockHtml);
+      expect(component.find(".allVersionsList li").at(fileIndex).html()).toBe(mockHtml);
 
-		});
+    });
 
-	});
+  });
 
 });
