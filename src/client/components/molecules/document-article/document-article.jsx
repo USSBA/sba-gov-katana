b5e7940c8509e10b8bfef5d2ec0dcd7c933af647
Avery Smith <asmith@Averys-MacBook-Pro.local>
2017-10-16 15:08:37 -0400

    DT-3809-last-updated moved 'last updated' date to come after 'effective' date

--- src/client/components/molecules/document-article/document-article.jsx
+++ src/client/components/molecules/document-article/document-article.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import _ from "lodash";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import {logPageEvent} from "../../../services/analytics.js";
@@ -14,7 +15,6 @@ import {TextSection} from "molecules";
 
 import * as NavigationActions from "../../../actions/navigation.js";
 
-import _ from "lodash";
 import s from "./document-article.scss";
 
 export class DocumentArticle extends React.Component {
@@ -53,6 +53,57 @@ export class DocumentArticle extends React.Component {
     this.props.actions.locationChange("/"+this.props.type+"/?" + queryString.stringify(params))
   }
 
+  renderDateLine(file) {
+
+    const {expirationDate, effectiveDate} = file;
+    const {data} = this.props;
+
+    const dates = [];
+
+    if (expirationDate) {
+      
+      dates.push({
+        title: "Expiration",
+        date: this.formatDate(expirationDate)
+      });
+
+    }
+
+    if (effectiveDate) {
+      
+      dates.push({
+        title: "Effective",
+        date: this.formatDate(effectiveDate)
+      });
+
+    }
+
+    if (data) {
+      
+      dates.push({
+        title: "Last Updated",
+        date: moment.unix(data.updated).format("MMM D, YYYY")
+      });
+
+    }
+
+    const dateLine = dates.map((object, index) => {
+
+      return ( <p className={s.dates}>
+
+          {index > 0 && <span className={s.dateSeperator}>{" "}|{" "}</span>}
+          <span className={s.date}>{object.title} {object.date}</span>
+
+        </p>
+
+      );
+
+    });
+
+    return dateLine;
+
+  }
+
   render() {
     const data = this.props.data
     const body = data.body && typeof data.body === "string" ? data.body: "";
@@ -71,13 +122,11 @@ export class DocumentArticle extends React.Component {
         <div className={"document-article " + s.page}>
           {documentTypeString ? <DocumentType type={documentTypeString} number={data.documentIdNumber}/> : undefined}
           <h1 className={"document-article-title " + s.title + " " + (documentTypeString? s.marginTop:"")}>{data.title}</h1>
-          <p className={s.dates}>
-            {data.updated ? <span className={s.date}>Last Updated {moment.unix(data.updated).format('MMM D, YYYY')}</span> : null}
-            {!currentFile || !currentFile.expirationDate ? null : <span className={s.dateSeperator}>{" "}|{" "}</span>}
-            {currentFile && currentFile.expirationDate ? <span className={s.date}>Expiration {this.formatDate(currentFile.expirationDate)}</span> : null}
-            {!currentFile || !currentFile.effectiveDate ? null : <span className={s.dateSeperator}>{" "}|{" "}</span>}
-            {currentFile && currentFile.effectiveDate ? <span className={s.date}>Effective {this.formatDate(currentFile.effectiveDate)}</span> : null}
-            </p>
+          
+          {!_.isEmpty(currentFile) && <div>
+            {this.renderDateLine(currentFile)}
+          </div>}
+
           {data.officeLink.url ? <div className={s.office}>By{" "}
             <BasicLink url={data.officeLink.url} text={data.officeLink.title}/>
           </div> : <span/>
