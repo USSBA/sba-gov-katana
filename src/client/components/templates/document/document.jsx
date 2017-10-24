706d19d26b02938aabacfecd500f999c03ed0e2f
Brian Le <blsymph@gmail.com>
2017-10-23 21:51:02 -0400

    DT-3828 Move versions list into separate component

--- src/client/components/templates/document/document.jsx
+++ src/client/components/templates/document/document.jsx
@@ -1,75 +1,10 @@
 import React from "react";
-import moment from "moment";
-import {
-  isObject,
-  isEmpty
-} from "lodash";
 
-import styles from "./document.scss";
+// import styles from "./document.scss";
+import { VersionsList } from "atoms";
 import { DocumentArticle } from "molecules";
 import { RelatedDocumentCards } from "organisms";
 import { logPageEvent } from "../../../services/analytics.js";
-
-const VersionsList = props => {
-  const { doc } = props;
-  const { documentIdNumber, documentIdType, files, title } = doc;
-
-  if (files.length <= 1) return null;
-
-  const list = files.map((file, index) => {
-    const { effectiveDate, fileUrl, version } = file;
-
-    const versionMessage = (
-        documentIdType === "SOP"
-        && documentIdNumber
-        && (!isObject(documentIdNumber) || !isEmpty(documentIdNumber)
-      )
-      ? documentIdNumber
-      : "Version") + " " + (version ? version : "N/A");
-
-    const effectiveDateMessage = `Effective: ${effectiveDate || 'N/A'}`;
-    const effectiveDateInTheFuture = moment(effectiveDate).isAfter(moment())
-    const eventConfig = {
-      category: 'Document-Version',
-      action: `docname - ${title}: previous version #${version || 'N/A'}`
-    };
-
-    return (
-      <li key={index}>
-        <strong>{versionMessage}</strong>
-        <strong>|</strong>
-        {effectiveDateMessage}.
-        <a
-          href={fileUrl}
-          onClick={_ => logPageEvent(eventConfig)}
-          target="_blank"
-        >
-          Download PDF<i
-            className="fa fa-file-pdf-o"
-            aria-hidden="true"
-          />
-        </a>
-        {effectiveDateInTheFuture
-          ? <strong
-            className={styles.future}
-            key={30}>Future Document
-          </strong>
-          : null
-        }
-      </li>
-    );
-  });
-
-  return (
-    <div className={styles.allVersionsList}>
-      <h3>All versions</h3>
-      <ul>
-        {list}
-      </ul>
-      <hr className={styles.hr}/>
-    </div>
-  );
-};
 
 class DocumentPage extends React.Component {
 
