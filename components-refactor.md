ca238cc95e6aa3f61b8a69fea6fda80358f725be
Jonathan King <jking@fearless.tech>
2017-11-14 20:51:56 -0500

    T1-7 Brian's work rebased on master

---
+++ components-refactor.md
@@ -0,0 +1,7 @@
+> Some of the components in the codebase need to be moved to/from atoms, molecules and organisms. This is a list keeping track of the changes that have been/will need to be made:
+
+# Components
+
+## Orphans
+- [x] move `modal-controller.jsx` to molecules
+
--- src/client/components/molecules/index.jsx
+++ src/client/components/molecules/index.jsx
@@ -6,8 +6,8 @@ import Card from './card/card.jsx'
 import CardGrid from './card-grid/card-grid.jsx'
 import ContactCardLookup from './contact-card-lookup/contact-card-lookup.jsx'
 import DocumentArticle from './document-article/document-article.jsx'
-import DocumentCard from './document-card/document-card.jsx'
 import DocumentArticleLookup from './document-article-lookup/document-article-lookup.jsx'
+import DocumentCard from './document-card/document-card.jsx'
 import DropdownMenu from './dropdown-menu/dropdown-menu.jsx'
 import FeaturedCallout from './featured-callout/featured-callout.jsx'
 import FeedbackForm from './feedback-form/feedback-form.jsx'
@@ -19,19 +19,21 @@ import ImageSection from './image-section/image-section.jsx'
 import Lookup from './lookup/lookup.jsx'
 import MenuTile from './menu-tile/menu-tile.jsx'
 import MenuTileWithLinks from './menu-tile-with-links/menu-tile-with-links.jsx'
+import ModalController from './modal-controller/modal-controller.jsx'
 import NaicsLookup from './naics-lookup/naics-lookup.jsx'
 import PageLinkGroup from './page-link-group/page-link-group.jsx'
 import Paginator from './paginator/paginator.jsx'
+import PagingLookup from './paging-lookup/paging-lookup.jsx'
 import ParagraphPlaceholder from './paragraph-placeholder/paragraph-placeholder.jsx'
 import PreviousNextSection from './previous-next/previous-next.jsx'
 import QuickLinks from './quick-links/quick-links.jsx'
 import ReadMore from './readmore/readmore.jsx'
 import ReadMoreSection from './readmore-section/readmore-section.jsx'
 import ResourceCard from './resource-card/resource-card.jsx'
+import SbaModal from './sba-modal/sba-modal.jsx'
 import SbicLookup from './sbic-lookup/sbic-lookup.jsx'
 import SearchBar from './search-bar/search-bar.jsx'
 import SectionHeader from './section-header/section-header.jsx'
-import SbaModal from './sba-modal/sba-modal.jsx'
 import SimpleCta from './simple-cta/simple-cta.jsx'
 import SmallInverseCta from './small-inverse-cta/small-inverse-cta.jsx'
 import StyleGrayBackground from './style-gray-background/style-gray-background.jsx'
@@ -41,10 +43,9 @@ import SuretyLookup from './surety-lookup/surety-lookup.jsx'
 import TenStepsNav from './ten-steps-nav/ten-steps-nav.jsx'
 import TenStepsSection from './ten-steps-section/ten-steps-section.jsx'
 import TextReadMoreSection from './text-readmore-section/text-readmore-section.jsx'
+import TextSection from './text-section/text-section.jsx'
 import TitleBox from './title-box/title-box.jsx'
 import TitleSection from './title-section/title-section.jsx'
-import TextSection from './text-section/text-section.jsx'
-import PagingLookup from './paging-lookup/paging-lookup.jsx'
 
 export {
   Breadcrumb,
@@ -55,8 +56,8 @@ export {
   CardGrid,
   ContactCardLookup,
   DocumentArticle,
-  DocumentCard,
   DocumentArticleLookup,
+  DocumentCard,
   DropdownMenu,
   FeaturedCallout,
   FeedbackForm,
@@ -68,19 +69,21 @@ export {
   Lookup,
   MenuTile,
   MenuTileWithLinks,
+  ModalController,
   NaicsLookup,
   PageLinkGroup,
   Paginator,
+  PagingLookup,
   ParagraphPlaceholder,
   PreviousNextSection,
   QuickLinks,
   ReadMore,
   ReadMoreSection,
   ResourceCard,
+  SbaModal,
   SbicLookup,
   SearchBar,
   SectionHeader,
-  SbaModal,
   SimpleCta,
   SmallInverseCta,
   StyleGrayBackground,
@@ -90,8 +93,7 @@ export {
   TenStepsNav,
   TenStepsSection,
   TextReadMoreSection,
-  TitleBox,
-  TitleSection,
   TextSection,
-  PagingLookup
+  TitleBox,
+  TitleSection
 }
