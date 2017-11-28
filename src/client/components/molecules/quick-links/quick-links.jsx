a9f806fa29c6d023f23f1c25dba4e0d3a38b5ebc
Avery Smith <avery@averysmith.pro>
2017-11-28 16:23:49 -0500

    AA-182-dev put 7(a) string back

--- src/client/components/molecules/quick-links/quick-links.jsx
+++ src/client/components/molecules/quick-links/quick-links.jsx
@@ -64,7 +64,7 @@ class QuickLinks extends PureComponent {
         this.props.actions.fetchContentIfNeeded('articles-' + index, 'articles', {
           sortBy: 'Last Updated',
           type: 'all',
-          program: quickLink.articleProgram,
+          program: '7(a)',
           start: 0,
           end: 3
         })
