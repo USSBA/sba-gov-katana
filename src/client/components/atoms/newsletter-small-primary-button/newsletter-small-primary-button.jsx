7730263c6e513a9fa4950d88db80c77765e0cd45
Brian Le <blsymph@gmail.com>
2017-12-11 12:11:14 -0500

    T1-0000 Hotfix button base

--- src/client/components/atoms/newsletter-small-primary-button/newsletter-small-primary-button.jsx
+++ src/client/components/atoms/newsletter-small-primary-button/newsletter-small-primary-button.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './newsletter-small-primary-button.scss'
+import { ButtonBase } from 'atoms'
 
 const NewsletterSmallPrimaryButton = props => (
   <ButtonBase {...props} buttonClassName={styles.NewsletterSmallPrimaryButton} />
