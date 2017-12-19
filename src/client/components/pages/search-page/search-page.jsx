aa4a53d75d362b2bfad285a0c91301786245fc64
Avery Smith <avery@averysmith.pro>
2017-12-18 19:49:10 -0500

    DTT-12 added search bar components

--- src/client/components/pages/search-page/search-page.jsx
+++ src/client/components/pages/search-page/search-page.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import { SmallInverseSecondaryButton, TextInput } from 'atoms'
 
-const SearchPage = () => {
-  return <div>Search Page</div>
+class SearchPage extends PureComponent {
+  constructor() {
+    super()
+
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  componentWillMount() {
+    const searchTerm = document.location.search.split('?q=')[1]
+
+    this.setState({ searchTerm })
+  }
+
+  render() {
+    const { searchTerm } = this.state
+
+    return (
+      <div>
+        <h1>Search</h1>
+        <SearchBar searchTerm={searchTerm} />
+      </div>
+    )
+  }
+}
+
+const SearchBar = props => {
+  const { searchTerm } = props
+
+  return (
+    <div>
+      <TextInput
+        id="search"
+        errorText={'Please enter the correct thing.'}
+        label="What are you looking for?"
+        validationState={''}
+        value={searchTerm}
+      />
+      <SmallInverseSecondaryButton url="#" text="Search" />
+    </div>
+  )
 }
 
 export default SearchPage
--- test/jest/client/components/pages/search-page/search-page.test.jsx
+++ test/jest/client/components/pages/search-page/search-page.test.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import SearchPage from 'client/components/pages/search-page/search-page.jsx'
 import renderer from 'react-test-renderer'
+import { shallow, mount } from 'enzyme'
 
 describe('SearchPage', () => {
   test('to match snapshot', () => {
     const component = renderer.create(<SearchPage />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  describe('TextInput', () => {
+    it('has one input field', () => {
+      const component = mount(<SearchPage />)
+      const result = component.find('TextInput').length
+      const expected = 1
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('SmallInverseSecondaryButton', () => {
+    it('has one submit button', () => {
+      const component = mount(<SearchPage />)
+      const result = component.find('SmallInverseSecondaryButton').length
+      const expected = 1
+
+      expect(result).toEqual(expected)
+    })
+
+    it('the submit button has a label of "Search"', () => {
+      const component = mount(<SearchPage />)
+      const result = component.find('SmallInverseSecondaryButton').props().text
+      const expected = 'Search'
+
+      expect(result).toEqual(expected)
+    })
   })
 })
