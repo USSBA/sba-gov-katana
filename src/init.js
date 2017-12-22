a89f7933b73874af78d247282a7463cd4a544ce7
Jonathan King <jking@fearless.tech>
2017-12-22 12:38:29 -0500

    XX-0000 removes lendermatch code

--- src/init.js
+++ src/init.js
@@ -1,6 +1,5 @@
 import { init as initDatabase } from './models/db.js'
-import * as cron from './util/cron.js'
 function init() {
-  return Promise.all([initDatabase(), cron.init()])
+  return Promise.all([initDatabase()])
 }
 export default init
