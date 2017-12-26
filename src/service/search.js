38b98da3c799ddc777170a6e267d8d8fe983fc52
Avery Smith <avery@averysmith.pro>
2017-12-26 16:08:36 -0500

    DDT-12 formatted search result to match AWS formatting

--- src/service/search.js
+++ src/service/search.js
@@ -3,75 +3,55 @@ function search(req, res) {
 
   console.log('AA', req)
 
-  let result = []
+  let result = [
+    {
+      hits: {
+        found: 0,
+        hit: []
+      }
+    }
+  ]
 
   if (decodeURIComponent(req.term) !== 'no new taxes') {
     result = [
       {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
-      },
-      {
-        title: 'Title',
-        description:
-          'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
-        url: 'https://sba.gov/lorem-ipsum/'
+        status: {
+          rid: 'rd+5+r0oMAo6swY=',
+          'time-ms': 9
+        },
+        hits: {
+          found: 3,
+          start: 0,
+          hit: [
+            {
+              id: 'tt1951265',
+              fields: {
+                title: 'The Hunger Games: Mockingjay - Part 1',
+                description:
+                  'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
+                url: 'https://sba.gov/lorem-ipsum/'
+              }
+            },
+            {
+              id: 'tt1951264',
+              fields: {
+                title: 'The Hunger Games: Catching Fire',
+                description:
+                  'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
+                url: 'https://sba.gov/lorem-ipsum/'
+              }
+            },
+            {
+              id: 'tt1392170',
+              fields: {
+                title: 'The Hunger Games',
+                description:
+                  'Excepturi quod vel dignissimos. Aut ut eligendi repellendus. Necessitatibus dicta commodi voluptatem molestiae praesentium hic accusamus cupiditate. Nesciunt at ab est. Voluptate et enim nisi voluptates aliquam magnam.',
+                url: 'https://sba.gov/lorem-ipsum/'
+              }
+            }
+          ]
+        }
       }
     ]
   }
