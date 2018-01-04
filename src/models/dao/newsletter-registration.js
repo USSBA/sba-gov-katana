06b923ca4cb95a4d044f97524d20c399ad931df8
Jonathan King <jking@fearless.tech>
2018-01-04 07:55:29 -0500

    DTT-20 some more fixes for using devMode without a running database

--- src/models/dao/newsletter-registration.js
+++ src/models/dao/newsletter-registration.js
@@ -1,34 +1,49 @@
 /* eslint-disable camelcase */
+import config from "config";
 const govDeliveryCountyZip = require('../drupal7/drupal-govdelivery-county-zip.js').govDeliveryCountyZip
 const govDeliveryDistrictCounty = require('../drupal7/drupal-govdelivery-district-county.js')
   .govDeliveryDistrictCounty
 const _ = require('lodash')
 
 function fetchOneGovDeliveryZipCodeRecord(zipCode) {
-  return govDeliveryCountyZip.findOne({
-    where: {
-      zip_code: zipCode
-    }
-  })
+  if (config.get('developmentOptions.devMode')) {
+    return Promise.resolve({})
+  } else {
+    return govDeliveryCountyZip.findOne({
+      where: {
+        zip_code: zipCode
+      }
+    })
+  }
 }
 
 function fetchGovDeliveryDistrictCounties(countyName, stateCode) {
-  return govDeliveryDistrictCounty.findAll({
-    where: {
-      state_code: stateCode,
-      county_name: countyName
-    }
-  })
+  if (config.get('developmentOptions.devMode')) {
+    return Promise.resolve({})
+  } else {
+    return govDeliveryDistrictCounty.findAll({
+      where: {
+        state_code: stateCode,
+        county_name: countyName
+      }
+    })
+  }
 }
 
 function registerForNewsletter(userEmailAddress, userZipCode) {
-  // get zipcode Data
-  return fetchOneGovDeliveryZipCodeRecord(userZipCode).then(function(zipCodeRecord) {
-    return fetchGovDeliveryDistrictCounties(
-      _.replace(zipCodeRecord.county_name, ' County', ''),
-      zipCodeRecord.state_code
-    )
-  })
+  if (config.get('developmentOptions.devMode')) {
+    return Promise.resolve({})
+  } else {
+    // get zipcode Data
+    return fetchOneGovDeliveryZipCodeRecord(userZipCode).then(function(zipCodeRecord) {
+      return fetchGovDeliveryDistrictCounties(
+        _.replace(zipCodeRecord.county_name, ' County', ''),
+        zipCodeRecord.state_code
+      )
+    })
+  }
 }
 
-export { registerForNewsletter }
+export {
+  registerForNewsletter
+}
--- src/models/drupal7/drupal-seven-db-client.js
+++ src/models/drupal7/drupal-seven-db-client.js
@@ -1,15 +1,32 @@
+/* eslint-disable no-empty-function */
 import Sequelize from 'sequelize'
 const config = require('config')
-var drupalSeven = new Sequelize(
-  config.get('database.drupalSeven.databaseName'),
-  config.get('database.drupalSeven.user'),
-  config.get('database.drupalSeven.password'),
-  {
-    host: config.get('database.drupalSeven.host'),
-    dialect: 'mysql',
-    pool: config.get('database.pool')
+
+
+var drupalSeven;
+if (config.get('developmentOptions.devMode')) {
+  drupalSeven = {
+    define: () => {
+      return {
+        removeAttribute: () => {}
+      }
+    },
+    query: () => {
+      return Promise.resolve({})
+    }
   }
-)
+} else {
+  drupalSeven = new Sequelize(
+    config.get('database.drupalSeven.databaseName'),
+    config.get('database.drupalSeven.user'),
+    config.get('database.drupalSeven.password'), {
+      host: config.get('database.drupalSeven.host'),
+      dialect: 'mysql',
+      pool: config.get('database.pool')
+    }
+  )
+}
+
 
 function executeDrupalSevenQuery(query) {
   return drupalSeven.query(query, {
