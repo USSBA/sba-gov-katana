90dcec1638811528a456999f89901e0a5cc132c1
Avery Smith <avery@averysmith.pro>
2018-01-31 12:19:07 -0500

    DTT-112 created initial Learning Center Detail Template test

---
+++ src/client/components/templates/learning-center/detail.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+class LearningCenterDetailTemplate extends PureComponent {
+  render() {
+    return <div>A</div>
+  }
+}
+
+export default LearningCenterDetailTemplate
---
+++ test/jest/client/components/templates/learning-center/detail.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
+
+import LearningCenterDetailTemplate from 'templates/learning-center/detail.jsx'
+
+describe('LearningCenterDetailTemplate', () => {
+  describe('Title', () => {
+    test('has a title', () => {})
+  })
+
+  describe('Summary', () => {
+    test('has a summary', () => {})
+  })
+
+  describe('Course', () => {
+    test('has a course link', () => {})
+  })
+
+  describe('Download Link', () => {
+    test('has a download link', () => {})
+  })
+
+  describe('Breadcrumb', () => {
+    test('has a breadcrumb trail', () => {})
+  })
+
+  describe('Mobile', () => {
+    test('has a mobile message that course is unavailable', () => {})
+  })
+})
