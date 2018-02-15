1c6f1d8b8b07accbd8f9c9364ef79d87a70892dd
Avery Smith <avery@averysmith.pro>
2018-02-15 14:43:30 -0500

    TA-175 added contracting officials icon

--- src/client/components/organisms/for-partners-tile-collection/icons.jsx
+++ src/client/components/organisms/for-partners-tile-collection/icons.jsx
@@ -4,6 +4,8 @@ import forPartnersIconSBICsColor from 'assets/images/for-partners/For_Partners_I
 import forPartnersIconSBICsWhite from 'assets/images/for-partners/For_Partners_Icon_SBICs_white.png'
 import forPartnersIconSuretyProvidersColor from 'assets/images/for-partners/For_Partners_Icon_Surety_Providers_color.png'
 import forPartnersIconSuretyProvidersWhite from 'assets/images/for-partners/For_Partners_Icon_Surety_Providers_white.png'
+import forPartnersIconContractingOfficialsColor from 'assets/images/for-partners/For_Partners_Icon_Contracting_Officials_color.png'
+import forPartnersIconContractingOfficialsWhite from 'assets/images/for-partners/For_Partners_Icon_Contracting_Officials_white.png'
 
 let fundingProgramIcons = [
   {
@@ -17,6 +19,10 @@ let fundingProgramIcons = [
   {
     icon: forPartnersIconSuretyProvidersColor,
     iconWhite: forPartnersIconSuretyProvidersWhite
+  },
+  {
+    icon: forPartnersIconContractingOfficialsColor,
+    iconWhite: forPartnersIconContractingOfficialsWhite
   }
 ]
 
