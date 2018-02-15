39be100ed2e7d1327fcde53da9f58c7782a49f9f
Jonathan King <jking@fearless.tech>
2018-02-15 12:50:27 -0500

    DTT-6 more fixes for babel on the server

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 rm -rf public/build
 rm -rf dist
-NODE_ENV=production $(npm bin)/babel src -d dist --ignore client/* --no-babelrc --env='{ env: { production: { targets: { node: "current" , "uglify" : "true"} } } }'  --plugins transform-object-rest-spread
-NODE_ENV=production $(npm bin)/webpack --config webpack.config.production.js
+NODE_ENV=production $(npm bin)/babel src -d dist --ignore client/* --no-babelrc --env='{ env: { production: { targets: { node: "current" , "uglify" : "true"} } } }'  --plugins transform-object-rest-spread,transform-es2015-modules-commonjs
+# NODE_ENV=production $(npm bin)/webpack --config webpack.config.production.js
