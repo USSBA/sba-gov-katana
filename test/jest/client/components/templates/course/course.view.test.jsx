e9c3e95db88b52a7c39bca6a1926f391ed92b660
Brian Le <blsymph@gmail.com>
2018-02-21 16:57:20 -0500

    T1-307 Do not show course on mobile devices

--- test/jest/client/components/templates/course/course.view.test.jsx
+++ test/jest/client/components/templates/course/course.view.test.jsx
@@ -12,43 +12,7 @@ import CourseView, {
   RelatedArticles,
   CTA
 } from 'templates/course/course.view.jsx'
-
-const sharedProps = {
-  isLoaded: true,
-  title: 'How to write a business plan',
-  course: {
-    url: 'https://www.youtube.com/embed/owsfdh4gxyc',
-    worksheets: [
-      {
-        description: 'How to write a business plan checklist',
-        url: '#'
-      }
-    ]
-  },
-  breadcrumbs: [
-    {
-      url: '#',
-      title: 'Learning Center'
-    },
-    {
-      url: '#',
-      title: 'Search results'
-    },
-    {
-      url: '#',
-      title: 'How to write a business plan'
-    }
-  ],
-  readMoreSectionItem: {
-    expandedCopyText: 'expanded text',
-    titleText: 'title text',
-    preview: 'preview text'
-  },
-  readMoreExpanded: true,
-  onToggleStatus: () => {
-    return false
-  }
-}
+import sharedProps from './course-test-props'
 
 describe('<CourseView />', () => {
   describe('Breadcrumb', () => {
