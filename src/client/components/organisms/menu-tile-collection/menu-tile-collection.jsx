abf8430e24526ff5da9af5b4fa328d6293abe27f
Kevin Lee <ki.lee@sba.gov>
2018-02-22 17:08:57 -0500

    T1-244 Create new component similar to TileCollection

---
+++ src/client/components/organisms/menu-tile-collection/menu-tile-collection.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import _ from 'lodash'
+import Tile from 'organisms'
+import s from './menu-tile-collection.scss'
+
+class MenuTileCollection extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      enteringInReverse: false,
+      markAsEnteringInReverse: false,
+      panelShowingHoverState: -1
+    }
+  }
+
+  onFocus(index) {
+    this.setState({
+      panelShowingHoverState: index,
+      enteringInReverse: this.state.markAsEnteringInReverse,
+      markAsEnteringInReverse: false
+    })
+  }
+
+  onBlur(index) {
+    this.setState({
+      panelShowingHoverState: -1,
+      markAsEnteringInReverse: false
+    })
+  }
+
+  onTabBackwards(index, recentlyEnteringInReverse) {
+    console.log('index', index)
+    console.log('recentlyEnteringInReverse', recentlyEnteringInReverse)
+    this.setState({
+      panelShowingHoverState: index,
+      markAsEnteringInReverse: true
+    })
+  }
+
+  makeTile(object, index) {
+    const iconElement = this.props.icons[index]
+    const tileProps = {
+      id: 'tile-' + index,
+      key: index,
+      data: object,
+      icon: iconElement.icon,
+      backgroundLines: iconElement.background,
+      iconWhite: iconElement.iconWhite,
+      size: this.props.icons.length,
+      uppercaseFirstWord: this.props.uppercaseFirstWord,
+      splitTitle: this.props.splitTitle,
+      onFocus: () => {
+        this.onFocus(index)
+      },
+      onBlur: () => {
+        this.onBlur(index)
+      },
+      onMouseExit: () => {
+        this.onBlur(index)
+      },
+      onTabBackwards: () => {
+        this.onTabBackwards(index)
+      },
+      enteringInReverse: this.state.enteringInReverse,
+      showHover: index === this.state.panelShowingHoverState,
+      neverDisplayChildrenOnHoverOverride: this.props.neverDisplayChildrenOnHoverOverride
+    }
+    return <Tile {...tileProps} />
+  }
+
+  render() {
+    if (!this.props.data) {
+      return <div />
+    } else if (this.props.data.length !== this.props.icons.length) {
+      console.error('Invalid number of icons provided to tile collection')
+      console.error(this.props.data)
+      console.error(this.props.icons)
+      return <div />
+    }
+    return <div>{this.props.data.map(this.makeTile.bind(this))}</div>
+  }
+}
+
+MenuTileCollection.defaultProps = {
+  data: [],
+  icons: {},
+  neverDisplayChildrenOnHoverOverride: false,
+  uppercaseFirstWord: false,
+  splitTitle: false
+}
+
+export default MenuTileCollection
