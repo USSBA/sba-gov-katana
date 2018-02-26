df688dda65a5da08b0c00716f1f56003950e8b51
Kevin Lee <ki.lee@sba.gov>
2018-02-26 15:39:07 -0500

    T1-244 Modify test to clone props

--- test/jest/client/components/templates/homepage/homepage.test.jsx
+++ test/jest/client/components/templates/homepage/homepage.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import clone from 'lodash'
 
 import HomePage from 'templates/homepage/homepage'
 
@@ -11,6 +12,7 @@ const testProps = {
 
 describe('HomePage', () => {
   test('should render HomePage', () => {
+    const props = clone(testProps)
     const component = shallow(<HomePage {...testProps} />)
     expect(component).toMatchSnapshot()
   })
