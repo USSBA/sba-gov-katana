2b3f67478c571c2d2a3ba36e14d8302afdc299e2
Kevin Lee <KevL927@users.noreply.github.com>
2018-02-27 18:48:46 -0500

    T1 244 dev b (#481)
    * T1-244 Rename filenames with color tag to purple and add blue version
    
    * Add Tile and MenuTileCollection in index
    
    * T1-244 Create new component similar to TileCollection
    
    * T1-244 Create MenuTileCollection with a copy of its own Tile component
    
    * T1-244 Delete duplicate component and make Tile and MenuTile components reusable
    
    * T1-244 Modify test to clone props
    
    * T1-244 Update theme to make sba-blue-dark usable
    
    * T1-244 Fix mobile nav for tile collection and colors for each page
    
    * T1-244 Fix color for menus
    
    * T1-244 Organize code

--- src/client/components/organisms/tile-collection/tile.jsx
+++ src/client/components/organisms/tile-collection/tile.jsx
@@ -79,46 +79,56 @@ class Tile extends React.Component {
   }
 
   render() {
+    const {
+      backgroundLines,
+      data,
+      enteringInReverse,
+      icon,
+      iconWhite,
+      id,
+      pathname,
+      showHover,
+      size,
+      splitTitle,
+      locationActions,
+      uppercaseFirstWord
+    } = this.props
     const baseTileData = {
-      link: this.props.data.fullUrl,
-      children: this.props.data.children,
-      description: this.props.data.description,
-      icon: this.props.icon,
-      iconWhite: this.props.iconWhite,
-      largeTitle: this.props.splitTitle ? this._formatLargeTitle() : this.props.data.title,
-      smallTitle: this.props.splitTitle ? this._formatSmallTitle() : '',
-      uppercaseFirstWord: this.props.uppercaseFirstWord
+      link: data.fullUrl,
+      children: data.children,
+      description: data.description,
+      icon: icon,
+      iconWhite: iconWhite,
+      largeTitle: splitTitle ? this._formatLargeTitle() : this.props.data.title,
+      smallTitle: splitTitle ? this._formatSmallTitle() : '',
+      uppercaseFirstWord: uppercaseFirstWord
     }
 
     const hoverTile = this.isLinkToPage() ? (
-      <MenuTile {...baseTileData} inverse id={this.props.id + '-hover'} />
+      <MenuTile {...baseTileData} inverse id={id + '-hover'} />
     ) : (
       <MenuTileWithLinks
         {...baseTileData}
-        id={this.props.id + '-hover'}
-        locationActions={this.props.locationActions}
-        autoFocusOnLast={this.props.enteringInReverse}
+        id={id + '-hover'}
+        locationActions={locationActions}
+        autoFocusOnLast={enteringInReverse}
       />
     )
 
     let widthStyle = null
-    switch (this.props.size && this.props.pathname) {
-      case 3 && undefined:
-        widthStyle = s.tileThree
-        break
-      case 4 && undefined:
-        widthStyle = s.tileFour
-        break
-      case 5 && undefined:
-        widthStyle = s.tileFive
-        break
-      case 4 && '/':
-        widthStyle = s.homePageTileFour
+    if (size === 3 && pathname === undefined) {
+      widthStyle = s.tileThree
+    } else if (size === 4 && pathname === undefined) {
+      widthStyle = s.tileFour
+    } else if (size === 5 && pathname === undefined) {
+      widthStyle = s.tileFive
+    } else if (size === 4 && pathname === '/') {
+      widthStyle = s.homePageTileFour
     }
 
     const toggleMenuLink = (
       <BasicLink
-        text={`toggle ${this.props.data.title} menu`}
+        text={`toggle ${data.title} menu`}
         myClassName={s.tabDisplayMenu}
         onClick={e => {
           e ? e.preventDefault() : ''
@@ -131,7 +141,7 @@ class Tile extends React.Component {
 
     return (
       <div
-        id={this.props.id}
+        id={id}
         className={s.tile + ' ' + widthStyle}
         onClick={this._openNavMenu.bind(this)}
         onMouseEnter={this._mouseEnterTile.bind(this)}
@@ -139,16 +149,8 @@ class Tile extends React.Component {
         onKeyDown={this.handleTileKeyDown.bind(this)}
       >
         {toggleMenuLink}
-        {this.props.showHover ? (
-          hoverTile
-        ) : (
-          <MenuTile id={this.props.id + '-static'} pathname={this.props.pathname} {...baseTileData} />
-        )}
-        {this.props.backgroundLines ? (
-          <img className={s.backgroundLines} src={this.props.backgroundLines} alt="" />
-        ) : (
-          undefined
-        )}
+        {showHover ? hoverTile : <MenuTile id={id + '-static'} pathname={pathname} {...baseTileData} />}
+        {backgroundLines ? <img className={s.backgroundLines} src={backgroundLines} alt="" /> : undefined}
       </div>
     )
   }
