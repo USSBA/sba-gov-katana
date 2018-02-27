7746c53985d69200b30869a4faa6b417603ca84d
jrwatts <jordan.r.watts@gmail.com>
2018-02-27 10:48:00 -0500

    TA-249 replaces contracting and for partner panel icons

--- src/client/components/organisms/federal-contracting-tile-collection/icons.jsx
+++ src/client/components/organisms/federal-contracting-tile-collection/icons.jsx
@@ -1,9 +1,9 @@
-import iconColor1 from 'assets/images/federal-contracting/Federal_Contracting_Icon_Section_1_color.png'
-import iconWhite1 from 'assets/images/federal-contracting/Federal_Contracting_Icon_Section_1_white.png'
-import iconColor2 from 'assets/images/federal-contracting/Federal_Contracting_Icon_Section_2_color.png'
-import iconWhite2 from 'assets/images/federal-contracting/Federal_Contracting_Icon_Section_2_white.png'
-import iconColor3 from 'assets/images/federal-contracting/Federal_Contracting_Icon_Section_3_color.png'
-import iconWhite3 from 'assets/images/federal-contracting/Federal_Contracting_Icon_Section_3_white.png'
+import iconColor1 from 'assets/images/federal-contracting/Understanding_Federal_Contracts_color.png'
+import iconWhite1 from 'assets/images/federal-contracting/Understanding_Federal_Contracts_white.png'
+import iconColor2 from 'assets/images/federal-contracting/SBA_Contracting_Assistance_Programs_color.png'
+import iconWhite2 from 'assets/images/federal-contracting/SBA_Contracting_Assistance_Programs_white.png'
+import iconColor3 from 'assets/images/federal-contracting/Counseling_and_Help_color.png'
+import iconWhite3 from 'assets/images/federal-contracting/Counseling_and_Help_white.png'
 
 const federalContractingIcons = [
   {
