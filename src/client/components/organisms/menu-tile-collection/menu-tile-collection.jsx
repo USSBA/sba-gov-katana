0c6ba5fa919103ca8a9124c1d588a655b2138204
Kevin Lee <KevL927@users.noreply.github.com>
2018-02-27 13:33:04 -0500

    T1 244 dev (#476)
    * T1-244 Rename filenames with color tag to purple and add blue version
    
    * Add Tile and MenuTileCollection in index
    
    * T1-244 Create new component similar to TileCollection
    
    * T1-244 Create MenuTileCollection with a copy of its own Tile component
    
    * T1-244 Delete duplicate component and make Tile and MenuTile components reusable
    
    * T1-244 Modify test to clone props
    
    * T1-244 Update theme to make sba-blue-dark usable

---
+++ src/client/components/organisms/menu-tile-collection/menu-tile-collection.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import _ from 'lodash'
+import { Tile } from 'organisms'
+import s from './menu-tile-collection.scss'
+import icons from '../../templates/homepage/businessGuideTileIcons'
+import data from '../../templates/homepage/business-guide-json'
+
+class MenuTileCollection extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      enteringInReverse: false,
+      markAsEnteringInReverse: false,
+      panelShowingHoverState: -1
+    }
+  }
+
+  onFocus(index) {
+    this.setState({
+      panelShowingHoverState: index,
+      enteringInReverse: this.state.markAsEnteringInReverse,
+      markAsEnteringInReverse: false
+    })
+  }
+
+  onBlur(index) {
+    this.setState({
+      panelShowingHoverState: -1,
+      markAsEnteringInReverse: false
+    })
+  }
+
+  onTabBackwards(index, recentlyEnteringInReverse) {
+    console.log('index', index)
+    console.log('recentlyEnteringInReverse', recentlyEnteringInReverse)
+    this.setState({
+      panelShowingHoverState: index,
+      markAsEnteringInReverse: true
+    })
+  }
+
+  makeTile(object, index) {
+    // const iconElement = this.props.icons[index]
+    const iconElement = icons[index]
+    const tileProps = {
+      id: 'tile-' + index,
+      key: index,
+      data: object,
+      icon: iconElement.icon,
+      backgroundLines: iconElement.background,
+      iconWhite: iconElement.iconWhite,
+      pathname: this.props.pathname,
+      size: icons.length,
+      uppercaseFirstWord: this.props.uppercaseFirstWord,
+      smallTile: this.props.smallTile,
+      splitTitle: this.props.splitTitle,
+      onFocus: () => {
+        this.onFocus(index)
+      },
+      onBlur: () => {
+        this.onBlur(index)
+      },
+      onMouseExit: () => {
+        this.onBlur(index)
+      },
+      onTabBackwards: () => {
+        this.onTabBackwards(index)
+      },
+      enteringInReverse: this.state.enteringInReverse,
+      showHover: index === this.state.panelShowingHoverState,
+      neverDisplayChildrenOnHoverOverride: this.props.neverDisplayChildrenOnHoverOverride
+    }
+    return <Tile {...tileProps} />
+  }
+
+  render() {
+    if (!data) {
+      return <div />
+    } else if (data.length !== icons.length) {
+      console.error('Invalid number of icons provided to tile collection')
+      console.error(data)
+      console.error(icons)
+      return <div />
+    }
+    return <div>{data.map(this.makeTile.bind(this))}</div>
+  }
+}
+
+MenuTileCollection.defaultProps = {
+  data: [],
+  icons: {},
+  neverDisplayChildrenOnHoverOverride: false,
+  uppercaseFirstWord: false,
+  splitTitle: false
+}
+
+export default MenuTileCollection
