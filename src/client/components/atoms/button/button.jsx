de878e6392a340e12ad8f6c5999e8954f6147da2
Brian Le <blsymph@gmail.com>
2018-03-14 11:48:13 -0400

    T1-376 Add base styling

---
+++ src/client/components/atoms/button/button.jsx
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react'
+
+import { BasicLink } from 'atoms'
+
+const Button = ({}) => {
+  const {} = props
+  return <BasicLink />
+}
+
+// Button.propTypes = {
+//   // TODO:
+//   // Primary buttons are filled; secondary buttons are outlined.
+//   type: PropTypes.oneOf(['primary', 'secondary']),
+//   // Style button in its alternate form, i.e. primary-alt with a different coloring.
+//   alternate: PropTypes.bool,
+//
+//   icon:
+//
+//   // type: primary | secondary
+//   // isAlternate: true | false
+//   // hasIcon: true | false
+//   // hasOutline: true | false
+// isDisabled: true | false
+// Choose between buttons with more prounounced (primary) coloring
+// }
+
+// <Button type="primary" alternate icon outline disabled />
+// <Button primary alternate icon outline disabled />
+
+export default Button
