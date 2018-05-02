bcd0898ab69c4d16c7fdb0274ad7074aab56f3a8
Kevin Lee <KevL927@users.noreply.github.com>
2018-05-02 10:05:25 -0400

    T1 591 dev (#537) (#540)
    * T1-591 Create classname on detail-card to stop affecting other table styles and update test
    
    * T1-591 Create classname for tables and update style for rebrand
    
    * T1-591 Uncomment css style
    
    T1-591 Fix font weight for row label
    
    T1-591 Place top borderon table body
    
    T1-591 Fix small size table without header
    
    T1-591 Fix spacing for table without header in table
    
    T1-591 Fix border color spilling out on table header

--- src/client/components/atoms/text-section/text-section.jsx
+++ src/client/components/atoms/text-section/text-section.jsx
@@ -7,8 +7,9 @@ class TextSection extends React.Component {
   parseTables() {
     const $ = cheerio.load(this.props.text)
     $('table').each((i, table) => {
-      let headers = []
+      const headers = []
       $(table)
+        .addClass('text-section-table')
         .find('thead > tr > th')
         .each((i, theader) => {
           headers.push(theader.children[0].data)
@@ -36,13 +37,22 @@ class TextSection extends React.Component {
 
         tds = $(trow).find('td')
 
-        tds.each((index, tdata) => {
-          const html = "<div class='table-data-wrapper'>" + $(tdata).html() + '</div>'
-          $(tdata).html(html)
+        if (headers.length < 1) {
+          tds.each((index, tdata) => {
+            const html = '<div class="table-data">' + $(tdata).html() + '</div>'
+            $(tdata)
+              .html(html)
+              .addClass('index-' + index)
+          })
+        } else {
+          tds.each((index, tdata) => {
+            const html = "<div class='table-data-wrapper'>" + $(tdata).html() + '</div>'
+            $(tdata).html(html)
 
-          const label = "<div class='table-header-label'>" + headers[index] + ':</div>'
-          $(tdata).prepend(label)
-        })
+            const label = "<div class='table-header-label'>" + headers[index] + ':</div>'
+            $(tdata).prepend(label)
+          })
+        }
       })
     })
     return $.html()
