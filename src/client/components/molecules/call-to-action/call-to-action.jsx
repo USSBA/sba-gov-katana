1adb0707ff0f7efadc32cece0d46f4999119cfcc
Brian Le <blsymph@gmail.com>
2018-05-03 11:35:55 -0400

    T1-351 Fix vertical alignment for small variation

--- src/client/components/molecules/call-to-action/call-to-action.jsx
+++ src/client/components/molecules/call-to-action/call-to-action.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
-import { camelCase, isEmpty } from 'lodash'
+import { camelCase, isEmpty, startsWith } from 'lodash'
 
 import styles from './call-to-action.scss'
 import { Button, DecorativeDash } from 'atoms'
@@ -23,20 +23,26 @@ const CallToAction = props => {
 
   return (
     <div id="call-to-action" className={className}>
-      {variation === 'largeVariation' ? (
+      {startsWith(variation, 'large') ? (
         <div className={styles.image} title={imageAlt} style={backgroundImageStyle} />
       ) : null}
       <div className={styles.content}>
-        {variation !== 'buttonOnlyVariation' ? <h3>{headline}</h3> : null}
-        {variation !== 'smallVariation' && variation !== 'buttonOnlyVariation' ? (
+        {!startsWith(variation, 'buttonOnly') ? (
+          <div className="column">
+            <h3>{headline}</h3>
+          </div>
+        ) : null}
+        {startsWith(variation, 'medium') || startsWith(variation, 'large') ? (
           <div>
             <DecorativeDash width={1.611} />
             <p className={styles.blurb}>{blurb}</p>
           </div>
         ) : null}
-        <Button alternate={variation !== 'buttonOnlyVariation'} primary url={btnUrl}>
-          {btnTitle}
-        </Button>
+        <div className="column">
+          <Button alternate={!startsWith(variation, 'buttonOnly')} primary url={btnUrl}>
+            {btnTitle}
+          </Button>
+        </div>
       </div>
     </div>
   )
