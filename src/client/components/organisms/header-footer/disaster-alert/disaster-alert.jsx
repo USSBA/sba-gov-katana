6e0562fbe2e3e07afe23c3bbe67d7a52212e03bc
Kevin Lee <KevL927@users.noreply.github.com>
2018-05-14 11:30:39 -0400

    T1 561 dev (#551)
    * T1-561 Change number and underline color to sba-red
    
    * T1-561 Show Content only when course worksheet is available
    
    * T1-561 Update style for disaster alert
    
    * T1-561 Lower z-index number
    
    * T1-561 Update font color and bulletpoint in OL and UL

--- src/client/components/organisms/header-footer/disaster-alert/disaster-alert.jsx
+++ src/client/components/organisms/header-footer/disaster-alert/disaster-alert.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
 import styles from './disaster-alert.scss'
 import exitIcon from '../../../../../../public/assets/svg/exit-modal-close.svg'
@@ -7,29 +7,37 @@ import { Link } from 'atoms'
 const DisasterAlert = props => {
   const { buttonText, description, link, onClose, visible } = props
 
-  if (visible) {
-    return (
-      <div className={styles.wrapper} id="disaster-alert">
-        <div className={styles.alert}>
-          <div className={styles.alertIcon + ' fa fa-exclamation-triangle'} aria-hidden="true" />
-          <div className={styles.disasterDescription}>{description}</div>
-          <img className={styles.alertClose} onClick={onClose} src={exitIcon} alt="Close" />
-          <Link className={styles.alertButton} to={link}>
-            {buttonText}
-          </Link>
-        </div>
-      </div>
-    )
-  } else {
+  if (!visible) {
     return null
   }
+
+  const icon = <i className="fa fa-exclamation-triangle" aria-hidden="true" />
+
+  return (
+    <div className={styles.alert} id="disaster-alert">
+      <div className={styles.column}>{icon}</div>
+      <div className={styles.description}>
+        <h6>
+          {icon}
+          {description}
+          <Link className={styles.button} to={link}>
+            {buttonText}
+          </Link>
+        </h6>
+      </div>
+      <div className={styles.column}>
+        <img className={styles.close} onClick={onClose} src={exitIcon} alt="Close" />
+      </div>
+    </div>
+  )
 }
 
 DisasterAlert.defaultProps = {
-  visible: false,
-  link: '',
-  buttonText: '',
-  description: ''
+  visible: false
+}
+
+DisasterAlert.propTypes = {
+  visible: PropTypes.bool.isRequired
 }
 
 export default DisasterAlert
