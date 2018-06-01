57f84602a3469abead16f7edd6b48781f50aedf3
Avery Smith <avery@averysmith.pro>
2018-06-01 17:30:49 -0400

    TA-560 stubbed unit test added

---
+++ test/jest/client/components/pages/office-lookup-page/office-lookup-page.test.js
@@ -0,0 +1,25 @@
+import react from 'react'
+import renderer from 'react-test-renderer'
+import { shallow, mount } from 'enzyme'
+
+describe('OfficeLookupPage', () => {
+  it('should detect map object on page', () => {
+    // unit test here
+  })
+
+  it('should not detect map on tablet breakpoint', () => {
+    // unit test here
+  })
+
+  it('should not detect map on mobile breakpoint', () => {
+    // unit test here
+  })
+
+  it('should center map on zipcode location search', () => {
+    // unit test here
+  })
+
+  it('should update result on new search event', () => {
+    // unit test here
+  })
+})
