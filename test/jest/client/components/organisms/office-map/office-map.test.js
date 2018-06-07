b349674d9edc0d5f820ea3b9737b6d18630169d0
Avery Smith <avery@averysmith.pro>
2018-06-07 10:42:35 -0400

    TA-560 added breakpoint to hide on mobile and tablet

--- test/jest/client/components/organisms/office-map/office-map.test.js
+++ test/jest/client/components/organisms/office-map/office-map.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme'
 import OfficeMap from 'organisms/office-map/office-map.jsx'
 
 describe('OfficeMap', () => {
-  it.only('should instantiate google map object', () => {
+  it('should instantiate google map object', () => {
     const component = shallow(<OfficeMap />)
     expect(component.find('#google-map').length).toEqual(1)
   })
