a556bc3ca977ec48ec85b5bd7689aca56ce45712
Avery Smith <avery@averysmith.pro>
2018-06-08 19:36:28 -0400

    TA-560 added query paramter functionality to map

--- src/client/components/organisms/office-map/google-map.jsx
+++ src/client/components/organisms/office-map/google-map.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
-import { compose, withProps } from 'recompose'
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import { GoogleMap, Marker } from 'react-google-maps'
 
-const MyMapComponent = compose(
-  withProps({
-    /**
-     * Note: create and replace your own key in the Google console.
-     * https://console.developers.google.com/apis/dashboard
-     * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
-     */
-    googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyDceQl00lHHE0R9QrY5gonqr4Bu2Q9vLe8&v=3.exp&libraries=geometry,drawing,places',
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100vh` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+const OfficeMap = props => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{
+      lat: -34.397,
+      lng: 150.644
+    }}
+  >
+    {props.isMarkerShown && (
+      <Marker
+        position={{
+          lat: -34.397,
+          lng: 150.644
+        }}
+      />
+    )}
+    sfsf
   </GoogleMap>
-))
+)
 
-export default MyMapComponent
+export default OfficeMap
--- src/client/components/organisms/office-map/office-map.jsx
+++ src/client/components/organisms/office-map/office-map.jsx
@@ -1,15 +1,160 @@
 import React from 'react'
-import GoogleMap from './google-map.jsx'
+import { compose, withProps } from 'recompose'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import axios from 'axios'
+import queryString from 'query-string'
 import styles from './office-map.scss'
+var geocoder = require('google-geocoder')
 
-class OfficeMap extends React.PureComponent {
+const GOOGLE_MAPS_API_KEY = 'AIzaSyDceQl00lHHE0R9QrY5gonqr4Bu2Q9vLe8'
+const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`
+const geo = geocoder({
+  key: GOOGLE_MAPS_API_KEY
+})
+
+const OfficeMap = compose(
+  withProps({
+    googleMapURL,
+    loadingElement: <div style={{ height: `100vh` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
+    mapElement: <div style={{ height: `100vh` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => {
+  const { markers } = props
+  let googleMapProps = {}
+  if (markers.length > 0) {
+    googleMapProps.ref = props.onMapMounted
+    googleMapProps.defaultZoom = 4
+
+    const bounds = new google.maps.LatLngBounds()
+    for (let i = 0; i < markers.length; i++) {
+      bounds.extend(markers[i])
+    }
+    props.setBounds(bounds)
+    googleMapProps.defaultCenter = bounds.getCenter()
+  } else {
+    // default to zipcode 20001
+    googleMapProps.defaultZoom = 10
+    googleMapProps.defaultCenter = {
+      lat: 38.910353,
+      lng: -77.017739
+    }
+  }
+
+  return (
+    <GoogleMap {...googleMapProps}>
+      {markers.map((item, index) => (
+        <Marker
+          key={index}
+          position={{
+            lat: item.lat,
+            lng: item.lng
+          }}
+        />
+      ))}
+    </GoogleMap>
+  )
+})
+
+class OfficeMapApp extends React.PureComponent {
+  constructor() {
+    super()
+
+    this.state = {
+      points: [],
+      bounds: {}
+    }
+  }
+
+  componentWillMount() {
+    // get office data from endpoint
+    // update state points object with results
+
+    let endpoint = '/api/content/offices.json'
+    if (window.location.search.length > 0) {
+      const params = queryString.parse(window.location.search)
+      let qs = ''
+      if (params.zipCode) {
+        qs += `zipCode=${params.zipCode}&`
+      }
+      if (params.start && typeof params.start === 'number') {
+        qs += `start=${params.start}`
+      }
+      endpoint = `${endpoint}?${qs}`
+    }
+
+    axios.get(endpoint).then(result => {
+      this.getLatLngs(result.data.hit).then(result => {
+        const points = result
+        this.setState({ points })
+      })
+    })
+  }
+
+  getLatLngs(items) {
+    let geolocations = []
+    items.forEach(item => {
+      // if geolocation data available
+      // set geolocation
+      // else
+      // query geocoder service for geolocation
+
+      let promise
+      if (item.fields.geolocation) {
+        const latLng = item.fields.geolocation[0].split(',')
+        promise = Promise.resolve({
+          lat: Number(latLng[0]),
+          lng: Number(latLng[1])
+        })
+      } else {
+        promise = new Promise((resolve, reject) => {
+          const address = `${item.fields.location_street_address} ${item.fields.location_city} ${
+            item.fields.location_state
+          } ${item.fields.location_zipcode}`
+          geo.find(address, (err, result) => {
+            resolve({
+              lat: result[0].location.lat,
+              lng: result[0].location.lng
+            })
+          })
+        })
+      }
+
+      geolocations.push(promise)
+    })
+
+    return Promise.all(geolocations)
+  }
+
+  setBounds(bounds) {
+    this.bounds = bounds
+  }
+
+  onMapMounted(mapRef) {
+    // after map has mounted
+    // auto-zoom the map to fit all marker points
+    if (mapRef !== null) {
+      mapRef.fitBounds(this.bounds)
+    }
+  }
+
   render() {
+    // pass array of points with lats and lngs
+
+    const { points } = this.state
+
     return (
       <div id="google-map" className={styles.googleMap}>
-        <GoogleMap isMarkerShown />
+        <OfficeMap
+          markers={this.state.points}
+          setBounds={this.setBounds.bind(this)}
+          onMapMounted={this.onMapMounted.bind(this)}
+        />
       </div>
     )
   }
 }
 
-export default OfficeMap
+export default OfficeMapApp
