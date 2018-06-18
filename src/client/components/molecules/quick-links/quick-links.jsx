98ecbafb7ff765760d10dd9a9d9b3691fdebb687
Kevin Lee <ki.lee@sba.gov>
2018-06-18 19:14:20 -0400

    T1-746 Make QuickLinks component compatible with articles
    T1-746 Set width on decorative dash
    
    T1-746 Add mapStateToProps dynamically
    
    T1-746 Remove comment
    
    T1-746 Add logic to search category

--- src/client/components/molecules/quick-links/quick-links.jsx
+++ src/client/components/molecules/quick-links/quick-links.jsx
@@ -2,7 +2,7 @@ import Promise from 'bluebird'
 import React, { PureComponent } from 'react'
 import moment from 'moment'
 import queryString from 'querystring'
-import _ from 'lodash'
+import { chain, isEmpty } from 'lodash'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
@@ -15,7 +15,7 @@ function getCurrentFile(files) {
   let found = null
 
   if (files) {
-    found = _.chain(files)
+    found = chain(files)
       .filter(item => moment(item.effectiveDate).isSameOrBefore(moment()))
       .sortBy('effectiveDate')
       .last()
@@ -40,9 +40,9 @@ class QuickLinks extends PureComponent {
       if (quickLink.type === 'documentLookup') {
         this.props.actions.fetchContentIfNeeded('documents-' + index, 'documents', {
           sortBy: 'Last Updated',
-          type: _.isEmpty(quickLink.documentType[0]) ? 'all' : quickLink.documentType[0],
-          program: _.isEmpty(quickLink.documentProgram[0]) ? 'all' : quickLink.documentProgram[0],
-          activity: _.isEmpty(quickLink.documentActivity[0]) ? 'all' : quickLink.documentActivity[0],
+          type: isEmpty(quickLink.documentType[0]) ? 'all' : quickLink.documentType[0],
+          program: isEmpty(quickLink.documentProgram[0]) ? 'all' : quickLink.documentProgram[0],
+          activity: isEmpty(quickLink.documentActivity[0]) ? 'all' : quickLink.documentActivity[0],
           start: 0,
           end: 3
         })
@@ -54,8 +54,9 @@ class QuickLinks extends PureComponent {
     this.props.data.typeOfLinks.map((quickLink, index) => {
       if (quickLink.type === 'articleLookup') {
         this.props.actions.fetchContentIfNeeded('articles-' + index, 'articles', {
-          sortBy: 'Created',
-          type: 'all',
+          sortBy: 'Last Updated',
+          articleCategory: isEmpty(quickLink.articleCategory[0]) ? 'all' : quickLink.articleCategory[0],
+          program: isEmpty(quickLink.articleProgram[0]) ? 'all' : quickLink.articleProgram[0],
           start: 0,
           end: 3
         })
@@ -131,7 +132,7 @@ const LatestDocumentsCard = props => {
 
             // Add a prefix to SOPs with the format {DOC_TYPE} {DOC_NUMBER} ({DOC_VERSION}) - {DOC_TITLE}
             let titlePrefix = ''
-            if (doc.documentIdType === 'SOP' && !_.isEmpty(doc.documentIdNumber)) {
+            if (doc.documentIdType === 'SOP' && !isEmpty(doc.documentIdNumber)) {
               titlePrefix = doc.documentIdType + ' ' + doc.documentIdNumber + ' - '
             }
             const linkTitle =
@@ -179,6 +180,7 @@ const ArticlesCard = props => {
         <h4 className={s.title}>{props.sectionHeaderText}</h4>
         <Link
           to={`/article?${queryString.stringify({
+            articleCategory: props.articleCategory,
             program: props.articleProgram
           })}`}
           className={s.seeAll}
@@ -186,7 +188,7 @@ const ArticlesCard = props => {
           See all
         </Link>
       </div>
-      <DecorativeDash />
+      <DecorativeDash width={1.667} />
       <div>
         {articles && articles.items.length ? (
           <div>
@@ -258,12 +260,15 @@ QuickLinks.defaultProps = {
   }
 }
 
-function mapReduxStateToProps(reduxState) {
-  return {
-    'documents-0': reduxState.contentReducer['documents-0'],
-    'documents-1': reduxState.contentReducer['documents-1'],
-    'articles-2': reduxState.contentReducer['articles-2']
-  }
+function mapReduxStateToProps(reduxState, ownProps) {
+  const documentsAndArticles = {}
+
+  Object.keys(reduxState.contentReducer).forEach(key => {
+    if (key.includes('documents-') || key.includes('articles-')) {
+      documentsAndArticles[key] = reduxState.contentReducer[key]
+    }
+  })
+  return documentsAndArticles
 }
 
 function mapDispatchToProps(dispatch) {
