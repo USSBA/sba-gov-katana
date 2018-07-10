7fa79261582539d69bdabaf8ff488d2c172c482e
Laura Semesky <lsemesky@gmail.com>
2018-07-10 15:51:23 -0400

    TA-681 integrating panel

--- src/client/components/organisms/results/results.jsx
+++ src/client/components/organisms/results/results.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from './results.scss'
 import { Paginator } from 'molecules'
+import { SearchInfoPanel } from 'atoms'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import Search from '../../atoms/icons/search'
 
 class Results extends React.PureComponent {
   super() {
@@ -12,7 +15,7 @@ class Results extends React.PureComponent {
     return <div>{children}</div>
   }
 
-  renderPaginator(children) {
+  renderPaginator() {
     const { total, pageSize, pageNumber, onBack, onForward } = this.props
 
     return (
@@ -28,8 +31,23 @@ class Results extends React.PureComponent {
     )
   }
 
+  renderSearchInfoPanel() {
+    const { total, pageSize, pageNumber, searchTermName, submittedFieldValues } = this.props
+    const searchTerm = submittedFieldValues[searchTermName]
+    return (
+      <div className={styles.searchInfoPanel}>
+        <SearchInfoPanel
+          pageNumber={pageNumber}
+          pageSize={pageSize}
+          total={total}
+          searchTerm={searchTerm}
+        />
+      </div>
+    )
+  }
+
   render() {
-    const { children, id, resultId, paginate, scroll, extraClassName } = this.props
+    const { children, id, resultId, paginate, scroll, extraClassName, hasSearchInfoPanel } = this.props
     const childrenWithProps = this.props.items.map((item, index) => {
       const mappedChildren = React.Children.map(children, child => {
         return React.cloneElement(child, {
@@ -42,14 +60,15 @@ class Results extends React.PureComponent {
 
     const className = extraClassName ? `${styles.container} ${extraClassName}` : styles.container
 
-    const divProps = {}
-    if (scroll) {
-      divProps.className = styles.scroll
-    }
+    const divClassName = classNames({
+      [styles.scroll]: scroll,
+      [styles.searchInfoPanelPadding]: hasSearchInfoPanel
+    })
 
     return (
       <div id={id} className={className}>
-        <div {...divProps}>{childrenWithProps}</div>
+        {hasSearchInfoPanel && this.renderSearchInfoPanel()}
+        <div className={divClassName}>{childrenWithProps}</div>
         {paginate && this.renderPaginator()}
       </div>
     )
@@ -61,7 +80,9 @@ Results.defaultProps = {
   id: null,
   resultId: 'result',
   paginate: false,
-  scroll: false
+  scroll: false,
+  hasSearchInfoPanel: false,
+  searchTermName: ''
 }
 
 Results.propTypes = {
@@ -69,7 +90,9 @@ Results.propTypes = {
   id: PropTypes.string, //.isRequired,
   resultId: PropTypes.string,
   paginate: PropTypes.bool,
-  scroll: PropTypes.bool
+  scroll: PropTypes.bool,
+  hasSearchInfoPanel: PropTypes.bool,
+  searchTermName: PropTypes.string
 }
 
 export default Results
