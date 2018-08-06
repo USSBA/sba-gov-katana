7071acaa475f6d60d1657d5e01a2e4b57cb95995
Avery Smith <avery@averysmith.pro>
2018-08-06 15:57:52 -0400

    TA-563 added detail panel and basic formatting

--- src/client/components/organisms/results/results.jsx
+++ src/client/components/organisms/results/results.jsx
@@ -13,6 +13,14 @@ class Results extends React.PureComponent {
     this.renderPaginator = this.renderPaginator.bind(this)
   }
 
+  constructor() {
+    super()
+
+    this.state = {
+      selectedItem: {}
+    }
+  }
+
   renderDefaultView(children) {
     return <div>{children}</div>
   }
