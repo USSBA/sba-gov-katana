9e9e56c284318eafa156b4bc78fa0899f94fcad9
Laura Semesky <lsemesky@gmail.com>
2018-08-21 13:34:39 -0400

    TA-680 Addressed feedback and added additional test case

--- test/jest/client/components/atoms/style-wrapper-div/style-wrapper-div.test.js
+++ test/jest/client/components/atoms/style-wrapper-div/style-wrapper-div.test.js
@@ -10,6 +10,8 @@ describe('Style Wrapper Div', () => {
       prop2: 'prop2',
       prop3: 'prop3'
     }
+    // note: these could be any React components (not native html) but to
+    // limit component inter-dependencies chose to reuse this component
     const child1 = <StyleWrapperDiv />
     const child2 = <StyleWrapperDiv />
     const child3 = <StyleWrapperDiv />
@@ -26,9 +28,56 @@ describe('Style Wrapper Div', () => {
     expect(componentWrapper.childAt(1).props()).toEqual(props)
     expect(componentWrapper.childAt(2).props()).toEqual(props)
   })
-  it('Contains a div with the passed in style', () => {
+  it('Contains a div with the passed in class name', () => {
     const className = 'style'
     const componentWrapper = shallow(<StyleWrapperDiv className={className} />)
     expect(componentWrapper.find('div').prop('className')).toBe(className)
+  })
+  it('Does not pass the className prop to children', () => {
+    const className = 'style'
+
+    // note: these could be any React components (not native html) but to
+    // limit component inter-dependencies chose to reuse this component
+    const child1 = <StyleWrapperDiv />
+    const child2 = <StyleWrapperDiv />
+    const child3 = <StyleWrapperDiv />
+
+    const componentWrapper = mount(
+      <StyleWrapperDiv className={className}>
+        {child1}
+        {child2}
+        {child3}
+      </StyleWrapperDiv>
+    )
+  })
+  it('Does not pass the className prop to children but passes all other props', () => {
+    const className = 'style'
+    const props = {
+      prop1: 'prop1',
+      prop2: 'prop2',
+      prop3: 'prop3'
+    }
+    // note: these could be any React components (not native html) but to
+    // limit component inter-dependencies chose to reuse this component
+    const child1 = <StyleWrapperDiv />
+    const child2 = <StyleWrapperDiv />
+    const child3 = <StyleWrapperDiv />
+
+    const componentWrapper = mount(
+      <StyleWrapperDiv {...props} className={className}>
+        {child1}
+        {child2}
+        {child3}
+      </StyleWrapperDiv>
+    )
+    // check that className wasn't passed to children
+    expect(componentWrapper.childAt(0).prop('className')).toBeUndefined()
+    expect(componentWrapper.childAt(1).prop('className')).toBeUndefined()
+    expect(componentWrapper.childAt(2).prop('className')).toBeUndefined()
+
+    // check that all other props are there
+    expect(componentWrapper.childAt(0).props()).toEqual(props)
+    expect(componentWrapper.childAt(1).props()).toEqual(props)
+    expect(componentWrapper.childAt(2).props()).toEqual(props)
   })
 })
