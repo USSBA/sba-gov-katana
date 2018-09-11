f537c4013f5b3d3dc35a4fce253dedb16b607f3e
Kevin Lee <KevL927@users.noreply.github.com>
2018-09-11 15:19:32 -0400

    T1-935 Send user browser language setting as part of headers in a GET… (#683)
    * T1-935 Send user browser language setting as part of headers in a GET request
    
    * T1-935 Comment out sending accept-language as part of headers
    
    * T1-935 Refactor sending in headers
    
    * T1-935 Add console log for browser language setting
    
    * T1-935 Add console.log for header
    
    * T1-935 Add console.log for preferredLang
    
    * T1-935 Add another console.log to inspect request
    
    * T1-935 Remove console.logs
    
    * T1-935 Add console.log to check headers
    
    * T1-935 console.log preferredLang
    
    * T1-935 Add console.log for headers
    
    * T1-935 Update console.log to check all headers
    
    * T1-935 Remove console.log
    
    * T1-935 Remove if statement

--- src/controllers/content.js
+++ src/controllers/content.js
@@ -8,10 +8,12 @@ function fetchContentById(req, res) {
   if (req.params && req.params.type && req.params.id) {
     const type = req.params.type
     const id = req.params.id
+    const userPreferredLanguage = req.headers['accept-language']
     const uri =
       'https://' + path.join(config.get('content.endpoint'), 'Prod', 'api', 'content', type, id + '.json')
     axios
       .get(uri, {
+        headers: { 'Accept-Language': userPreferredLanguage },
         params: req.query,
         responseType: 'json',
         // eslint-disable-next-line arrow-body-style
