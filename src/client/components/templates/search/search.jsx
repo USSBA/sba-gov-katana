8fd736ce445b3f3928b87f750dedcd363406984f
Aaron Brooks <brooksa321@gmail.com>
2018-09-18 14:53:05 -0400

    removed unnecessary console log

--- src/client/components/templates/search/search.jsx
+++ src/client/components/templates/search/search.jsx
@@ -63,7 +63,9 @@ class SearchTemplate extends React.PureComponent {
     delete queryParams.pageSize
     //calculate the actual  start value from the pageNumber parameter
     let { pageNumber } = queryParams
-    const { searchParams: { pageSize } } = this.state
+    const {
+      searchParams: { pageSize }
+    } = this.state
     pageNumber = isNaN(parseInt(pageNumber, 10)) ? 1 : parseInt(pageNumber, 10)
     //not using the calculate startIndex function here because we won't know the count yet
     queryParams.start = Math.max(0, (pageNumber - 1) * pageSize)
@@ -188,7 +190,10 @@ class SearchTemplate extends React.PureComponent {
 
   renderPaginator() {
     const { count } = this.props
-    const { results, searchParams: { pageSize } } = this.state
+    const {
+      results,
+      searchParams: { pageSize }
+    } = this.state
     const pageNumber = this.calculatePageNumber()
 
     let result = <div />
@@ -261,8 +266,10 @@ class SearchTemplate extends React.PureComponent {
   }
 
   render() {
-    const { results, searchParams: { pageSize } } = this.state
-    console.log('***************', this.state.results)
+    const {
+      results,
+      searchParams: { pageSize }
+    } = this.state
     const { children, count, extraClassName, paginate, isZeroState } = this.props
 
     const childrenWithProps = React.Children.map(children, child => {
@@ -351,5 +358,8 @@ function mapDispatchToProps(dispatch) {
     actions: bindActionCreators(ContentActions, dispatch)
   }
 }
-export default connect(mapReduxStateToProps, mapDispatchToProps)(SearchTemplate)
+export default connect(
+  mapReduxStateToProps,
+  mapDispatchToProps
+)(SearchTemplate)
 export { SearchTemplate }
