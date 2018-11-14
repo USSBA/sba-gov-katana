f995ea1632361d369e222948fbb38294b312338c
Kevin Lee <ki.lee@sba.gov>
2018-11-14 15:41:52 -0500

    T1-1034 Move translations to external object. Make functions more stateless

--- src/client/components/organisms/section-nav/section-nav.jsx
+++ src/client/components/organisms/section-nav/section-nav.jsx
@@ -7,6 +7,7 @@ import { bindActionCreators } from 'redux'
 import constants from '../../../services/constants.js'
 import hurricaneIcon from 'assets/images/funding-programs/Funding_Programs_Icon_Disaster_white.png'
 import styles from './section-nav.scss'
+import { TRANSLATIONS } from '../../../translations.js'
 import * as ModalActions from '../../../actions/show-modal.js'
 import { determineMenuTileData, getLanguageOverride } from '../../../services/utils.js'
 import { Link } from 'atoms'
@@ -20,32 +21,37 @@ class SectionNav extends React.Component {
       if (sectionTitle === titleConstants.disasterAssistance) {
         sectionNavIcon = hurricaneIcon
       }
-      this.props.actions.showMobileSectionNav(this.getNthLineage(-2), sectionNavIcon, this.getBacklinkUrl())
+      this.props.actions.showMobileSectionNav(
+        this.getNthLineage(-2),
+        sectionNavIcon,
+        this.getBacklinkUrl(this.getNthLineage(0), this.getNthLineage(-2), langCode)
+      )
     }
   }
 
-  isBusinessGuide() {
-    return (
-      this.getNthLineage(0).fullUrl === '/business-guide' ||
-      this.getNthLineage(0).fullUrl === '/guia-de-negocios'
-    )
+  isBusinessGuide(parentSiteMap, langCode) {
+    let result
+    if (langCode === 'es') {
+      result = parentSiteMap.fullUrl === '/guia-de-negocios'
+    } else {
+      result = parentSiteMap.fullUrl === '/business-guide'
+    }
+    return result
   }
 
   getNthLineage(n) {
     return _.nth(this.props.lineage, n)
   }
 
-  makeNavLinks(langCode) {
-    const section = this.getNthLineage(-2)
-    const currentPage = this.getNthLineage(-1)
-    const navLinks = section.children.map(function(item, index) {
+  makeNavLinks(sectionSiteMap, currentPageSiteMap, langCode) {
+    const navLinks = sectionSiteMap.children.map(function(item, index) {
       const titleLinkData = determineMenuTileData(langCode, item)
       let currentLinkClass = ''
       if (
-        titleLinkData.fullUrl === currentPage.fullUrl ||
+        titleLinkData.fullUrl === currentPageSiteMap.fullUrl ||
         (langCode === 'es' &&
-          currentPage.spanishTranslation &&
-          titleLinkData.fullUrl === currentPage.spanishTranslation.fullUrl)
+          currentPageSiteMap.spanishTranslation &&
+          titleLinkData.fullUrl === currentPageSiteMap.spanishTranslation.fullUrl)
       ) {
         currentLinkClass = styles.currentNavLink
       }
@@ -64,8 +70,7 @@ class SectionNav extends React.Component {
     return navLinks
   }
 
-  makeNavigationTitle(langCode) {
-    const titleSiteMap = this.getNthLineage(-2)
+  makeNavigationTitle(titleSiteMap, langCode) {
     let sectionTitle
     titleSiteMap.spanishTranslation && langCode === 'es'
       ? (sectionTitle = titleSiteMap.spanishTranslation.title)
@@ -86,40 +91,46 @@ class SectionNav extends React.Component {
     return this.props.position === 'bottom' ? styles.stickyBottom : null
   }
 
-  getBacklinkUrl(langCode) {
-    const businessGuideSiteMap = this.getNthLineage(0)
-    const parentSiteMap = this.getNthLineage(-2)
+  getBacklinkUrl(businessGuideSiteMap, parentSiteMap, langCode) {
     let backLink
     if (businessGuideSiteMap.spanishTranslation && parentSiteMap.spanishTranslation && langCode === 'es') {
-      backLink = (this.isBusinessGuide()
+      backLink = (this.isBusinessGuide(businessGuideSiteMap, langCode)
         ? businessGuideSiteMap.spanishTranslation
         : parentSiteMap.spanishTranslation
       ).fullUrl
     } else {
-      backLink = (this.isBusinessGuide() ? businessGuideSiteMap : parentSiteMap).fullUrl
+      backLink = (this.isBusinessGuide(businessGuideSiteMap, langCode)
+        ? businessGuideSiteMap
+        : parentSiteMap
+      ).fullUrl
     }
     return backLink
   }
 
   getBacklinkText(langCode) {
+    const { allTopics, backTo } = TRANSLATIONS
     const titleSiteMap = this.getNthLineage(-2)
-    let backTo
+    let backToText
     let backText
 
     if (titleSiteMap.spanishTranslation && langCode === 'es') {
-      backTo = 'Regresar a'
-      backText = this.isBusinessGuide() ? 'todos los temas' : titleSiteMap.spanishTranslation.title
+      backToText = backTo.es.text
+      backText = this.isBusinessGuide(this.getNthLineage(0), langCode)
+        ? allTopics.es.text
+        : titleSiteMap.spanishTranslation.title
     } else {
-      backTo = 'Back to'
-      backText = this.isBusinessGuide() ? 'all topics' : titleSiteMap.title
+      backToText = backTo.en.text
+      backText = this.isBusinessGuide(this.getNthLineage(0), langCode)
+        ? allTopics.en.text
+        : titleSiteMap.title
     }
-    return `${backTo} ${backText}`
+    return `${backToText} ${backText}`
   }
 
   render() {
     const langCode = getLanguageOverride()
-    const navLinks = this.makeNavLinks(langCode)
-    const navigationTitle = this.makeNavigationTitle(langCode)
+    const navLinks = this.makeNavLinks(this.getNthLineage(-2), this.getNthLineage(-1), langCode)
+    const navigationTitle = this.makeNavigationTitle(this.getNthLineage(-2), langCode)
     return (
       <div
         id="article-navigation-desktop"
@@ -129,7 +140,7 @@ class SectionNav extends React.Component {
         <Link
           id="article-navigation-back-button-desktop"
           className={styles.backLink}
-          to={this.getBacklinkUrl(langCode)}
+          to={this.getBacklinkUrl(this.getNthLineage(0), this.getNthLineage(-2), langCode)}
         >
           {this.getBacklinkText(langCode)}
         </Link>
