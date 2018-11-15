00026d5410ab85fc376930ce45e92e1db2bc5abd
Kevin Lee <KevL927@users.noreply.github.com>
2018-11-15 17:33:42 -0500

    T1 1052 update mobile nav (#746)
    * T1-1034 Fix title on section nav
    
    * T1-954 Fix translation for Back To link and text
    
    * T1-1034 Move translations to external object. Make functions more stateless
    
    * T1-1034 Refactor function to DRY and easier readability
    
    * T1-1052 Update snapshots for test
    
    * T1-1052 Determine language and render correct text
    
    * T1-1052 Add Spanish language for Disaster Assistance
    
    * T1-1052 rebase from T1-1034 branch
    
    * T1-1052 Remove map
    DispatchToProps
    
    * T1-1052 Remove commented out lines
    
    * T1-1052 Support multiple languages for Back to all topics link on Basic Page template
    
    * T1-1052 Fix text for English

--- src/client/components/templates/basic-page/basic-page.jsx
+++ src/client/components/templates/basic-page/basic-page.jsx
@@ -8,6 +8,7 @@ import * as paragraphMapper from '../paragraph-mapper.jsx'
 import { Breadcrumb, FeedbackForm, PreviousNextSection, RemoveMainLoader, TitleSection } from 'molecules'
 import { SectionNav } from 'organisms'
 import { getLanguageOverride } from '../../../services/utils.js'
+import { TRANSLATIONS } from '../../../translations.js'
 
 class BasicPage extends React.Component {
   constructor(props) {
@@ -129,6 +130,9 @@ class BasicPage extends React.Component {
     )
 
     const langCode = getLanguageOverride()
+    const { allTopics, backTo } = TRANSLATIONS
+    const backToText = backTo[langCode].text
+    const allTopicsText = allTopics[langCode].text
     return (
       <div className={`basicpage ${styles.articleContainer}`}>
         <RemoveMainLoader />
@@ -145,7 +149,7 @@ class BasicPage extends React.Component {
         >
           <div className={`basicpage-backlinkmobile ${styles.backLinkMobile}`}>
             <a id="backToallTopicsMobile" href="" onClick={this.handleBackLinkClicked}>
-              Back to all topics
+              {`${backToText} ${allTopicsText}`}
             </a>
           </div>
           <div key={1} className={`basicpage-breadcrumb ${styles.breadcrumb}`}>
