a4b34377d248c8fecc947db00764b3fac88e5c00
Brian Le <blsymph@gmail.com>
2018-11-16 17:07:51 -0500

    T1-00 Hotfix breadcrumbs for Spanish pages

--- src/client/components/molecules/breadcrumb/breadcrumb.jsx
+++ src/client/components/molecules/breadcrumb/breadcrumb.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { last, take } from 'lodash'
+import { last, size, take } from 'lodash'
 
 import styles from './breadcrumb.scss'
 import { Link, SmallIcon } from 'atoms'
 import { getLanguageOverride } from '../../../services/utils.js'
 
 class Breadcrumb extends React.Component {
-  makeLastAnchor(tail, getLanguage) {
-    let title = tail.title
-    let url = tail.url
+  makeLastAnchor(tail, languageOverride) {
+    let { title, url } = tail
+    const { spanishTranslation } = tail
 
-    if (tail && getLanguage === 'es') {
-      title = tail.spanishTranslation.title
-      url = tail.spanishTranslation.url
+    if (tail && spanishTranslation && languageOverride === 'es') {
+      title = spanishTranslation.title
+      url = spanishTranslation.url
     }
 
     return (
@@ -25,24 +25,28 @@ class Breadcrumb extends React.Component {
   }
 
   render() {
-    const getLanguage = getLanguageOverride()
-    if (this.props.items && this.props.items.length > 0) {
-      const tail = last(this.props.items)
-      const tailAnchor = this.makeLastAnchor(tail, getLanguage)
-      const rest = take(this.props.items, this.props.items.length - 1)
+    const { items } = this.props
+    const languageOverride = getLanguageOverride()
+
+    if (size(items)) {
+      const tail = last(items)
+      const tailAnchor = this.makeLastAnchor(tail, languageOverride)
+      const rest = take(items, items.length - 1)
+
       return (
         <div className={styles.breadcrumb}>
           <SmallIcon fontAwesomeIconClassName="home" onClick={() => {}} />
           <span className={styles.slash}>/</span>
           {rest ? (
             rest.map((item, index) => {
-              let title = item.title
-              let url = item.url
+              let { title, url } = item
+              const { spanishTranslation } = item
 
-              if (item.spanishTranslation && getLanguage === 'es') {
-                title = item.spanishTranslation.title
-                url = item.spanishTranslation.url
+              if (spanishTranslation && languageOverride === 'es') {
+                title = spanishTranslation.title
+                url = spanishTranslation.url
               }
+
               return [
                 <Link id={'breadcrumb-level' + index} to={url}>
                   {title}
