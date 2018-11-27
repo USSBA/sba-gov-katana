01809f1b00adbfd414eaed9c0ca802c8c7994763
Avery Smith <avery@averysmith.pro>
2018-11-27 16:45:45 -0500

    TA-556 extirpate redux from components (#758)
    * TA-556 extirpated redux from quick-links
    
    * TA-556 fixed tests and async calls for fetchSiteContent
    
    * TA-556 extirpated redux from the global-search component
    
    * TA-556 updated global-search test
    
    * TA-556 removed explicit GlobalSearch pointer export declaration

--- test/jest/client/components/organisms/global-search/global-search.test.jsx
+++ test/jest/client/components/organisms/global-search/global-search.test.jsx
@@ -3,19 +3,16 @@ import renderer from 'react-test-renderer'
 import _ from 'lodash'
 import { shallow } from 'enzyme'
 
-import { GlobalSearch } from 'organisms/global-search/global-search'
+import GlobalSearch from 'organisms/global-search/global-search'
+
+import mockGetContentData from '../../test-data/global-search-get-content-data.json'
 
 jest.mock('../../../../../../src/client/services/utils.js')
-
-const fetchContentIfNeeded = jest.fn()
 
 const testPropsOne = {
   title: 'My Courses',
   type: 'courses',
   taxonomyFilters: ['businessStage'],
-  actions: {
-    fetchContentIfNeeded: fetchContentIfNeeded
-  },
   location: {
     query: {
       topic: 'All'
@@ -27,9 +24,6 @@ const testPropsTwo = {
   title: 'My Courses',
   type: 'courses',
   taxonomyFilters: ['businessStage', 'program'],
-  actions: {
-    fetchContentIfNeeded: fetchContentIfNeeded
-  },
   location: {
     query: {
       topic: 'Starting a business'
@@ -37,7 +31,28 @@ const testPropsTwo = {
   }
 }
 
-describe('GlobalSearch', () => {
+describe('GlobalSearch', () => {  
+  var mockGetTaxonomies, mockGetContent
+  beforeEach(() => {
+    mockGetTaxonomies = jest.spyOn(GlobalSearch.prototype, 'getTaxonomies')
+    mockGetContent = jest.spyOn(GlobalSearch.prototype, 'getContent')
+    const mockGetTaxonomiesData = [{
+      name: 'businessStage',
+      terms: [
+        'Plan your business',
+        'Launch your business',
+        'Manage your business',
+        'Grow your business' ]
+      }]
+    mockGetTaxonomies.mockReturnValue(mockGetTaxonomiesData)
+    mockGetContent.mockReturnValue(mockGetContentData)
+  })
+
+  afterEach(() => {
+    mockGetTaxonomies.mockRestore()
+    mockGetContent.mockRestore()
+  })
+
   test('should render with one taxonomyFilter', () => {
     const props = _.clone(testPropsOne)
     const component = renderer.create(<GlobalSearch {...props} />)
@@ -49,6 +64,7 @@ describe('GlobalSearch', () => {
     const props = _.clone(testPropsOne)
     const component = renderer.create(<GlobalSearch {...props} />)
     const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
   })
 
   test('should render with two taxonomyFilter', () => {
