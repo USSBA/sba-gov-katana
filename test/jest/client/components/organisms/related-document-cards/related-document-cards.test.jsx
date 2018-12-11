fa4679309f01d78897ff11e6b4f132d907e60dde
amyd11 <40372898+amyd11@users.noreply.github.com>
2018-12-11 15:05:35 -0500

    TA-1284 Refactor related document cards to remove redux (#772)
    * refactor related document cards to remove redux
    
    * cleaned up code, fix typo
    
    * refactor/add testing using mock funtion
    
    * add more tests, clean up existing tests
    
    * fix to remove document data which has been deleted
    
    * change reference of state variable to destructured syntax
    
    * TA-1284 move functionality from locationChange function in navigation.js into related-document-cards component

--- test/jest/client/components/organisms/related-document-cards/related-document-cards.test.jsx
+++ test/jest/client/components/organisms/related-document-cards/related-document-cards.test.jsx
@@ -1,32 +1,115 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
-import sinon from 'sinon'
 import { shallow } from 'enzyme'
+import { RelatedDocumentCards, DetailCardCollection } from 'organisms'
+import * as helper from 'client/fetch-content-helper'
 
-import { RelatedDocumentCards } from 'client/components/organisms/related-document-cards/related-document-cards'
+// TODO: write test(s) for sortRelatedDocuments function
 
-const doc = relatedDocuments => ({
+// extend/modify this document data object for other testing
+const data = JSON.stringify({
   documentIdType: 'SBA form',
-  relatedDocuments,
+  relatedDocuments: [],
   summary: 'This is a test document.',
   type: 'document',
   title: 'test document',
-  id: 9999,
+  id: 1111,
   updated: 1515091190,
   created: 1515091172,
-  url: 'sba-form-object-object-test-document'
+  url: 'sba-form-test-document'
 })
 
-const jsx = (
-  <RelatedDocumentCards data={doc([333, 444])} fetchContentIfNeeded={_ => Promise.resolve({ data: doc })} />
-)
+describe('RelatedDocumentCards', () => {
+  let mockFetchSiteContent
 
-describe('related document cards', () => {
-  test('renders correctly', () => {
-    const tree = renderer.create(jsx).toJSON()
-    expect(tree).toMatchSnapshot()
+  beforeEach(() => {
+    mockFetchSiteContent = jest.spyOn(helper, 'fetchSiteContent')
+  })
+  afterEach(() => {
+    mockFetchSiteContent.mockRestore()
   })
 
-  // test('filters out deleted related documents after mounting', () => {
-  // })
+  test('should not render DetailDocumentCards component when there are no related documents', () => {
+    let customData = JSON.parse(data)
+    mockFetchSiteContent.mockReturnValue('')
+
+    const component = shallow(<RelatedDocumentCards data={customData} />)
+    setImmediate(() => {
+      expect(component.find(DetailCardCollection).length).toEqual(0)
+    })
+  })
+
+  test('should render DetailDocumentCards component when there are related documents', () => {
+    let customData = JSON.parse(data)
+    customData.relatedDocuments.push(2222)
+
+    mockFetchSiteContent.mockReturnValue(customData)
+
+    const component = shallow(<RelatedDocumentCards data={customData} />)
+    setImmediate(() => {
+      expect(component.find(DetailCardCollection).length).toEqual(1)
+    })
+  })
+
+  test('should call fetchSiteContent() for each related document', () => {
+    let customData = JSON.parse(data)
+    customData.relatedDocuments.push(2222, 3333)
+
+    mockFetchSiteContent.mockReturnValue(customData)
+
+    shallow(<RelatedDocumentCards data={customData} />)
+    setImmediate(() => {
+      expect(mockFetchSiteContent.mock.calls.length).toEqual(customData.relatedDocuments.length)
+    })
+  })
+
+  test('should update state with the related document data', () => {
+    const firstRelatedDocumentData = JSON.parse(data)
+    firstRelatedDocumentData.documentIdType = 'Alternative form'
+    firstRelatedDocumentData.id = 2222
+
+    const secondRelatedDocumentData = JSON.parse(data)
+    secondRelatedDocumentData.documentIdType = 'Random form'
+    secondRelatedDocumentData.id = 3333
+
+    const mainDocumentData = JSON.parse(data)
+    mainDocumentData.relatedDocuments.push(2222, 3333)
+
+    mockFetchSiteContent.mockReturnValueOnce(firstRelatedDocumentData)
+    mockFetchSiteContent.mockReturnValueOnce(secondRelatedDocumentData)
+
+    const mockSortedDocumentsInState = {
+      'Alternative form': [
+        {
+          created: 1515091172,
+          documentIdType: 'Alternative form',
+          id: 2222,
+          relatedDocuments: [],
+          summary: 'This is a test document.',
+          title: 'test document',
+          type: 'document',
+          updated: 1515091190,
+          url: 'sba-form-test-document'
+        }
+      ],
+      'Random form': [
+        {
+          created: 1515091172,
+          documentIdType: 'Random form',
+          id: 3333,
+          relatedDocuments: [],
+          summary: 'This is a test document.',
+          title: 'test document',
+          type: 'document',
+          updated: 1515091190,
+          url: 'sba-form-test-document'
+        }
+      ]
+    }
+
+    const component = shallow(<RelatedDocumentCards data={mainDocumentData} />)
+    setImmediate(() => {
+      const updatedState = component.state('sortedDocuments')
+      expect(updatedState).toMatchObject(mockSortedDocumentsInState)
+    })
+  })
 })
