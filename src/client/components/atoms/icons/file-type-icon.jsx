9d4689ae55330e1d798f65954a70084b53a0c37a
Amy DiPasquale <amyd@Amys-MBP.localdomain>
2019-01-15 15:26:08 -0500

    TA-1400 update comment

--- src/client/components/atoms/icons/file-type-icon.jsx
+++ src/client/components/atoms/icons/file-type-icon.jsx
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react'
 const FileTypeIcon = ({ fileExtension }) => {
   let icon = null
   if (fileExtension === 'pdf') {
-    // data-cy prop allows us to name this for use in cypress testing
+    // data-cy prop allows us to name this for use in cypress testing.
     icon = <i className="fa fa-file-pdf-o" data-cy="pdf icon" />
   }
   return icon
