bf67b3b91c7445719952d2326d49f51abb0f0343
Amy DiPasquale <amyd@Amys-MBP.localdomain>
2019-01-15 14:43:44 -0500

    TA-1400 remove unused style on icon

--- src/client/components/atoms/icons/file-type-icon.jsx
+++ src/client/components/atoms/icons/file-type-icon.jsx
@@ -1,11 +1,10 @@
 import React, { PropTypes } from 'react'
-import s from './icons.scss'
 
 const FileTypeIcon = ({ fileExtension }) => {
   let icon = null
   if (fileExtension === 'pdf') {
     // data-cy prop allows us to name this for use in cypress testing
-    icon = <i className={s.pdf + ' fa fa-file-pdf-o'} data-cy="pdf icon" />
+    icon = <i className="fa fa-file-pdf-o" data-cy="pdf icon" />
   }
   return icon
 }
--- test/jest/client/components/atoms/icons/file-type-icon.test.jsx
+++ test/jest/client/components/atoms/icons/file-type-icon.test.jsx
@@ -4,13 +4,13 @@ import { FileTypeIcon } from 'atoms'
 
 describe('FileTypeIcon', () => {
   test('it should return pdf icon when the file extension is pdf', () => {
-    const pdfHtml = '<i class="pdf fa fa-file-pdf-o" data-cy="pdf icon"></i>'
-    const component = shallow(<FileTypeIcon {...{ fileExtension: 'pdf' }} />)
+    const pdfHtml = '<i class="fa fa-file-pdf-o" data-cy="pdf icon"></i>'
+    const component = shallow(<FileTypeIcon fileExtension="pdf" />)
     expect(component.html()).toBe(pdfHtml)
   })
 
   test('it should return null when there is no icon for the given file extension', () => {
-    const component = shallow(<FileTypeIcon {...{ fileExtension: 'fakeExtension' }} />)
+    const component = shallow(<FileTypeIcon fileExtension="fakeExtension" />)
     expect(component.html()).toBe(null)
   })
 })
