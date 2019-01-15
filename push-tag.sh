7d78ff27e426eae07019cda75057b1724eda9466
Jonathan King <jking@fearless.tech>
2019-01-15 12:40:19 +0000

    TA-1411 updates the push tag script to also accept development integration environment common names

--- push-tag.sh
+++ push-tag.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-# First Parameter should be: mint, demo, int-*
-git tag $1 -f && git push origin $1 -f
+DEVELOPER_OR_ENVIRONMENT=$1
+ENVIRONMENT_NAME=$DEVELOPER_OR_ENVIRONMENT
+
+if [ $DEVELOPER_OR_ENVIRONMENT == "avery" ]; then
+  ENVIRONMENT_NAME="int-as"
+elif [ $DEVELOPER_OR_ENVIRONMENT == "brian"  ]; then
+  ENVIRONMENT_NAME="int-bl"
+elif [ $DEVELOPER_OR_ENVIRONMENT == "kevin"  ]; then
+  ENVIRONMENT_NAME="int-kl"
+elif [ $DEVELOPER_OR_ENVIRONMENT == "ryan"  ]; then
+  ENVIRONMENT_NAME="int-rh"
+fi
+
+echo "Pushing to ${ENVIRONMENT_NAME}"
+
+git tag ${ENVIRONMENT_NAME} -f && git push origin ${ENVIRONMENT_NAME} -f
