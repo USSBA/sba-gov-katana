ce2e98f84705ed2d08c1156d2512b3e097def707
Avery Smith <avery@averysmith.pro>
2019-01-21 14:47:50 -0500

    TA-1398 extirpated extraneous code from Link component

--- src/client/components/atoms/link/link.jsx
+++ src/client/components/atoms/link/link.jsx
@@ -7,7 +7,7 @@ import config from '../../../services/client-config'
 
 const Link = (props, context) => {
   const { router } = context
-  const { children, forceClientRouting, to, ...nativeProps } = props
+  const { children, to, ...nativeProps } = props
 
   if (!to) {
     // Make sure the link is still tabbable.
@@ -35,38 +35,14 @@ const Link = (props, context) => {
   }
 
   const linkElement = <ReactRouterLink children={children} {...nativeProps} to={href} />
-  const redirectPaths = config.katanaRedirectPaths
-
-  // If the link does not point to a url that can be handled by React Router,
-  // i.e. a non-D8 url
-  if (
-    // TODO: For now, route all links through the server, since client-side
-    // routing will be removed in the future.
-    !forceClientRouting ||
-    (startsWith(href, '/') &&
-      // TODO: This check simply exists to block code path for jest tests when
-      // client config is undefined.
-      !isEmpty(redirectPaths) &&
-      !redirectPaths.includes(href.split('/')[1]))
-  ) {
-    // Adding a `target` forces react-router to send a request to the server.
-    return React.cloneElement(linkElement, { target: '_self' })
-  } else {
-    return linkElement
-  }
+  return linkElement
 }
 
 Link.contextTypes = {
   router: PropTypes.object
 }
 
-Link.defaultProps = {
-  forceClientRouting: false
-}
-
 Link.propTypes = {
-  // Force client-side routing, except for urls that are not defined in katanaRedirectPaths.
-  forceClientRouting: PropTypes.bool,
 
   // A url to a linked resource or location.
   to: PropTypes.oneOfType([
