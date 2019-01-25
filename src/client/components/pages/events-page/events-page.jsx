5b4f278d91530844783ec534daa6804001e2fb24
Aaron Brooks <brooksa321@gmail.com>
2019-01-25 10:00:57 -0500

    Added events to router and created events component

---
+++ src/client/components/pages/events-page/events-page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class EventsPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>event's page</h3>
+      </div>
+    )
+  }
+}
+
+export default EventsPage
