96fa0947d8cf48cfc0dd0ff326413ab8f4510fd4
Aaron Brooks <brooksa321@gmail.com>
2019-01-28 11:21:51 -0500

    Feature ta 1529 (#800)
    * Added events to router and created events component
    
    * Centered events heading
    
    * modified events page
    
    * modifies routes and component to standardize naming
    
    * changed event to events

---
+++ src/client/components/pages/event-lookup-page/event-lookup-page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const styles = {
+  textAlign: 'center'
+}
+
+class EventLookupPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3 style={styles}>events find</h3>
+      </div>
+    )
+  }
+}
+
+export default EventLookupPage
--- src/client/components/routes.jsx
+++ src/client/components/routes.jsx
@@ -57,6 +57,10 @@ let OfficeLookupPage = props => (
   <Async componentProps={props} load={import('./pages/office-lookup-page/office-lookup-page.jsx')} />
 )
 
+let EventLookupPage = props => (
+  <Async componentProps={props} load={import('./pages/event-lookup-page/event-lookup-page.jsx')} />
+)
+
 import { Route, IndexRoute, IndexRedirect, Redirect } from 'react-router'
 import constants from '../services/constants.js'
 import clientConfig from '../services/client-config.js'
@@ -80,6 +84,8 @@ const mainRoutes = [
   <Redirect key={49} from="/resource-partner-survey" to="/resource-partner-survey/" />,
   <Route key={56} path="/local-assistance/find/" component={OfficeLookupPage} />,
   <Redirect key={57} from="/local-assistance/find" to="/local-assistance/find/" />,
+  <Route key={56} path="/events/find/" component={EventLookupPage} />,
+  <Redirect key={57} from="/events/find" to="/events/find/" />,
   <Route key={12} path="/business-guide/10-steps-start-your-business/" component={TenStepsLandingPage} />,
   <Route
     key={13}
