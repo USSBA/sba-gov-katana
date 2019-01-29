6d866374783924b161c30e2a64cfc0524dc08b82
Amy DiPasquale <ald5044@gmail.com>
2019-01-29 13:53:03 -0500

    TA-1566 add header and keyword search box to front end

--- src/client/components/pages/event-lookup-page/event-lookup-page.jsx
+++ src/client/components/pages/event-lookup-page/event-lookup-page.jsx
@@ -1,14 +1,62 @@
 import React from 'react'
 
-const styles = {
-  textAlign: 'center'
-}
+import styles from './event-lookup-page.scss'
+import { fetchSiteContent } from '../../../fetch-content-helper'
+import { Button, SearchIcon, TextInput } from 'atoms'
 
 class EventLookupPage extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      events: null
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.events
+  }
+
+  componentWillMount() {
+    fetchSiteContent('events').then(results => {
+      this.setState({ events: results })
+    })
+  }
+
+  renderKeywordSearch() {
+    const textInputProps = {
+      placeholder: 'Enter keyword',
+      id: 'event-lookup-text-input',
+      errorText: 'Please enter a keyword or phase.',
+      label: 'Search',
+      validationState: '',
+      onChange: () => {},
+      value: ''
+    }
+    return (
+      <div className={styles.searchBox}>
+        <TextInput {...textInputProps} />
+        <div className={styles.searchIcon}>
+          <SearchIcon aria-hidden="true" />
+        </div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
-        <h3 style={styles}>events find</h3>
+        <div className={styles.banner}>
+          <h2 className={styles.header}>Find events</h2>
+          <div>
+            {this.renderKeywordSearch()}
+            <div className={styles.applyButton}>
+              <Button primary alternate onClick={() => {}}>
+                Apply
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
