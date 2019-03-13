d05263f22a2e68ea419e1051dc3e29a6fead9cfe
Avery Smith <avery@averysmith.pro>
2019-03-13 16:53:20 -0400

    TA-1579 added registration button and test (#823)
    * TA-1579 added registration button and test
    
    * TA-1579 added registrationUrl guard clause
    
    * TA-1579 print url to console
    
    * merged

--- src/client/components/templates/event/event.jsx
+++ src/client/components/templates/event/event.jsx
@@ -1,11 +1,38 @@
 import React, { Component, PropTypes } from 'react'
-import { DecorativeDash } from 'atoms'
+import { Button, DecorativeDash } from 'atoms'
 import classNames from 'classnames'
+import moment from 'moment'
+import { isEmpty } from 'lodash'
 import styles from './event.scss'
+import { LeaveSbaModal } from 'organisms'
+import he from 'he'
 
 class Event extends Component {
+  constructor() {
+    super()
+    this.state = {
+      modalIsOpen: false
+    }
+  }
+
+  handleRegisterButtonClick(e) {
+    e.preventDefault()
+    this.setModalState(true)
+  }
+
+  setModalState(isOpen) {
+    this.setState({ modalIsOpen: isOpen })
+  }
+
+  handleClose() {
+    this.setModalState(false)
+  }
+
   render() {
-    const { title, description } = this.props.eventData
+    const { title, description, registrationUrl } = this.props.eventData
+
+    const startDate = moment(this.props.eventData).format('dddd, MMMM D')
+    const startDateDetails = moment(this.props.eventData.startDate).format('dddd, MMMM D, YYYY')
 
     // classNames is not necessary when this is created for future extensibility
     // delete this comment if you modify the classNames below to include logic
@@ -14,22 +41,44 @@ class Event extends Component {
       [styles.container]: true
     })
 
-    const titleClassName = classNames({
-      'event-title': true,
-      [styles.title]: true
+    // const titleClassName = classNames({
+    //   'event-title': true,
+    //   [styles.title]: true
+    // })
+
+    const iconClassName = classNames({
+      'fa fa-external-link': true,
+      [styles.registerButtonIcon]: true
     })
 
     return (
       <div className={containerClassNames}>
-        <h1 className={titleClassName} data-cy="event-title">
-          {title}
-        </h1>
-        <div className={styles.description}>
+        <div className={styles.header}>
+          <h3 id="event-header-date">{startDate}</h3>
+          <h1 data-cy="event-title">{title}</h1>
+        </div>
+        <div className={styles.columnA}>
           <DecorativeDash aria-hidden="true" width={80} />
-          <h4 className={styles.descriptionLabel} data-cy="event-escription-label">
+          <h4 className={styles.descriptionLabel} data-cy="event-description-label">
             Description
           </h4>
-          <p data-cy="event-description">{description}</p>
+          <p id="event-details-description" data-cy="event-description">
+            {description}
+          </p>
+        </div>
+        <div className={styles.columnB}>
+          {!isEmpty(registrationUrl) && <div className={styles.button} data-cy="registration">
+            <Button className="register-button" primary onClick={this.handleRegisterButtonClick.bind(this)}>
+              REGISTER <i aria-hidden="true" className={iconClassName} />
+            </Button>
+            <LeaveSbaModal
+              closeLeaveSba={this.handleClose.bind(this)}
+              isOpen={this.state.modalIsOpen}
+              url={registrationUrl}
+              shouldOpenNewWindow
+              data-cy="leave sba modal"
+            />
+          </div>}
         </div>
       </div>
     )
--- test/jest/client/components/templates/event/event.test.jsx
+++ test/jest/client/components/templates/event/event.test.jsx
@@ -30,4 +30,35 @@ describe('Event Template', () => {
     expect(result).toHaveLength(expected)
     expect(result.text()).toBe(description)
   })
+  test('should render a registration button', () => {
+    const props = {
+      eventData: {
+        registrationUrl: 'my-url'
+      }
+    }
+    const component = shallow(<Event {...props} />)
+    const result = component.find('.register-button')
+    const expected = 1
+    expect(result).toHaveLength(expected)
+  })
+  test('should not render a registration button with an empty string', () => {
+    const props = {
+      eventData: {
+        registrationUrl: ''
+      }
+    }
+    const component = shallow(<Event {...props} />)
+    const result = component.find('.register-button')
+    const expected = 0
+    expect(result).toHaveLength(expected)
+  })
+  test('should not render a registration button with no registrationUrl value', () => {
+    const props = {
+      eventData: {}
+    }
+    const component = shallow(<Event {...props} />)
+    const result = component.find('.register-button')
+    const expected = 0
+    expect(result).toHaveLength(expected)
+  })
 })
