a01231186693d7892b07d404b28368bc11c9f0f6
Brian Le <blsymph@gmail.com>
2019-03-21 17:19:44 -0400

    T1-921 Add person lookup page with client-side filtering and sorting

---
+++ test/jest/client/components/atoms/multiselect/multiselect.test.js
@@ -0,0 +1,47 @@
+/*global expect*/
+
+/*eslint-disable no-unused-vars*/
+import React from 'react'
+/*eslint-enable no-unused-vars*/
+import ReactSelect from 'react-select-v1'
+import { MultiSelect } from 'atoms'
+import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
+import _ from 'lodash'
+import { getLanguageOverride } from '../../../../../../src/client/services/utils.js'
+
+jest.mock('../../../../../../src/client/services/utils.js', () => ({
+  getLanguageOverride: jest.fn()
+}))
+
+describe('MultiSelect', () => {
+  test('placeholder value renders in Spanish', () => {
+    getLanguageOverride.mockReturnValueOnce('es')
+    const component = shallow(
+      <MultiSelect
+        label="Good label"
+        name="select"
+        options={['Z1', 'N2', 'T1', 'A1']}
+        autoFocus
+        multi={false}
+      />
+    )
+
+    const wrapper = component.find('Select')
+    expect(wrapper.props().placeholder).toEqual('Selecciona...')
+  })
+
+  test('placeholder value renders in English', () => {
+    const component = shallow(
+      <MultiSelect
+        label="Good label"
+        name="select"
+        options={['Z1', 'N2', 'T1', 'A1']}
+        autoFocus
+        multi={false}
+      />
+    )
+    const wrapper = component.find('Select')
+    expect(wrapper.props().placeholder).toEqual('Select...')
+  })
+})
