9c63f775585783d6cffae0b93cd3b15802f8f44f
Aaron Brooks <brooksa321@gmail.com>
2019-03-25 14:57:44 -0400

    Feature ta 1754 (#822)
    * corrected column styling. Added jest tests
    
    * Added all details functionality
    
    * cleaned up switch statement
    
    * added proper id's to new elements
    
    * rebased off master
    
    * tabbing order updates and styling tweaks
    
    * added jest tests
    
    * added he to the lint exceptions
    
    * Fixed location and organizer display on empty data set. Modified tests accordingly
    
    * modified time to include minutes
    
    * fixed tabbing of empty div
    
    * fixed colum A tabbing
    
    * fixed incorect tabable tab in fa icon
    
    * Resolved icon formatting and renamed css class to be more descriptive
    
    * swapped small icon componenet for raw fa icons
    
    * Added time of day when start date and end date differ
    
    * changed reoccur to recur and modified jest test
    
    * Modified recurring events div spacing to match mockup
    
    * added bottom padding to contact name
    
    * TA-1754 WIP styling updates
    
    * cleaned up unnecessary styling code
    
    * Added padding at top of date in tablet mode

--- src/client/components/templates/event/event.jsx
+++ src/client/components/templates/event/event.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { Button, DecorativeDash } from 'atoms'
+import { Button, DecorativeDash, SmallIcon } from 'atoms'
 import classNames from 'classnames'
 import moment from 'moment'
 import { isEmpty } from 'lodash'
@@ -29,10 +29,69 @@ class Event extends Component {
   }
 
   render() {
-    const { title, description, registrationUrl } = this.props.eventData
+    const {
+      title,
+      description,
+      timezone,
+      recurring,
+      recurringType,
+      cost,
+      location,
+      contact,
+      registrationUrl,
+      locationType
+    } = this.props.eventData
 
-    const startDate = moment(this.props.eventData).format('dddd, MMMM D')
-    const startDateDetails = moment(this.props.eventData.startDate).format('dddd, MMMM D, YYYY')
+    const startDate = moment.parseZone(this.props.eventData.startDate).format('dddd, MMMM D')
+    const startDateDetails = moment.parseZone(this.props.eventData.startDate).format('dddd, MMMM D, YYYY')
+
+    const startingTimeSuffix = moment.parseZone(this.props.eventData.startDate).format('a')
+    let startTime =
+      moment.parseZone(this.props.eventData.startDate).format('mm') === '00'
+        ? moment.parseZone(this.props.eventData.startDate).format('h')
+        : moment.parseZone(this.props.eventData.startDate).format('h:mm')
+    const endTime =
+      moment.parseZone(this.props.eventData.endDate).format('mm') === '00'
+        ? moment.parseZone(this.props.eventData.endDate).format('ha')
+        : moment.parseZone(this.props.eventData.endDate).format('h:mma')
+
+    if (
+      moment.parseZone(this.props.eventData.startDate).format('a') !==
+      moment.parseZone(this.props.eventData.endDate).format('a')
+    ) {
+      startTime = startTime + startingTimeSuffix
+    }
+    const eventTime = `${startTime}-${endTime} ${timezone}`
+
+    let recurringDetail
+    if (recurring === 'Yes') {
+      switch (recurringType) {
+        case 'Recurs monthly (same week &amp; same day of week)':
+          recurringDetail = 'Recurs same day every month'
+          break
+        case 'Recurs bi-weekly':
+          recurringDetail = 'Recurs bi-weekly'
+          break
+        case 'Recurs weekly':
+          recurringDetail = 'Recurs weekly'
+          break
+        case 'Recurs daily':
+          recurringDetail = 'Recurs daily'
+          break
+        default:
+          recurringDetail = 'This is a recuring event'
+          break
+      }
+    }
+
+    const costDetail = cost === '0.00' ? 'Free' : '$' + cost
+
+    const address =
+      location.address && location.city && location.state && location.zipcode
+        ? location.address + '\n' + location.city + ',' + location.state + ' ' + location.zipcode
+        : null
+
+    const link = 'https://maps.google.com?q=' + encodeURIComponent(address)
 
     // classNames is not necessary when this is created for future extensibility
     // delete this comment if you modify the classNames below to include logic
@@ -56,31 +115,117 @@ class Event extends Component {
     return (
       <div className={containerClassNames}>
         <div className={styles.header}>
-          <h3 id="event-header-date">{startDate}</h3>
-          <h1 data-cy="event-title">{eventTitle}</h1>
+          <h3 id="event-header-date" tabIndex="0" data-cy="event-header-date">
+            {startDate}
+          </h3>
+          <h1 data-cy="event-title" tabIndex="0">
+            {eventTitle}
+          </h1>
         </div>
-        <div className={styles.columnA}>
-          <DecorativeDash aria-hidden="true" width={80} />
-          <h4 className={styles.descriptionLabel} data-cy="event-description-label">
-            Description
-          </h4>
-          <p id="event-details-description" data-cy="event-description">
-            {description}
-          </p>
-        </div>
-        <div className={styles.columnB}>
-          {!isEmpty(registrationUrl) && <div className={styles.button} data-cy="registration">
-            <Button className="register-button" primary onClick={this.handleRegisterButtonClick.bind(this)}>
-              REGISTER <i aria-hidden="true" className={iconClassName} />
-            </Button>
-            <LeaveSbaModal
-              closeLeaveSba={this.handleClose.bind(this)}
-              isOpen={this.state.modalIsOpen}
-              url={registrationUrl}
-              shouldOpenNewWindow
-              data-cy="leave sba modal"
-            />
-          </div>}
+        <div className={styles.page}>
+          <div className={styles.columnA}>
+            <DecorativeDash aria-hidden="true" width={80} />
+            <h4 className={styles.descriptionLabel} data-cy="event-description-label" tabIndex="0">
+              Description
+            </h4>
+            <p id="event-details-description" data-cy="event-description" tabIndex="0gi">
+              {description}
+            </p>
+          </div>
+          <div className={styles.columnB} tabIndex="0">
+            {!isEmpty(registrationUrl) && (
+              <div className={styles.button} data-cy="registration">
+                <Button
+                  className="register-button"
+                  primary
+                  onClick={this.handleRegisterButtonClick.bind(this)}
+                >
+                  REGISTER <i aria-hidden="true" className={iconClassName} />
+                </Button>
+                <LeaveSbaModal
+                  closeLeaveSba={this.handleClose.bind(this)}
+                  isOpen={this.state.modalIsOpen}
+                  url={registrationUrl}
+                  shouldOpenNewWindow
+                  data-cy="leave sba modal"
+                />
+              </div>
+            )}
+            <div className={styles.detailsBox}>
+              <h3 tabIndex="0">Date and time</h3>
+              <div>
+                <div id="event-details-date" data-cy="event-details-date" tabIndex="0">
+                  {startDateDetails}
+                </div>
+                <div id="event-details-time" data-cy="event-details-time" tabIndex="0">
+                  {eventTime}
+                </div>
+                {!isEmpty(recurringDetail) ? (
+                  <div
+                    className={styles.eventDetailsRecurring}
+                    id="event-details-recurring"
+                    data-cy="event-details-recurring"
+                    tabIndex="0"
+                  >
+                    {recurringDetail}
+                  </div>
+                ) : (
+                  <p tabIndex="-1" />
+                )}
+              </div>
+              <div>
+                <h3 tabIndex="0">Cost</h3>
+                <p tabIndex="0" data-cy="event-details-cost">
+                  {costDetail}
+                </p>
+              </div>
+              <h3 tabIndex="0">Location</h3>
+              <div>
+                {locationType === 'In Person' ? (
+                  <p id="event-details-location" tabIndex="0" data-cy="event-details-location">
+                    {location.name}
+                    <br />
+                    {location.address}, {location.address_additional}
+                    <br />
+                    {location.city}, {location.state} {location.zipcode}
+                    <br />
+                    <a id="event-details-location-link" href={link}>
+                      View on Map
+                    </a>
+                  </p>
+                ) : (
+                  <p id="event-details-location" tabIndex="0">
+                    Online
+                  </p>
+                )}
+              </div>
+              {!isEmpty(contact.name) && (
+                <div>
+                  <h3 tabIndex="0">Event Organizer</h3>
+                  <div
+                    className={styles.contactName}
+                    id="event-details-organizer"
+                    data-cy="event-details-organizer"
+                    tabIndex="0"
+                  >
+                    {contact.name}
+                  </div>
+                  <div className={styles.contactIcon}>
+                    {!isEmpty(contact.email) && <i className={'fa fa-envelope-o'} aria-hidden="true" />}
+                  </div>
+                  <div tabIndex="0" data-cy="email">
+                    {contact.email}
+                  </div>
+                  <div className={styles.contactIcon}>
+                    {!isEmpty(contact.phone) && <i className={'fa fa-phone'} aria-hidden="true" />}
+                  </div>
+                  <div tabIndex="0" data-cy="event-details-phone">
+                    {contact.phone}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     )
