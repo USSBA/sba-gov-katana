1aa4cacc5db26e56a9b3386ad4f29c052ffbce81
Kevin Lee <ki.lee@sba.gov>
2019-03-25 12:47:15 -0400

    T1-1304 Change to button component and render options using function

--- src/client/components/organisms/oha-westlaw-form/oha-westlaw-form.jsx
+++ src/client/components/organisms/oha-westlaw-form/oha-westlaw-form.jsx
@@ -1,5 +1,30 @@
 import React from 'react'
+
+import { Button } from 'atoms'
 import styles from './oha-westlaw-form.scss'
+
+const selectOptions = {
+  tncDecisionType: [
+    { value: '', label: 'All' },
+    { value: 'BDP! MSB!', label: '8(a) Business Development' },
+    { value: 'DEV', label: 'Development Company' },
+    { value: 'WOSB!', label: 'Economically-Disadvantaged Women-Owned Small Business' },
+    { value: 'NAI! SIC!', label: 'NAICS & SIC' },
+    { value: 'VET! SDV!', label: 'Service-Disabled Veteran Owned Small Business Concern' },
+    { value: 'SIZ!', label: 'Size Decisions' },
+    { value: 'SDB!', label: 'Small Disadvantaged Business Decisions' },
+    { value: 'WBC', label: "Women's Business Center" },
+    { value: 'WOSB!', label: 'Women-Owned Small Business' }
+  ],
+  tncDate: [
+    { value: '', label: 'None' },
+    { value: 'last 3 months', label: 'Last 3 Months' },
+    { value: 'last 6 months', label: 'Last 6 Months' },
+    { value: 'last 1 year', label: 'Last 1 Year' },
+    { value: 'last 3 years', label: 'Last 3 Years' },
+    { value: 'last 10 years', label: 'Last 10 Years' }
+  ]
+}
 
 class OHAWestlawForm extends React.Component {
   constructor(props) {
@@ -14,118 +39,121 @@ class OHAWestlawForm extends React.Component {
     }
   }
 
-  handleSubmit(event, searchData) {
-    const { method, query } = searchData
-    // if (Q.length < 1) {
-    //   alert("Please enter a number.");
-    // }
-    event.preventDefault()
-    const finalQuery = encodeURIComponent(query)
-    const url = `https://govt.westlaw.com/sbaoha/Search/Results?t_Method=${method}_querytext=${finalQuery}`
-    window.location = url
-  }
-
   handleChange(event) {
     const { name, value } = event.target
     this.setState({ [name]: value })
   }
 
+  handleSubmit(event, searchData) {
+    const { method, query } = searchData
+    const finalQuery = encodeURIComponent(query)
+    const url = `https://govt.westlaw.com/sbaoha/Search/Results?t_Method=${method}_querytext=${finalQuery}`
+
+    event.preventDefault()
+    window.location = url
+  }
+
+  renderOption(optionName) {
+    return (
+      <select name={optionName} value={this.state[optionName]} onChange={e => this.handleChange(e)}>
+        {selectOptions[optionName].map(({ label, value }, index) => {
+          return (
+            <option key={index} value={value}>
+              {label}
+            </option>
+          )
+        })}
+      </select>
+    )
+  }
+
   render() {
-    const { title } = this.props
-    const { appealNumber, appellantName, tncText, tncDecisionType, tncDate, winText } = this.state
+    const { appealNumber, appellantName, tncDate, tncDecisionType, tncText, winText } = this.state
 
     return (
       <div>
-        <h1>{title}</h1>
-        <div>
-          <form onSubmit={e => this.handleSubmit(e, { method: 'tnc&t', query: `CI(${appealNumber})` })}>
-            <legend>Search by Appeal Number</legend>
-            <label>
-              Enter numeric portion only:
-              <input
-                name="appealNumber"
-                type="number"
-                value={appealNumber}
-                onChange={e => this.handleChange(e)}
-              />
-            </label>
-            <input type="submit" value="Go" />
+        <h2>{this.props.title}</h2>
+        <form onSubmit={e => this.handleSubmit(e, { method: 'tnc&t', query: `CI(${appealNumber})` })}>
+          <h3>Search by Appeal Number</h3>
+          <label>
+            Enter numeric portion only:
+            <input
+              name="appealNumber"
+              type="number"
+              value={appealNumber}
+              onChange={e => this.handleChange(e)}
+              required
+            />
             Example: 5248
-          </form>
-        </div>
-        <div>
-          <form onSubmit={e => this.handleSubmit(e, { method: 'tnc&t', query: `TI(${appellantName})` })}>
-            <legend>Search by Appellant Name</legend>
-            <label>
-              Enter name:
-              <input
-                name="appellantName"
-                type="text"
-                value={appellantName}
-                onChange={e => this.handleChange(e)}
-              />
-            </label>
-            <input type="submit" value="Go" />
+          </label>
+          <Button id="appeal-number-submit" type="submit" primary>
+            Go
+          </Button>
+        </form>
+        <form onSubmit={e => this.handleSubmit(e, { method: 'tnc&t', query: `TI(${appellantName})` })}>
+          <h3>Search by Appellant Name</h3>
+          <label>
+            Enter name:
+            <input
+              name="appellantName"
+              type="text"
+              value={appellantName}
+              onChange={e => this.handleChange(e)}
+              required
+            />
             Examples: ACME "Secure Network Systems"
-          </form>
-        </div>
-        <div>
-          <form onSubmit={e => this.handleSubmit(e, { method: 'win&t', query: `${winText}` })}>
-            <legend>Plain text search</legend>
-            <label>
-              Enter search terms:
-              <input name="winText" type="text" value={winText} onChange={e => this.handleChange(e)} />
-            </label>
-            <input type="submit" value="Go" />
+          </label>
+          <Button id="appellant-name-submit" type="submit" primary>
+            Go
+          </Button>
+        </form>
+        <form onSubmit={e => this.handleSubmit(e, { method: 'win&t', query: `${winText}` })}>
+          <h3>Plain text search</h3>
+          <label>
+            Enter search terms:
+            <input
+              name="winText"
+              type="text"
+              value={winText}
+              onChange={e => this.handleChange(e)}
+              required
+            />
             Example: ostensible subcontractor
-          </form>
-        </div>
-        <div>
-          <form
-            onSubmit={e =>
-              this.handleSubmit(e, {
-                method: 'tnc&t',
-                query: `${tncText}${tncDecisionType !== '' ? `& pr(${tncDecisionType})` : ''}${
-                  tncDate !== '' ? `& da(${tncDate})` : ''
-                }`
-              })
-            }
-          >
-            <legend>Plain text search</legend>
-            <label>
-              Enter search terms:
-              <input name="tncText" type="term" value={tncText} onChange={e => this.handleChange(e)} />
-            </label>
-            <label>Decision type (optional)</label>
-            <select name="tncDecisionType" value={tncDecisionType} onChange={e => this.handleChange(e)}>
-              <option value="" defaultValue>
-                All
-              </option>
-              <option value="BDP! MSB!">8(a) Business Development</option>
-              <option value="DEV">Development Company</option>
-              <option value="WOSB!">Economically-Disadvantaged Women-Owned Small Business</option>
-              <option value="NAI! SIC!">NAICS & SIC</option>
-              <option value="VET! SDV!">Service-Disabled Veteran Owned Small Business Concern</option>
-              <option value="SIZ!">Size Decisions</option>
-              <option value="SDB!">Small Disadvantaged Business Decisions</option>
-              <option value="WBC">Women's Business Center</option>
-              <option value="WOSB!">Women-Owned Small Business</option>
-            </select>
-            <label>Date Restriction (optional)</label>
-            <select name="tncDate" value={tncDate} onChange={e => this.handleChange(e)}>
-              <option value="" defaultValue>
-                None
-              </option>
-              <option value="last 3 months">Last 3 Months</option>
-              <option value="last 6 months">Last 6 Months</option>
-              <option value="last 1 year">Last 1 Year</option>
-              <option value="last 3 years">Last 3 years</option>
-              <option value="last 10 years">Last 10 Years</option>
-            </select>
-            <input type="submit" value="Go" />
+          </label>
+          <Button id="win-text-submit" type="submit" primary>
+            Go
+          </Button>
+        </form>
+        <form
+          onSubmit={e =>
+            this.handleSubmit(e, {
+              method: 'tnc&t',
+              query: `${tncText}${tncDecisionType !== '' ? `& pr(${tncDecisionType})` : ''}${
+                tncDate !== '' ? `& da(${tncDate})` : ''
+              }`
+            })
+          }
+        >
+          <h3>Boolean search with field limits</h3>
+          <label>
+            Enter search terms:
+            <input
+              name="tncText"
+              type="text"
+              value={tncText}
+              onChange={e => this.handleChange(e)}
+              required
+            />
             Example: ostensible subcontractor
-          </form>
-        </div>
+          </label>
+          <label>Decision type (optional)</label>
+          {this.renderOption('tncDecisionType')}
+          <label>Date Restriction (optional)</label>
+          {this.renderOption('tncDate')}
+          <Button id="win-text-submit" type="submit" primary>
+            Go
+          </Button>
+        </form>
       </div>
     )
   }
