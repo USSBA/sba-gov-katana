a3f8df911389a1fe6bdf3484c05472ef0278d800
Amy DiPasquale <ald5044@gmail.com>
2019-04-05 15:25:48 -0400

    TA-2174 set up blog page to output JSON blog data

--- src/client/components/templates/blog/blog.jsx
+++ src/client/components/templates/blog/blog.jsx
@@ -1,7 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 class Blog extends Component {
-  render() {}
+  render() {
+    return <p>{JSON.stringify(this.props.blogData)}</p>
+  }
+}
+
+Blog.defaultProps = {
+  blogData: null
+}
+
+Blog.propTypes = {
+  blogData: PropTypes.object
 }
 
 export default Blog
