1c279dcb4b8fb9d912f8b1435d6f9dc90bcf96d8
Candace <cdcampbell96@gmail.com>
2019-04-11 16:20:08 -0400

    TA-2139 Added jest tests for global search results

--- test/jest/client/components/pages/search-page/search-page.test.jsx
+++ test/jest/client/components/pages/search-page/search-page.test.jsx
@@ -4,6 +4,7 @@ import renderer from 'react-test-renderer'
 import { shallow, mount } from 'enzyme'
 // Quiet warnings about OnTouchTap
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import { CloudWatchLogs } from 'aws-sdk'
 injectTapEventPlugin()
 
 const mockFunction = jest.fn()
@@ -50,10 +51,30 @@ describe('SearchPage', () => {
     })
   })
 
+  describe('Results', () => {
+    it('should display results when searchResults does not equal 0', () => {
+      const component = shallow(<SearchPage {...props} />)
+
+      component.setState({
+        searchTerm: 'my search term',
+        searchResults: [
+          {
+            id: '78',
+            title: 'Write your business plan'
+          }
+        ]
+      })
+
+      const result = component.find('ResultsList').length
+      const expected = 1
+      expect(result).toEqual(expected)
+    })
+  })
+
   describe('ResultsList', () => {
     describe('Paginator', () => {
       it('has two paginator instances', () => {
-        const props = {
+        const customProps = {
           searchResults: [
             {
               title: 'Title',
@@ -65,7 +86,7 @@ describe('SearchPage', () => {
           pageSize: 5,
           itemCount: 1
         }
-        const component = mount(<ResultsList {...props} />)
+        const component = mount(<ResultsList {...customProps} />)
         const result = component.find('Paginator').length
         const expected = 1
 
@@ -80,6 +101,7 @@ describe('SearchPage', () => {
       component.setState({
         searchTerm: 'my search term'
       })
+
       const result = component.find('SuggestedRouteCard').length
       const expected = 1
       expect(result).toEqual(expected)
@@ -92,6 +114,35 @@ describe('SearchPage', () => {
       const result = component.find('SuggestedRouteCard').length
       const expected = 0
       expect(result).toEqual(expected)
+    })
+  })
+
+  describe('NoResults', () => {
+    it('should display no results message when there are  0 searchResults', () => {
+      const component = shallow(<SearchPage {...props} />)
+
+      component.setState({
+        searchTerm: 'my search term',
+        searchResults: []
+      })
+
+      const result = component.find('.results-message')
+      const expected = "Sorry, we couldn't find anything matching that query."
+      expect(result.text()).toEqual(expected)
+    })
+  })
+
+  describe('LoadingState', () => {
+    it('should display loading message when results have not yet been found', () => {
+      const component = shallow(<SearchPage {...props} />)
+
+      component.setState({
+        searchTerm: 'my search term'
+      })
+
+      const result = component.find('.results-message')
+      const expected = 'loading...'
+      expect(result.text()).toEqual(expected)
     })
   })
 })
