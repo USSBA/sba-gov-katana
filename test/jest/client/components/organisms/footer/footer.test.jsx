79b6761d23f96ddefce16a2255141c51aa9d73c3
Kevin Lee <ki.lee@sba.gov>
2019-04-11 14:31:24 -0400

    T1-1330 Add snapshot test

---
+++ test/jest/client/components/organisms/footer/footer.test.jsx
@@ -0,0 +1,24 @@
+/*global expect*/
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import sinon from 'sinon'
+import { Footer } from 'organisms'
+import * as utils from '../../../../../../src/client/services/utils.js'
+
+describe('Front Page Hero', () => {
+  let stubGetLanguageOverride
+  beforeAll(() => {
+    stubGetLanguageOverride = sinon.stub(utils, 'getLanguageOverride')
+    stubGetLanguageOverride.returns('en')
+  })
+  afterAll(() => {
+    stubGetLanguageOverride.restore()
+  })
+
+  test('should render correctly', () => {
+    const tree = shallow(<Footer />)
+    expect(tree).toMatchSnapshot()
+  })
+})
