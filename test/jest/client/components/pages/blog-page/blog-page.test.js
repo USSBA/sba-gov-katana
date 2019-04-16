739f86ebbd37b4b3942de646c8a2c7beab5ffa81
amyd11 <40372898+amyd11@users.noreply.github.com>
2019-04-16 13:28:47 -0400

    TA-2174 Setup Blog Page Template (#849)
    * TA-2173 add routes for blogs, create blog-page.jsx
    
    * TA-2173 create blog-lookup-page and add to routes.jsx
    
    * TA-2173 use id for blog post page, remove blog-lookup code
    
    * TA-2173 simplify blog-page.jsx
    
    * TA-2174 set up blog page to output JSON blog data
    
    * TA-2174 Add RTL test for blog, fixing error button
    
    * TA-2174 add tests
    
    * TA-2174 remove catch since fetchRestContent handles catch
    
    * TA-2174 update test description
    
    * TA-2174 add styles to loader
    
    * TA-2174 add data-cy tag to loader

---
+++ test/jest/client/components/pages/blog-page/blog-page.test.js
@@ -0,0 +1,91 @@
+/*eslint-disable no-undefined*/
+
+import React from 'react'
+import { render, cleanup, waitForElement } from 'react-testing-library'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from 'client/reducers'
+import 'jest-dom/extend-expect'
+import BlogPage from 'pages/blog-page/blog-page.jsx'
+import * as fetchContentHelper from 'client/fetch-content-helper.js'
+
+const mockBlogData = {
+  author: null,
+  blogBody: {},
+  blogTags: 'International',
+  office: {},
+  summary: 'Some stuff',
+  type: 'blog',
+  title: 'Fearless',
+  id: 1,
+  updated: 1554483661,
+  created: 1554414745,
+  langCode: 'en',
+  url: '/blog/18343'
+}
+
+afterEach(cleanup)
+
+describe('Blog page', () => {
+  it('renders blog content when BlogPage receives data back from the api', async () => {
+    const fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
+    fetchRestContentStub.mockImplementation(() => Promise.resolve(mockBlogData))
+
+    const initialState = undefined
+    const enhancer = applyMiddleware(thunk)
+    const store = createStore(reducers, initialState, enhancer)
+
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <BlogPage id="1" />
+      </Provider>
+    )
+
+    expect(fetchRestContentStub).toHaveBeenCalledTimes(1)
+
+    const content = await waitForElement(() => getByTestId('blog-content'))
+    expect(content).toBeInTheDocument()
+
+    fetchRestContentStub.mockRestore()
+  })
+
+  it('renders error page when BlogPage receives null data back from the api', async () => {
+    const fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
+    fetchRestContentStub.mockImplementation(() => Promise.resolve(null))
+
+    const initialState = undefined
+    const enhancer = applyMiddleware(thunk)
+    const store = createStore(reducers, initialState, enhancer)
+
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <BlogPage id="1" />
+      </Provider>
+    )
+
+    expect(fetchRestContentStub).toHaveBeenCalledTimes(1)
+
+    const content = await waitForElement(() => getByTestId('blog-error'))
+    expect(content).toBeInTheDocument()
+
+    fetchRestContentStub.mockRestore()
+  })
+
+  it('renders loader when BlogPage is waiting for an api response', async () => {
+    const fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
+
+    const initialState = undefined
+    const enhancer = applyMiddleware(thunk)
+    const store = createStore(reducers, initialState, enhancer)
+
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <BlogPage id="1" />
+      </Provider>
+    )
+
+    expect(fetchRestContentStub).toHaveBeenCalledTimes(1)
+    expect(getByTestId('blog-loader')).toBeInTheDocument()
+  })
+})
