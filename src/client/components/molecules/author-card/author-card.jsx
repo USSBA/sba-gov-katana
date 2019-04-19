2ba2377759a49f56c35b88e2e2e47d872b5b5d46
Avery Smith <avery@averysmith.pro>
2019-04-19 18:13:54 -0400

    TA-2300 improved tests and added ui design for 'with image' state

--- src/client/components/molecules/author-card/author-card.jsx
+++ src/client/components/molecules/author-card/author-card.jsx
@@ -1,16 +1,59 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { isEmpty } from 'lodash'
+import classNames from 'classnames'
+import { DecorativeDash, Link } from 'atoms'
+import styles from './author-card.scss'
 
 class AuthorCard extends PureComponent {
 	render() {
-		
+
+		const { name, title, bio, picture, id } = this.props
+		const className = classNames({
+		    [styles.card]: true
+		})
+
 		return (
 
-			<div data-testid={'authorCard'}>
-				AuthorCard
+			<div data-testid={'authorCard'} tabIndex="0" className={className}>
+				{!isEmpty(picture) && <div data-testid={'picture'} tabIndex="0" className={styles.image}>
+					<img src={picture} alt="photo of author" />
+				</div>}
+				<div className={styles.info}>
+					<h4 data-testid={'name'} tabIndex="0">
+						<a href={`#${id}`}>{name}</a>
+					</h4>
+					<div data-testid={'title'} tabIndex="0" className={styles.title}>
+						<i>{title}</i>
+					</div>
+					<DecorativeDash width={30} />
+					<div data-testid={'bio'} tabIndex="0" className={styles.bio}>
+						{bio}
+					</div>
+					<div data-testid={'read-more'} tabIndex="0">
+						<a href={`#${id}`}>Read More</a>
+					</div>
+				</div>
 			</div>
 
 		)
 	}
 }
+
+AuthorCard.propTypes = {
+	name: PropTypes.string,
+	title: PropTypes.string,
+	bio: PropTypes.string,
+	id: PropTypes.number
+}
+
+AuthorCard.defaultProps = {
+	name: "Full Name",
+	title: "title",
+	picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Linda_McMahon_official_photo.jpg/440px-Linda_McMahon_official_photo.jpg",
+	bio: "Eaque totam ad exercitationem tempora rerum natus ea voluptates. Reiciendis recusandae exercitationem optio perspiciatis rem optio. Rerum velit veniam eos temporibus suscipit debitis dolores.",
+	id: 10
+}
+
 
 export default AuthorCard
