cdea89574a8f73d58a4c5704e036272682868ffa
Pat Kelsh <pkelsh@gmail.com>
2019-05-10 13:48:30 -0400

    TA-2390 cleaned up imports

--- src/client/components/molecules/card/card.jsx
+++ src/client/components/molecules/card/card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import { isEmpty, snakeCase } from 'lodash'
+import { isEmpty } from 'lodash'
 
 import styles from './card.scss'
 import { DecorativeDash, Link } from 'atoms'
