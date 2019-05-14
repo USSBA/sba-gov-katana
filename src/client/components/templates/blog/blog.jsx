f26b19503d2b50d4ef5bfd075d82a42a57604310
Aaron Brooks <brooksa321@gmail.com>
2019-05-14 10:08:07 -0400

    Fixed href link url

--- src/client/components/templates/blog/blog.jsx
+++ src/client/components/templates/blog/blog.jsx
@@ -10,14 +10,17 @@ class Blog extends Component {
   render() {
     const { blogData } = this.props
     const blogParagraphs = blogData.blogBody
+    const categoryLink =
+      blogData.blogCategory === 'SBA News and Views' ? '/blogs/news-and-views' : '/blogs/industry-word'
     const blogPage = blogParagraphs.map(function(item, index) {
       return (
-        <div key={index.toString()}>
+        <div key={index.toString()} data-testid={'postBlogSection'}>
           {!isEmpty(blogData.blogBody[index].blogSectionImage.url) && (
-            <ImageSection
+            <img
               className={styles.center}
-              data-testid={'postSectionText'}
+              data-testid={'postSectionImage'}
               src={blogData.blogBody[index].blogSectionImage.url}
+              alt={blogData.blogBody[index].blogSectionImage.alt}
             />
           )}
           <div
@@ -31,7 +34,7 @@ class Blog extends Component {
     return (
       <div className={styles.container}>
         <h1 data-testid={'postTitle'}>{blogData.title}</h1>
-        <ByLine blogData={blogData} />
+        <ByLine blogData={blogData} categoryLink={categoryLink} />
         <h5 data-testid={'postSummary'}>{blogData.summary}</h5>
         <hr className={styles.hr} />
         <div data-testid={'postBlogBody'} className={styles.postBlogBody}>
@@ -55,7 +58,7 @@ Blog.propTypes = {
   blogData: PropTypes.object
 }
 
-const ByLine = ({ blogData }) => (
+const ByLine = ({ blogData, categoryLink }) => (
   <div data-testid={'byline'}>
     <p>
       <span data-testid={'postAuthor'}>
@@ -64,18 +67,7 @@ const ByLine = ({ blogData }) => (
       <span data-testid={'postDate'}>on {moment.unix(blogData.created).format('MMMM DD, YYYY')}</span>
       <br />
       <span data-testid={'postCategory'}>
-        Category:{' '}
-        <a
-          href={
-            '/blogs/' +
-            blogData.blogCategory
-              .split(' ')
-              .join('-')
-              .toLowerCase()
-          }
-        >
-          {blogData.blogCategory}
-        </a>
+        Category: <a href={categoryLink}>{blogData.blogCategory}</a>
       </span>
     </p>
   </div>
