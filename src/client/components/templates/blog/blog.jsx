51b484c4b0c2abd7cc2b82a9992c06642c7e0c41
Aaron Brooks <brooksa321@gmail.com>
2019-05-14 14:45:05 -0400

    Feature ta 2276 (#868)
    * Added basic blog page setup
    
    * Allowed for any number of blog images and paragraphs
    
    * center alighed images
    
    * Added testing tags and mechanism for categories to be a link
    
    * Completed Jest Tests
    
    * author card object fix, and sba news and views category page fix
    
    * Center images
    
    * Added testing tags for images and section text
    
    * Fixed href link url
    
    * Added tab index to page elements
    
    * updated tab index

--- src/client/components/templates/blog/blog.jsx
+++ src/client/components/templates/blog/blog.jsx
@@ -8,13 +8,48 @@ import moment from 'moment'
 class Blog extends Component {
   render() {
     const { blogData } = this.props
+    const blogParagraphs = blogData.blogBody
+    const categoryLink =
+      blogData.blogCategory === 'SBA News and Views' ? '/blogs/news-and-views' : '/blogs/industry-word'
+    const blogPage = blogParagraphs.map(function(item, index) {
+      return (
+        <div key={index.toString()} data-testid={'postBlogSection'}>
+          {!isEmpty(blogData.blogBody[index].blogSectionImage.url) && (
+            <img
+              className={styles.center}
+              data-testid={'postSectionImage'}
+              src={blogData.blogBody[index].blogSectionImage.url}
+              alt={blogData.blogBody[index].blogSectionImage.alt}
+              tabIndex="0"
+            />
+          )}
+          <div
+            data-testid={'postSectionText'}
+            tabIndex="0"
+            dangerouslySetInnerHTML={{ __html: blogData.blogBody[index].blogSectionText }}
+          />
+        </div>
+      )
+    })
+
     return (
       <div className={styles.container}>
-        <ByLine blogData={blogData} />
-        <p>{JSON.stringify(blogData)}</p>
+        <h1 data-testid={'postTitle'} tabIndex="0">
+          {blogData.title}
+        </h1>
+        <ByLine blogData={blogData} categoryLink={categoryLink} />
+        <h5 data-testid={'postSummary'} tabIndex="0">
+          {blogData.summary}
+        </h5>
+        <hr className={styles.hr} />
+        <div data-testid={'postBlogBody'} className={styles.postBlogBody}>
+          {blogPage}
+        </div>
         <br />
-        <p>AuthorCard.border=true</p>
+        <hr className={styles.hr} />
+        <h3 data-testid={'postAuthorSectionTitle'}>About the author</h3>
         <AuthorCard {...blogData.author} />
+        <br />
       </div>
     )
   }
@@ -28,15 +63,19 @@ Blog.propTypes = {
   blogData: PropTypes.object
 }
 
-const ByLine = ({ blogData }) => (
+const ByLine = ({ blogData, categoryLink }) => (
   <div data-testid={'byline'}>
     <p>
-      <span data-testid={'postAuthor'}>
+      <span data-testid={'postAuthor'} tabIndex="0">
         By <a href={blogData.author.url}>{blogData.author.name}</a>
       </span>{' '}
-      <span data-testid={'postDate'}>on {moment.unix(blogData.created).format('MM/DD/YYYY')}</span>
+      <span data-testid={'postDate'} tabIndex="0">
+        on {moment.unix(blogData.created).format('MMMM DD, YYYY')}
+      </span>
       <br />
-      <span data-testid={'postCategory'}>Category: {blogData.blogCategory}</span>
+      <span data-testid={'postCategory'} tabIndex="0">
+        Category: <a href={categoryLink}>{blogData.blogCategory}</a>
+      </span>
     </p>
   </div>
 )
--- test/jest/client/components/pages/blog-page/blog-page.test.js
+++ test/jest/client/components/pages/blog-page/blog-page.test.js
@@ -12,7 +12,16 @@ import * as fetchContentHelper from 'client/fetch-content-helper.js'
 
 const mockBlogData = {
   author: 18024,
-  blogBody: {},
+  blogBody: [
+    {
+      id: 20260,
+      type: 'blogPost',
+      blogSectionImage: {},
+      blogSectionText:
+        '<p>Working in the <a href="https://www.sba.gov/offices/headquarters/oit">Office of International Trade</a> at the <a href="https://www.sba.gov/">Small Business Administration</a> (SBA), we are inspired every time we hear a successful export story from an U.S. small business. &nbsp;This is particularly true when they have used the services of SBA or one of our Federal or state partners in export promotion.&nbsp;</p>\r\n\r\n<p>That is one of the reasons why the SBA will be participating in the 2019 National Small Business Exporter Summit taking place April 9 in Savannah, Georgia.&nbsp; The National Small Business Exporter Summit is a unique training and networking event that features actual small business exporters sharing their success stories with other small businesses, educators, counselors, and stakeholders.</p>\r\n\r\n<p>It is an excellent forum to learn about international trade from <a href="///C:/Users/JHDavis/AppData/Local/Packages/Microsoft.MicrosoftEdge_8wekyb3d8bbwe/TempState/Downloads/nasbite.org/annual-conference/wp-content/uploads/2019/03/2019-Summit-Presenter-Honoree-Writeups-3-23-19.pdf">top small and medium-sized exporters</a> from across the United States. &nbsp;Sessions will highlight best-practices and lessons learned that have contributed to each company’s export success. &nbsp;Summit attendees will leave armed with insights into practical tools, techniques, strategies, and resources they can leverage to guide export expansion.&nbsp;</p>\r\n\r\n<p>Federal government agencies that promote exporting opportunities provide impactful services for small businesses selling to international customers.&nbsp;&nbsp; Still, as much as we try to get the word out, the best testimony for our services come from real small business exporters.&nbsp; And there are several Commerce Export Award winners and SBA Exporters of the Year in the mix of honorees and presenters at this year’s event.</p>\r\n\r\n<p>Again, SBA will be there, along with other Federal export service providers.&nbsp; So, if you do make it, please come visit us in the exhibition area.&nbsp; We look forward to partnering with you to reach new markets and find global customers.&nbsp;</p>\r\n\r\n<p>If you are interested, <a href="https://nasbite.org/annual-conference/registration/">register online here</a>.</p>\r\n\r\n<p>If you cannot attend, the SBA and our federal partners have programs to help you expand into new markets.&nbsp; Here are a few resources to assist you with growing your business through international sales:</p>\r\n\r\n<ul>\r\n\t<li>To assess your current export readiness, check out these <a href="https://www.mbda.gov/news/blog/2013/09/export-readiness-assessment-tools">Export Readiness Assessment Tools</a>.</li>\r\n\t<li>Visit <a href="https://www.export.gov/How-to-Export">Export.gov</a> for videos with tips to getting started and links to other resources available to small business exporters and those aspiring to sell products and services overseas.</li>\r\n\t<li>Need help with your export business planning?&nbsp; Consult with your local <a href="https://www.sba.gov/local-assistance/find/?type=Small%20Business%20Development%20Center&amp;pageNumber=1">Small Business Development Center</a> (SBDC).</li>\r\n\t<li>Are you an established exporter looking to expand into new foreign markets?&nbsp; The Department of Commerce staffs <a href="https://2016.export.gov/usoffices/index.asp">U.S. Export Assistance Centers</a> around the country that provide assistance with market research and entry.</li>\r\n\t<li>Already have export business that needs financing?&nbsp; Reach out to one of our <a href="https://www.sba.gov/managing-business/exporting/us-export-assistance-centers%20">SBA International Trade Finance Specialists</a> at U.S. Export Assistance Centers around the country.</li>\r\n</ul>\r\n'
+    }
+  ],
+  blogCategory: 'SBA News and Views',
   blogTags: 'International',
   office: {},
   summary: 'Some stuff',
@@ -159,6 +168,29 @@ describe('Blog page', () => {
       content = await waitForElement(() => getByTestId('postDate'))
       expect(content).toBeInTheDocument()
       content = await waitForElement(() => getByTestId('postCategory'))
+      expect(content).toBeInTheDocument()
+    })
+    it('should contain postTitle, postSummary and postAuthorSectionTitle', async () => {
+      const fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
+      fetchRestContentStub.mockImplementation(fetchRestContentStubCallback)
+
+      const initialState = undefined
+      const enhancer = applyMiddleware(thunk)
+      const store = createStore(reducers, initialState, enhancer)
+
+      const { getByTestId } = render(
+        <Provider store={store}>
+          <BlogPage id="1" />
+        </Provider>
+      )
+
+      let content = await waitForElement(() => getByTestId('postTitle'))
+      expect(content).toBeInTheDocument()
+      content = await waitForElement(() => getByTestId('postSummary'))
+      expect(content).toBeInTheDocument()
+      content = await waitForElement(() => getByTestId('postBlogBody'))
+      expect(content).toBeInTheDocument()
+      content = await waitForElement(() => getByTestId('postAuthorSectionTitle'))
       expect(content).toBeInTheDocument()
     })
   })
