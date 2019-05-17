122c6c2d6d98437014a459652a162b8e9ac33d06
Kevin Lee <ki.lee@sba.gov>
2019-05-17 12:55:56 -0400

    T1-1331 Add test-ids

--- src/client/components/atoms/form-error-message/form-error-message.jsx
+++ src/client/components/atoms/form-error-message/form-error-message.jsx
@@ -4,7 +4,11 @@ import styles from './form-error-message.scss'
 class FormErrorMessage extends React.Component {
   render() {
     return (
-      <p id={this.props.errorFor + '-error'} className={styles.errorText}>
+      <p
+        id={this.props.errorFor + '-error'}
+        className={styles.errorText}
+        data-testid={this.props.errorFor + '-error'}
+      >
         {this.props.errorText}
       </p>
     )
