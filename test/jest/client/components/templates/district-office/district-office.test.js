2ecac2361744e0c1a4e9e80da912a2780ac5b420
Pat Kelsh <pkelsh@gmail.com>
2019-08-12 14:22:34 -0400

    TA-2918 cleaned up test imports

--- test/jest/client/components/templates/district-office/district-office.test.js
+++ test/jest/client/components/templates/district-office/district-office.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, cleanup, waitForElement } from 'react-testing-library'
 import 'jest-dom/extend-expect'
-import axiosMock from 'axios'
 
 import DistrictOfficeTemplate from 'templates/district-office/district-office.jsx'
 
@@ -15,7 +14,6 @@ describe('District Office template', () => {
   it('renders the newsletter sign up component', () => {
     const { getByTestId } = render(<DistrictOfficeTemplate office={mockOfficeData} />)
 
-    // const content = await waitForElement(() => getByTestId('office-newsletter'))
     const content = getByTestId('office-newsletter')
     expect(content).toBeInTheDocument()
   })
