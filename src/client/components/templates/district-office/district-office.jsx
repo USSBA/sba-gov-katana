4a34e069f20be221889103acb1013216619873e3
Pat Kelsh <pkelsh@fearless.tech>
2019-08-14 10:55:09 -0400

    TA-2921 created lender match component (#914)
    * TA-2921 created lender match component
    
    * TA-2921 style updates

--- src/client/components/templates/district-office/district-office.jsx
+++ src/client/components/templates/district-office/district-office.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Button } from 'atoms'
 import { CallToAction, NewsletterForm } from 'molecules'
 import styles from './district-office.scss'
 
@@ -9,11 +10,15 @@ class DistrictOfficeTemplate extends React.Component {
     return (
       <div className={styles.content}>
         <p>{office.title}</p>
+
         <div className={styles.section}>
           <NewsletterSignup />
         </div>
         <div className={styles.section}>
           <CTA />
+        </div>
+        <div className={styles.section}>
+          <LenderMatch />
         </div>
       </div>
     )
@@ -49,6 +54,26 @@ const CTA = () => {
       buttonAction={buttonAction}
       image={image}
     />
+  )
+}
+
+const LenderMatch = () => {
+  const lenderHeadline = 'Lender Match'
+  const lenderBlurb =
+    'Lender Match is a free online referral tool that connects small businesses with particpating SBA-approved lenders'
+  const lenderButton = {
+    url: '/lendermatch',
+    title: 'Learn More'
+  }
+
+  return (
+    <div className={styles.lenderMatch} data-testid={'office-lender-match'}>
+      <h3>{lenderHeadline}</h3>
+      <p>{lenderBlurb}</p>
+      <Button primary alternate url={lenderButton.url}>
+        {lenderButton.title}
+      </Button>
+    </div>
   )
 }
 
