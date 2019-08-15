3e3ddbcb4863e073a945a63e20d4b901738521aa
Amy DiPasquale <adipasquale@fearless.tech>
2019-08-15 16:53:50 -0400

    TA-2919 styling for mobile, fix formatting code indentation

--- src/client/components/templates/district-office/district-office.jsx
+++ src/client/components/templates/district-office/district-office.jsx
@@ -8,47 +8,47 @@ import styles from './district-office.scss'
 
 class DistrictOfficeTemplate extends React.Component {
   constructor() {
-		super()
-		this.state = {
-			events: []
-		}
-	}
+    super()
+    this.state = {
+      events: []
+    }
+  }
 
-	async componentDidMount() {
-		const { office } = this.props
-		const { items } = await fetchSiteContent('events', {
-			pageSize: 5,
-			officeId: office.id
-		})
-		// when the events content api is set to D8, then pageSize=5 will do the work for us
-		// but since the events content api is set to D7, slice the first 5 items off the response
-		const events = items.slice(0, 5)
-		this.setState({ events })
-	}
+  async componentDidMount() {
+    const { office } = this.props
+    const { items } = await fetchSiteContent('events', {
+      pageSize: 5,
+      officeId: office.id
+    })
+    // when the events content api is set to D8, then pageSize=5 will do the work for us
+    // but since the events content api is set to D7, slice the first 5 items off the response
+    const events = items.slice(0, 5)
+    this.setState({ events })
+  }
   render() {
     const { events } = this.state
-		const { office } = this.props
+    const { office } = this.props
     return (
-    	<div>
-		    <div className={styles.content}>
-		        <p>{office.title}</p>
+      <div>
+        <div className={styles.content}>
+          <p>{office.title}</p>
 
-		        <div className={styles.section}>
-		          <NewsletterSignup />
-		        </div>
-	    	</div>
-	        <div className={styles.section}>
-	          <Events items={events} />
-	        </div>
-	        <div className={styles.content}>
-		        <div className={styles.section}>
-		          <CTA />
-		        </div>
-		        <div className={styles.section}>
-		          <LenderMatch />
-		        </div>
-	      	</div>
-	    </div>
+          <div className={styles.section}>
+            <NewsletterSignup />
+          </div>
+        </div>
+        <div className={styles.section}>
+          <Events items={events} />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.section}>
+            <CTA />
+          </div>
+          <div className={styles.section}>
+            <LenderMatch />
+          </div>
+        </div>
+      </div>
     )
   }
 }
@@ -63,17 +63,21 @@ const NewsletterSignup = () => {
 }
 
 const Events = ({ items }) => {
-	return (
-		<div data-testid="events" className={styles.events}>
-			<h2>Upcoming events and workshops</h2>
-			{ items.length > 0 && <Results items={items}><EventResult /></Results>}
-			<div className={styles.button} data-testid="events-button">
-				<a href="/events/">
-					<Button primary>Find More Events</Button>
-				</a>
-			</div>
-		</div>
-	)
+  return (
+    <div data-testid="events" className={styles.events}>
+      <h2>Upcoming events and workshops</h2>
+      {items.length > 0 && (
+        <Results items={items}>
+          <EventResult />
+        </Results>
+      )}
+      <div className={styles.button} data-testid="events-button">
+        <a href="/events/">
+          <Button primary>Find More Events</Button>
+        </a>
+      </div>
+    </div>
+  )
 }
 
 const CTA = () => {
