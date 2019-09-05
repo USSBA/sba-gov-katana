19876dcdb4b9c07968a26f79e9946d69f1c04f8b
Amy DiPasquale <adipasquale@fearless.tech>
2019-09-05 16:04:53 -0400

    TA-2912 add tests for generic card collection

--- test/jest/client/components/organisms/generic-card-collection/generic-card-collection.test.jsx
+++ test/jest/client/components/organisms/generic-card-collection/generic-card-collection.test.jsx
@@ -22,7 +22,27 @@ describe('Generic card collection', () => {
     expect(genericCardCollection).toBeInTheDocument()
   })
 
-  it('will render the card container for each component in the array for cardsContent', () => {
+  it('does NOT render a card container component if there is no cardsContent prop', () => {
+    const { queryAllByTestId } = render(<GenericCardCollection />)
+
+    const expectedNumberOfCardContainers = 0
+    const cardContainers = queryAllByTestId('card-container')
+    expect(cardContainers.length).toEqual(expectedNumberOfCardContainers)
+  })
+
+  it('will render the card container for one component in the array for cardsContent', () => {
+    const cardContent = <MockComponent />
+    const cardsContent = []
+    cardsContent.push(cardContent)
+
+    const { queryAllByTestId } = render(<GenericCardCollection cardsContent={cardsContent} />)
+
+    const expectedNumberOfCardContainers = cardsContent.length
+    const cardContainers = queryAllByTestId('card-container')
+    expect(cardContainers.length).toEqual(expectedNumberOfCardContainers)
+  })
+
+  it('will render the card container for each component when there are multiple components in cardsContent', () => {
     const cardContent = <MockComponent />
     const cardsContent = []
     cardsContent.push(cardContent)
