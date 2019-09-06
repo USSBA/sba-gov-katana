0b22dc6457cc559f2cfca5945e58fbfb1e62bfac
Amy DiPasquale <adipasquale@fearless.tech>
2019-09-06 11:20:51 -0400

    TA-2912 remove unhelpful line in test

--- test/jest/client/components/templates/person/person.test.jsx
+++ test/jest/client/components/templates/person/person.test.jsx
@@ -98,7 +98,6 @@ describe('Person page', () => {
     const subComponent = component.find('ContactCard')
     const subComponentProps = subComponent.getElement().props
 
-    expect(Object.keys(subComponentProps).length).toEqual(4)
     expect(subComponentProps.phoneNumber).toEqual('205-290-7009')
     expect(subComponentProps.email).toEqual('thomas.todt@sba.gov')
   })
