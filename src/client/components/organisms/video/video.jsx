aa82a4d86edd72519caa7dcfc5a4fa88048b15db
Kevin Lee <ki.lee@sba.gov>
2019-09-24 16:04:53 -0400

    T1-1446 Add video component for video paragraph type

---
+++ src/client/components/organisms/video/video.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from './video.scss'
+
+class Video extends React.Component {
+  render() {
+    return (
+      <div
+        className="video"
+        style={{
+          position: 'relative',
+          paddingBottom: '56.25%' /* 16:9 */,
+          paddingTop: 25,
+          height: 0
+        }}
+      >
+        <iframe
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          }}
+          src={`https://www.youtube.com/embed/${this.props.youtubeId}`}
+          frameBorder="0"
+        />
+      </div>
+    )
+  }
+}
+
+export default Video
