c975fb31e7bb40ab07856e05ba860b52a353046f
cdcampbell24 <34345885+cdcampbell24@users.noreply.github.com>
2019-09-30 14:22:16 -0400

    made styling changes to district office page (#943)

--- src/client/components/organisms/success-stories/success-stories.jsx
+++ src/client/components/organisms/success-stories/success-stories.jsx
@@ -24,12 +24,11 @@ class SuccessStories extends PureComponent {
     if (blogs.length > 0) {
       blogs = blogs.slice(0, 3)
     }
-    
+
     this.setState({ blogs })
   }
 
   render() {
-
     const { blogs } = this.state
     const { officeId } = this.props
 
@@ -49,25 +48,24 @@ class SuccessStories extends PureComponent {
       }
       return (
         <div data-testid={`success-story-card`} key={index}>
-          <Card
-            index={index}
-            item={item}
-            numCards={3}
-          />
-        </div>)
-      })
+          <Card index={index} item={item} numCards={3} />
+        </div>
+      )
+    })
     return (
       <div>
-        {cards.length > 0 && <div data-testid={'success-stories'} className={styles.successStories}>
-          <h2>Success Stories posts</h2>
-          {cards}
-          <div className={styles.clear} />
-          <div className={styles.button} data-testid="success-stories-button">
-            <a href={`/blogs/success-stories/${officeId}`}>
-              <Button primary>View All Posts</Button>
-            </a>
+        {cards.length > 0 && (
+          <div data-testid={'success-stories'} className={styles.successStories}>
+            <h2>Success Story posts</h2>
+            {cards}
+            <div className={styles.clear} />
+            <div className={styles.button} data-testid="success-stories-button">
+              <a href={`/blogs/success-stories/${officeId}`}>
+                <Button primary>View All Posts</Button>
+              </a>
+            </div>
           </div>
-        </div>}
+        )}
       </div>
     )
   }
