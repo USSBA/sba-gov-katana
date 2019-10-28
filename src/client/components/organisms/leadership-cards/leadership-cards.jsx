ea6939549682173d555d819357b4223c76e97004
Avery Smith <avery@averysmith.pro>
2019-10-28 14:49:46 -0400

    TA-3203 added leadership cards and tests

---
+++ src/client/components/organisms/leadership-cards/leadership-cards.jsx
@@ -0,0 +1,51 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { GenericCardCollection } from 'organisms'
+import { AuthorCard } from 'molecules'
+import styles from './leadership-cards.scss'
+import { fetchRestContent } from '../../../fetch-content-helper'
+
+class LeadershipCards extends PureComponent {
+	constructor() {
+		super()
+		this.state = {
+		  leaders: []
+		}
+	}
+
+	async componentDidMount() {
+		const { data } = this.props
+
+		let leaders = []
+		for (let item in data) {
+			leaders.push(await fetchRestContent(data[item].person))
+		}
+
+		this.setState({ leaders })
+	}
+	
+	render() {
+		const { leaders } = this.state
+
+		const cards = leaders.map(({ name, title, shortBio, url }, index) => {
+			return (
+			<div data-testid={'leader-card'} key={index} className={styles.threeColumn}>
+				<AuthorCard name={name} title={title} shortBio={shortBio} url={url} />
+			</div>
+			)
+		})
+		return (
+			<div className={styles.leadership}>
+				<h3>Leadership</h3>
+				{cards}
+				<div className={styles.clear} />
+			</div>
+		)
+	}
+}
+
+LeadershipCards.propTypes = {
+	"data": PropTypes.array
+}
+
+export default LeadershipCards
---
+++ test/jest/client/components/organisms/leadership-cards/leadership-cards.test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { render, cleanup, waitForElement, within } from 'react-testing-library'
+import { LeadershipCards } from 'organisms'
+import { when } from 'jest-when'
+import 'jest-dom/extend-expect'
+import * as fetchContentHelper from 'client/fetch-content-helper.js'
+import data from '../../test-data/leadership-cards/leadership-cards.json'
+import persons from '../../test-data/leadership-cards/persons.json'
+
+describe('LeadershipCards', () => {
+	let fetchRestContentStub
+
+	beforeEach(function() {
+	  fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
+
+	  const { leadership } = data.paragraphs[1]
+
+	  when(fetchRestContentStub)
+	    .calledWith(leadership[0].person)
+	    .mockImplementationOnce(() => persons[0])
+
+	  when(fetchRestContentStub)
+	    .calledWith(leadership[1].person)
+	    .mockImplementationOnce(() => persons[1])
+
+	  when(fetchRestContentStub)
+	    .calledWith(leadership[2].person)
+	    .mockImplementationOnce(() => persons[2])
+	})
+
+	afterEach(function() {
+	  fetchRestContentStub.mockReset()
+	  cleanup()
+	})
+
+	it('should display 3 LeadershipCards', async () => {
+		const { getAllByTestId } = render(<LeadershipCards data={data.paragraphs[1].leadership} />)
+		const leaders = await waitForElement(() => getAllByTestId('leader-card'))
+		expect(leaders.length).toEqual(3)
+	})
+	it('should display no LeadershipCards', async () => {
+		const { queryByTestId } = render(<LeadershipCards />)
+		const leaders = queryByTestId('leader-card')
+		expect(leaders).not.toBeInTheDocument()
+	})
+})
