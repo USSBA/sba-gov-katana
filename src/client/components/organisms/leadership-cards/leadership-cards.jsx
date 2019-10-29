056a0f6de7f1338cc39f1fd37176d5620dedd3ce
Avery Smith <avery@averysmith.pro>
2019-10-29 00:15:09 -0400

    TA-3203 added guard clause for null persons refactored to utilize GenericCardCollection

--- src/client/components/organisms/leadership-cards/leadership-cards.jsx
+++ src/client/components/organisms/leadership-cards/leadership-cards.jsx
@@ -1,8 +1,10 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { AuthorCard } from 'molecules'
+import { GenericCardCollection } from 'organisms'
 import styles from './leadership-cards.scss'
 import { fetchRestContent } from '../../../fetch-content-helper'
+import { compact, chunk } from 'lodash'
 
 class LeadershipCards extends PureComponent {
 	constructor() {
@@ -14,32 +16,48 @@ class LeadershipCards extends PureComponent {
 
 	async componentDidMount() {
 		const { data } = this.props
-
+		
 		let leaders = []
 		for (let item in data) {
 			leaders.push(await fetchRestContent(data[item].person))
 		}
-
 		this.setState({ leaders })
 	}
 	
 	render() {
 		const { leaders } = this.state
-
-		const cards = leaders.map(({ name, title, shortBio, url }, index) => {
+		const items = compact(leaders)
+		const cardsContent = items.map(({ name, title, shortBio, url }, index) => {
 			return (
-			<div data-testid={'leader-card'} key={index} className={styles.threeColumn}>
-				<AuthorCard name={name} title={title} shortBio={shortBio} url={url} />
+			<div data-testid={'leader-card'} className={styles.leaderCard} key={index}>
+				<AuthorCard name={name} title={title} shortBio={shortBio} url={url} border={false} mode={'grid'} />
 			</div>
 			)
 		})
-		return (
-			<div className={styles.leadership}>
-				<h3>Leadership</h3>
-				{cards}
-				<div className={styles.clear} />
-			</div>
-		)
+
+		let html = null
+		if (cardsContent.length === 4) {
+			const cardsContentSections = chunk(cardsContent, 2)
+			html = (
+				<div>
+					<h3>Leadership</h3>
+					{cardsContentSections.map((cardsContentSection, index) => (
+						<div key={index}>
+							<GenericCardCollection cardsContent={cardsContentSection} />
+						</div>
+					))}
+				</div>
+			)
+		} else {
+			html = (
+				<div>
+					<h3>Leadership</h3>
+					<GenericCardCollection cardsContent={cardsContent} />
+		        </div>
+	        )
+		}
+
+		return html
 	}
 }
 
