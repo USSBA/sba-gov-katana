c99fd3c5aa3d870ad4b7f0d4121265e9a17cfd71
Amy DiPasquale <adipasquale@fearless.tech>
2019-11-12 14:02:53 -0500

    TA-3334 add tab indicies for hero

--- src/client/components/organisms/hero/hero.jsx
+++ src/client/components/organisms/hero/hero.jsx
@@ -50,8 +50,10 @@ class Hero extends React.Component {
           alt={alt}
         >
           <div className={styles.callout} ref={ref => (this.callout = ref)}>
-            <h1 data-testid="title">{title}</h1>
-            <h2 className={styles.message} data-testid="message">
+            <h1 tabIndex="0" data-testid="title">
+              {title}
+            </h1>
+            <h2 className={styles.message} tabIndex="0" data-testid="message">
               {message}
             </h2>
             {buttons &&
