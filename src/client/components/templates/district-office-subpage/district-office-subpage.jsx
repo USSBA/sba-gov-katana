b952559b652766ed43fe97c78ae3e2b7ef7f7833
Avery Smith <avery@averysmith.pro>
2019-12-02 17:16:34 -0500

    TA-3314 created subpage navigation and unit tests

--- src/client/components/templates/district-office-subpage/district-office-subpage.jsx
+++ src/client/components/templates/district-office-subpage/district-office-subpage.jsx
@@ -6,180 +6,228 @@ import { listenForOverlap } from 'element-overlap'
 import classNames from 'classnames'
 import { Loader } from 'atoms'
 import { TitleSection } from 'molecules'
-import { SectionNav } from 'organisms'
 import basicPageStyles from '../basic-page/basic-page.scss'
 import styles from './district-office-subpage.scss'
+import sectionNavStyles from '../../organisms/section-nav/section-nav.scss'
 import { fetchRestContent, fetchSiteContent } from '../../../fetch-content-helper.js'
 import * as paragraphMapper from '../paragraph-mapper.jsx'
 import { getLanguageOverride } from '../../../services/utils.js'
 
 class DistrictOfficeSubPage extends Component {
+  constructor() {
+    super()
+    this.state = {
+      sectionTitle: '',
+      data: {},
+      loadingState: 'unloaded',
+      slideLeftNavIn: false,
+      slideContentIn: false,
+      displayMobileNav: false,
+      currentPosition: 'top'
+    }
 
-	constructor() {
-	    super()
-	    this.state = {
-	      data: {},
-	      loadingState: 'unloaded',
-	      slideLeftNavIn: false,
-	      slideContentIn: false,
-	      displayMobileNav: false,
-	      currentPosition: 'top'
-	    }
+    this.handleSectionNavigationEnter = this.handleSectionNavigationEnter.bind(this)
+    this.handleTopWaypointEnter = this.handleTopWaypointEnter.bind(this)
+    this.handleTopWaypointLeave = this.handleTopWaypointLeave.bind(this)
+    this.handleBackLinkClicked = this.handleBackLinkClicked.bind(this)
+    this.handleOverlap = this.handleOverlap.bind(this)
+  }
+  async componentDidMount() {
+    const { pageConnectorId } = this.props.params
 
-	    this.handleSectionNavigationEnter = this.handleSectionNavigationEnter.bind(this)
-	    this.handleTopWaypointEnter = this.handleTopWaypointEnter.bind(this)
-	    this.handleTopWaypointLeave = this.handleTopWaypointLeave.bind(this)
-	    this.handleBackLinkClicked = this.handleBackLinkClicked.bind(this)
-	    this.handleOverlap = this.handleOverlap.bind(this)
-	}
+    if (pageConnectorId) {
+      this.setState(
+        {
+          loadingState: 'isLoading'
+        },
+        async () => {
+          const { title: sectionTitle, subpages } = await fetchRestContent(pageConnectorId)
+          const data = []
+          for (let i = 0; i < subpages.length; i++) {
+            data.push(await fetchRestContent(subpages[i].id))
+          }
+          this.setState({ sectionTitle, data, loadingState: 'isLoaded' })
+        }
+      )
+    }
 
-	async componentDidMount() {
-	    const { subPageId } = this.props.params
-	    if (subPageId) {
-	      this.setState(
-	        {
-	          loadingState: 'isLoading'
-	        },
-	        async () => {
-	          const data = await fetchRestContent(subPageId)
-	          this.setState({ data, loadingState: 'isLoaded' })
-	        }
-	      )
-	    }
+    listenForOverlap('#article-navigation-desktop', '#sba-footer', this.handleOverlap, {
+      listenOn: 'scroll'
+    })
+  }
+  handleBackLinkClicked(e) {
+    e.preventDefault()
+    this.setState({
+      slideLeftNavIn: false,
+      slideContentIn: false,
+      displayMobileNav: true
+    })
+  }
+  handleTopWaypointEnter() {
+    this.setState({ currentPosition: 'top' })
+  }
+  handleTopWaypointLeave() {
+    this.setState({ currentPosition: 'middle' })
+  }
+  handleBottomWaypointLeave() {
+    this.setState({ currentPosition: 'middle' })
+  }
+  handleSectionNavigationEnter() {
+    if (this.state.currentPosition === 'bottom') {
+      this.setState({ currentPosition: 'middle' })
+    }
+  }
+  handleOverlap() {
+    this.setState({ currentPosition: 'bottom' })
+  }
+  makeSectionHeaders(paragraphData) {
+    /* eslint-disable-next-line array-callback-return */
+    const sectionHeaders = paragraphData.map(function(item, index, paragraphArray) {
+      if (item && item.type && item.type === 'sectionHeader') {
+        return {
+          id: paragraphMapper.makeSectionHeaderId(index),
+          text: item.text
+        }
+      }
+    })
 
-	    /*listenForOverlap('#article-navigation-desktop', '#sba-footer', this.handleOverlap, {
-	      listenOn: 'scroll'
-	    })*/
-	}
+    return compact(sectionHeaders)
+  }
+  makeParagraphs(paragraphData) {
+    const paragraphList = paragraphMapper.makeParagraphs(paragraphData)
+    const wrapperClassMapping = {
+      other: basicPageStyles.textSection,
+      textSection: basicPageStyles.textSection,
+      textReadMoreSection: 'none',
+      sectionHeader: basicPageStyles.sectionHeader,
+      subsectionHeader: basicPageStyles.subsectionHeader,
+      image: basicPageStyles.image,
+      lookup: basicPageStyles.lookup,
+      callToAction: basicPageStyles.callToAction,
+      cardCollection: basicPageStyles.cardCollection,
+      styleGrayBackground: basicPageStyles.textSection
+    }
+    const wrapped = paragraphMapper.wrapParagraphs(paragraphList, wrapperClassMapping)
+    return wrapped
+  }
+  render() {
+    const { officeId, pageConnectorId, subPageId } = this.props.params
+    const { sectionTitle, data, loadingState, currentPosition, displayMobileNav } = this.state
+    let currentPage, className, langCode, paragraphs, sectionHeaders
 
-	makeSectionHeaders(paragraphData) {
-		/* eslint-disable-next-line array-callback-return */
-		const sectionHeaders = paragraphData.map(function(item, index, paragraphArray) {
-		  if (item && item.type && item.type === 'sectionHeader') {
-		    return {
-		      id: paragraphMapper.makeSectionHeaderId(index),
-		      text: item.text
-		    }
-		  }
-		})
+    if (!isEmpty(data)) {
+      currentPage = data.find(item => String(item.id) === subPageId) || data[0]
+      langCode = getLanguageOverride()
+      paragraphs = this.makeParagraphs(currentPage.paragraphs)
+      sectionHeaders = this.makeSectionHeaders(currentPage.paragraphs)
+      className = classNames({
+        'district-office-subpage-titlesection': true,
+        [styles.content]: true
+      })
+    }
 
-		return compact(sectionHeaders)
-	}
-
-	makeParagraphs(paragraphData) {
-		const paragraphList = paragraphMapper.makeParagraphs(paragraphData)
-		const wrapperClassMapping = {
-			other: basicPageStyles.textSection,
-			textSection: basicPageStyles.textSection,
-			textReadMoreSection: 'none',
-			sectionHeader: basicPageStyles.sectionHeader,
-			subsectionHeader: basicPageStyles.subsectionHeader,
-			image: basicPageStyles.image,
-			lookup: basicPageStyles.lookup,
-			callToAction: basicPageStyles.callToAction,
-			cardCollection: basicPageStyles.cardCollection,
-			styleGrayBackground: basicPageStyles.textSection
-		}
-		const wrapped = paragraphMapper.wrapParagraphs(paragraphList, wrapperClassMapping)
-		return wrapped
-	}
-
-	handleBackLinkClicked(e) {
-		e.preventDefault()
-		this.setState({
-		  slideLeftNavIn: false,
-		  slideContentIn: false,
-		  displayMobileNav: true
-		})
-	}
-
-	handleTopWaypointEnter() {
-		this.setState({ currentPosition: 'top' })
-	}
-
-	handleTopWaypointLeave() {
-		this.setState({ currentPosition: 'middle' })
-	}
-
-	handleBottomWaypointLeave() {
-		this.setState({ currentPosition: 'middle' })
-	}
-
-	handleSectionNavigationEnter() {
-		if (this.state.currentPosition === 'bottom') {
-		  this.setState({ currentPosition: 'middle' })
-		}
-	}
-
-	handleOverlap() {
-		this.setState({ currentPosition: 'bottom' })
-	}
-
-	sectionNavigation(langCode) {
-		return this.props.lineage ? (
-		  <SectionNav
-		    onTopEnter={this.handleSectionNavigationEnter}
-		    position={this.state.currentPosition}
-		    displayMobileNav={this.state.displayMobileNav}
-		    lineage={null}
-		    langCode={langCode}
-		  />
-		) : (
-		  <div />
-		)
-	}
-
-	render() {
-		const { data, loadingState } = this.state
-		let className, langCode, paragraphs, sectionHeaders
-
-		if (!isEmpty(data)) {
-			langCode = getLanguageOverride()
-			paragraphs = this.makeParagraphs(data.paragraphs)
-			sectionHeaders = this.makeSectionHeaders(data.paragraphs)
-			className = classNames({
-			    "district-office-subpage-titlesection": true,
-			    [styles.content]: true
-			})
-		}
-
-		return (
-			<div>
-				{loadingState === 'isLoading' && <div className={styles.loaderContainer}><Loader /></div>}
-				{loadingState === 'isLoaded' && (
-		          <div>
-		            {!isEmpty(data) ? (
-		            	<div>
-		            		<Waypoint
-					          topOffset="30px"
-					          onEnter={this.handleTopWaypointEnter}
-					          onLeave={this.handleTopWaypointLeave}
-					        />
-					        <div className="district-office-subpage-sectionnavigation">{this.sectionNavigation(langCode)}</div>
-		              		<div data-testid="district-office-subpage-titlesection" className={className}>
-								<TitleSection
-									key={1}
-									gridClass={basicPageStyles.titleSection}
-									sectionHeaders={sectionHeaders}
-									title={data.title}
-									summary={data.summary}
-								/>{' '}
-								{paragraphs}
-							</div>
-		            	</div>
-		            ) : (
-		              <div>No Data Found</div>
-		            )}
-		          </div>
-		        )}
-			</div>
-		)
-	}
+    return (
+      <div>
+        {loadingState === 'isLoading' && (
+          <div className={styles.loaderContainer}>
+            <Loader />
+          </div>
+        )}
+        {loadingState === 'isLoaded' && (
+          <div>
+            {!isEmpty(data) ? (
+              <div>
+                <Waypoint
+                  topOffset="30px"
+                  onEnter={this.handleTopWaypointEnter}
+                  onLeave={this.handleTopWaypointLeave}
+                />
+                <div
+                  data-testid="district-office-subpage-sectionnavigation"
+                  className="district-office-subpage-sectionnavigation"
+                >
+                  <SectionNav
+                    onTopEnter={this.handleSectionNavigationEnter}
+                    position={currentPosition}
+                    displayMobileNav={displayMobileNav}
+                    data={data}
+                    sectionTitle={sectionTitle}
+                    langCode={langCode}
+                    officeId={officeId}
+                    pageConnectorId={pageConnectorId}
+                    subPageId={!isEmpty(subPageId) ? subPageId : data[0].id}
+                  />
+                </div>
+                <div data-testid="district-office-subpage-titlesection" className={className}>
+                  <TitleSection
+                    key={1}
+                    gridClass={basicPageStyles.titleSection}
+                    sectionHeaders={sectionHeaders}
+                    title={currentPage.title}
+                    summary={currentPage.summary}
+                  />{' '}
+                  {paragraphs}
+                </div>
+              </div>
+            ) : (
+              <div>No Data Found</div>
+            )}
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
 DistrictOfficeSubPage.propTypes = {
   params: PropTypes.object,
   lineage: PropTypes.object
+}
+
+const SectionNav = ({ officeId, pageConnectorId, subPageId, sectionTitle, data, position, onTopEnter }) => {
+  const stickyFunctionTop = () => {
+    return position === 'middle' ? sectionNavStyles.stickyTop : null
+  }
+  const stickyFunctionBottom = () => {
+    return position === 'bottom' ? sectionNavStyles.stickyBottom : null
+  }
+  const makeNavLinks = (_data, id) => {
+    const result = _data.map(function(item, index) {
+      const currentLinkClass = String(id) === String(item.id) ? sectionNavStyles.currentNavLink : ''
+      return (
+        <li className={currentLinkClass} key={index}>
+          <a
+            className="article-navigation-article-link-desktop"
+            id={'desktop-article-link-' + index}
+            href={`/offices/district/${officeId}/${pageConnectorId}/${item.id}`}
+          >
+            {item.title}
+          </a>
+        </li>
+      )
+    })
+    return result
+  }
+  const navLinks = makeNavLinks(data, subPageId)
+  return (
+    <div
+      id="article-navigation-desktop"
+      className={sectionNavStyles.sectionNav + ' ' + stickyFunctionTop() + ' ' + stickyFunctionBottom()}
+    >
+      <Waypoint topOffset="30px" onEnter={onTopEnter} />
+      <a
+        id="article-navigation-back-button-desktop"
+        className={sectionNavStyles.backLink}
+        href={`/offices/district/${officeId}/`}
+      >
+        Back to District Office Main Page
+      </a>
+      <span id="article-navigation-title-desktop">
+        <h3>{sectionTitle}</h3>
+      </span>
+      <ul>{navLinks}</ul>
+    </div>
+  )
 }
 
 export default DistrictOfficeSubPage
--- test/jest/client/components/templates/district-office-subpage/district-office-subpage.test.js
+++ test/jest/client/components/templates/district-office-subpage/district-office-subpage.test.js
@@ -1,54 +1,201 @@
 import React from 'react'
-import { render, cleanup, waitForElement, within } from 'react-testing-library'
+import { render, cleanup, waitForElement } from 'react-testing-library'
 import DistrictOfficeSubPageTemplate from 'templates/district-office-subpage/district-office-subpage.jsx'
 import { when, resetAllWhenMocks } from 'jest-when'
 import 'jest-dom/extend-expect'
 import * as fetchContentHelper from 'client/fetch-content-helper.js'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from 'client/reducers'
 
+const mockSubPageData = [
+  {
+    paragraphs: [
+      {
+        id: 22889,
+        type: 'sectionheader',
+        text: 'about us'
+      },
+      {
+        id: 22890,
+        type: 'textsection',
+        text:
+          "<p>welcome to the 2019-2020 edition of the u.s. small business administration’s washington metropolitan area small business resource guide, covering the district of columbia, northern virginia and suburban maryland. with an increasingly diverse and well-educated workforce, the nation's capital and surrounding areas rank among the best locations to develop a successful small business. the sba helps make the american dream of small business ownership a reality. we are the only federal agency dedicated to helping our nation’s 30 million small businesses start, grow, expand, or recover after a disaster.</p>\r\n\r\n<p>over the past year, the sba washington metropolitan area district office has empowered small businesses to:</p>\r\n\r\n<ul>\r\n\t<li>find an ally or mentor via our network of sba-funded resource partners, which includes score, small business development centers, women’s business centers, and the veterans business outreach center.</li>\r\n\t<li>access over $335 million in sba-guaranteed loans through 52 local banks, credit unions, community-based lenders, and microlenders. these qualified borrowers hired over 4,900 new employees, bought needed equipment, and built or renovated facilities.</li>\r\n\t<li>gain more than $4.6 billion in federal contracting awards. the sba offers business development and technical support to nearly 1,000 local companies enrolled in the 8(a) business development program.</li>\r\n\t<li>our region is an innovation hub, home to more than 90,000 small businesses employing over 1 million people. the washington metro area generates a gdp of $530 billion annually, placing it among the 25 top producing countries. just as the american spirit of freedom and independence is deeply tied to our local history, so are we deeply committed to ensuring entrepreneurialism continues to strengthen the economy for everyone, now and into the future. stay informed about sba events near you and get valuable washington metro area business information by following us on twitter at @sba_dcmetro. register for email updates at sba.gov/updates.</li>\r\n</ul>\r\n"
+      }
+    ],
+    summary:
+      'welcome to the 2019-2020 edition of the u.s. small business administration’s washington metropolitan area small business resource guide.',
+    type: 'localresources',
+    title: 'washington metropolitan area resources',
+    id: 20242,
+    updated: 1572629482,
+    created: 1572629417,
+    langcode: 'en'
+  },
+  {
+    paragraphs: [
+      {
+        id: 22883,
+        type: 'sectionHeader',
+        text: 'The Startup Logistics'
+      },
+      {
+        id: 22884,
+        type: 'textSection',
+        text: '<p>www.google.com</p>\r\n\r\n<p>www.sba.gov</p>\r\n\r\n<p>www.bixal.com</p>\r\n'
+      }
+    ],
+    summary:
+      'Need to do research on your clients and location? View consumer and business data for your area using the Census Business Builder.',
+    type: 'localResources',
+    title: 'How to start a business in the Washington Metropolitan Area',
+    id: 20241,
+    updated: 1572629390,
+    created: 1572629291,
+    langCode: 'en'
+  },
+  {
+    paragraphs: [
+      {
+        id: 22877,
+        type: 'sectionHeader',
+        text: 'District of Columbia'
+      },
+      {
+        id: 22878,
+        type: 'textSection',
+        text: '<p>Bank of America</p>\r\n\r\n<p>Wells Fargo</p>\r\n\r\n<p>USAA</p>\r\n\r\n<p>Chase</p>\r\n'
+      }
+    ],
+    summary: 'The Washington area funding resources you need to know about.',
+    type: 'localResources',
+    title: 'Washington Metropolitan Area Funding Programs',
+    id: 20240,
+    updated: 1572629266,
+    created: 1572629128,
+    langCode: 'en'
+  }
+]
 
+const mockPageConnectorData = {
+  office: 6395,
+  subpages: [
+    {
+      id: 20242,
+      url: '/node/20242'
+    },
+    {
+      id: 20241,
+      url: '/node/20241'
+    },
+    {
+      id: 20240,
+      url: '/node/20240'
+    }
+  ],
+  type: 'pageConnector',
+  title: 'Washington Metropolitan Area B',
+  id: 20243,
+  updated: 1573247583,
+  created: 1572634173,
+  langCode: 'en',
+  url: '//washington-metropolitan-area-b'
+}
 
 afterEach(cleanup)
 
-const mockData = {
-    "paragraphs": [{
-            "id": 22889,
-            "type": "sectionHeader",
-            "text": "About us"
-        },
-        {
-            "id": 22890,
-            "type": "textSection",
-            "text": "<p>Welcome to the 2019-2020 edition of the U.S. Small Business Administration’s Washington Metropolitan Area Small Business Resource Guide, covering the District of Columbia, northern Virginia and suburban Maryland. With an increasingly diverse and well-educated workforce, the nation's capital and surrounding areas rank among the best locations to develop a successful small business. The SBA helps make the American dream of small business ownership a reality. We are the only federal agency dedicated to helping our nation’s 30 million small businesses start, grow, expand, or recover after a disaster.</p>\r\n\r\n<p>Over the past year, the SBA Washington Metropolitan Area District Office has empowered small businesses to:</p>\r\n\r\n<ul>\r\n\t<li>Find an ally or mentor via our network of SBA-funded Resource Partners, which includes SCORE, Small Business Development Centers, Women’s Business Centers, and the Veterans Business Outreach Center.</li>\r\n\t<li>Access over $335 million in SBA-guaranteed loans through 52 local banks, credit unions, community-based lenders, and microlenders. These qualified borrowers hired over 4,900 new employees, bought needed equipment, and built or renovated facilities.</li>\r\n\t<li>Gain more than $4.6 billion in federal contracting awards. The SBA offers business development and technical support to nearly 1,000 local companies enrolled in the 8(a) Business Development Program.</li>\r\n\t<li>Our region is an innovation hub, home to more than 90,000 small businesses employing over 1 million people. The Washington metro area generates a GDP of $530 billion annually, placing it among the 25 top producing countries. Just as the American spirit of freedom and independence is deeply tied to our local history, so are we deeply committed to ensuring entrepreneurialism continues to strengthen the economy for everyone, now and into the future. Stay informed about SBA events near you and get valuable Washington metro area business information by following us on Twitter at @sba_dcmetro. Register for email updates at sba.gov/updates.</li>\r\n</ul>\r\n"
-        }
-    ],
-    "summary": "Welcome to the 2019-2020 edition of the U.S. Small Business Administration’s Washington Metropolitan Area Small Business Resource Guide.",
-    "type": "localResources",
-    "title": "Washington Metropolitan Area Resources",
-    "id": 20242,
-    "updated": 1572629482,
-    "created": 1572629417,
-    "langCode": "en"
-}
-
 jest.mock('atoms/link/link.jsx', () => {
-	return {
-		__esModule: true,
-		default: () => <div />
-	}
+  return {
+    __esModule: true,
+    default: () => <div />
+  }
+})
+
+jest.mock('element-overlap', () => {
+  return {
+    listenForOverlap: () => {
+      return {
+        handleOverlap: () => false
+      }
+    }
+  }
 })
 
 describe('DistrictOfficeSubPageTemplate', () => {
-	it('should render content', async () => {
-		const params = {
-			subPageId: 20242
-		}
-		const fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
-		when(fetchRestContentStub).calledWith(20242).mockImplementationOnce(() => {
-			return Promise.resolve(mockData)
-		})
+  let store
 
-    	const { getByTestId, getAllByTestId } = render(<DistrictOfficeSubPageTemplate params={params} />)
-	    let content = await waitForElement(() => getByTestId('district-office-subpage-titlesection'))
-	    expect(content).toBeInTheDocument()
-	})
+  beforeAll(() => {
+    const initialState = undefined
+    const enhancer = applyMiddleware(thunk)
+    store = createStore(reducers, initialState, enhancer)
+  })
+  it('should render content', async () => {
+    const params = {
+      pageConnectorId: 20243
+    }
+
+    const fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
+    when(fetchRestContentStub)
+      .calledWith(20243)
+      .mockImplementationOnce(() => {
+        return Promise.resolve(mockPageConnectorData)
+      })
+    when(fetchRestContentStub)
+      .calledWith(20242)
+      .mockImplementationOnce(() => {
+        return Promise.resolve(mockSubPageData[0])
+      })
+    when(fetchRestContentStub)
+      .calledWith(20241)
+      .mockImplementationOnce(() => {
+        return Promise.resolve(mockSubPageData[1])
+      })
+    when(fetchRestContentStub)
+      .calledWith(20240)
+      .mockImplementationOnce(() => {
+        return Promise.resolve(mockSubPageData[2])
+      })
+    const { getByTestId, getAllByTestId } = render(
+      <Provider store={store}>
+        <DistrictOfficeSubPageTemplate params={params} />
+      </Provider>
+    )
+    const content = await waitForElement(() => getByTestId('district-office-subpage-titlesection'))
+    expect(content).toBeInTheDocument()
+  })
+  it('should render side navigation', async () => {
+    const params = {
+      pageConnectorId: 20243
+    }
+
+    const fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
+    when(fetchRestContentStub)
+      .calledWith(20243)
+      .mockImplementationOnce(() => {
+        return Promise.resolve(mockPageConnectorData)
+      })
+    when(fetchRestContentStub)
+      .calledWith(20242)
+      .mockImplementationOnce(() => {
+        return Promise.resolve(mockSubPageData[0])
+      })
+    when(fetchRestContentStub)
+      .calledWith(20241)
+      .mockImplementationOnce(() => {
+        return Promise.resolve(mockSubPageData[1])
+      })
+    when(fetchRestContentStub)
+      .calledWith(20240)
+      .mockImplementationOnce(() => {
+        return Promise.resolve(mockSubPageData[2])
+      })
+    const { getByTestId, getAllByTestId } = render(
+      <Provider store={store}>
+        <DistrictOfficeSubPageTemplate params={params} />
+      </Provider>
+    )
+    const content = await waitForElement(() => getByTestId('district-office-subpage-sectionnavigation'))
+    expect(content).toBeInTheDocument()
+  })
 })
