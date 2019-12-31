64f947a23ac9e7468f16f1d2152f74942e085e9c
Pat Kelsh <pkelsh@gmail.com>
2019-12-31 13:47:43 -0500

    TA-3466 use flag for events display

--- src/client/components/templates/district-office/district-office.jsx
+++ src/client/components/templates/district-office/district-office.jsx
@@ -7,6 +7,7 @@ import { Hero, LocationInfoSection, NewsReleases, EventResult, Results, SuccessS
 import { fetchRestContent, fetchSiteContent } from '../../../fetch-content-helper'
 import twitterThumbnail from 'assets/images/footer/twitter.png'
 import styles from './district-office.scss'
+import clientConfig from '../../../services/client-config.js'
 
 class DistrictOfficeTemplate extends React.Component {
   constructor() {
@@ -186,22 +187,25 @@ const NewsletterSignup = () => {
 
 // Events component temporarily removed as per TA-3466. Uncomment when events backend is completed.
 const Events = ({ items }) => {
-  // return (
-  //   <div data-testid="events" className={styles.events}>
-  //     <h2>Upcoming events and workshops</h2>
-  //     {items.length > 0 && (
-  //       <Results items={items}>
-  //         <EventResult />
-  //       </Results>
-  //     )}
-  //     <div className={styles.button} data-testid="events-button">
-  //       <a href="/events/find/" className={styles.buttonLink}>
-  //         Find More Events
-  //       </a>
-  //     </div>
-  //   </div>
-  // )
-  return <div />
+  if (clientConfig.useD8EventsBackend) {
+    return (
+      <div data-testid="events" className={styles.events}>
+        <h2>Upcoming events and workshops</h2>
+        {items.length > 0 && (
+          <Results items={items}>
+            <EventResult />
+          </Results>
+        )}
+        <div className={styles.button} data-testid="events-button">
+          <a href="/events/find/" className={styles.buttonLink}>
+            Find More Events
+          </a>
+        </div>
+      </div>
+    )
+  } else {
+    return <div />
+  }
 }
 
 const CTA = () => {
