805b155419c8c530a48b92de2a1ad98b88740762
Avery Smith <avery@averysmith.pro>
2020-04-03 16:57:05 -0400

    TA-3632 removed comment and typo

--- test/jest/client/components/pages/lender-lookup-page/lender-lookup-page.test.jsx
+++ test/jest/client/components/pages/lender-lookup-page/lender-lookup-page.test.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { mount } from 'enzyme'
 
 import LenderLookupPage from 'pages/lender-lookup-page/lender-lookup-page.jsx'
-// const config = require('config')
 import * as clientConfig from 'client/services/client-config'
 import { MultiSelect } from 'atoms'
 import moment from 'moment'
 
 describe('LenderLookupPage', () => {
-  it('should seelct the rapid lenders dropdown', () => {
+  it('should selct the rapid lenders dropdown', () => {
     const component = mount(<LenderLookupPage />)
     const result = component.find('[data-cy="has-filed-2019-taxes"]')
     expect(result).toHaveLength(1)
