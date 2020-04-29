0938a70e3d01a3daec74abe2ac08ae917fab9229
Ksenia Coulter <kcoulter@Ksenias-MBP.cable.rcn.com>
2020-04-29 10:41:15 -0400

    Make lenderResultCard component. Style according to mockups

---
+++ src/client/components/molecules/lender-result-card/lender-result-card.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from './lender-result-card.scss'
+
+const LenderResultCard = props => {
+  const { link, phoneNumber, streetAddress, title, className: cn, testId } = props
+
+  const fields = {
+    address: {
+      name: 'contact address',
+      text: streetAddress,
+      icon: 'map-marker',
+      ariaLabel: 'address icon'
+    },
+    phoneNumber: {
+      name: 'contact phone',
+      text: phoneNumber,
+      icon: 'phone',
+      href: `tel:${phoneNumber}`,
+      ariaLabel: 'phone icon'
+    },
+    website: {
+      name: 'contact link',
+      text: 'Website',
+      icon: 'globe',
+      href: link,
+      ariaLabel: 'website icon'
+    }
+  }
+
+  return (
+    <div data-testid={testId} className={styles.lenderResultCard}>
+      <div className={styles.firstColumn}>
+        {title && (
+          <h4
+            data-testid="lender card title"
+            data-cy="lender card title"
+            tabIndex="0"
+            className={styles.h4}
+          >
+            {title}
+          </h4>
+        )}
+        <div
+          data-testid={fields.address.name}
+          data-cy={fields.address.name}
+          className={styles.row}
+          key={Math.random()}
+        >
+          <span dangerouslySetInnerHTML={{ __html: fields.address.text }} tabIndex="0" />
+        </div>
+        <div
+          data-testid={fields.phoneNumber.name}
+          data-cy={fields.phoneNumber.name}
+          className={styles.row}
+          key={Math.random()}
+        >
+          <span dangerouslySetInnerHTML={{ __html: fields.phoneNumber.text }} tabIndex="0" />
+        </div>
+      </div>
+      <div className={styles.secondColumn}>
+        <a className={styles.websiteLink} href={fields.website.href}>
+          <i
+            aria-label={fields.website.ariaLabel}
+            className={`fa fa-${fields.website.icon}`}
+            tabIndex="0"
+          />
+          <span dangerouslySetInnerHTML={{ __html: fields.website.text }} tabIndex="0" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+LenderResultCard.propTypes = {
+  city: PropTypes.string,
+  link: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  phoneNumber: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  streetAddress: PropTypes.string,
+  title: PropTypes.string,
+  testId: PropTypes.string
+}
+
+LenderResultCard.defaultProps = {
+  border: true,
+  testId: 'lender-card'
+}
+
+export default LenderResultCard
--- src/client/components/organisms/lender-detail/lender-detail.jsx
+++ src/client/components/organisms/lender-detail/lender-detail.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classNames from 'classnames'
 import styles from './lender-detail.scss'
-import { ContactCard } from 'molecules'
+import { LenderResultCard } from 'molecules'
 
 class LenderDetail extends React.PureComponent {
   componentDidMount() {
@@ -31,20 +31,13 @@ class LenderDetail extends React.PureComponent {
 
     const address1 = fields.address ? fields.address[0] : ''
     const address2 = fields.address_additional ? fields.address_additional[0] : ''
-    const firstName = fields.contact_first_name ? fields.contact_first_name[0] : ''
-    const lastName = fields.contact_last_name ? fields.contact_last_name[0] : ''
 
     const contactProps = {
-      city: fields.city ? fields.city[0] : '',
       streetAddress: this.formatStreetAddress(address1, address2),
-      state: fields.state ? fields.state[0] : '',
-      zipCode: fields.zipcode ? fields.zipcode[0] : '',
-      personTitle: this.formatName(firstName, lastName),
-      email: '',
       phoneNumber: fields.bank_phone ? fields.bank_phone[0] : '',
-      fax: fields.contact_fax ? fields.contact_fax[0] : '',
       link: fields.website ? fields.website[0] : '',
-      border: false
+      border: false,
+      title: fields.lender_name[0]
     }
 
     const lenderDetailClassName = classNames({
@@ -53,15 +46,7 @@ class LenderDetail extends React.PureComponent {
 
     return (
       <div id="lender-detail" className={lenderDetailClassName} tabIndex="0">
-        <div>
-          <div>
-            <div className={styles.close}></div>
-            <h2 tabIndex="0" role="heading" className="lender-name">
-              {this.props.item.fields.lender_name[0]}
-            </h2>
-          </div>
-        </div>
-        <ContactCard {...contactProps} />
+        <LenderResultCard {...contactProps} />
       </div>
     )
   }
