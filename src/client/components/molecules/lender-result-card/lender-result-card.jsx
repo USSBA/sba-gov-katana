909503e96e93f58564947ba1d38c384c68e96a80
Ksenia Coulter <kzherdeva@ucdavis.edu>
2020-04-30 09:53:25 -0400

    TC-66 UI updates to lender results list (#1045)
    * Make lenderResultCard component. Style according to mockups
    
    * Make map visible on medium screens
    
    * Style results for medium screens
    
    * Add call button to small screens, add circle around icons
    
    * Add test coverage to lender result card
    
    Co-authored-by: Ksenia Coulter <kcoulter@Ksenias-MBP.cable.rcn.com>

---
+++ src/client/components/molecules/lender-result-card/lender-result-card.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from './lender-result-card.scss'
+import classNames from 'classnames'
+
+const LenderResultCard = props => {
+  const { link, phoneNumber, streetAddress, title, className: cn, testId } = props
+
+  const fields = {
+    address: {
+      name: 'contact address',
+      text: streetAddress,
+      icon: 'map-marker',
+      ariaLabel: 'address icon'
+    },
+    phoneNumber: {
+      name: 'contact phone',
+      text: phoneNumber,
+      icon: 'phone',
+      href: `${phoneNumber ? `tel:${phoneNumber}` : ``}`,
+      ariaLabel: 'phone icon'
+    },
+    website: {
+      name: 'contact link',
+      text: 'Website',
+      icon: 'globe',
+      href: link || '',
+      ariaLabel: 'website icon'
+    }
+  }
+
+  const lenderWebsiteClassName = classNames({
+    [styles.disabled]: !fields.website.href,
+    [styles.iconContainer]: true
+  })
+
+  const lenderPhoneClassName = classNames({
+    [styles.phoneNumber]: true,
+    [styles.disabled]: !fields.phoneNumber.href,
+    [styles.iconContainer]: true
+  })
+
+  return (
+    <div data-testid={testId} className={styles.lenderResultCard}>
+      <div className={styles.firstColumn}>
+        {title && (
+          <h4
+            data-testid="lender card title"
+            data-cy="lender card title"
+            tabIndex="0"
+            className={styles.h4}
+          >
+            {title}
+          </h4>
+        )}
+        <div
+          data-testid={fields.address.name}
+          data-cy={fields.address.name}
+          className={styles.row}
+          key={Math.random()}
+        >
+          <span tabIndex="0">{fields.address.text}</span>
+        </div>
+        <div
+          data-testid={fields.phoneNumber.name}
+          data-cy={fields.phoneNumber.name}
+          className={styles.row}
+          key={Math.random()}
+        >
+          <span tabIndex="0">{fields.phoneNumber.text}</span>
+        </div>
+      </div>
+      <div className={styles.secondColumn}>
+        <a className={lenderWebsiteClassName} href={fields.website.href}>
+          <i
+            aria-label={fields.website.ariaLabel}
+            className={`fa fa-${fields.website.icon}`}
+            tabIndex="0"
+          />
+          <span tabIndex="0">{fields.website.text}</span>
+        </a>
+        <a className={lenderPhoneClassName} href={fields.phoneNumber.href}>
+          <i
+            aria-label={fields.phoneNumber.ariaLabel}
+            className={`fa fa-${fields.phoneNumber.icon}`}
+            tabIndex="0"
+          />
+          <span tabIndex="0">Call</span>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+LenderResultCard.propTypes = {
+  city: PropTypes.string,
+  link: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  phoneNumber: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  streetAddress: PropTypes.string,
+  title: PropTypes.string,
+  testId: PropTypes.string
+}
+
+LenderResultCard.defaultProps = {
+  border: true,
+  testId: 'lender-card'
+}
+
+export default LenderResultCard
