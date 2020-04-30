9db38f49e3a4c6e8bc4e958805bc1b5e5666e7c4
Ksenia Coulter <kzherdeva@ucdavis.edu>
2020-04-30 15:15:55 -0400

    TC-66 Add scroll bar to lender results (#1047)
    * Add scrollbar to results lift
    
    * Add city, state, and zipcode back to lender results display
    
    * Adjust test for lender result card
    
    Co-authored-by: Ksenia Coulter <kcoulter@Ksenias-MBP.cable.rcn.com>

--- src/client/components/organisms/lender-detail/lender-detail.jsx
+++ src/client/components/organisms/lender-detail/lender-detail.jsx
@@ -37,7 +37,10 @@ class LenderDetail extends React.PureComponent {
       phoneNumber: fields.bank_phone ? fields.bank_phone[0] : '',
       link: fields.website ? fields.website[0] : '',
       border: false,
-      title: fields.lender_name[0]
+      title: fields.lender_name[0],
+      state: fields.state ? fields.state[0] : '',
+      zipCode: fields.zipcode ? fields.zipcode[0] : '',
+      city: fields.city ? fields.city[0] : ''
     }
 
     const lenderDetailClassName = classNames({
