f9125e9ebea181e3dc2b290f3237f3310518b5d7
Jenny Wagner <jennywagg@gmail.com>
2020-05-07 15:42:54 -0400

    TA-3548 more refactoring on document-page and some testing

--- src/client/components/pages/document-page/document-page.jsx
+++ src/client/components/pages/document-page/document-page.jsx
@@ -1,69 +1,38 @@
 import React from 'react'
-import { size } from 'lodash'
 
 import DocumentArticle from '../../templates/document-article/document-article.jsx'
 import ErrorPage from '../error-page/error-page.jsx'
 import styles from './document-page.scss'
 import { Loader } from 'atoms'
-import { fetchSiteContent, fetchRestContent } from '../../../fetch-content-helper'
+import { fetchRestContent } from '../../../fetch-content-helper'
 
 class DocumentPage extends React.Component {
   constructor() {
     super()
     this.state = {
-      loading: false
+      isLoading: true,
+      document: null
     }
   }
 
   async componentDidMount() {
-    const {
-      location: { pathname }
-    } = this.props
-    await this.fetchDocument(pathname)
-  }
-
-  async componentWillReceiveProps(nextProps) {
-    const {
-      location: { pathname }
-    } = this.props
-    const {
-      location: { pathname: nextPathname }
-    } = nextProps
-
-    // Re-render the page with new document data when we remain on `/documents`
-    // and the DocumentPage but the location has changed.
-    if (pathname !== nextPathname) {
-      await this.fetchDocument(nextPathname)
-    }
+    const document = await fetchRestContent(this.props.id)
+    this.setState({ isLoading: false, document })
   }
 
   render() {
-    const {
-      location: { pathname }
-    } = this.props
-    const { document } = this.state
+    const { document, isLoading } = this.state
 
-    if (pathname && document !== null) {
-      if (document) {
-        return <DocumentArticle document={document} />
-      } else {
-        return (
-          <div className={styles.container}>
-            <Loader />
-          </div>
-        )
-      }
+    if (isLoading) {
+      return (
+        <div className={styles.container}>
+          <Loader />
+        </div>
+      )
+    } else if (document) {
+      return <DocumentArticle document={document} />
     } else {
       return <ErrorPage />
-    }
-  }
-
-  async fetchDocument() {
-    try {
-      const document = await fetchRestContent(this.props.id)
-      this.setState({ document })
-    } catch (e) {
-      console.error(e)
     }
   }
 }
