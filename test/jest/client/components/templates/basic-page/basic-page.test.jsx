262655ef277ebc8fa327af8ddd9f827d01b43554
Kevin Lee <ki.lee@sba.gov>
2020-06-02 13:29:39 -0400

    T1-1504 Add test to check if Bizzie loads on contact page

--- test/jest/client/components/templates/basic-page/basic-page.test.jsx
+++ test/jest/client/components/templates/basic-page/basic-page.test.jsx
@@ -4,13 +4,22 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Waypoint from 'react-waypoint'
 
-import { TitleSection, Breadcrumb, PreviousNextSection, RemoveMainLoader } from 'molecules'
+import {
+  TitleSection,
+  BizzieChatbotWidget,
+  Breadcrumb,
+  PreviousNextSection,
+  RemoveMainLoader
+} from 'molecules'
 import { SectionNav } from 'organisms'
 import { listenForOverlap as mockListenForOverlap } from 'element-overlap'
 import { BasicPage } from 'templates/basic-page/basic-page.jsx'
 
 const title = 'My Basic Page'
 const summary = 'My Basic Page Summary'
+const firstPath = { pathname: '/about-sba' }
+const secondPath = { pathname: '/business-guide' }
+const thirdPath = { pathname: '/funding-programs' }
 
 jest.mock('element-overlap')
 
@@ -20,7 +29,7 @@ describe('BasicPage', () => {
   })
 
   test('renders default data', () => {
-    const component = shallow(<BasicPage title="" summary="" />)
+    const component = shallow(<BasicPage title="" summary="" location="" />)
     expect(component.find('.basicpage-mobilenav')).toHaveLength(1)
     expect(component.find('.basicpage-previousnext')).toHaveLength(1)
     expect(component.find('.basicpage-sectionnavigation')).toHaveLength(1)
@@ -28,49 +37,53 @@ describe('BasicPage', () => {
   })
 
   test('renders the mobilenav when displayMobileNav state is true', () => {
-    const BasicPageComponent = shallow(<BasicPage title="" summary="" />)
+    const BasicPageComponent = shallow(<BasicPage title="" summary="" location="" />)
     BasicPageComponent.setState({ displayMobileNav: true })
     expect(BasicPageComponent.find('.basicpage-mobilenav').hasClass('hideContainer')).toEqual(true)
   })
 
   test("doesn't render the mobilenav when displayMobileNav state is false", () => {
-    const BasicPageComponent = shallow(<BasicPage title="" summary="" />)
+    const BasicPageComponent = shallow(<BasicPage title="" summary="" location="" />)
     BasicPageComponent.setState({ displayMobileNav: false })
     expect(BasicPageComponent.find('.basicpage-mobilenav').hasClass('container')).toEqual(true)
   })
 
   test('render a TitleSection component', () => {
-    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} />)
+    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} location={firstPath} />)
     expect(BasicPageComponent.find(TitleSection).length).toEqual(1)
     expect(BasicPageComponent.find(TitleSection).prop('title')).toEqual(title)
     expect(BasicPageComponent.find(TitleSection).prop('summary')).toEqual(summary)
   })
 
   test("doesn't render a Breadcrumb component when no lineage prop is passed", () => {
-    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} />)
+    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} location={secondPath} />)
     expect(BasicPageComponent.find(Breadcrumb).length).toEqual(0)
   })
 
   test('renders a Breadcrumb component when no lineage prop is passed', () => {
     const lineage = [{ title: 'onelineage', fullUrl: 'http://good.com/good' }]
-    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} lineage={lineage} />)
+    const BasicPageComponent = shallow(
+      <BasicPage title={title} summary={summary} lineage={lineage} location={firstPath} />
+    )
     expect(BasicPageComponent.find(Breadcrumb).length).toEqual(1)
     expect(BasicPageComponent.find(Breadcrumb).prop('items')).toBeDefined()
   })
 
   test('renders RemoveMainLoader component', () => {
-    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} />)
+    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} location={thirdPath} />)
     expect(BasicPageComponent.find(RemoveMainLoader).length).toEqual(1)
   })
 
   test('renders Waypoint library', () => {
-    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} />)
+    const BasicPageComponent = shallow(<BasicPage title={title} summary={summary} location={firstPath} />)
     expect(BasicPageComponent.find(Waypoint).length).toEqual(1)
   })
 
   test('has a section navigation when there is a lineage', () => {
     const lineage = [{ title: 'lineage1', fullUrl: 'http://example.com/lineage1' }]
-    const component = shallow(<BasicPage title={title} summary={summary} lineage={lineage} />)
+    const component = shallow(
+      <BasicPage title={title} summary={summary} lineage={lineage} location={secondPath} />
+    )
     const sectionNav = component.find(SectionNav)
 
     expect(sectionNav.prop('lineage')).toEqual(lineage)
@@ -85,7 +98,7 @@ describe('BasicPage', () => {
   })
 
   test('has no section navigation when there is no lineage', () => {
-    const component = shallow(<BasicPage title={title} summary={summary} />)
+    const component = shallow(<BasicPage title={title} summary={summary} location={thirdPath} />)
     const sectionNav = component.find(SectionNav)
 
     expect(sectionNav.length).toEqual(0)
@@ -93,7 +106,9 @@ describe('BasicPage', () => {
 
   test('has previous and next buttons when there is a lineage', () => {
     const lineage = [{ title: 'lineage1', fullUrl: 'http://example.com/lineage1' }]
-    const component = shallow(<BasicPage title={title} summary={summary} lineage={lineage} />)
+    const component = shallow(
+      <BasicPage title={title} summary={summary} lineage={lineage} location={firstPath} />
+    )
     const sectionNav = component.find(PreviousNextSection)
 
     expect(sectionNav.length).toEqual(1)
@@ -103,9 +118,31 @@ describe('BasicPage', () => {
 
   test('has no previous and next buttons when there is no lineage', () => {
     mockListenForOverlap.mockImplementation(() => null)
-    const component = shallow(<BasicPage title={title} summary={summary} />)
+    const component = shallow(<BasicPage title={title} summary={summary} location={secondPath} />)
     const sectionNav = component.find(PreviousNextSection)
 
     expect(sectionNav.length).toEqual(0)
+  })
+
+  test('has Bizzie Chatbot Widget component rendered on Contact page', () => {
+    const component = shallow(
+      <BasicPage
+        title={title}
+        summary={summary}
+        location={{ pathname: '/about-sba/organization/contact-sba' }}
+      />
+    )
+    const bizzieChatbot = component.find(BizzieChatbotWidget)
+
+    expect(component.state('displayBizzieBot')).toEqual(true)
+    expect(bizzieChatbot.length).toEqual(1)
+  })
+
+  test('has Bizzie Chatbot Widget component not rendered when not on Contact page', () => {
+    const component = shallow(<BasicPage title={title} summary={summary} location={thirdPath} />)
+    const bizzieChatbot = component.find(BizzieChatbotWidget)
+
+    expect(component.state('displayBizzieBot')).toEqual(false)
+    expect(bizzieChatbot.length).toEqual(0)
   })
 })
