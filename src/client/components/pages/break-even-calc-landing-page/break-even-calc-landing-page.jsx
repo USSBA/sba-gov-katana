cd79a79c1656471369e9bcd6e900794e0a27620b
Ksenia Coulter <kcoulter@Ksenias-MBP.cable.rcn.com>
2020-06-05 16:39:18 -0400

    Create landing page and routing for it

---
+++ src/client/components/pages/break-even-calc-landing-page/break-even-calc-landing-page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class BreakEvenCalculatorPage extends React.Component {
+  render() {
+    return <h1>Break Even Calculator</h1>
+  }
+}
+
+export default BreakEvenCalculatorPage
