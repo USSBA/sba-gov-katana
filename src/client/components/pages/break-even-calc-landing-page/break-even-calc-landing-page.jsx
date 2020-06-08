32fbc7e09b7f58f9e14003fb4beaa46b4ffc5ee4
Ksenia Coulter <kcoulter@Ksenias-MBP.cable.rcn.com>
2020-06-08 10:09:32 -0400

    Add gradient background

--- src/client/components/pages/break-even-calc-landing-page/break-even-calc-landing-page.jsx
+++ src/client/components/pages/break-even-calc-landing-page/break-even-calc-landing-page.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-<<<<<<< HEAD
 import { Button, DecorativeDash } from 'atoms'
-import { Accordion } from 'organisms'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState
+} from 'react-accessible-accordion'
+
 import { Breadcrumb } from 'molecules'
 import styles from './break-even-calc-landing-page.scss'
+import accordionStyles from './accordion.scss'
 import { faqFields, benefitFields } from './content'
 
 class BreakEvenCalculatorPage extends React.Component {
@@ -38,35 +46,38 @@ class BreakEvenCalculatorPage extends React.Component {
             the level of production at which the costs of production equals the revenues for a product.
           </p>
           <p>
-            This is <strong>an important calculation for your business plan</strong>. Potential investors in
-            a business not only want to know the return to expect on their investments, but also the point
-            when they will realize this return. This is because some companies may take years before turning
-            a profit, often losing money in the first few months or years before breaking even. For this
-            reason, break-even point is an important part of any business plan presented to a potential
-            investor. It can also be a useful tool in future planning for existing businesses to prove their
-            potential turnaround after disaster scenarios.
+            For any new business, this is <strong>an important calculation in your business plan</strong>.
+            Potential investors in a business not only want to know the return to expect on their
+            investments, but also the point when they will realize this return. This is because some
+            companies may take years before turning a profit, often losing money in the first few months or
+            years before breaking even. For this reason, break-even point is an important part of any
+            business plan presented to a potential investor.
           </p>
-          <div className={styles.benefitsTitle}>Benefits of a break-even analysis</div>
+          <p>
+            For existing businesses, this can be a useful tool not only in analyzing costs and evaluating
+            profits they’ll earn at different sales volumes, but also to prove their potential turnaround
+            after disaster scenarios.
+          </p>
+          <h2 className={styles.benefitsTitle}>Benefits of a break-even analysis</h2>
           <div className={styles.benefits}>
             {benefitFields.map(benefit => (
-              <div className={styles.benefitsCol}>
-                <p className={styles.subHeading}>{benefit.title}</p>
+              <div className={styles.benefitsCol} key={benefit.title}>
+                <h3 className={styles.subHeading}>{benefit.title}</h3>
                 <p className={styles.benefitsContent}>{benefit.description}</p>
               </div>
             ))}
           </div>
         </div>
-        <div className={styles.gradientBackground}>
+        <div>
+          <div className={styles.gradientBackground}></div>
           <div className={styles.infoContainer}>
             <div className={styles.row}>
               <div className={styles.calcImage}>
                 <img src="/assets/images/break-even-calculator/Calculator_icon.svg" alt="calculator icon" />
               </div>
               <div className={styles.calcText}>
-                <h2>
-                  Getting Started <br />
-                  Break-Even Point Analysis
-                </h2>
+                <h2>Getting Started </h2>
+                <h2>Break-Even Point Analysis</h2>
                 <p>
                   Create your break-even analysis with this calculator and determine your business’s
                   break-even point in units using the following formula:
@@ -95,29 +106,50 @@ class BreakEvenCalculatorPage extends React.Component {
                   </li>
                 </ul>
                 <div className={styles.buttonContainer}>
-                  <Button primary>Start Analysis</Button>
+                  <Button primary url="breakevenpointcalculator/calculate">
+                    Start Analysis
+                  </Button>
                 </div>
+              </div>
+            </div>
+            <div className={accordionStyles.accordionContainer}>
+              <h3>Tips and Tricks</h3>
+              <DecorativeDash width={77} />
+              <div className={accordionStyles.accordionContent}>
+                <Accordion allowZeroExpanded>
+                  {faqFields.map(faq => (
+                    <AccordionItem key={faq.question}>
+                      <AccordionItemHeading className={accordionStyles.accordionHeading}>
+                        <AccordionItemButton className={accordionStyles.accordionButton}>
+                          <h3>
+                            {faq.question}
+                            <AccordionItemState>
+                              {({ expanded }) => {
+                                return expanded ? (
+                                  <i className={accordionStyles.accordionIcon + ' fa fa-chevron-up'} />
+                                ) : (
+                                  <i className={accordionStyles.accordionIcon + ' fa fa-chevron-down'} />
+                                )
+                              }}
+                            </AccordionItemState>
+                          </h3>
+                        </AccordionItemButton>
+                      </AccordionItemHeading>
+                      <AccordionItemPanel>
+                        <div
+                          className={accordionStyles.accordionText}
+                          dangerouslySetInnerHTML={{ __html: faq.answer }}
+                        />
+                      </AccordionItemPanel>
+                    </AccordionItem>
+                  ))}
+                </Accordion>
               </div>
             </div>
           </div>
         </div>
-        <div className={styles.accordionContainer}>
-          <h3>Tips and Tricks</h3>
-          <DecorativeDash width={77} />
-          <div className={styles.accordionContent}>
-            {faqFields.map(faq => (
-              <Accordion title={faq.question} content={faq.answer} />
-            ))}
-          </div>
-        </div>
       </div>
     )
-=======
-
-class BreakEvenCalculatorPage extends React.Component {
-  render() {
-    return <h1>Break Even Calculator</h1>
->>>>>>> Create landing page and routing for it
   }
 }
 
