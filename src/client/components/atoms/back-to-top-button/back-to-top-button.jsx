0ec04978300a5a00a83168de495f72c9acbf308e
Avery Smith <avery@averysmith.pro>
2020-08-27 16:41:53 -0400

    TA-3827 added back-to-top button and tests

---
+++ src/client/components/atoms/back-to-top-button/back-to-top-button.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import styles from './back-to-top-button.module.scss'
+import classNames from 'classnames'
+
+class BackToTopButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      enabled: false
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+    this.shouldUpdate = true
+    this.update()
+  }
+  update() {
+    const SCROLL_THRESHOLD = 600
+    const enabled = window.scrollY > SCROLL_THRESHOLD
+    this.setState({ enabled })
+    this.ticking = false
+  }
+  handleScroll() {
+    if (!this.ticking) {
+      this.ticking = true
+      requestAnimationFrame(() => {
+        if (this.shouldUpdate) {
+          this.update()
+        }
+      })
+    }
+  }
+  componentWillUnmount() {
+    this.shouldUpdate = false
+  }
+  resetYPos = () => {
+    window.scrollTo(0, 0)
+  }
+  render() {
+    const { enabled } = this.state
+    const className = classNames({
+      [styles.backToTopButton]: true,
+      [styles.disabled]: !enabled
+    })
+    return (
+      <button
+        data-testid="back-to-top-button"
+        className={className}
+        onClick={e => {
+          e.preventDefault()
+          this.resetYPos()
+        }}
+      >
+        <span data-testid="arrow" className={styles.arrow} />
+        <strong>Back to Top</strong>
+      </button>
+    )
+  }
+}
+export default BackToTopButton
