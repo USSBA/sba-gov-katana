5ce9e55d1b19a80a09803e8e03ef8dd83db477ff
Avery Smith <avery@averysmith.pro>
2020-08-27 16:41:53 -0400

    TA-3827 added back-to-top button and tests

---
+++ test/jest/client/components/atoms/back-to-top-button/back-to-top-button.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { fireEvent, render, cleanup } from 'react-testing-library'
+import { BackToTopButton } from 'atoms'
+import 'jest-dom/extend-expect'
+
+afterEach(cleanup)
+
+describe('BackToTopButton', () => {
+  it('renders the back-to-top button component', () => {
+    const { getByTestId } = render(<BackToTopButton />)
+    expect(getByTestId('back-to-top-button')).toBeInTheDocument()
+  })
+  it('renders arrow graphic', () => {
+    const { getByTestId } = render(<BackToTopButton />)
+    const button = getByTestId('back-to-top-button')
+    const arrow = getByTestId('arrow')
+    expect(button).toContainElement(arrow)
+  })
+})
