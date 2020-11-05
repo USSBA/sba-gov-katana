78be157d814c99411ac440c462f39c5c80c8fda0
peggles2 <peggles2@users.noreply.github.com>
2020-11-05 13:20:28 -0500

    TC-557 - get content types available for use (#1141)
    * TC-557 - get content types available for use
    
    * update beccalc to run a test for drupal content

--- test/jest/client/components/pages/break-even-calc-landing-page/break-even-calc-landing-page.test.jsx
+++ test/jest/client/components/pages/break-even-calc-landing-page/break-even-calc-landing-page.test.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { render, cleanup, waitForElement } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+
+import * as fetchContentHelper from 'client/fetch-content-helper.js'
 
 import BreakEvenCalculatorPage from 'pages/break-even-calc-landing-page/break-even-calc-landing-page.jsx'
+let fetchRestContentStub
 
 describe('BreakEvenCalculatorPage', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(<BreakEvenCalculatorPage />)
     expect(wrapper).toHaveLength(1)
+  })
+
+  it('calls dashio to get data', async () => {
+    global.nodeId = '123'
+
+    fetchRestContentStub = jest.spyOn(fetchContentHelper, 'fetchRestContent')
+    fetchRestContentStub.mockReturnValue({})
+
+    const { getByTestId } = render(<BreakEvenCalculatorPage />)
+    await waitForElement(() => getByTestId('breakevencalc'))
+
+    expect(fetchRestContentStub).toBeCalledTimes(1)
   })
 })
