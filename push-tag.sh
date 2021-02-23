ba1f6916ef584d03347edb2f0d4af109aa7bcf44
Candace <cdcampbell96@gmail.com>
2021-02-23 13:51:18 -0500

    Cleaned up scripts

--- push-tag.sh
+++ push-tag.sh
@@ -2,16 +2,6 @@
 DEVELOPER_OR_ENVIRONMENT=$1
 ENVIRONMENT_NAME=$DEVELOPER_OR_ENVIRONMENT
 
-if [ $DEVELOPER_OR_ENVIRONMENT == "avery" ]; then
-  ENVIRONMENT_NAME="int-as"
-elif [ $DEVELOPER_OR_ENVIRONMENT == "brian"  ]; then
-  ENVIRONMENT_NAME="int-bl"
-elif [ $DEVELOPER_OR_ENVIRONMENT == "kevin"  ]; then
-  ENVIRONMENT_NAME="int-kl"
-elif [ $DEVELOPER_OR_ENVIRONMENT == "ryan"  ]; then
-  ENVIRONMENT_NAME="int-rh"
-fi
-
 echo "Pushing to ${ENVIRONMENT_NAME}"
 
 git tag ${ENVIRONMENT_NAME} -f && git push origin ${ENVIRONMENT_NAME} -f
