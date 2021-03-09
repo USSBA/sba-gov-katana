2f2d6fa1dc857d27bf9050947ec9d11f4d3aa475
Ari Perez <ariperez83@gmail.com>
2021-03-09 13:31:17 -0700

    Improve Handling of Card Collection layouts

--- src/client/components/organisms/card-collection/card-collection.jsx
+++ src/client/components/organisms/card-collection/card-collection.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { chunk, size } from 'lodash'
 
 import breakpoints from '../../../styles/base/_variables.scss'
 import styles from './card-collection.scss'
 import { Card } from 'molecules'
 
 const cardsPerRowMap = {
-  '2': 2,
-  '3': 3,
-  '4': 4,
-  '5': 3,
-  '6': 6,
-  '7': 4,
-  '8': 4,
-  '9': 3,
-  '10': 4,
-  '11': 4,
-  '12': 6
+  '2': { full: [2], tablet: [2], mobile: [1, 1] }, //2
+  '3': { full: [3], tablet: [3], mobile: [1, 1, 1] }, //3,
+  '4': { full: [4], tablet: [2, 2], mobile: [1, 1, 1, 1] }, //4,
+  '5': { full: [3, 2], tablet: [3, 2], mobile: [1, 1, 1, 1, 1] }, //3,
+  '6': { full: [3, 3], tablet: [3, 3], mobile: [1, 1, 1, 1, 1, 1] }, //6,
+  '7': { full: [4, 3], tablet: [3, 3, 1], mobile: [1, 1, 1, 1, 1, 1, 1] }, //4,
+  '8': { full: [4, 4], tablet: [2, 2, 2, 2], mobile: [1, 1, 1, 1, 1, 1, 1, 1] }, //4,
+  '9': { full: [3, 3, 3], tablet: [3, 3, 3], mobile: [1, 1, 1, 1, 1, 1, 1, 1, 1] } //4,
 }
 
 const medium = parseInt(breakpoints.breakpointMedium, 10)
@@ -26,53 +22,57 @@ const large = parseInt(breakpoints.breakpointLarge, 10)
 class CardCollection extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
-      cardsPerRow: cardsPerRowMap[props.cards.length]
+      cardsPerRow: cardsPerRowMap[props.cards.length].full
     }
   }
 
   componentDidMount() {
-    const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-
-    if (viewportWidth >= medium && viewportWidth < large && this.props.cards.length === 4) {
-      this.setState({ cardsPerRow: 2 })
-    }
+    this.updateCardsPerRow()
   }
 
   updateCardsPerRow() {
     const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    const { cardsPerRow } = this.state
     const cardsLength = cardsPerRowMap[this.props.cards.length]
 
     if (viewportWidth >= medium && viewportWidth < large) {
-      cardsPerRow !== 2 && this.setState({ cardsPerRow: 2 })
+      this.setState({ cardsPerRow: cardsLength.tablet })
+    } else if (viewportWidth < medium) {
+      this.setState({ cardsPerRow: cardsLength.mobile })
     } else {
-      cardsPerRow !== cardsLength && this.setState({ cardsPerRow: cardsLength })
+      this.setState({ cardsPerRow: cardsLength.full })
     }
   }
 
   render() {
     const { leftAligned, parentIndex, cards, numberOverride, cardAriaLabel } = this.props
     const { cardsPerRow } = this.state
-    const numCards = numberOverride ? numberOverride : size(this.props.cards)
-    const cardComponents = cards.map(function(item, index) {
-      return (
-        <Card
-          parentIndex={parentIndex}
-          key={index}
-          item={item}
-          index={index}
-          numCards={numCards}
-          leftAligned={leftAligned}
-          cardAriaLabel={cardAriaLabel}
-        />
-      )
-    })
 
     // this is here to patch up a bug where the cards do not stack properly at certain points in the 'medium' breakpoint section
     window.addEventListener('resize', this.updateCardsPerRow.bind(this))
 
-    const rows = chunk(cardComponents, cardsPerRow)
+    let currentCardIndex = 0
+    const rows = cardsPerRow.map(function(count) {
+      let row = []
+      let i
+
+      for (i = 0; i < count; i++) {
+        row.push(
+          <Card
+            parentIndex={parentIndex}
+            key={currentCardIndex}
+            item={cards[currentCardIndex]}
+            index={currentCardIndex}
+            numCards={count}
+            leftAligned={leftAligned}
+            cardAriaLabel={cardAriaLabel}
+          />
+        )
+        currentCardIndex++
+      }
+      return row
+    })
     return (
       <div className={styles.cardCollection}>
         {rows.map(function(item, index) {
