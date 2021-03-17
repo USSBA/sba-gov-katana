5620661be3c2499c4ef3cdc1d2fd06a0284a0c68
chris-poe <chris.poe92@gmail.com>
2021-03-16 22:29:51 -0400

    Fix aria labels for success modal

--- src/client/components/molecules/office-contact-modal/office-contact-success.jsx
+++ src/client/components/molecules/office-contact-modal/office-contact-success.jsx
@@ -17,11 +17,11 @@ const OfficeContactSuccess = ({ modalActions }) => {
       <div className={styles.content}>
         <div className={styles.divider} />
         <div className={styles.success}>
-          <i className={styles.icon + ' fa fa-check-circle'} />
-          <h4 className={styles.successMessage}>Your request has been sent</h4>
+          <i className={styles.icon + ' fa fa-check-circle'} tabIndex="0" aria-label="success checkmark" />
+          <h4 className={styles.successMessage} tabIndex="0">Your request has been sent</h4>
         </div>
-        <h6 className={styles.messageTitle}>Thank you for contacting your district office.</h6>
-        <p className={styles.message}>
+        <h6 className={styles.messageTitle} tabIndex="0">Thank you for contacting your district office.</h6>
+        <p className={styles.message} tabIndex="0">
           You will receive an email confirmation with your request details within the next hour. Someone
           from your District Office will respond during standard business hours.
         </p>
